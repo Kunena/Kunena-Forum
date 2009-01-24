071503486409cf21fe523efaf96cf04075fa6b6c
fxstein <oliver@kunena.org>
2009-01-24 08:55:13 +0000

    frontend source replaced with lib
    git-svn-id: file:///Users/oratzes/svn/kunena@129 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/legacy/administrator/components/com_kunena/lib/fx.upgrade.class.php
+++ trunk/legacy/administrator/components/com_kunena/lib/fx.upgrade.class.php
@@ -16,7 +16,7 @@
 // ensure this file is being included by a parent file
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
-include_once ($mainframe->getCfg("absolute_path") . "/components/com_kunena/sources/fb_debug.php");
+include_once ($mainframe->getCfg("absolute_path") . "/components/com_kunena/lib/fb_debug.php");
 
 class fx_Upgrade {
 	var $component=null;

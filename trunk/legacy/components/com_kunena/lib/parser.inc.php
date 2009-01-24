17091c8d86b8f5a518c1f85ab2d20d77fd9a0d95
fxstein <oliver@kunena.org>
2009-01-24 10:58:33 +0000

    kunena.parser.base.php
    git-svn-id: file:///Users/oratzes/svn/kunena@162 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/legacy/components/com_kunena/lib/parser.inc.php
+++ trunk/legacy/components/com_kunena/lib/parser.inc.php
@@ -10,7 +10,7 @@
 ############################################################################
 # CATEGORY: Parser.TagParser                 DEVELOPMENT DATUM: 13.11.2007 #
 # VERSION:  00.08.00                         LAST EDIT   DATUM: 12.12.2007 #
-# FILENAME: parser.inc.php                                                 #
+# FILENAME: kunena.parser.base.php                                                 #
 # AUTOR:    Miro Dietiker, MD Systems, All rights reserved                 #
 # LICENSE:  http://www.gnu.org/copyleft/gpl.html GNU/GPL                   #
 # CONTACT: m.dietiker@md-systems.ch        © 2007 Miro Dietiker 13.11.2007 #

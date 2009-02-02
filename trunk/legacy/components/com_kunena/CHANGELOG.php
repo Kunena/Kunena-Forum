73af422dbda1920677bb77189d19118dd8f9a07e
noelhunter <noel@camelcity.com>
2009-02-02 22:37:27 +0000

    ^ Fix css typos for forum tools menu, add z-index
    git-svn-id: file:///Users/oratzes/svn/kunena@275 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/legacy/components/com_kunena/CHANGELOG.php
+++ trunk/legacy/components/com_kunena/CHANGELOG.php
@@ -37,6 +37,7 @@ Kunena 1.0.8
   load it instead of Kunena template's kunena.forum.css
 ^ Change font sizes in kunena.forum.css for default_ex from px to relative sizes (small, medium, etc)
 ^ Change names in for forum tools in kunena.forum.css from fireboard to kunena, add z-index:2 to menu
+^ Fix css typos for forum tools menu, add z-index
 
 2-February-2009 Matias
 ^ Move forced width from message text to [code] tag

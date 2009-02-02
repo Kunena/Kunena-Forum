9cc2d680756c09ffa1dcafed105017cf4fdafdb0
fxstein <oliver@kunena.org>
2009-02-02 23:11:25 +0000

    Empty CKunenaUser class
    git-svn-id: file:///Users/oratzes/svn/kunena@277 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/legacy/components/com_kunena/lib/kunena.user.class.php
+++ trunk/legacy/components/com_kunena/lib/kunena.user.class.php
@@ -6,12 +6,18 @@
 * @Copyright (C) 2009 www.kunena.com All rights reserved
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
 * @link http://www.kunena.com
-*
 **/
 
 // Dont allow direct linking
 defined ('_VALID_MOS') or die('Direct Access to this location is not allowed.');
 
+class CKunenaUser
+{
+	function CKunenaUser($userid)
+	{
 
+	}
+
+}
 
 ?>

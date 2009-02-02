1697e93e540c4ebea9bfa954b3d477a16b0ee951
fxstein <oliver@kunena.org>
2009-02-02 22:56:36 +0000

    + Empty kunena.user.class.php skeleton
    git-svn-id: file:///Users/oratzes/svn/kunena@276 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/legacy/components/com_kunena/lib/kunena.user.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @version $Id:kunena.pdf.php 150 2009-01-24 10:42:53Z fxstein $
+* Kunena Component - CKunenaUser class
+* @package Kunena
+* @Copyright (C) 2009 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*
+**/
+
+// Dont allow direct linking
+defined ('_VALID_MOS') or die('Direct Access to this location is not allowed.');
+
+
+
+?>

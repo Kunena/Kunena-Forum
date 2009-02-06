230bff121f71b528be59283d4fe90ccde5ebc4ae
fxstein <oliver@kunena.org>
2009-02-06 23:17:09 +0000

    Initial modrewrite utility, added header, set svn properties
    git-svn-id: file:///Users/oratzes/svn/kunena@318 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/tools/modrewrite/error.php
+++ trunk/tools/modrewrite/error.php
@@ -1,4 +1,14 @@
 <?php
+/**
+* @version $Id$
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2008 - 2009 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+**/
+
 // This file is designed to be called by the rewrite rules in htaccess
 // It will reqrite the URL and redirect
 // Noel Hunter, Feb 6, 2009
@@ -7,7 +17,7 @@
 $url=$_SERVER['QUERY_STRING'];
 
 // Replace old string with new.  Add ?redirected in case you need to detect redirects
-$url=str_replace('fireboard','kunena',$url); 
+$url=str_replace('fireboard','kunena',$url);
 $url="index.php?redirected".$url;
 
 // Go to new URL

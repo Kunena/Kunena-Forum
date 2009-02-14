990955a759c5bdc12dc43bd73851b8cebf6a67dc
fxstein <oliver@kunena.org>
2009-02-14 23:22:45 +0000

    # Regression: Disabled the submit button because of incorrect type
    git-svn-id: file:///Users/oratzes/svn/kunena@399 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/legacy/components/com_kunena/CHANGELOG.php
+++ trunk/legacy/components/com_kunena/CHANGELOG.php
@@ -38,6 +38,7 @@ Kunena 1.0.8
 ^ Preview button label now with capital 'P'
 # Incorrect button css classes on write message screen fixed
 ^ Extra spacing for text buttons to conform with Joomla button style
+# Regression: Disabled the submit button because of incorrect type
 
 13-February-2009 Matias
 # Try 2: Use default_ex template if current template is missing
--- trunk/legacy/components/com_kunena/template/default/fb_write.html.php
+++ trunk/legacy/components/com_kunena/template/default/fb_write.html.php
@@ -280,7 +280,7 @@ include_once(KUNENA_ABSSOURCESPATH . 'kunena.bbcode.js.php');
 		?>
         <tr>
             <td colspan = "2" style = "text-align: center;">
-                <input type="button" name="submit"  class="fb_button" value="<?php echo ' '._GEN_CONTINUE.' ';?>" onclick="return submitForm()" onmouseover = "helpline('submit')" />
+                <input type="submit" name="submit"  class="fb_button" value="<?php echo ' '._GEN_CONTINUE.' ';?>" onclick="return submitForm()" onmouseover = "helpline('submit')" />
                 <input type="button" name="preview" class="fb_button" value="<?php echo ' '._PREVIEW.' ';?>"      onClick="fbGetPreview(document.postform.message.value,<?php echo KUNENA_COMPONENT_ITEMID?>);" onmouseover = "helpline('preview');" />
                 <input type="button" name="cancel"  class="fb_button" value="<?php echo ' '._GEN_CANCEL.' ';?>"   onclick="javascript:window.history.back();" onmouseover = "helpline('cancel')" />
             </td>

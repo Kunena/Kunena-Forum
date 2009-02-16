7640f3cb35a31ea7077575fea4413f83f1b55332
fxstein <oliver@kunena.org>
2009-02-16 07:06:54 +0000

    + Add Changelog to MyKunena Plugin
    git-svn-id: file:///Users/oratzes/svn/kunena@431 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/3rdparty/jomSocial/mykunena/CHANGELOG.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* @version $Id: CHANGELOG.php 428 2009-02-16 05:28:09Z fxstein $
+* MyKunena Plugin
+* @package MyKunena
+*
+* @Copyright (C) 2009 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+
+// no direct access
+defined( '_VALID_MOS' ) or die( 'Restricted access' );
+?>
+
+Changelog
+------------
+This is a non-exhaustive (but still near complete) changelog for
+the Kunena 1.x, including beta and release candidate versions.
+The Kunena 1.x is based on the FireBoard releases but includes some
+drastic technical changes.
+Legend:
+
+* -> Security Fix
+# -> Bug Fix
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+MyKunena 1.0.0
+
+15-Februray-2009 fxstein
++ Initial plugin structure

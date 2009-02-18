794a3f5e47f5e3bc10b345298ff36b9152d0c806
fxstein <oliver@kunena.org>
2009-02-18 08:20:34 +0000

    New 1.0.9 branch for future bug fixes in release 1.0 (STRICLY NO NEW FUNCTIONALITY!)
    git-svn-id: file:///Users/oratzes/svn/kunena@459 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ branches/1.0.9/components/com_kunena/lib/kunena.mail.php
@@ -0,0 +1,53 @@
+<?php
+/**
+* @version $Id$
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2008 - 2009 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*
+* Based on FireBoard Component
+* @Copyright (C) 2006 - 2007 Best Of Joomla All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.bestofjoomla.com
+*
+* Based on Joomlaboard Component
+* @copyright (C) 2000 - 2004 TSMF / Jan de Graaff / All Rights Reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @author TSMF & Jan de Graaff
+**/
+
+defined('_VALID_MOS') or die('Direct Access to this location is not allowed.');
+
+class fbMail
+{
+    /**
+    * function to send mails (uses mosmail in mambo 4.5.1 and later, falls back to phpmail if mosmail doesn't exist
+    *
+    * @param string $fromMail
+    * @param string $fromName
+    * @param string $toMail
+    * @param string $subject
+    * @param string $body
+    *             message to be send
+    */
+    function send($fromMail, $fromName, $toMail, $subject, $body)
+    {
+        if (function_exists(mosMail))
+            mosMail($fromMail, $fromName, $toMail, $subject, $body);
+        else
+        {
+            $headers = "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/plain;" . _ISO . "\r\n";
+            $headers .= "From: $fromName <$fromMail>\r\n";
+            $headers .= "Reply-To: $fromName <$fromMail>\r\n";
+            $headers .= "X-Priority: 3\r\n";
+            $headers .= "X-MSMail-Priority: Low\r\n";
+            $headers .= "X-Mailer: Mambo Open Source 4.5\r\n";
+            mail($toMail, $subject, $body, $headers);
+        }
+    }
+}
+?>

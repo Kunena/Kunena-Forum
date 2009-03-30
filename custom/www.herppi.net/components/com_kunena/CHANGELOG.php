6f93bde946c372aea72fd92306caa64935ee7267
mahagr <matias@kunena.org>
2009-03-30 23:55:13 +0000

    # [#15554] Custom branch: Forum Tools / Message Topic layout issues in   FF2 and IE6 # [#15554] Custom branch: Sticky icon in the wrong place in IE6 # [#15554] Custom branch: Possible IE6 crash fixed when Show Avatar on   Categories list = Yes # [#15554] Custom branch: CSS Conflicts between Joomla themes and   Kunena mostly fixed by adding #Kunena to the rules
    git-svn-id: file:///Users/oratzes/svn/kunena@562 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- custom/www.herppi.net/components/com_kunena/CHANGELOG.php
+++ custom/www.herppi.net/components/com_kunena/CHANGELOG.php
@@ -32,6 +32,12 @@ Legend:
 
 CUSTOM BRANCH
 
+31-March-2009 Matias
+# [#15554] Custom branch: Forum Tools / Message Topic layout issues in FF2 and IE6
+# [#15554] Custom branch: Sticky icon in the wrong place in IE6 
+# [#15554] Custom branch: Possible IE6 crash fixed when Show Avatar on Categories list = Yes
+# [#15554] Custom branch: CSS Conflicts between Joomla themes and Kunena mostly fixed by adding #Kunena to the rules
+
 30-March-2009 Matias
 # [#15554] Custom branch: Moved time related functions to CKunenaTimeformat:
   KUNENA_timeformat -> CKunenaTimeformat::showDate

88c4fc6b7704616e821c54cd917750774c543e25
fxstein <oliver@kunena.org>
2009-04-09 04:55:54 +0000

    [#15886] Delete old & invalid trunk/1.5 tree
    git-svn-id: file:///Users/oratzes/svn/kunena@619 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.5/1.5/components/com_kunena/template/default/plugin/recentposts/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// $Id: index.php 462 2007-12-10 00:05:53Z fxstein $
-/*-----------------------------------------------
-this is a dummy file and intentionally left blank
-do not remove from ditribution package
-/*----------------------------------------------*/
-?>

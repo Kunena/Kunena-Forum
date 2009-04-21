92ada70220e7455663a5488e942e97ba60caad7f
mahagr <matias@kunena.org>
2009-04-21 19:30:57 +0000

    # [#16081] Fatal error in kunena.config.class.php
    git-svn-id: file:///Users/oratzes/svn/kunena@634 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.0/components/com_kunena/CHANGELOG.php
+++ branches/1.0/components/com_kunena/CHANGELOG.php
@@ -38,6 +38,7 @@ Kunena 1.0.10
 # [#16070] Forum Jump: links are broken in sh404SEF
 # [#16092] File upload does not work in some servers
 # [#16084] Search: Call to undefined function mb_substr()
+# [#16081] Fatal error in kunena.config.class.php
 
 5-April-2009 fxstein
 ^ [#15799] Prpepare version information for 1.0.10 builds

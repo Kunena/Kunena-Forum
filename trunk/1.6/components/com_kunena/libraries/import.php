7895ac7d8cdcfd5f903fad0289f2b8876c177350
LouisLandry <louis.landry@joomla.org>
2009-08-15 02:01:46 +0000

    ^ [#17628] Added first cut at import for Kunena libraries.
    git-svn-id: file:///Users/oratzes/svn/kunena@953 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/1.6/components/com_kunena/libraries/import.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Kunena
+ * @subpackage	com_kunena
+ * @copyright	Copyright (C) 2008 - 2009 Kunena Team. All rights reserved.
+ * @license		GNU General Public License <http://www.gnu.org/copyleft/gpl.html>
+ * @link		http://www.kunena.com
+ */
+
+defined('JPATH_BASE') or die;
+
+// Define the Kunena Libraries path constant.
+if (!defined('KPATH_LIBRARIES')) {
+	define('KPATH_LIBRARIES', realpath(dirname(dirname(__FILE__))));
+}
+
+/**
+ * Kunena Libraries intelligent file importer.
+ *
+ * @param	string	A dot syntax path.
+ * @return	boolean	True on success
+ * @since	1.0
+ */
+function kimport($path)
+{
+	if (strpos($path, 'kunena') === 0) {
+		return JLoader::import($path, KPATH_LIBRARIES, '');
+	}
+	else {
+		return JLoader::import($path, null, 'libraries.');
+	}
+}

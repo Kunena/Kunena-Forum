1501c7cd11171a444b964b0d0d7b64a6d34d8cee
fxstein <oliver@kunena.org>
2009-08-15 02:14:10 +0000

    ^ [#17628] Initial file systems changes for MVC next gen layout
    git-svn-id: file:///Users/oratzes/svn/kunena@954 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/1.6/components/com_kunena/kunena.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @version $Id: kunena.php 938 2009-07-23 23:39:48Z mahagr $
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2008 - 2009 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+**/
+
+// Dont allow direct linking
+defined( '_JEXEC' ) or die('Restricted access');
+
+
+
+?>

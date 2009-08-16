cd8ec110390b3eaacd2413a675ddcacc4a5f2dcc
severdia <severdia@gmail.com>
2009-08-16 06:10:24 +0000

    [#17628] Create component & admin language files
    git-svn-id: file:///Users/oratzes/svn/kunena@987 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6/components/com_kunena/views/recent/tmpl/default_top.php
+++ trunk/1.6/components/com_kunena/views/recent/tmpl/default_top.php
@@ -14,35 +14,35 @@ defined('_JEXEC') or die;
 		<div class="top_info_box">
 			<div class="choose_hours">
 				<select class="input_hours" onchange="document.location.href=this.options[this.selectedIndex].value;" name="select">
-					<option value="/forum/latest/show/sel-4">4 Hours</option>
-					<option value="/forum/latest/show/sel-8">8 Hours</option>
-					<option value="/forum/latest/show/sel-12">12 Hours</option>
-					<option value="/forum/latest/show/sel-24">24 Hours</option>
-					<option value="/forum/latest/show/sel-48">48 Hours</option>
-					<option value="/forum/latest/show/sel-168">Week</option>
-					<option selected="selected" value="/forum/latest/show/sel-720">Month</option>
-					<option value="/forum/latest/show/sel-8760">Year</option>
+					<option value="/forum/latest/show/sel-4">4 <?php echo JText::_('K_HOURS'); ?></option>
+					<option value="/forum/latest/show/sel-8">8 <?php echo JText::_('K_HOURS'); ?></option>
+					<option value="/forum/latest/show/sel-12">12 <?php echo JText::_('K_HOURS'); ?></option>
+					<option value="/forum/latest/show/sel-24">24 <?php echo JText::_('K_HOURS'); ?></option>
+					<option value="/forum/latest/show/sel-48">48 <?php echo JText::_('K_HOURS'); ?></option>
+					<option value="/forum/latest/show/sel-168"><?php echo JText::_('K_WEEK'); ?></option>
+					<option selected="selected" value="/forum/latest/show/sel-720"><?php echo JText::_('K_MONTH'); ?></option>
+					<option value="/forum/latest/show/sel-8760"><?php echo JText::_('K_YEAR'); ?></option>
 				</select>
 			</div>
 			<div class="jump_box">
 				<form id="jump_forum" name="jumpto" method="post" target="_self" action="/forum">
 					<input type="hidden" name="func" value="showcat"/>
 					<select name="catid" class="choose_forum" onchange="if(this.options[this.selectedIndex].value> 0){ this.form.submit() }">
-						<option value="0" selected="selected">Board Categories&#32;</option>
+						<option value="0" selected="selected"><?php echo JText::_('K_BOARD_CATEGORIES'); ?></option>
 						<option value="94">Kunena - To Speak!</option>
 						<option value="77">...&nbsp;General Talk about Kunena</option>
 						<option value="119">...&nbsp;Feature Requests</option>
 						<option value="136">...&nbsp;Templates and Design</option>
 					</select>
-					<input type="submit" name="Go" class="go_forum" value="Go"/>
+					<input type="submit" name="<?php echo JText::_('K_GO'); ?>" class="go_forum" value="<?php echo JText::_('K_GO'); ?>"/>
 				</form>
 			</div>
 			<div class="pagination_box">
-				Page: <span>1</span>
+				<?php echo JText::_('K_PAGE'); ?>: <span>1</span>
 				<a href="/forum/latest/page-2/sel-720" title="Page 2">2</a>
 				<a href="/forum/latest/page-3/sel-720" title="Page 3">3</a>
 				<a href="/forum/latest/page-4/sel-720" title="Page 4">4</a>...
 				<a href="/forum/latest/page-22/sel-720" title="Page 22">22</a>
 			</div>
-			<div class="discussions"><span>647</span> Discussions</div>
+			<div class="discussions"><span>647</span> <?php echo JText::_('K_DISCUSSIONS'); ?></div>
 		</div>

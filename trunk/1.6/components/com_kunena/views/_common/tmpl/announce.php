b73f014391469087627e32b62b2990158a41f39e
severdia <severdia@gmail.com>
2009-08-16 21:36:53 +0000

    [#17628] Added announcement include.
    git-svn-id: file:///Users/oratzes/svn/kunena@1000 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/1.6/components/com_kunena/views/_common/tmpl/announce.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @version		$Id:$
+ * @package		Kunena
+ * @subpackage	com_kunena
+ * @copyright	Copyright (C) 2008 - 2009 Kunena Team. All rights reserved.
+ * @license		GNU General Public License <http://www.gnu.org/copyleft/gpl.html>
+ * @link		http://www.kunena.com
+ */
+
+defined('_JEXEC') or die;
+?>
+							<h3><a href="3">Kunena 1.0.11 & 1.5.4 Released</a></h3>	
+							<p class="announce_summary"><span>07/11/2009 00:45</span>The Kunena Team announces the immediate availability of Kunena 1.5.4 and 1.0.11. These are important
+											security releases and users are urged to update immediately. These releases also fix a number of other minor and major issues.
+											<a href="/forum/announcement/read/id-8"><?php echo JText::_('K_READ_MORE'); ?></a></p>
+							
+							<div class="clr"></div>

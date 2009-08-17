743a40a80754123d7a3460828e8723b6669b44bb
fxstein <oliver@kunena.org>
2009-08-17 07:09:54 +0000

    + [#17654] Add new bbcode parser: nbbc
    git-svn-id: file:///Users/oratzes/svn/kunena@1013 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/1.6/components/com_kunena/libraries/html/bbcode.php
@@ -0,0 +1,56 @@
+<?php
+/**
+* @version $Id: $
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2008 - 2009 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*
+**/
+// Dont allow direct linking
+defined( '_JEXEC' ) or die('Restricted access');
+
+kimport('html.bbcode.nbbcode');
+
+/**
+ * The Kunena bbcode parser
+ *
+ * @package		Kunena
+ * @subpackage	com_kunena
+ * @version		1.0
+ */
+class KBBCode extends BBCode
+{
+	/**
+	 * Object Constructor
+	 *
+	 * @param
+	 * @return	void
+	 * @since	1.0
+	 */
+	function __construct()
+	{
+		parent::__construct();
+
+	}
+
+	/**
+	 * Get Singleton Instance
+	 *
+	 * @param
+	 * @return	void
+	 * @since	1.0
+	 */
+	public function &getInstance()
+    {
+        static $instance;
+        if (!$instance)
+        {
+            $userinfo = new KBBCode();
+        }
+        return $instance;
+    }
+
+}

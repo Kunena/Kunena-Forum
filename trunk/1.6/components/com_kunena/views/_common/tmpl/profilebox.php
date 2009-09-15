511a3e96fe33178e64477d5fa8930ac32039e995
mahagr <matias@kunena.org>
2009-09-15 22:44:23 +0000

    ^ [#17628] Profilebox cleanup
    git-svn-id: file:///Users/oratzes/svn/kunena@1069 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6/components/com_kunena/views/_common/tmpl/profilebox.php
+++ trunk/1.6/components/com_kunena/views/_common/tmpl/profilebox.php
@@ -10,41 +10,31 @@
 
 defined('_JEXEC') or die;
 ?>
-					<div class="fb-msgview-l-cover"><span class="view-username"> <a
-						href="/index.php/kunena/fbprofile/userid-62" title=""
-						rel="nofollow">admin</a> </span> <br />
-					<a href="/index.php/kunena/fbprofile/userid-62" title=""
-						rel="nofollow"><span class="fb_avatar"><img border="0"
-						src="http://kunena15/images/fbfiles/avatars/nophoto.jpg" alt="" /></span></a>
+<div class="profilebox">
+	<div>
+		<?php echo JHtml::_('klink.user', 'atag', $this->profile->userid, $this->escape($this->profile->name), $this->escape($this->profile->name)); ?>
+	</div>
+	<div>
+		<?php echo JHtml::_('klink.user', 'atag', $this->profile->userid, '<img src="http://kunena15/images/fbfiles/avatars/nophoto.jpg" alt="" />', $this->escape($this->profile->name)); ?>
+	</div>
+	<div>Admin</div>
 
-					<div class="viewcover">Admin</div>
-
-					<div class="viewcover"><img
-						src="http://kunena15/components/com_kunena/template/default_ex/images/english/ranks/rankadmin.gif"
-						alt="" /></div>
-
-
-					<div class="viewcover"><strong>Posts: 0</strong></div>
-					<div class="viewcover">
-					<table border="0" cellspacing="0" cellpadding="0" align="center">
-						<tr>
-							<td width="64"><img
-								src="http://kunena15/components/com_kunena/template/default_ex/images/english/graph/col9m.png"
-								height="4" width="0" alt="graph"
-								style="border: 0px solid #333333" /><img
-								src="http://kunena15/components/com_kunena/template/default_ex/images/english/emoticons/graph.gif"
-								height="4" width="60" alt="graph"
-								style="border: 0px solid #333333" /></td>
-						</tr>
-					</table>
-					</div>
-					<img
-						src="http://kunena15/components/com_kunena/template/default_ex/images/english/icons/offline.gif"
-						border="0" alt="User Offline" /> <a
-						href="/index.php/kunena/fbprofile/userid-62" title=""
-						rel="nofollow"><img
-						src="http://kunena15/components/com_kunena/template/default_ex/images/english/icons/profile.gif"
-						alt="Click here to see the profile of this user" border="0"
-						title="Click here to see the profile of this user" /></a> <br />
-
-					</div>
+	<div>
+		<img src="http://kunena15/components/com_kunena/template/default_ex/images/english/ranks/rankadmin.gif" alt="" />
+	</div>
+	<div>Posts: <?php echo $this->profile->posts; ?></div>
+	<div>
+		<table>
+			<tr>
+				<td width="64">
+					<img src="http://kunena15/components/com_kunena/template/default_ex/images/english/graph/col9m.png" height="4" width="0" alt="graph" />
+					<img src="http://kunena15/components/com_kunena/template/default_ex/images/english/emoticons/graph.gif" height="4" width="60" alt="graph" />
+				</td>
+			</tr>
+		</table>
+	</div>
+	<div>
+		<img src="http://kunena15/components/com_kunena/template/default_ex/images/english/icons/offline.gif" alt="User Offline" />
+		<?php echo JHtml::_('klink.user', 'atag', $this->profile->userid, '<img src="http://kunena15/components/com_kunena/template/default_ex/images/english/icons/profile.gif" alt="Click here to see the profile of this user" title="Click here to see the profile of this user" />', $this->escape($this->profile->name)); ?>
+	</div>
+</div>

7c19572195dcb257641e9b74a3ade2652f1a3072
810 <schumacher1986@hotmail.com>
2009-10-16 19:17:21 +0000

    custom Branch 810: 16-okt-2009 810 ^ [#18267] Improve default_setup.php
    git-svn-id: file:///Users/oratzes/svn/kunena@1114 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6-810/administrator/components/com_kunena/views/config/tmpl/default_setup.php
+++ branches/1.6-810/administrator/components/com_kunena/views/config/tmpl/default_setup.php
@@ -86,9 +86,58 @@ echo $pane->startPanel('Basics', 'Basics');?>
 				<input type="text" name="config[new_indicator]" id="config_new_indicator" value="<?php echo $this->options->get('new_indicator'); ?>" size="5" />
 			</td>
 		</tr>
-
-
-
+        </tbody>
+	</table>
+<legend>Extra Settings</legend>
+<table width="100%" class="admintable" cellspacing="1">
+		<tr>
+			<td width="40%" class="key">
+				<label for="config_rss_enabled" class="hasTip" title="Met de RSS feed optie kunnen derden de laatste berichten bekijken op hun desktop/RSS aplicatie (zie bijvoorbeeld rssreader.com">Enable rss</label>
+			</td>
+			<td>
+				<select name="config[rss_enabled]" id="rss_enabled">
+					<option value="0"<?php echo (($this->options->get('rss_enabled', 1) == 0) ? ' selected="selected"' : ''); ?>>No</option>
+					<option value="1"<?php echo (($this->options->get('rss_enabled', 1) == 1) ? ' selected="selected"' : ''); ?>>Yes</option>
+				</select>
+			</td>
+		</tr>
+<table width="100%" class="admintable" cellspacing="1" align="right">
+		<tr>
+			<td width="40%" class="key">
+				<label for="config_rss_default" class="hasTip" title="Kies tussen RSS feeds op basis van onderwerp of post. Op basis van onderwerp betekent dat enkel 1 inschrijving per onderwerp getoond zal worden in de RSS feed, onafhankelijk van hoeveel posts er zijn gemaakt binnen het onderwerp. Door draad maakt een kortere meer compacte RSS feed maar zal niet iedere gemaakt antwoord laten zien.">Standaard RSS type</label>
+			</td>
+			<td>
+				<select name="config[rss_default]" id="rss_default">
+					<option value="0"<?php echo (($this->options->get('rss_default', 1) == 0) ? ' selected="selected"' : ''); ?>>topic</option>
+					<option value="1"<?php echo (($this->options->get('rss_default', 1) == 1) ? ' selected="selected"' : ''); ?>>post</option>
+				</select>
+			</td>
+		</tr>
+        
+        <table width="100%" class="admintable" cellspacing="1" align="right">
+		<tr>
+			<td width="40%" class="key">
+				<label for="config_rss_history" class="hasTip" title="Met de RSS feed optie kunnen derden de laatste berichten bekijken op hun desktop/RSS aplicatie (zie bijvoorbeeld rssreader.com">history rss</label>
+			</td>
+			<td>
+				<select name="config[rss_history]" id="rss_history">
+					<option value="0"<?php echo (($this->options->get('rss_history', 1) == 0) ? ' selected="selected"' : ''); ?>>No</option>
+					<option value="1"<?php echo (($this->options->get('rss_history', 1) == 1) ? ' selected="selected"' : ''); ?>>Yes</option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td width="40%" class="key" align="right">
+				<label for="config_pdf_enabled" class="hasTip" title=" Schakel dit op Ja als u gebruikers wilt toestaan om een PDF document te genereren van het forumonderwerp.
+Het is een simpel PDF document; zonder opmaak of layout maar het bevat alle tekst van het forumonderwerp.">Enable pdf</label>
+			</td>
+			<td>
+				<select name="config[pdf_enabled]" id="pdf_enabled">
+					<option value="0"<?php echo (($this->options->get('pdf_enabled', 1) == 0) ? ' selected="selected"' : ''); ?>>No</option>
+					<option value="1"<?php echo (($this->options->get('pdf_enabled', 1) == 1) ? ' selected="selected"' : ''); ?>>Yes</option>
+				</select>
+			</td>
+		</tr>
 
 
 	</tbody>
@@ -97,6 +146,129 @@ echo $pane->startPanel('Basics', 'Basics');?>
 <?php
 echo $pane->endPanel();
 echo $pane->startPanel('Frontend', 'Frontend');?>
+<fieldset>
+	<legend><?php echo JText::_('Frontend'); ?>
+    </legend>
+
+	<legend>Uiterlijk</legend>
+	<table class="admintable" cellspacing="1">
+	<tbody>
+		<tr>
+			<td width="40%" class="key">
+				<label for="config_thread_by_page" class="hasTip" title="thread_by_page.">thread_by_page</label>
+			</td>
+			<td>
+				<input type="text" name="config[thread_by_page]" id="thread_by_page" value="<?php echo $this->options->get('thread_by_page'); ?>" size="30" />
+ 
+			</td>
+		</tr>
+		<tr>
+			<td width="40%" class="key">
+				<label for="config_board_email" class="hasTip" title="Board Email::This is the board's e-mail address. Make this a valid e-mail address.">Board Email</label>
+			</td>
+			<td>
+				<input type="text" name="config[board_email]" id="config_board_email" value="<?php echo $this->options->get('board_email'); ?>" size="30" />
+			</td>
+		</tr>
+		<tr>
+			<td width="40%" class="key">
+				<label for="config_avatar_src" class="hasTip" title="Forum Offline::...">Forum Offline</label>
+			</td>
+			<td>
+				<select name="config[board_offline]" id="config_board_offline">
+					<option value="no"<?php echo (($this->options->get('board_offline', 'kunena') == 'no') ? ' selected="selected"' : ''); ?>>No</option>
+					<option value="yes"<?php echo (($this->options->get('board_offline', 'kunena') == 'yes') ? ' selected="selected"' : ''); ?>>Yes</option>
+				</select>
+			</td>
+		</tr>
+
+	</tbody>
+	</table>
+	<legend>Layout Settings</legend>
+	<table class="admintable" cellspacing="1">
+	<tbody>
+
+		<tr>
+			<td width="40%" class="key">
+				<label for="config_threads_per_page" class="hasTip" title="Threads per Page::Number of threads to be displayed per page.">Threads per Page</label>
+			</td>
+			<td>
+				<input type="text" name="config[threads_per_page]" id="config_threads_per_page" value="<?php echo $this->options->get('threads_per_page'); ?>" size="5" />
+			</td>
+		</tr>
+		<tr>
+			<td width="40%" class="key">
+				<label for="config_messages_per_page" class="hasTip" title="Messages per Page::Number of messages to be displayed per page.">Messages per Page</label>
+			</td>
+			<td>
+				<input type="text" name="config[messages_per_page]" id="config_messages_per_page" value="<?php echo $this->options->get('messages_per_page'); ?>" size="5" />
+			</td>
+		</tr>
+		<tr>
+			<td width="40%" class="key">
+				<label for="config_new_indicator" class="hasTip" title="New Indicator::If set, characters will highlight new messages.">New Indicator</label>
+			</td>
+			<td>
+				<input type="text" name="config[new_indicator]" id="config_new_indicator" value="<?php echo $this->options->get('new_indicator'); ?>" size="5" />
+			</td>
+		</tr>
+        </tbody>
+	</table>
+<legend>Extra Settings</legend>
+<table width="100%" class="admintable" cellspacing="1">
+		<tr>
+			<td width="40%" class="key">
+				<label for="config_rss_enabled" class="hasTip" title="Met de RSS feed optie kunnen derden de laatste berichten bekijken op hun desktop/RSS aplicatie (zie bijvoorbeeld rssreader.com">Enable rss</label>
+			</td>
+			<td>
+				<select name="config[rss_enabled]" id="rss_enabled">
+					<option value="0"<?php echo (($this->options->get('rss_enabled', 1) == 0) ? ' selected="selected"' : ''); ?>>No</option>
+					<option value="1"<?php echo (($this->options->get('rss_enabled', 1) == 1) ? ' selected="selected"' : ''); ?>>Yes</option>
+				</select>
+			</td>
+		</tr>
+<table width="100%" class="admintable" cellspacing="1" align="right">
+		<tr>
+			<td width="40%" class="key">
+				<label for="config_rss_default" class="hasTip" title="Kies tussen RSS feeds op basis van onderwerp of post. Op basis van onderwerp betekent dat enkel 1 inschrijving per onderwerp getoond zal worden in de RSS feed, onafhankelijk van hoeveel posts er zijn gemaakt binnen het onderwerp. Door draad maakt een kortere meer compacte RSS feed maar zal niet iedere gemaakt antwoord laten zien.">Standaard RSS type</label>
+			</td>
+			<td>
+				<select name="config[rss_default]" id="rss_default">
+					<option value="0"<?php echo (($this->options->get('rss_default', 1) == 0) ? ' selected="selected"' : ''); ?>>topic</option>
+					<option value="1"<?php echo (($this->options->get('rss_default', 1) == 1) ? ' selected="selected"' : ''); ?>>post</option>
+				</select>
+			</td>
+		</tr>
+        
+        <table width="100%" class="admintable" cellspacing="1" align="right">
+		<tr>
+			<td width="40%" class="key">
+				<label for="config_rss_history" class="hasTip" title="Met de RSS feed optie kunnen derden de laatste berichten bekijken op hun desktop/RSS aplicatie (zie bijvoorbeeld rssreader.com">history rss</label>
+			</td>
+			<td>
+				<select name="config[rss_history]" id="rss_history">
+					<option value="0"<?php echo (($this->options->get('rss_history', 1) == 0) ? ' selected="selected"' : ''); ?>>No</option>
+					<option value="1"<?php echo (($this->options->get('rss_history', 1) == 1) ? ' selected="selected"' : ''); ?>>Yes</option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td width="40%" class="key" align="right">
+				<label for="config_pdf_enabled" class="hasTip" title=" Schakel dit op Ja als u gebruikers wilt toestaan om een PDF document te genereren van het forumonderwerp.
+Het is een simpel PDF document; zonder opmaak of layout maar het bevat alle tekst van het forumonderwerp.">Enable pdf</label>
+			</td>
+			<td>
+				<select name="config[pdf_enabled]" id="pdf_enabled">
+					<option value="0"<?php echo (($this->options->get('pdf_enabled', 1) == 0) ? ' selected="selected"' : ''); ?>>No</option>
+					<option value="1"<?php echo (($this->options->get('pdf_enabled', 1) == 1) ? ' selected="selected"' : ''); ?>>Yes</option>
+				</select>
+			</td>
+		</tr>
+
+
+	</tbody>
+	</table>
+</fieldset>
 <?php
 echo $pane->endPanel();
 echo $pane->startPanel('Avatars', 'Avatars');?>

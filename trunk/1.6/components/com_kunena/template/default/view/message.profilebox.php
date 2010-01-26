6c844a82fd0ba21d5215f2d725dfe334838215ec
severdia <severdia@gmail.com>
2010-01-26 11:40:54 +0000

    # [#19356] Reworked profile area on posts
    git-svn-id: file:///Users/oratzes/svn/kunena@1777 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6/components/com_kunena/template/default/view/message.profilebox.php
+++ trunk/1.6/components/com_kunena/template/default/view/message.profilebox.php
@@ -16,81 +16,82 @@ $kunena_config = & CKunenaConfig::getInstance ();
 
 if ($kunena_config->avposition == 'left' || $kunena_config->avposition == 'right') {
 ?>
-<div class="kunena-profile">
-	<span class="view-username">
-		<?php if ($this->userinfo->userid) { echo CKunenaLink::GetProfileLink ( $kunena_config, $this->kunena_message->userid, $this->msg_html->username );
-			} else { echo $this->msg_html->username; } ?>
-	</span>
-		<?php if ($kunena_config->userlist_usertype)
-			echo '<span class = "msgusertype">(' . $this->msg_html->usertype . ')</span>'; ?>
-	<br />
+	<ul id="kpost-profile">
+		<li class="kpost-username">
+			<?php if ($this->userinfo->userid) { echo CKunenaLink::GetProfileLink ( $kunena_config, $this->kunena_message->userid, $this->msg_html->username );
+				} else { echo $this->msg_html->username; } ?>
+		</li>
+		<li class="kpost-usertype">
+			<?php if ($kunena_config->userlist_usertype)
+				echo '<span class = "msgusertype">(' . $this->msg_html->usertype . ')</span>'; ?>
+		</li>
+		<li class="kpost-avatar">
 		<?php if ($this->kunena_message->userid > 0) {
 			echo CKunenaLink::GetProfileLink ( $kunena_config, $this->kunena_message->userid, $this->msg_html->avatar );
 			} else { echo $this->msg_html->avatar; } ?>
-	<div class="viewcover">
-		<?php if (isset ( $this->msg_html->userrank )) { echo $this->msg_html->userrank; } ?>
-	</div>
-	<div class="viewcover">
-		<?php if (isset ( $this->msg_html->userrankimg )) { echo $this->msg_html->userrankimg; } ?>
-	</div>
-	<div class="onlineimg">
+		</li>
+		<li class="kpost-userrank">
+			<?php if (isset ( $this->msg_html->userrank )) { echo $this->msg_html->userrank; } ?>
+		</li>
+		<li class="kpost-userrank-img">
+			<?php if (isset ( $this->msg_html->userrankimg )) { echo $this->msg_html->userrankimg; } ?>
+		</li>
+		<!-- This CSS class needs to by dymanic like on profile page. -->
+		<li class="kpost-online-status-yes"> </li>
+		<!--  The markup needs to be removed from these tags and each enclosed as a list item. --> 
 		<?php if (isset ( $this->msg_html->posts )) { echo $this->msg_html->posts; }
-			if (isset ( $this->msg_html->points )) { echo $this->msg_html->points; }
-			if (isset ( $this->msg_html->online )) { echo $this->msg_html->online; } ?>
-	</div>
-		<?php if (isset ( $this->msg_html->pms )) { echo $this->msg_html->pms; } ?>
-	<div class="smallicons">
-		<?php 	if (file_exists ( KUNENA_ABSTMPLTPATH . DS . 'profile' . DS . 'socialbuttons.php')) {
-		include (KUNENA_ABSTMPLTPATH . DS . 'profile' . DS . 'socialbuttons.php');
-	} else {
-		include (KUNENA_PATH_TEMPLATE_DEFAULT . DS . 'profile' . DS . 'socialbuttons.php');
-	} ?>
-	<div class="viewcover">
-		<?php if (isset ( $this->msg_html->personal )) { echo $this->msg_html->personal; } ?>
-	</div>
-	</div>
-</div>
-
+				if (isset ( $this->msg_html->points )) { echo $this->msg_html->points; }
+				if (isset ( $this->msg_html->online )) { echo $this->msg_html->online; } ?>
+			<?php if (isset ( $this->msg_html->pms )) { echo $this->msg_html->pms; } ?>
+		<li class="kpost-smallicons">
+			<?php 	if (file_exists ( KUNENA_ABSTMPLTPATH . DS . 'profile' . DS . 'socialbuttons.php')) {
+			include (KUNENA_ABSTMPLTPATH . DS . 'profile' . DS . 'socialbuttons.php');
+		} else {
+			include (KUNENA_PATH_TEMPLATE_DEFAULT . DS . 'profile' . DS . 'socialbuttons.php');
+		} ?>
+		</li>
+		<li class="kpost-personal">
+			<?php if (isset ( $this->msg_html->personal )) { echo $this->msg_html->personal; } ?>
+		</li>
+</ul>
 <?php } else { ?>
-
-<div class="kunena-topbottom-avatar">
-	<?php if ($this->kunena_message->userid > 0) {
-	echo CKunenaLink::GetProfileLink ( $kunena_config, $this->kunena_message->userid, $this->msg_html->avatar );
-	} else { echo $this->msg_html->avatar; } ?>
-</div>
-<div class="kunena-profile-mid">
-	<?php if ($this->userinfo->userid) {
-	echo CKunenaLink::GetProfileLink ( $kunena_config, $this->kunena_message->userid, $this->msg_html->username );
-	} else { echo $this->msg_html->username; } ?>
-	<?php if ($kunena_config->userlist_usertype)
-	echo '<span class = "msgusertype">(' . $this->msg_html->usertype . ')</span>'; ?>
-	<br />
-	<?php if (isset ( $this->msg_html->userrank )) {
-	echo $this->msg_html->userrank; } ?>
-	<br />
-	<?php if (isset ( $this->msg_html->userrankimg )) {
-	echo $this->msg_html->userrankimg; } ?>
-</div>
-<div class="kunena-personal">
-	<?php if (isset ( $this->msg_html->personal )) {
-	echo $this->msg_html->personal; } ?>
-</div>
-<div class="kunena-profile-right">
-	<?php if (isset ( $this->msg_html->posts )) { echo $this->msg_html->posts; }
-		if (isset ( $this->msg_html->points )) { echo $this->msg_html->points; } ?>
-	<div class="viewcover"><div class="onlineimg">
-		<?php if (isset ( $this->msg_html->online )) { echo $this->msg_html->online; } ?></div><?php
-		 if (isset ( $this->msg_html->pms )) { echo $this->msg_html->pms; } ?>
-	</div>
-</div>
-<div class="kunena-profile-right">
-	<div>
-		<?php 	if (file_exists ( KUNENA_ABSTMPLTPATH . DS . 'profile' . DS . 'socialbuttons.php')) {
-		include (KUNENA_ABSTMPLTPATH . DS . 'profile' . DS . 'socialbuttons.php');
-	} else {
-		include (KUNENA_PATH_TEMPLATE_DEFAULT . DS . 'profile' . DS . 'socialbuttons.php');
-	} ?>
-	</div>
-</div>
-
+	<ul id="kpost-profiletop">
+		<li class="kpost-smallicons">
+			<?php 	if (file_exists ( KUNENA_ABSTMPLTPATH . DS . 'profile' . DS . 'socialbuttons.php')) {
+			include (KUNENA_ABSTMPLTPATH . DS . 'profile' . DS . 'socialbuttons.php');
+		} else {
+			include (KUNENA_PATH_TEMPLATE_DEFAULT . DS . 'profile' . DS . 'socialbuttons.php');
+		} ?>
+		</li>
+		<li class="kpost-personal">
+			<?php if (isset ( $this->msg_html->personal )) { echo $this->msg_html->personal; } ?>
+		</li>
+		<li class="kpost-avatar">
+		<?php if ($this->kunena_message->userid > 0) {
+			echo CKunenaLink::GetProfileLink ( $kunena_config, $this->kunena_message->userid, $this->msg_html->avatar );
+			} else { echo $this->msg_html->avatar; } ?>
+		</li>
+		<li class="kpost-username">
+			<?php if ($this->userinfo->userid) { echo CKunenaLink::GetProfileLink ( $kunena_config, $this->kunena_message->userid, $this->msg_html->username );
+				} else { echo $this->msg_html->username; } ?>
+		</li>
+		<li class="kpost-usertype">
+			<?php if ($kunena_config->userlist_usertype)
+				echo '<span class = "msgusertype">(' . $this->msg_html->usertype . ')</span>'; ?>
+		</li>
+		
+		<li class="kpost-userrank">
+			<?php if (isset ( $this->msg_html->userrank )) { echo $this->msg_html->userrank; } ?>
+		</li>
+		<li class="kpost-userrank-img">
+			<?php if (isset ( $this->msg_html->userrankimg )) { echo $this->msg_html->userrankimg; } ?>
+		</li>
+		<!-- This CSS class needs to by dymanic like on profile page. -->
+		<li class="kpost-online-status-yes"> </li>
+		<!--  The markup needs to be removed from these tags and each enclosed as a list item. --> 
+		<?php if (isset ( $this->msg_html->posts )) { echo $this->msg_html->posts; }
+				if (isset ( $this->msg_html->points )) { echo $this->msg_html->points; }
+				if (isset ( $this->msg_html->online )) { echo $this->msg_html->online; } ?>
+	</ul>
 <?php } ?>
+

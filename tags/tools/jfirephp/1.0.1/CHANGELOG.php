45966a3e7a81eff25f839ae5dec4ddfee59bd210
fxstein <oliver@kunena.org>
2010-01-31 03:52:25 +0000

    [#19590] JFirePHP 1.0.1 build 53
    git-svn-id: file:///Users/oratzes/svn/kunena@1829 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ tags/tools/jfirephp/1.0.1/CHANGELOG.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* @version $Id$
+* MyKunena Plugin
+* @package MyKunena
+*
+* @Copyright (C) 2010 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+
+// no direct access
+defined('_JEXEC') or die();
+?>
+<!--
+
+Changelog
+------------
+This is a non-exhaustive (but still near complete) changelog for
+the JFirePHP plugin for Joomla 1.5, including beta and release candidate versions.
+Legend:
+
+* -> Security Fix
+# -> Bug Fix
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+JFirePHP 1.0.1
+
+31-January-2010 fxstein
+# [#19590] Missing language file removed from manifest
+# [#19590] Missing quotes on JFIREPHP define
+
+JFirePHP 1.0.0
+
+30-January-2010 fxstein
++ [#19590] Initial version
+
+-->
---
+++ tags/tools/jfirephp/1.0.1/COPYRIGHT.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* @version $Id$
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2008 - 2009 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+
+// no direct access
+defined( '_JEXEC' ) or die();
+?>
+
+<!--
+
+JFirePHP includes or is derivative of works distributed under the following copyright notices:
+
+F  i  r  e  P  H  P  C  o  r  e
+---
+Copyright:  Christoph Dorn <christoph@christophdorn.com>
+  			Michael Day    <manveru.alma@gmail.com>
+
+License:	BSD Open-Source License
+
+-->

b79a2c60540233f2150abb3afcb6b22ce4eb0aee
severdia <severdia@gmail.com>
2010-02-01 02:49:24 +0000

    + [#19356] More new icons
    git-svn-id: file:///Users/oratzes/svn/kunena@1855 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6/components/com_kunena/CHANGELOG.php
+++ trunk/1.6/components/com_kunena/CHANGELOG.php
@@ -37,6 +37,7 @@ Kunena 1.6.0-DEV
 31-Jan-2010 severdia
 + [#19383] Added uknown gender icon/option
 + [#19356] Moved icons to proper folders, new icons
++ [#19356] More new icons
 
 31-Jan-2010 Matias
 + [#19383] Revise Profile Page: added Started Topics and Posted Topics tabs

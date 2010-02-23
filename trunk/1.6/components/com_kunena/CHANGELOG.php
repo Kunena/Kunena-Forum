735dee5949d4416bf1aa256d63cbec65861793c7
fxstein <oliver@kunena.org>
2010-02-23 04:18:14 +0000

    + [#19380] New upload file processing based on config option
    git-svn-id: file:///Users/oratzes/svn/kunena@1981 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6/components/com_kunena/CHANGELOG.php
+++ trunk/1.6/components/com_kunena/CHANGELOG.php
@@ -34,10 +34,10 @@ Legend:
 
 Kunena 1.6.0-DEV
 
-21-Feb-2010 severdia
+23-Feb-2010 severdia
 ^ [#19356] New PM icon
 
-22-Feb-2010 fxstein
+23-Feb-2010 fxstein
 + [#19380] New MIME imagetype attachment config option
 + [#19380] New upload file processing based on config option
 

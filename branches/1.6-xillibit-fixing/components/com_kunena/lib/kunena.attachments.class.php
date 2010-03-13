37a16a250b3b71afe858984d3c6898c9f2e60287
xillibit <xillibit@gmail.com>
2010-03-13 15:57:43 +0000

    [#18627] Create fresh branch 1.6-xillibit-fixing
    git-svn-id: file:///Users/oratzes/svn/kunena@2057 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ branches/1.6-xillibit-fixing/components/com_kunena/lib/kunena.attachments.class.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @version $Id$
+ * Kunena Component - CKunenaAjaxHelper class
+ * @package Kunena
+ *
+ * @Copyright (C) 2010 www.kunena.com All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.com
+ **/
+
+// Dont allow direct linking
+defined ( '_JEXEC' ) or die ();
+
+class CKunenaAttachments {
+	protected $_db;
+	protected $_my;
+	protected $_session;
+
+	function __construct() {
+		$this->_db = &JFactory::getDBO ();
+		$this->_my = &JFactory::getUser ();
+		$this->_session = & CKunenaSession::getInstance ();
+	}
+
+	function &getInstance() {
+		static $instance = NULL;
+
+		if (! $instance) {
+			$instance = new CKunenaAttachments ( );
+		}
+		return $instance;
+	}
+
+	function upload($mesid=0, $key='kattachment', $ajax=true) {
+		require_once (KUNENA_PATH_LIB .DS. 'kunena.upload.class.php');
+		$upload = new CKunenaUpload();
+		$upload->uploadFile(KUNENA_PATH_UPLOADED . DS . $this->_my->id, $key, $ajax);
+		$fileinfo = $upload->getFileInfo();
+
+		$folder = KUNENA_RELPATH_UPLOADED . '/' . $this->_my->id;
+		if ($fileinfo['ready'] === true) {
+			if(JDEBUG == 1 && defined('JFIREPHP')){
+				FB::log('Kunena save attachment: ' . $fileinfo['name']);
+			}
+			$this->_db->setQuery ( "INSERT INTO #__kunena_attachments (mesid, userid, hash, size, folder, filetype, filename) values (" .
+				(int)$mesid . "," . (int)$this->_my->id . "," . $this->_db->quote ( $fileinfo['hash'] ) . "," .
+				$this->_db->quote ( $fileinfo['size'] ) . "," . $this->_db->quote ( $folder ) . "," . $this->_db->quote ( $fileinfo['mime'] ) . "," .
+				$this->_db->quote ( $fileinfo['name'] ) . ")" );
+
+				if (! $this->_db->query () || $this->_db->getErrorNum()) {
+				$upload->fail(JText::_('COM_KUNENA_UPLOAD_ERROR_ATTACHMENT_DATABASE_STORE'));
+				$fileinfo = $upload->fileInfo();
+			}
+		}
+			if(JDEBUG == 1 && defined('JFIREPHP')){
+				FB::log('Kunena save attachment ready');
+			}
+		return $fileinfo;
+	}
+
+	function multiupload($mesid=0) {
+		$fileinfo = array();
+		foreach ($_FILES as $key=>$file) {
+			if ($file['error'] != UPLOAD_ERR_NO_FILE) $fileinfo[] = $this->upload($mesid, $key, false);
+		}
+		return $fileinfo;
+	}
+
+	function assign($mesid) {
+		if (!$mesid) return;
+		$this->_db->setQuery ( "UPDATE #__kunena_attachments SET mesid=".(int)$mesid." WHERE userid=".(int)$this->_my->id." AND mesid=0" );
+		$this->_db->query ();
+		check_dberror ( "Unable to assign attachments to message ".$mesid );
+	}
+
+}
---
+++ branches/1.6-xillibit-fixing/components/com_kunena/template/default/profile/editsettings.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @version $Id$
+ * Kunena Component
+ * @package Kunena
+ *
+ * @Copyright (C) 2008 - 2010 Kunena Team All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.com
+ *
+ **/
+defined( '_JEXEC' ) or die();
+require_once ( KUNENA_PATH_FUNCS .DS. 'profile.php');
+$userprofile = new CKunenaProfile($this->user->id);
+$kunena_config =& CKunenaConfig::getInstance();
+?>
+
+<h2><?php echo JText::_('COM_KUNENA_PROFILE_EDIT_SETTINGS_TITLE'); ?></h2>
+<form action="<?php echo CKunenaLink::GetProfileSettingsURL($kunena_config, $this->user->id, '', $rel='nofollow', $redirect=false,'savesettings'); ?>" method="post" name="kprofileEditing">
+<table
+	class="<?php
+	echo isset ( $this->objCatInfo->class_sfx ) ? ' kblocktable' . $this->objCatInfo->class_sfx : '';
+	?>" id="kflattable">
+	<tbody>
+		<tr  class="ksectiontableentry2">
+			<td class="td-0 km center"><?php echo JText::_('COM_KUNENA_USER_ORDER'); ?></td>
+			<td><?php
+					$mesordering[] = JHTML::_('select.option', 0, JText::_('COM_KUNENA_USER_ORDER_ASC'));
+					$mesordering[] = JHTML::_('select.option', 1, JText::_('COM_KUNENA_USER_ORDER_DESC'));
+					echo JHTML::_('select.genericlist', $mesordering, 'messageordering', 'class="inputbox" size="1"', 'value', 'text', $userprofile->profile->ordering);
+			?></td>
+		</tr>
+		<tr  class="ksectiontableentry1">
+			<td class="td-0 km center"><?php echo JText::_('COM_KUNENA_USER_HIDEEMAIL'); ?></td>
+			<td><?php
+					$hideEmail[] = JHTML::_('select.option', 0, JText::_('COM_KUNENA_A_NO'));
+					$hideEmail[] = JHTML::_('select.option', 1, JText::_('COM_KUNENA_A_YES'));
+					echo JHTML::_('select.genericlist', $hideEmail, 'hidemail', 'class="inputbox" size="1"', 'value', 'text', $userprofile->profile->hideEmail);
+
+					?></td>
+		</tr>
+		<tr  class="ksectiontableentry2">
+			<td class="td-0 km center"><?php echo JText::_('COM_KUNENA_USER_SHOWONLINE'); ?></td>
+			<td><?php
+					$showonline[] = JHTML::_('select.option', 0, JText::_('COM_KUNENA_A_NO'));
+					$showonline[] = JHTML::_('select.option', 1, JText::_('COM_KUNENA_A_YES'));
+					echo JHTML::_('select.genericlist', $showonline, 'showonline', 'class="inputbox" size="1"', 'value', 'text', $userprofile->profile->showOnline);
+
+					?></td>
+		</tr>
+		<tr class="ksectiontableentry1">
+			<td class="td-0 km center" colspan="2"><input class="kbutton kbutton ks" type="submit" value="<?php echo JText::_('COM_KUNENA_GEN_SUBMIT'); ?>" name="Submit" /></td>
+		</tr>
+	</tbody>
+</table>
+</form>

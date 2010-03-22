4685b1b616c6b473f2f486198439271cf6ae41ad
mahagr <matias@kunena.org>
2010-03-22 13:31:18 +0000

    [#19600] Kunena 1.5.10 Build 1877
    git-svn-id: file:///Users/oratzes/svn/kunena@2091 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ tags/1.5.10/1.5/components/com_kunena/lib/kunena.session.class.php
@@ -0,0 +1,139 @@
+<?php
+/**
+* @version $Id: kunena.search.class.php 661 2009-05-01 08:28:21Z mahagr $
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2008 - 2009 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*
+**/
+// Dont allow direct linking
+defined( '_JEXEC' ) or die('Restricted access');
+
+require_once (KUNENA_PATH_LIB .DS. "kunena.config.class.php");
+
+class CKunenaSession extends JTable
+{
+	var $userid = 0;
+	var $allowed = 'na';
+	var $lasttime = 0;
+	var $readtopics = '';
+	var $currvisit = 0;
+	protected $_exists = false;
+	protected $_sessiontimeout = false;
+	private static $_instance;
+
+	function __construct(&$kunena_db)
+	{
+		$fbConfig =& CKunenaConfig::getInstance();
+		parent::__construct('#__fb_sessions', 'userid', $kunena_db);
+		$this->lasttime = time() + $fbConfig->board_ofset - KUNENA_SECONDS_IN_YEAR;
+		$this->currvisit = time() + $fbConfig->board_ofset;
+	}
+
+	function &getInstance( $updateSessionInfo=false )
+	{
+		if (!self::$_instance) {
+			$kunena_my = &JFactory::getUser();
+			$kunena_db = &JFactory::getDBO();
+			self::$_instance = new CKunenaSession($kunena_db);
+			if ($kunena_my->id) self::$_instance->load($kunena_my->id);
+			if ($updateSessionInfo) self::$_instance->updateSessionInfo();
+		}
+		return self::$_instance;
+	}
+
+	function load( $oid=null )
+	{
+		$ret = parent::load($oid);
+		if ($ret === true) $this->_exists = true;
+		$this->userid = (int)$oid;
+
+		return $ret;
+	}
+
+	function store( $updateNulls=false )
+	{
+		$fbConfig =& CKunenaConfig::getInstance();
+
+		// Finally update current visit timestamp before saving
+		$this->currvisit = time() + $fbConfig->board_ofset * KUNENA_SECONDS_IN_HOUR;
+
+		$k = $this->_tbl_key;
+
+		if( $this->$k && $this->_exists === true )
+		{
+			$ret = $this->_db->updateObject( $this->_tbl, $this, $this->_tbl_key, $updateNulls );
+		}
+		else
+		{
+			$ret = $this->_db->insertObject( $this->_tbl, $this, $this->_tbl_key );
+		}
+		if( !$ret )
+		{
+			$this->setError(get_class( $this ).'::store failed - '.$this->_db->getErrorMsg());
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+	function isNewUser()
+	{
+		return !$this->_exists;
+	}
+
+	function isNewSession()
+	{
+		return $this->_sessiontimeout;
+	}
+
+	function markAllCategoriesRead()
+	{
+		$fbConfig =& CKunenaConfig::getInstance();
+
+		$this->lasttime = time() + $fbConfig->board_ofset * KUNENA_SECONDS_IN_HOUR;
+		$this->readtopics = '';
+	}
+
+	function updateSessionInfo()
+	{
+		$fbConfig =& CKunenaConfig::getInstance();
+
+		// perform session timeout check
+		$this->_sessiontimeout = ($this->currvisit + $fbConfig->fbsessiontimeout) < time() + $fbConfig->board_ofset * KUNENA_SECONDS_IN_HOUR;
+
+		// If this is a new session, reset the lasttime colum with the timestamp
+		// of the last saved currvisit - only after that can we reset currvisit to now before the store
+		if ($this->isNewSession())
+		{
+			$this->lasttime = $this->currvisit;
+			$this->readtopics = '';
+		}
+	}
+
+	function updateAllowedForums($my_id, $aro_group, $acl)
+	{
+		// check to see if we need to refresh the allowed forums cache
+		// get all accessaible forums if needed (eg on forum modification, new session)
+		if (!$this->allowed or $this->allowed == 'na' or $this->isNewSession()) {
+			$allow_forums = CKunenaTools::getAllowedForums($my_id, $aro_group->id, $acl);
+
+			if (!$allow_forums)
+			{
+				$allow_forums = '0';
+			}
+
+			if ($allow_forums != $this->allowed)
+			{
+				$this->allowed = $allow_forums;
+			}
+		}
+	}
+}
+
+?>
---
+++ tags/1.5.10/1.5/components/com_kunena/template/default/plugin/who/who.php
@@ -0,0 +1,146 @@
+<?php
+/**
+* @version $Id$
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2008 - 2009 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*
+* Based on FireBoard Component
+* @Copyright (C) 2006 - 2007 Best Of Joomla All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.bestofjoomla.com
+*
+* Based on Joomlaboard Component
+* @copyright (C) 2000 - 2004 TSMF / Jan de Graaff / All Rights Reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @author TSMF & Jan de Graaff
+**/
+
+// Dont allow direct linking
+defined( '_JEXEC' ) or die('Restricted access');
+
+$app =& JFactory::getApplication();
+$fbConfig =& CKunenaConfig::getInstance();
+?>
+
+<?php
+if ($fbConfig->showwhoisonline > 0)
+{
+?>
+<div class="<?php echo $boardclass; ?>_bt_cvr1">
+<div class="<?php echo $boardclass; ?>_bt_cvr2">
+<div class="<?php echo $boardclass; ?>_bt_cvr3">
+<div class="<?php echo $boardclass; ?>_bt_cvr4">
+<div class="<?php echo $boardclass; ?>_bt_cvr5">
+    <table class = "fb_blocktable " id="fb_whoispage" border = "0" cellspacing = "0" cellpadding = "0" width="100%">
+        <thead>
+            <tr>
+                <th colspan = "4">
+                   <div class = "fb_title_cover">
+                        <span class="fb_title"><?php echo $app->getCfg('sitename'); ?> - <?php echo _WHO_WHOIS_ONLINE; ?></span>
+                    </div>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr class = "fb_sth">
+                <th class = "th-1 <?php echo $boardclass; ?>sectiontableheader">
+<?php echo _WHO_ONLINE_USER; ?>
+
+                </td>
+
+                <th class = "th-2 <?php echo $boardclass; ?>sectiontableheader"><?php echo _WHO_ONLINE_TIME; ?>
+                </th>
+
+                <th class = "th-3 <?php echo $boardclass; ?>sectiontableheader"><?php echo _WHO_ONLINE_FUNC; ?>
+                </th>
+            </tr>
+
+            <?php
+            $query = "SELECT w.*, u.id, u.username, f.showOnline FROM #__fb_whoisonline AS w LEFT JOIN #__users AS u ON u.id=w.userid LEFT JOIN #__fb_users AS f ON u.id=f.userid ORDER BY w.time DESC";
+            $kunena_db->setQuery($query);
+            $users = $kunena_db->loadObjectList();
+            $k = 0; //for alternating rows
+            $tabclass = array
+            (
+                "sectiontableentry1",
+                "sectiontableentry2"
+            );
+
+            foreach ($users as $user)
+            {
+                $k = 1 - $k;
+
+                if ($user->userid == 0) {
+                    $user->username = _KUNENA_GUEST;
+                } else if ($user->showOnline < 1 && !$is_Moderator) {
+                	continue;
+                }
+
+                $time = date("H:i:s", $user->time + $fbConfig->board_ofset*3600);
+            ?>
+
+                <tr class = "<?php echo $boardclass.''.$tabclass[$k] ;?>">
+                    <td class = "td-1">
+                        <div style = "float: right; width: 14ex;">
+                        </div>
+
+                        <span>
+
+                        <?php
+                        if ($user->userid == 0) {
+                            echo $user->username;
+                        }
+                        else
+                        {
+				echo CKunenaLink::GetProfileLink($fbConfig, $user->userid, $user->username);
+                        }
+                        ?>
+
+                        </span>
+
+                        <?php
+                        if ($is_Moderator)
+                        {
+                        ?>
+
+                            (<?php echo $user->userip; ?>)
+
+                        <?php
+                        }
+                        ?>
+                    </td>
+
+                    <td class = "td-2" nowrap = "nowrap"><?php echo $time; ?>
+                    </td>
+
+                    <td class = "td-3">
+                        <strong><a href = "<?php echo $user->link;?>" target = "_blank"><?php echo $user->what; ?></a></strong>
+                    </td>
+                </tr>
+
+        <?php
+            }
+        ?>
+    </table>
+</div>
+</div>
+</div>
+</div>
+</div>
+<?php
+}
+else
+{
+?>
+
+    <div style = "border:1px solid #FF6600; background: #FF9966; padding:20px; text-align:center;">
+        <h1>Not Active</h1>
+    </div>
+
+<?php
+}
+?>

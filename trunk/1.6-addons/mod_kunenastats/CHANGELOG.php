6eaf2f842fa4f93cba34ed84ee0b123ac3ba8a17
xillibit <xillibit@gmail.com>
2010-04-03 09:24:43 +0000

    + [#20095] Create initial structure
    git-svn-id: file:///Users/oratzes/svn/kunena@2158 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/1.6-addons/mod_kunenastats/CHANGELOG.php
@@ -0,0 +1,37 @@
+<?php
+/**
+* @version $Id$
+* KunenaStats Module
+* @package Kunena Stats
+*
+* @Copyright (C) 2009 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+
+// no direct access
+defined( '_VALID_MOS' ) or die( 'Restricted access' );
+?>
+<!--
+Changelog
+------------
+This is a non-exhaustive (but still near complete) changelog for
+the KunenaStats module, including beta and release candidate versions.
+Legend:
+
+* -> Security Fix
+# -> Bug Fix
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+KunenaStats 1.0
+
+3-Apr 2010 Xillibit
++ [#20095] Create initial structure
+
+
+-->

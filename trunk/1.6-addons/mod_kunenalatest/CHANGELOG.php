0867372bb8ce345a914a55fef27aa63277aec65d
xillibit <xillibit@gmail.com>
2010-04-09 13:19:24 +0000

    ^ [#20081] Put new changelog file because the previous file was broken
    git-svn-id: file:///Users/oratzes/svn/kunena@2189 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/1.6-addons/mod_kunenalatest/CHANGELOG.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* @version $Id$
+* KunenaLatest Module
+* @package Kunena latest
+*
+* @Copyright (C) 2009 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+
+// no direct access
+defined( '_VALID_MOS' ) or die( 'Restricted access' );
+?>
+<!--
+Changelog
+------------
+This is a non-exhaustive (but still near complete) changelog for
+the KunenaLatest module, including beta and release candidate versions.
+Legend:
+
+* -> Security Fix
+# -> Bug Fix
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+KunenaLatest 1.0
+
+9-Apr-2010 Xillibit
+^ [#20081] Little change to avoid try to list the results when there are nothing to display
+
+2-Apr-2010 Xillibit
+^ [#20081] Set new Id to all files and put build files
+^ [#20081] Littles changes on the module filenames
++ [#20081] Initial module structure
+
+-->

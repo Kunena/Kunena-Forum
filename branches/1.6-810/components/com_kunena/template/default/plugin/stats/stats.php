73ae3d057e45173fedfa3c7ae47f9343df49cb83
810 <schumacher1986@hotmail.com>
2010-04-18 09:25:57 +0000

    # [#20076] update branch
    git-svn-id: file:///Users/oratzes/svn/kunena@2298 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ branches/1.6-810/components/com_kunena/template/default/plugin/stats/stats.php
@@ -0,0 +1,350 @@
+<?php
+/**
+* @version $Id$
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2008 - 2010 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*
+* Based on FireBoard Component
+* @Copyright (C) 2006 - 2007 Best Of Joomla All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.bestofjoomla.com
+*
+* Based on Joomlaboard Component
+* @copyright (C) 2000 - 2004 TSMF / Jan de Graaff / All Rights Reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @author TSMF & Jan de Graaff
+**/
+
+// Dont allow direct linking
+defined( '_JEXEC' ) or die();
+
+
+$kunena_config =& CKunenaConfig::getInstance();
+$document =& JFactory::getDocument();
+
+$document->setTitle(JText::_('COM_KUNENA_STAT_FORUMSTATS') . ' - ' . stripslashes($kunena_config->board_title));
+
+if($kunena_config->showstats):
+
+$this->loadGenStats();
+$this->loadUserStats();
+$this->loadTopicStats();
+$this->loadPollStats();
+
+$forumurl = 'index.php?option=com_kunena';
+
+$userlist1 = CKunenaLink::GetUserlistLink('', $this->totalmembers);
+
+?>
+
+        <!-- BEGIN: GENERAL STATS -->
+<?php if($kunena_config->showgenstats): ?>
+<div class="k_bt_cvr1">
+<div class="k_bt_cvr2">
+<div class="k_bt_cvr3">
+<div class="k_bt_cvr4">
+<div class="k_bt_cvr5">
+        <table  class = "kblocktable" id ="kmorestat" border = "0" cellspacing = "0" cellpadding = "0" width="100%">
+            <thead>
+                <tr>
+                    <th>
+                        <div class = "ktitle_cover km">
+                            <span class="ktitle kl"><?php echo stripslashes($kunena_config->board_title); ?> <?php echo JText::_('COM_KUNENA_STAT_FORUMSTATS'); ?></span>
+                        </div>
+                        <img id = "BoxSwitch__morestat_tbody" class = "hideshow" src = "<?php echo KUNENA_URLIMAGESPATH . 'shrink.gif' ; ?>" alt = ""/>
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody id = "morestat_tbody">
+                <tr class = "ksth ks">
+                    <th class = "th-1 ksectiontableheader" align="left" width="50%"><?php echo JText::_('COM_KUNENA_STAT_GENERAL_STATS'); ?>
+                    </th>
+                </tr>
+
+                <tr class = "ksectiontableentry1">
+                    <td class = "td-1" align="left">
+<?php echo JText::_('COM_KUNENA_STAT_TOTAL_USERS'); ?>:<b> <?php echo $userlist1;?></b>
+                    &nbsp; <?php echo JText::_('COM_KUNENA_STAT_LATEST_MEMBERS'); ?>:<b> <?php echo CKunenaLink::GetProfileLink($this->lastestmemberid, $this->lastestmember); ?></b>
+
+                <br/> <?php echo JText::_('COM_KUNENA_STAT_TOTAL_MESSAGES'); ?>: <b> <?php echo $this->totalmsgs; ?></b> &nbsp;
+    <?php echo JText::_('COM_KUNENA_STAT_TOTAL_SUBJECTS'); ?>: <b> <?php echo $this->totaltitles; ?></b> &nbsp; <?php echo JText::_('COM_KUNENA_STAT_TOTAL_SECTIONS'); ?>: <b> <?php echo $this->totalcats; ?></b> &nbsp; <?php echo JText::_('COM_KUNENA_STAT_TOTAL_CATEGORIES'); ?>: <b> <?php echo $this->totalsections; ?></b>
+
+                <br/> <?php echo JText::_('COM_KUNENA_STAT_TODAY_OPEN_THREAD'); ?>: <b> <?php echo $this->todayopen; ?></b> &nbsp; <?php echo
+    JText::_('COM_KUNENA_STAT_YESTERDAY_OPEN_THREAD'); ?>: <b> <?php echo $this->yesterdayopen; ?></b> &nbsp; <?php echo JText::_('COM_KUNENA_STAT_TODAY_TOTAL_ANSWER'); ?>: <b> <?php echo $this->todayanswer; ?></b> &nbsp; <?php echo JText::_('COM_KUNENA_STAT_YESTERDAY_TOTAL_ANSWER'); ?>: <b> <?php echo $this->yesterdayanswer; ?></b>
+
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+</div>
+</div>
+</div>
+</div>
+<?php endif; ?>
+<!-- FINISH: GENERAL STATS -->
+
+<?php
+$tabclass = array
+(
+"sectiontableentry1",
+"sectiontableentry2"
+);
+$k = 0;
+?>
+
+
+<!-- B: Pop Subject -->
+<?php if($this->showpopsubjectstats): ?>
+<div class="k_bt_cvr1">
+<div class="k_bt_cvr2">
+<div class="k_bt_cvr3">
+<div class="k_bt_cvr4">
+<div class="k_bt_cvr5">
+<table class = "kblocktable " id="kpopsubmorestat"  cellpadding = "0" cellspacing = "0" border = "0" width = "100%">
+  <thead>
+    <tr>
+      <th colspan="3">
+      <div class = "ktitle_cover km"> <span class="ktitle kl"><?php echo JText::_('COM_KUNENA_STAT_TOP'); ?> <strong><?php echo $kunena_config->popsubjectcount; ?></strong> <?php echo JText::_('COM_KUNENA_STAT_POPULAR'); ?> <?php echo JText::_('COM_KUNENA_STAT_POPULAR_USER_KGSG'); ?></span> </div>
+      <img id = "BoxSwitch__kpopsubstats_tbody" class = "hideshow" src = "<?php echo KUNENA_URLIMAGESPATH . 'shrink.gif' ; ?>" alt = ""/>
+      </th>
+    </tr>
+  </thead>
+  <tbody id = "kpopsubstats_tbody">
+   <tr  class = "ksth" >
+      <th class = "th-1 ksectiontableheader" align="left" width="50%"> <?php echo JText::_('COM_KUNENA_GEN_SUBJECT') ;?></th>
+      <th class = "th-2 ksectiontableheader" width="40%">&nbsp;  </th>
+      <th class = "th-3 ksectiontableheader" align="center" width="10%"></th>
+    </tr>
+ <?php foreach ($this->toptitles as $toptitle)
+       {
+	   $k = 1 - $k;
+		   if ($toptitle->hits == $this->toptitlehits) {
+		   $barwidth = 100;
+		   }
+		   else {
+		   $barwidth = round(($toptitle->hits * 100) / $this->toptitlehits);
+		   }
+?>
+
+    <tr class = "k<?php echo $tabclass[$k]; ?>">
+      <td class="td-1" align="left">
+       <?php echo CKunenaLink::GetThreadLink( 'view', $toptitle->catid, $toptitle->id, KunenaParser::parseText (stripslashes($toptitle->subject)), '' ); ?>
+      </td>
+      <td  class="td-2">
+       <img class = "jr-forum-stat-bar" src = "<?php echo KUNENA_TMPLTMAINIMGURL.'/images/backgrounds/bar.png';?>" alt = "" height = "10" width = "<?php echo $barwidth;?>%"/>
+      </td>
+      <td  class="td-3">
+	  <?php echo $toptitle->hits; ?> <?php echo JText::_('COM_KUNENA_USRL_HITS') ;?>
+       </td>
+    </tr>
+<?php }   ?>
+  </tbody>
+</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+<?php endif; ?>
+<!-- F: Pop Subject -->
+
+
+<!-- B: Pop Poll -->
+<?php if($this->showpoppollstats): ?>
+<div class="k_bt_cvr1">
+<div class="k_bt_cvr2">
+<div class="k_bt_cvr3">
+<div class="k_bt_cvr4">
+<div class="k_bt_cvr5">
+<table class = "kblocktable " id="kpopsubmorestat"  cellpadding = "0" cellspacing = "0" border = "0" width = "100%">
+  <thead>
+    <tr>
+      <th colspan="3">
+      <div class = "ktitle_cover km"> <span class="ktitle kl"><?php echo JText::_('COM_KUNENA_STAT_TOP'); ?> <strong><?php echo $kunena_config->poppollscount; ?></strong> <?php echo JText::_('COM_KUNENA_STAT_POPULAR'); ?> <?php echo JText::_('COM_KUNENA_STAT_POPULAR_POLLS_KGSG'); ?></span> </div>
+      <img id = "BoxSwitch__kpopsubstats_tbody" class = "hideshow" src = "<?php echo KUNENA_URLIMAGESPATH . 'shrink.gif' ; ?>" alt = ""/>
+      </th>
+    </tr>
+  </thead>
+  <tbody id = "kpopsubstats_tbody">
+   <tr  class = "ksth" >
+      <th class = "th-1 ksectiontableheader" align="left" width="50%"> <?php echo JText::_('COM_KUNENA_POLL_NAME');?></th>
+      <th class = "th-2 ksectiontableheader" width="40%">&nbsp;  </th>
+      <th class = "th-3 ksectiontableheader" align="center" width="10%"></th>
+    </tr>
+ <?php foreach($this->toppolls as $toppoll)
+       {
+       if($toppoll->total != "0")
+       {
+	   $k = 1 - $k;
+		   if ($toppoll->total == $this->toppollvotes) {
+          $barwidth = 100;
+		   }
+		   else {
+		    if($toppoll->total== null){
+          $toppoll->total = "0";
+        }
+		    $barwidth = round(($toppoll->total * 100) / $this->toppollvotes);
+		   }
+?>
+
+    <tr class = "k<?php echo $tabclass[$k]; ?>">
+      <td class="td-1" align="left">
+       <?php echo CKunenaLink::GetThreadLink( 'view', $toppoll->catid, $toppoll->threadid, kunena_htmlspecialchars($toppoll->title), '' ); ?>
+      </td>
+      <td  class="td-2">
+       <img class = "jr-forum-stat-bar" src = "<?php echo KUNENA_TMPLTMAINIMGURL.'/images/backgrounds/bar.png';?>" alt = "" height = "10" width = "<?php echo $barwidth;?>%"/>
+      </td>
+      <td  class="td-3">
+	  <?php echo $toppoll->total; ?> <?php echo JText::_('COM_KUNENA_USRL_VOTES') ;?>
+       </td>
+    </tr>
+<?php }
+}  ?>
+  </tbody>
+</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+<?php endif; ?>
+<!-- F: Pop Polls -->
+
+
+<!-- B: User Messages -->
+<?php if($this->showpopuserstats): ?>
+<div class="k_bt_cvr1">
+<div class="k_bt_cvr2">
+<div class="k_bt_cvr3">
+<div class="k_bt_cvr4">
+<div class="k_bt_cvr5">
+<table class = "kblocktable " id="kpopusermsgmorestat"  cellpadding = "0" cellspacing = "0" border = "0" width = "100%">
+  <thead>
+    <tr>
+      <th colspan="3">
+      <div class = "ktitle_cover km"> <span class="ktitle kl"><?php echo JText::_('COM_KUNENA_STAT_TOP'); ?> <strong><?php echo $kunena_config->popusercount; ?></strong> <?php echo JText::_('COM_KUNENA_STAT_POPULAR'); ?> <?php echo JText::_('COM_KUNENA_STAT_POPULAR_USER_TMSG'); ?></span></div>
+      <img id = "BoxSwitch__kpopusermsgstats_tbody" class = "hideshow" src = "<?php echo KUNENA_URLIMAGESPATH . 'shrink.gif' ; ?>" alt = ""/>
+      </th>
+    </tr>
+  </thead>
+  <tbody id = "kpopusermsgstats_tbody">
+   <tr  class = "ksth" >
+      <th class = "th-1 ksectiontableheader" align="left" width="50%"><?php echo JText::_('COM_KUNENA_USRL_USERNAME') ;?></th>
+      <th class = "th-2 ksectiontableheader" width="40%">&nbsp;  </th>
+      <th class = "th-3 ksectiontableheader" align="center" width="10%"></th>
+    </tr>
+<?php
+
+	foreach ($this->topposters as $poster)
+	{
+
+	$k = 1 - $k;
+
+	if ($poster->posts == $this->topmessage) {
+	$barwidth = 100;
+	}
+	else {
+	$barwidth = round(($poster->posts * 100) / $this->topmessage);
+	}
+?>
+
+    <tr class = "k<?php echo $tabclass[$k]; ?>">
+      <td  class="td-1"  align="left">
+
+         <?php echo CKunenaLink::GetProfileLink($poster->userid, $poster->username); ?>
+
+</td>
+      <td  class="td-2">
+         <img class = "jr-forum-stat-bar" src = "<?php echo KUNENA_TMPLTMAINIMGURL.'/images/backgrounds/bar.png';?>" alt = "" height = "10" width = "<?php echo $barwidth;?>%"/>
+                                    </td>
+      <td  class="td-3">
+	  <?php echo $poster->posts; ?> <?php echo JText::_('COM_KUNENA_USRL_POSTS') ;?>
+       </td>
+    </tr>
+<?php }   ?>
+  </tbody>
+</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+<?php endif; ?>
+<!-- F: User Messages -->
+
+
+<!-- B: Pop User  -->
+<?php if($this->showpopuserstats): ?>
+<div class="k_bt_cvr1">
+<div class="k_bt_cvr2">
+<div class="k_bt_cvr3">
+<div class="k_bt_cvr4">
+<div class="k_bt_cvr5">
+<table class = "kblocktable " id="kpopuserhitmorestat"  cellpadding = "0" cellspacing = "0" border = "0" width = "100%">
+  <thead>
+    <tr>
+      <th colspan="3">
+      <div class = "ktitle_cover km"> <span class="ktitle kl"><?php echo JText::_('COM_KUNENA_STAT_TOP'); ?> <strong><?php echo $kunena_config->popusercount; ?></strong> <?php echo JText::_('COM_KUNENA_STAT_POPULAR'); ?> <?php echo JText::_('COM_KUNENA_STAT_POPULAR_USER_GSG'); ?></span> </div>
+      <img id = "BoxSwitch__kpopuserhitstats_tbody" class = "hideshow" src = "<?php echo KUNENA_URLIMAGESPATH . 'shrink.gif' ; ?>" alt = ""/>
+      </th>
+    </tr>
+  </thead>
+  <tbody id = "kpopuserhitstats_tbody">
+   <tr  class = "ksth ks" >
+      <th class = "th-1 ksectiontableheader"  align="left" width="50%"> <?php echo JText::_('COM_KUNENA_USRL_USERNAME') ;?></th>
+      <th class = "th-2 ksectiontableheader" width="40%">&nbsp;  </th>
+      <th class = "th-3 ksectiontableheader" align="center" width="10%"></th>
+    </tr>
+
+<?php
+foreach ($this->topprofiles as $topprofile)
+{
+$k = 1 - $k;
+if ($topprofile->hits == $this->topprofilehits) {
+$barwidth = 100;
+}
+else {
+$barwidth = round(($topprofile->hits * 100) / $this->topprofilehits);
+}
+?>
+
+    <tr class = "k<?php echo $tabclass[$k]; ?>">
+      <td  class="td-1"  align="left">
+        <?php echo CKunenaLink::GetProfileLink($topprofile->user_id, $topprofile->user); ?>
+</td>
+      <td  class="td-2">
+         <img class = "jr-forum-stat-bar" src = "<?php echo KUNENA_TMPLTMAINIMGURL.'/images/backgrounds/bar.png';?>" alt = "" height = "10" width = "<?php echo $barwidth;?>%"/>
+                                    </td>
+      <td  class="td-3">
+	  <?php echo $topprofile->hits; ?> <?php echo JText::_('COM_KUNENA_USRL_HITS') ;?>
+       </td>
+    </tr>
+<?php }   ?>
+  </tbody>
+</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+<?php endif; ?>
+<!-- F: User User -->
+
+
+<?php
+// WHOISONLINE
+if (file_exists(KUNENA_ABSTMPLTPATH . '/plugin/who/whoisonline.php')) {
+    include(KUNENA_ABSTMPLTPATH . '/plugin/who/whoisonline.php');
+}
+else {
+    include(KUNENA_PATH_TEMPLATE_DEFAULT .DS. 'plugin/who/whoisonline.php');
+}
+// /WHOISONLINE
+
+endif;
---
+++ branches/1.6-810/components/com_kunena/template/default/profile/usertab.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * @version $Id$
+ * Kunena Component
+ * @package Kunena
+ *
+ * @Copyright (C) 2008 - 2010 Kunena Team All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.com
+ *
+ **/
+defined( '_JEXEC' ) or die();
+?>
+<div id="kprofile-rightcoltop">
+	<div class="kprofile-rightcol2">
+<?php
+	CKunenaTools::loadTemplate('/profile/socialbuttons.php');
+?>
+	</div>
+	<div class="kprofile-rightcol1">
+		<ul>
+			<li><span class="location"></span><strong><?php echo JText::_('COM_KUNENA_MYPROFILE_LOCATION'); ?>:</strong> <?php echo $this->location; ?></li>
+			<!--  The gender determines the suffix on the span class- gender-male & gender-female  -->
+			<li><span class="gender-<?php echo $this->genderclass; ?>"></span><strong><?php echo JText::_('COM_KUNENA_MYPROFILE_GENDER'); ?>:</strong> <?php echo $this->gender; ?></li>
+			<li class="bd"><span class="birthdate"></span><strong><?php echo JText::_('COM_KUNENA_MYPROFILE_BIRTHDATE'); ?>:</strong> <span title="<?php echo CKunenaTimeformat::showDate($this->profile->birthdate, 'ago', 'utc', 0); ?>"><?php echo CKunenaTimeformat::showDate($this->profile->birthdate, 'date', 'utc', 0); ?></span>
+			<!--  <a href="#" title=""><span class="bday-remind"></span></a> -->
+			</li>
+		</ul>
+	</div>
+</div>
+
+<div class="clrline"></div>
+<div id="kprofile-rightcolbot">
+	<div class="kprofile-rightcol2">
+		<ul>
+			<?php if ($this->config->showemail && (!$this->profile->hideEmail || CKunenaTools::isModerator($this->my->id))): ?><li><span class="email"></span><a href="mailto:<?php echo $this->user->email; ?>"><?php echo $this->user->email; ?></a></li><?php endif; ?>
+			<?php // FIXME: we need a better way to add http/https ?>
+			<li><span class="website"></span><a href="http://<?php echo kunena_htmlspecialchars(stripslashes($this->profile->websiteurl)); ?>" target="_blank"><?php echo kunena_htmlspecialchars(stripslashes($this->profile->websitename)); ?></a></li>
+		</ul>
+	</div>
+	<div class="kprofile-rightcol1">
+		<h4><?php echo JText::_('COM_KUNENA_MYPROFILE_ABOUTME'); ?></h4>
+		<p><?php echo $this->personalText; ?></p>
+		<h4><?php echo JText::_('COM_KUNENA_MYPROFILE_SIGNATURE'); ?></h4>
+		<div class="msgsignature"><div><?php echo $this->signature; ?></div></div>
+	</div>
+
+</div>
+
+<div class="clr"></div>
+
+<div id="kprofile-tabs">
+	<dl class="tabs">
+		<dt class="open"><?php echo JText::_('COM_KUNENA_USERPOSTS'); ?></dt>
+		<dd style="display: none;">
+			<?php $this->displayUserPosts(); ?>
+		</dd>
+		<?php if ($this->my->id == $this->user->id): ?>
+		<!--
+		<dt class="closed"><?php echo JText::_('COM_KUNENA_OWNTOPICS'); ?></dt>
+		<dd style="display: none;">
+			<?php //$this->displayOwnTopics(); ?>
+		</dd>
+		<dt class="closed"><?php echo JText::_('COM_KUNENA_USERTOPICS'); ?></dt>
+		<dd style="display: none;">
+			<?php //$this->displayUserTopics(); ?>
+		</dd>
+		-->
+		<?php if ($this->config->allowsubscriptions) :?>
+		<dt class="closed"><?php echo JText::_('COM_KUNENA_SUBSCRIPTIONS'); ?></dt>
+		<dd style="display: none;">
+			<?php $this->displaySubscriptions(); ?>
+		</dd>
+		<?php endif; ?>
+		<?php if ($this->config->allowfavorites) : ?>
+		<dt class="closed"><?php echo JText::_('COM_KUNENA_FAVORITES'); ?></dt>
+		<dd style="display: none;">
+			<?php $this->displayFavorites(); ?>
+		</dd>
+		<?php endif; ?>
+		<?php endif; if (CKunenaTools::isModerator($this->my->id) && $this->my->id != $this->user->id): ?>
+		<!-- Only visible to moderators and admins -->
+		<dt class="kprofile-modbtn"><?php echo JText::_('COM_KUNENA_MODERATE_THIS_USER'); ?></dt>
+		<dd class="kprofile-modtools">
+			<?php
+			$path = KUNENA_PATH_LIB.'/kunena.moderation.class.php';
+			require_once ($path);
+			$kunena_mod = CKunenaModeration::getInstance();
+			$iplist = $kunena_mod->getUserIPs ($this->user->id);
+			$useriplist = $kunena_mod->getUsernameMatchingIPs($this->user->id);
+			?>
+			<h4><?php echo JText::_('COM_KUNENA_MODERATE_USERIPS'); ?>:</h4>
+			<ul>
+				<?php
+				$usernames = array();
+				foreach ($iplist as $ip) {
+					$usernames = array_merge($usernames,$useriplist[$ip->ip]);
+					$username = array();
+					foreach ($usernames as $user) {
+						$username[] = CKunenalink::GetProfileLink($user->userid, $user->name, $rel='nofollow', $class='');
+					}
+					$username=implode(', ',$username);
+
+					if (!empty($useriplist[$ip->ip])) {
+				?>
+				<li><span><a href="http://ws.arin.net/whois/?queryinput=<?php echo $ip->ip; ?>" target="_blank"><?php echo $ip->ip; ?></a></span> (<?php echo JText::_('COM_KUNENA_MODERATE_OTHER_USERS_WITH_IP'); ?>: <?php echo $username; ?>)</li>
+				<?php
+					} else {
+					?>
+				<li><span><a href="http://ws.arin.net/whois/?queryinput=<?php echo $ip->ip; ?>" target="_blank"><?php echo $ip->ip; ?></a></span> (<?php echo JText::_('COM_KUNENA_MODERATE_OTHER_USERS_WITH_IP'); ?>: <?php echo JText::_('COM_KUNENA_MODERATION_USER_NONE_IPS'); ?>)</li>
+				<?php
+					}
+				} ?>
+			</ul>
+			<h4><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_USER'); ?>:</h4>
+			<form id="kform-ban" name="kformban" action="index.php" method="post">
+
+				<label for="ban-ip">
+				<span><?php echo JText::_('COM_KUNENA_MODERATE_BANIP'); ?></span>
+				<?php
+				$ipselect = array();
+				foreach ($iplist as $ip) {
+					$ipselect [] = JHTML::_ ( 'select.option', '0', JText::_('COM_KUNENA_MODERATE_SELECT_IP') );
+					$ipselect [] = JHTML::_ ( 'select.option', $ip->ip, $ip->ip );
+					$ipselect [] = JHTML::_ ( 'select.option', 'allips', JText::_('COM_KUNENA_MODERATE_ALLIPS') );
+				}
+
+				echo $lists = JHTML::_ ( 'select.genericlist', $ipselect, 'prof_ip_select', 'class="inputbox" size="1"', 'value', 'text' );
+				?>
+				</label>
+				<label for="ban-email"><input type="checkbox" id="ban-email" name="banemail" value="banemail" class="kcheckbox" />
+				<span onclick="document.kformban.banemail.checked=(! document.kformban.banemail.checked);"><?php echo JText::_('COM_KUNENA_MODERATE_BANEMAIL'); ?></span></label>
+				<label for="ban-username"><input type="checkbox" id="ban-username" name="banusername" value="banusername" class="kcheckbox" />
+				<span onclick="document.kformban.banusername.checked=(! document.kformban.banusername.checked);"><?php echo JText::_('COM_KUNENA_MODERATE_BANUSERNAME'); ?></span></label>
+				<label for="ban-delsignature"><input type="checkbox" id="ban-delsignature" name="delsignature" value="delsignature" class="kcheckbox" />
+				<span onclick="document.kformban.bandelposts.checked=(! document.kformban.bandelposts.checked);"><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_BAD_SIGNATURE'); ?></span></label>
+				<label for="ban-delavatar"><input type="checkbox" id="ban-delavatar" name="delavatar" value="delavatar" class="kcheckbox" />
+				<span onclick="document.kformban.delavatar.checked=(! document.kformban.delavatar.checked);"><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_BAD_AVATAR'); ?></span></label>
+				<label for="ban-delprofileinfo"><input type="checkbox" id="ban-delprofileinfo" name="delprofileinfo" value="delprofileinfo" class="kcheckbox" />
+				<span onclick="document.kformban.delprofileinfo.checked=(! document.kformban.delprofileinfo.checked);"><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_BAD_PROFILEINFO'); ?></span></label>
+				<label for="ban-delposts"><input type="checkbox" id="ban-delposts" name="bandelposts" value="bandelposts" class="kcheckbox" />
+				<span onclick="document.kformban.bandelposts.checked=(! document.kformban.bandelposts.checked);"><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_ALL_POSTS'); ?></span></label>
+				<input class="kbutton kbutton ks" type="submit" value="<?php echo JText::_('COM_KUNENA_MODERATE_DELETE_USER'); ?>" name="Submit" />
+				<input type="hidden" name="option" value="com_kunena" /> <input
+				type="hidden" name="func" value="banactions" /> <input
+				type="hidden" name="thisuserid" value="<?php echo $this->user->id; ?>" />
+			</form>
+	</dd>
+	<?php endif; ?>
+	</dl>
+</div>

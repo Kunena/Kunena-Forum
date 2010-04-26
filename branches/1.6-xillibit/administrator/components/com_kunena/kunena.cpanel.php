22bba6f01a6a55d4fd8c0681624a67813b333733
xillibit <xillibit@gmail.com>
2010-04-26 13:38:07 +0000

    [#18627] Create fresh branch 1.6-xillibit
    git-svn-id: file:///Users/oratzes/svn/kunena@2357 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ branches/1.6-xillibit/administrator/components/com_kunena/kunena.cpanel.php
@@ -0,0 +1,345 @@
+<?php
+/**
+* @version $Id$
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2008 - 2010 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+
+*
+* Based on FireBoard Component
+* @Copyright (C) 2006 - 2007 Best Of Joomla All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.bestofjoomla.com
+*
+* Based on Joomlaboard Component
+* @copyright (C) 2000 - 2004 TSMF / Jan de Graaff / All Rights Reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @author TSMF & Jan de Graaff
+**/
+/** ensure this file is being included by a parent file */
+defined( '_JEXEC' ) or die();
+
+?>
+<style>
+.kadmin-welcome {
+	clear:both;
+	margin:10px 0;
+	padding:10px;
+	font-size:12px;
+	color:#536482;
+	line-height:140%;
+	border:1px solid #ddd;
+}
+.kadmin-welcome h3 {
+	margin:0;
+	padding:0;
+}
+table.thisform {
+	width: 100%;
+	padding: 10px;
+	border-collapse: collapse;
+}
+table.thisform tr.row0 {
+	background-color: #F7F8F9;
+}
+table.thisform tr.row1 {
+	background-color: #eeeeee;
+}
+table.thisform th {
+	font-size: 15px;
+	font-weight: normal;
+	font-variant: small-caps;
+	padding-top: 6px;
+	padding-bottom: 2px;
+	padding-left: 4px;
+	padding-right: 4px;
+	text-align: left;
+	height: 25px;
+	color: #666666;
+	background: url(../images/background.gif);
+	background-repeat: repeat;
+}
+table.thisform td {
+	padding: 3px;
+	text-align: left;
+}
+.kadmin-statscover {
+	padding:0px;
+}
+table.kadmin-stat {
+	background-color:#FFFFFF;
+	border:1px solid #ddd;
+	padding:1px;
+	width:100%;
+}
+table.kadmin-stat th {
+	background:#EEE;
+	border-bottom:1px solid #CCC;
+	border-top:1px solid #EEE;
+	color:#666;
+	font-size:11px;
+	padding:3px 4px;
+	text-align:left;
+}
+table.kadmin-stat td {
+	font-size:11px;
+	line-height:140%;
+	padding:4px;
+	text-align:left;
+}
+table.kadmin-stat caption {
+	clear:both;
+	font-size:14px;
+	font-weight:bold;
+	margin:10px 0 2px 0;
+	padding:2px;
+	text-align:left;
+}
+table.kadmin-stat .col1 {
+	background-color:#F1F3F5;
+}
+table.kadmin-stat .col2 {
+	background-color: #FBFBFB;
+}
+div.icon a {
+	text-decoration:none;
+}
+div.icon-container {
+	float:left;
+}
+
+</style>
+<div class="kadmin-welcome">
+	<h3><?php echo JText::_('COM_KUNENA_WELCOME');?></h3>
+	<p><?php echo JText::_('COM_KUNENA_WELCOME_DESC');?></p>
+</div>
+<div style="border:1px solid #ddd; background:#FBFBFB;">
+	<table class = "thisform">
+		<tr class = "thisform">
+			<td width = "100%" valign = "top" class = "thisform">
+				<div id = "cpanel">
+					  <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=showconfig" title = "<?php echo JText::_('COM_KUNENA_C_FBCONFIGDESC');?>"> <img src = "components/com_kunena/images/kconfig.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_C_FBCONFIG'); ?> </span></a> </div>
+					  </div>
+					  <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=showAdministration" title = "<?php echo JText::_('COM_KUNENA_C_FORUMDESC');?>"> <img src = "components/com_kunena/images/kforumadm.png" align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_C_FORUM'); ?> </span></a> </div>
+					  </div>
+					  <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=showprofiles" title = "<?php echo JText::_('COM_KUNENA_C_USERDESC');?>"> <img src = "components/com_kunena/images/kuser.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_C_USER'); ?> </span> </a> </div>
+					  </div>
+					  <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=browseFiles" title = "<?php echo JText::_('COM_KUNENA_C_FILESDESC');?>"> <img src = "components/com_kunena/images/kfiles.png" align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_C_FILES'); ?> </span></a> </div>
+					  </div>
+					  <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=browseImages" title = "<?php echo JText::_('COM_KUNENA_C_IMAGESDESC');?>"> <img src = "components/com_kunena/images/kimages.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_C_IMAGES'); ?> </span></a> </div>
+					  </div>
+					   <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=showCss" title = "<?php echo JText::_('COM_KUNENA_C_CSSDESC');?>"> <img src = "components/com_kunena/images/kcss.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_C_CSS'); ?> </span></a> </div>
+					  </div>
+					   <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=pruneforum" title = "<?php echo JText::_('COM_KUNENA_C_PRUNETABDESC');?>"> <img src = "components/com_kunena/images/ktable.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_C_PRUNETAB'); ?> </span></a> </div>
+					  </div>
+					   <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=syncusers" title = "<?php echo JText::_('COM_KUNENA_C_SYNCEUSERSDESC');?>"> <img src = "components/com_kunena/images/kusers.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_SYNC_USERS'); ?> </span></a> </div>
+					  </div>
+					   <div class="icon-container">
+						<div class = "icon"> <a href = "http://www.kunena.com" target = "_blank" title = "<?php echo JText::_('COM_KUNENA_C_SUPPORTDESC');?>"> <img src = "components/com_kunena/images/ktechsupport.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_C_SUPPORT'); ?> </span></a> </div>
+					  </div>
+					   <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=showsmilies" title = "<?php echo JText::_('COM_KUNENA_EMOTICONS_EMOTICON_MANAGER');?>"> <img src = "components/com_kunena/images/ksmiley.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_EMOTICONS_EMOTICON_MANAGER');?> </span></a> </div>
+					  </div>
+					   <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=recount&no_html=1" title = "<?php echo JText::_('COM_KUNENA_RECOUNTFORUMS');?>"> <img src = "components/com_kunena/images/kupgrade.png" align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_RECOUNTFORUMS'); ?> </span></a> </div>
+					  </div>
+					   <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=ranks" title = "<?php echo JText::_('COM_KUNENA_RANK_MANAGER');?>"> <img src = "components/com_kunena/images/kranks.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_RANK_MANAGER'); ?> </span></a> </div>
+					  </div>
+					   <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=createmenu" title = "<?php echo JText::_('COM_KUNENA_CREATE_MENU');?>"> <img src = "images/menu.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_CREATE_MENU'); ?> </span></a> </div>
+					  </div>
+					   <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=showtrashview" title = "<?php echo JText::_('COM_KUNENA_TRASH_VIEW');?>"> <img src = "images/trash.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_TRASH_VIEW'); ?> </span></a> </div>
+					  </div>
+					  <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=showsystemreport" title = "<?php echo JText::_('COM_KUNENA_REPORT_SYSTEM');?>"> <img src = "components/com_kunena/images/report_conf.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_REPORT_SYSTEM'); ?> </span></a> </div>
+					  </div>
+					  <?php
+						$kn_version = CKunenaVersion::versionArray ();
+						 if (JString::strpos ( $kn_version->version, 'SVN' ) !== false) {
+					  ?>
+					  <div class="icon-container">
+						<div class = "icon"> <a href = "index.php?option=com_kunena&amp;task=install" title = "<?php echo JText::_('COM_KUNENA_SVN_INSTALL');?>"> <img src = "images/install.png"  align = "middle" border = "0"/> <span> <?php echo JText::_('COM_KUNENA_SVN_INSTALL'); ?> </span></a> </div>
+					  </div>
+					  <?php
+						}
+					  ?>
+				</div>
+       		</td>
+   		 </tr>
+  	</table>
+</div>
+<!-- BEGIN: STATS -->
+<div class="kadmin-statscover">
+  <?php include_once (JPATH_COMPONENT_ADMINISTRATOR .'/lib/kunena.stats.class.php'); ?>
+  <table cellspacing="1"  border="0" width="100%" class="kadmin-stat">
+    <caption><?php echo JText::_('COM_KUNENA_STATS_GEN_STATS'); ?></caption>
+    <col class="col1">
+    <col class="col2">
+    <col class="col1">
+    <col class="col2">
+    <thead>
+      <tr>
+        <th><?php echo JText::_('COM_KUNENA_STATISTIC');?></th>
+        <th><?php echo JText::_('COM_KUNENA_VALUE');?></th>
+        <th><?php echo JText::_('COM_KUNENA_STATISTIC');?></th>
+        <th><?php echo JText::_('COM_KUNENA_VALUE');?></th>
+      </tr>
+    </thead>
+    <?php $yesterday = mktime(0, 0, 0, date("m")  , date("d")-1, date("Y")); ?>
+    <tbody>
+      <tr>
+        <td><?php echo JText::_('COM_KUNENA_STATS_TOTAL_MEMBERS'); ?> </td>
+        <td><strong><?php echo jbStats::get_total_members(); ?></strong></td>
+        <td><?php echo JText::_('COM_KUNENA_STATS_TOTAL_SECTIONS'); ?> </td>
+        <td><strong><?php echo jbStats::get_total_sections();?></strong></td>
+      </tr>
+      <tr>
+        <td><?php echo JText::_('COM_KUNENA_STATS_TOTAL_REPLIES'); ?></td>
+        <td><strong><?php echo jbStats::get_total_messages() ;?></strong></td>
+        <td><?php echo JText::_('COM_KUNENA_STATS_TOTAL_CATEGORIES'); ?> </td>
+        <td><strong><?php echo jbStats::get_total_categories() ;?></strong></td>
+      </tr>
+      <tr>
+        <td><?php echo JText::_('COM_KUNENA_STATS_TOTAL_TOPICS'); ?></td>
+        <td><strong><?php echo jbStats::get_total_topics() ;?></strong></td>
+        <td><?php echo JText::_('COM_KUNENA_STATS_LATEST_MEMBER'); ?> </td>
+        <td><strong><?php echo jbStats::get_latest_member() ;?></strong></td>
+      </tr>
+      <tr>
+        <td><?php echo JText::_('COM_KUNENA_STATS_TODAY_TOPICS'); ?></td>
+        <td><strong><?php echo jbStats::get_total_topics(date("Y-m-d 00:00:01"),date("Y-m-d 23:59:59")) ;?></strong></td>
+        <td><?php echo JText::_('COM_KUNENA_STATS_YESTERDAY_TOPICS'); ?> </td>
+        <td><strong><?php echo jbStats::get_total_topics(date("Y-m-d 00:00:01",$yesterday),date("Y-m-d 23:59:59",$yesterday)) ;?></strong></td>
+      </tr>
+      <tr>
+        <td><?php echo JText::_('COM_KUNENA_STATS_TODAY_REPLIES'); ?></td>
+        <td><strong><?php echo jbStats::get_total_messages(date("Y-m-d 00:00:01"),date("Y-m-d 23:59:59")) ;?></strong></td>
+        <td><?php echo JText::_('COM_KUNENA_STATS_YESTERDAY_REPLIES'); ?></td>
+        <td><strong><?php echo jbStats::get_total_messages(date("Y-m-d 00:00:01",$yesterday),date("Y-m-d 23:59:59",$yesterday)) ; ?></strong></td>
+      </tr>
+    </tbody>
+  </table>
+  <!-- B: UserStat -->
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td width="49%" valign="top">
+        <table cellspacing="1"  border="0" width="100%" class="kadmin-stat">
+          <caption><?php echo JText::_('COM_KUNENA_STATS_TOP_POSTERS'); ?></caption>
+          <col class="col1">
+          <col class="col2">
+          <col class="col2">
+          <thead>
+            <tr>
+              <th><?php echo JText::_('COM_KUNENA_USRL_USERNAME');?></th>
+              <th></th>
+              <th><?php echo JText::_('COM_KUNENA_USRL_HITS');?></th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+				$KUNENA_top_posters=jbStats::get_top_posters();
+				foreach ($KUNENA_top_posters as $KUNENA_poster) {
+					if ($KUNENA_poster->posts == $KUNENA_top_posters[0]->posts) {
+						$barwidth = 100;
+					}
+					else {
+						$barwidth = round(($KUNENA_poster->posts * 100) / $KUNENA_top_posters[0]->posts);
+					}
+			?>
+            <tr>
+              <td><?php echo $KUNENA_poster->username;?> </td>
+              <td ><img style="margin-bottom:1px" src="<?php echo KUNENA_DIRECTURL.'/template/default/images/backgrounds/bar.png'; ?>" alt="" height="15" width="<?php echo $barwidth;?>"> </td>
+              <td ><?php echo $KUNENA_poster->posts;?></td>
+            </tr>
+            <?php
+				}
+				?>
+          </tbody>
+        </table>
+      </td>
+      <td width="1%">&nbsp;</td>
+      <td width="49%" valign="top">
+        <table cellspacing="1"  border="0" width="100%" class="kadmin-stat">
+          <caption><?php echo  JText::_('COM_KUNENA_STATS_POPULAR_PROFILE'); ?></caption>
+          <col class="col1">
+          <col class="col2">
+          <col class="col2">
+          <thead>
+            <tr>
+              <th><?php echo JText::_('COM_KUNENA_USRL_USERNAME');?></th>
+              <th></th>
+              <th><?php echo JText::_('COM_KUNENA_USRL_HITS');?></th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+		$fb_top_profiles=jbStats::get_top_profiles();
+		foreach ($fb_top_profiles as $fb_profile) {
+			if ($fb_profile->uhits == $fb_top_profiles[0]->uhits)
+				$barwidth = 100;
+			else
+				$barwidth = round(($fb_profile->uhits * 100) / $fb_top_profiles[0]->uhits);
+			?>
+            <tr>
+              <td><?php echo $fb_profile->username; ?></td>
+              <td ><img style="margin-bottom:1px" src="<?php echo KUNENA_DIRECTURL.'/template/default/images/backgrounds/bar.png'; ?>" alt="" height="15" width="<?php echo $barwidth;?>"> </td>
+              <td ><?php echo $fb_profile->uhits;?></td>
+            </tr>
+            <?php
+				}
+			?>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  </table>
+  <!-- F: UserStat -->
+  <!-- Begin : Top popular topics -->
+  <table cellspacing="1"  border="0" width="100%" class="kadmin-stat">
+    <caption><?php echo JText::_('COM_KUNENA_STATS_POPULAR_TOPICS'); ?></caption>
+    <col class="col1">
+    <col class="col2">
+    <col class="col2">
+    <thead>
+      <tr>
+        <th><?php echo JText::_('COM_KUNENA_USERPROFILE_TOPICS');?></th>
+        <th></th>
+        <th><?php echo JText::_('COM_KUNENA_USRL_HITS');?></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+		$KUNENA_top_posts=jbStats::get_top_topics();
+		foreach ($KUNENA_top_posts as $KUNENA_post) {
+			if ($KUNENA_post->hits == $KUNENA_top_posts[0]->hits) {
+				$barwidth = 100;
+			}
+			else {
+				$barwidth = round(($KUNENA_post->hits * 100) / $KUNENA_top_posts[0]->hits);
+			}
+			$link = KUNENA_LIVEURL.'&func=view&id='.$KUNENA_post->id.'&catid='.$KUNENA_post->catid;
+		?>
+      <tr>
+        <td ><a href="<?php echo $link;?>"><?php echo $KUNENA_post->subject;?></a> </td>
+        <td ><img src="<?php echo KUNENA_DIRECTURL.'/template/default/images/backgrounds/bar.png'; ?>" alt="" style="margin-bottom:1px" height="15" width="<?php echo $barwidth;?>"> </td>
+        <td ><?php echo $KUNENA_post->hits;?></td>
+      </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+  <!-- Finish : Top popular topics -->
+</div>
+<!-- FINISH: STATS -->

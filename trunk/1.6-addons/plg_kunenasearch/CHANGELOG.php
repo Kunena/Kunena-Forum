a85eef890bf6de2d91d6fd9659211b1224089904
xillibit <xillibit@gmail.com>
2010-05-01 09:44:04 +0000

    + [#20138] Initial plugin structure
    git-svn-id: file:///Users/oratzes/svn/kunena@2398 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/1.6-addons/plg_kunenasearch/CHANGELOG.php
@@ -0,0 +1,37 @@
+<?php
+/**
+* @version $Id: CHANGELOG.php 2159 2010-04-03 09:32:04Z xillibit $
+* KunenaSearch Plugin
+* @package Kunena Search
+*
+* @Copyright (C) 2009 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+
+// no direct access
+defined( '_VALID_MOS' ) or die( 'Restricted access' );
+?>
+<!--
+Changelog
+------------
+This is a non-exhaustive (but still near complete) changelog for
+the KunenaSearch plugin, including beta and release candidate versions.
+Legend:
+
+* -> Security Fix
+# -> Bug Fix
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+KunenaSearch 1.0
+
+01-May 2010 Xillibit
++ [#20138] Initial plugin structure 
+
+
+-->
---
+++ trunk/1.6-addons/plg_kunenasearch/README.txt
@@ -0,0 +1,23 @@
+/**
+* @version $Id: README.txt 2159 2010-04-03 09:32:04Z xillibit $
+* KunenaSearch Plugin
+* @package Kunena Search
+*
+* @Copyright (C) 2009 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+
+Kunena Search Plugin README
+
+PLEASE READ THIS ENTIRE FILE BEFORE INSTALLING Kunena Search @kunenaversion@!
+
+INTRODUCTION
+============
+
+Kunena Search enables your site visitors to see in joomla! search results the things from Kunea.
+
+Requirements: Joomla 1.5, Kunena Forum 1.6
+
+END OF README
+=============

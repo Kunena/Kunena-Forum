df513cd1343c7e51d180f9faa7ae195bd2d8433a
xillibit <xillibit@gmail.com>
2010-05-05 07:48:47 +0000

    [#18627] Create fresh branch 1.6-xillibit
    git-svn-id: file:///Users/oratzes/svn/kunena@2411 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ branches/1.6-xillibit/administrator/components/com_kunena/libraries/tables/kunenasession.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* @version $Id$
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2008 - 2010 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*
+**/
+// Dont allow direct linking
+defined( '_JEXEC' ) or die();
+
+require_once(dirname(__FILE__).DS.'kunena.php');
+
+class TableKunenaSession extends TableKunena
+{
+	var $userid = 0;
+	var $allowed = 'na';
+	var $allowedcats = null;
+	var $lasttime = 0;
+	var $readtopics = 0;
+	var $currvisit = 0;
+	protected $_exists = false;
+
+	function __construct($db) {
+		parent::__construct('#__fb_sessions', 'userid', $db);
+	}
+}
+
+?>

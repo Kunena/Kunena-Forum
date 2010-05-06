72e5b3919e6ee6c706798b0bd0f7b8e630fc66c9
xillibit <xillibit@gmail.com>
2010-05-06 02:51:06 +0000

    # [#19288] Fix regresion on who.class.php which breack the viewing users in pathway
    git-svn-id: file:///Users/oratzes/svn/kunena@2415 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6-xillibit/components/com_kunena/CHANGELOG.php
+++ branches/1.6-xillibit/components/com_kunena/CHANGELOG.php
@@ -34,6 +34,9 @@ Legend:
 
 Kunena 1.6.0-DEV
 
+6-May-2010 Xillibit
+# [#19288] Fix regresion on who.class.php which breack the viewing users in pathway
+
 5-May-2010 Xillibit
 # [#19288] Fix regression in kunena.who.class.php with method GetCategoryListURL() undefined
 # [#19288] Fix regresssion wrong hidden users display in whoisonline

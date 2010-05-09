a4bdc26a8ff7fdc84d0fc551e56a59a21fcc4e65
xillibit <xillibit@gmail.com>
2010-05-09 15:23:45 +0000

    [#18627] Create fresh branch 1.6-xillibit
    git-svn-id: file:///Users/oratzes/svn/kunena@2428 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ branches/1.6-xillibit/administrator/components/com_kunena/libraries/integration/communitybuilder/profile.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @version $Id$
+ * Kunena Component
+ * @package Kunena
+ *
+ * @Copyright (C) 2008 - 2010 Kunena Team All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.com
+ *
+ **/
+//
+// Dont allow direct linking
+defined( '_JEXEC' ) or die('');
+
+class KunenaProfileCommunityBuilder extends KunenaProfile
+{
+	protected $integration = null;
+
+	public function __construct() {
+		$this->integration = KunenaIntegration::getInstance ('communitybuilder');
+		if (! $this->integration || ! $this->integration->isLoaded())
+			return;
+		$this->priority = 50;
+	}
+
+	public function open()
+	{
+		$this->integration->open();
+	}
+
+	public function close()
+	{
+		$this->integration->close();
+	}
+
+/* TODO: do we need this anymore:
+	public function getForumTabURL()
+	{
+		return cbSef( 'index.php?option=com_comprofiler&amp;tab=getForumTab' . getCBprofileItemid() );
+	}
+*/
+
+	public function getUserListURL($action='')
+	{
+		return cbSef( 'index.php?option=com_comprofiler&amp;task=usersList' );
+	}
+
+	public function getProfileURL($user)
+	{
+		$user = KunenaFactory::getUser($user);
+		if ($user->userid == 0) return false;
+		// Get CUser object
+		$cbUser = CBuser::getInstance( $user->userid );
+		if($cbUser === null) return false;
+		return cbSef( 'index.php?option=com_comprofiler&task=userProfile&user=' .$user->userid. getCBprofileItemid() );
+	}
+
+	public function showProfile($user, &$msg_params)
+	{
+		global $_PLUGINS;
+
+		$kunenaConfig = KunenaFactory::getConfig();
+		$user = KunenaFactory::getUser($user);
+		$_PLUGINS->loadPluginGroup('user');
+		return implode( '', $_PLUGINS->trigger( 'forumSideProfile', array( 'kunena', null, $user->userid,
+			array( 'config'=> &$kunenaConfig, 'userprofile'=> &$user, 'msg_params'=>&$msg_params) ) ) );
+	}
+
+	public function trigger($event, &$params)
+	{
+		return $this->integration->trigger($event, $params);
+	}
+
+}

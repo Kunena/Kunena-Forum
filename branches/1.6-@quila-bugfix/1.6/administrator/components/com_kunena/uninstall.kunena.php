61f9464371393921d13dff01c01a2c4ef80ff283
dragan <dmilanovich@gmail.com>
2010-05-14 13:36:14 +0000

    + [#18267]  Custom 1.6 branches
    git-svn-id: file:///Users/oratzes/svn/kunena@2476 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ branches/1.6-@quila-bugfix/1.6/administrator/components/com_kunena/uninstall.kunena.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @version $Id$
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2008 - 2010 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+**/
+
+// Dont allow direct linking
+defined( '_JEXEC' ) or die();
+
+// Kunena wide defines
+require_once (JPATH_ROOT  .DS. 'components' .DS. 'com_kunena' .DS. 'lib' .DS. 'kunena.defines.php');
+require_once (KUNENA_PATH_ADMIN_INSTALL . DS . 'kunena.uninstall.php');
---
+++ branches/1.6-@quila-bugfix/1.6/components/com_kunena/template/default/rules.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @version $Id$
+ * Kunena Component
+ * @package Kunena
+ *
+ * @Copyright (C) 2008 - 2010 Kunena Team All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.com
+ **/
+
+// Dont allow direct linking
+defined ( '_JEXEC' ) or die ();
+
+$kunena_db = JFactory::getDBO ();
+$kunena_config = CKunenaConfig::getInstance ();
+$document = JFactory::getDocument ();
+
+$document->setTitle ( JText::_('COM_KUNENA_GEN_RULES') . ' - ' . stripslashes ( $kunena_config->board_title ) );
+
+$kunena_db->setQuery ( "SELECT introtext, id FROM #__content WHERE id='{$kunena_config->rules_cid}'" );
+$introtext = $kunena_db->loadResult ();
+check_dberror ( "Unable to load introtext." );
+?>
+<table class="kblock" id="kforumrules">
+	<thead>
+		<tr>
+			<th>
+				<h1><?php echo JText::_('COM_KUNENA_FORUM_RULES'); ?></h1>
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="krulesdesc">
+				<?php echo $introtext; ?>
+			</td>
+		</tr>
+	</tbody>
+</table>
+<!-- Begin: Forum Jump -->
+<table class="kblock" id="kbottomarea">
+	<tr>
+		<th class="th-right">
+			<?php
+			if ($kunena_config->enableforumjump) {
+				CKunenaTools::loadTemplate('/forumjump.php');
+			}
+			?>
+		</th>
+	</tr>
+</table>
+<!-- Finish: Forum Jump -->

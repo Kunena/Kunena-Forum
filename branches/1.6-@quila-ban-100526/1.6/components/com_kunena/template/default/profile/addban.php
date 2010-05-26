35ac37aa83b032df4aaa6c0350cbd57b5d1db174
dragan <dmilanovich@gmail.com>
2010-05-26 19:41:18 +0000

    # [#19244] Moderation feature: improved template in "Add Ban" tab (rules in kunena.forum.css)
    git-svn-id: file:///Users/oratzes/svn/kunena@2556 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6-@quila-ban-100526/1.6/components/com_kunena/template/default/profile/addban.php
+++ branches/1.6-@quila-ban-100526/1.6/components/com_kunena/template/default/profile/addban.php
@@ -19,21 +19,21 @@ JHTML::_('behavior.calendar');
 	$useriplist = $kunena_mod->getUsernameMatchingIPs($this->user->id);
 ?>
 
-<h2><?php echo JText::_('Add new ban'); ?></h2>
+<h2><?php echo JText::_('Add new ban for '); ?> <?php echo $this->profile->name; ?></h2>
 <form id="kform-ban" name="kformban" action="index.php" method="post">
-<table border="0" cellpadding="5" class="<?php echo isset ( $this->objCatInfo->class_sfx ) ? ' kblocktable' . $this->objCatInfo->class_sfx : ''; ?>">
+<table class="<?php echo isset ( $this->objCatInfo->class_sfx ) ? ' kblocktable' . $this->objCatInfo->class_sfx : ''; ?> kblock">
 	<tbody>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;padding: 1 10px;" width="40%"><?php //echo $this->id; ?><b>Username</b></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><?php echo $this->profile->username; ?> </td>
+		<td class="kcol-addban-left"><?php //echo $this->id; ?><b>Username</b></td>
+		<td class="kcol-addban-right" width="600"><?php echo $this->profile->username; ?> </td>
 	</tr>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('User ID'); ?></b></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"> <?php echo $this->profile->userid; ?> </td>
+		<td class="kcol-addban-left"><b><?php echo JText::_('User ID'); ?></b></td>
+		<td class="kcol-addban-right"> <?php echo $this->profile->userid; ?> </td>
 	</tr>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('Latest user IP'); ?></b><br /><span class="ks">Please select IP if you want to block</span></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%">
+		<td class="kcol-addban-left"><b><?php echo JText::_('Latest user IP'); ?></b><br /><span class="ks">Please select IP if you want to block</span></td>
+		<td class="kcol-addban-right">
 				<?php
 				$ipselect = array();
 				foreach ($iplist as $ip) {
@@ -45,13 +45,9 @@ JHTML::_('behavior.calendar');
 				?>
 		</td>
 	</tr>
-	<!--<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('Latest user IP'); ?></b><br /><span class="ks">Please write IP if you want to block</span></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><input type="text" name="aim" value="<?php echo $this->profile->AIM; ?>" size="30" /> </td>
-	</tr>-->
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('Ban Level'); ?></b><br /><span class="blueban ks"><font color ="blue">Blue ban = Ban in Kunena (read-only mode).</font></span><br /><span class="redban ks"><font color ="red">Red ban = Block in Joomla (even block login).</font></span></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><?php
+		<td class="kcol-addban-left" ><b><?php echo JText::_('Ban Level'); ?></b><br /><span class="blueban ks">Blue ban = Ban in Kunena (read-only mode).</span><br /><span class="redban ks">Red ban = Block in Joomla (even block login).</span></td>
+		<td class="kcol-addban-right"><?php
 					// make the select list for the view type
 					$gender[] = JHTML::_('select.option', 0, JText::_('Blue ban '));
 					$gender[] = JHTML::_('select.option', 1, JText::_('Red ban '));
@@ -60,52 +56,45 @@ JHTML::_('behavior.calendar');
 					?></td>
 	</tr>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('Start Time'); ?></b><br /><span class="ks">YYYY-MM-DD - HH:MM:SS</span></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><input class="inputbox" type="text" maxlength="15"
-				name="Start_Time" id="Start_Time"
-				value="" /> <img
-				src="templates/system/images/calendar.png" alt="Calendar"
-				onclick="showCalendar('Start_Time','%Y-%m-%d');$('Start_Time').removeProperty('style');"
-				onmouseover="javascript:$('helpbox').set('value', '<?php echo JText::_('COM_KUNENA_EDITOR_HELPLINE_POLLLIFESPAN'); ?>')" /></td>
+		<td class="kcol-addban-left"><b><?php echo JText::_('Start Time'); ?></b><br /><span class="ks">YYYY-MM-DD - HH:MM:SS</span></td>
+		<td class="kcol-addban-right">
+			<input class="inputbox" type="text" maxlength="15" name="Start_Time" id="Start_Time" value="" />
+				<img src="templates/system/images/calendar.png" alt="Calendar" onclick="showCalendar('Start_Time','%Y-%m-%d');$('Start_Time').removeProperty('style');"
+				onmouseover="javascript:$('helpbox').set('value', '<?php echo JText::_('COM_KUNENA_EDITOR_HELPLINE_POLLLIFESPAN'); ?>')" />
+		</td>
 	</tr>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('Expire Time'); ?></b><br /><span class="ks">YYYY-MM-DD - HH:MM:SS</span></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><input class="inputbox" type="text" maxlength="15"
-				name="Expire_Time" id="Expire_Time"
-				value="" /> <img
-				src="templates/system/images/calendar.png" alt="Calendar"
-				onclick="showCalendar('Expire_Time','%Y-%m-%d');$('Expire_Time').removeProperty('style');"
-				onmouseover="javascript:$('helpbox').set('value', '<?php echo JText::_('COM_KUNENA_EDITOR_HELPLINE_POLLLIFESPAN'); ?>')" /></td>
+		<td class="kcol-addban-left"><b><?php echo JText::_('Expire Time'); ?></b><br /><span class="ks">YYYY-MM-DD - HH:MM:SS</span></td>
+		<td class="kcol-addban-right">
+			<input class="inputbox" type="text" maxlength="15" name="Expire_Time" id="Expire_Time" value="" />
+				<img src="templates/system/images/calendar.png" alt="Calendar" onclick="showCalendar('Expire_Time','%Y-%m-%d');$('Expire_Time').removeProperty('style');"
+				onmouseover="javascript:$('helpbox').set('value', '<?php echo JText::_('COM_KUNENA_EDITOR_HELPLINE_POLLLIFESPAN'); ?>')" />
+		</td>
 	</tr>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('Public Reason'); ?></b><br /><span class="ks">Viewable Public reason of ban</span></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><textarea class=" required" name="signature"
-		id="" style="width:60%;height:5em;" ><?php
-			echo kunena_htmlspecialchars ( $this->profile->signature );
-			?></textarea></td>
+		<td class="kcol-addban-left"><b><?php echo JText::_('Public Reason'); ?></b><br /><span class="ks">Viewable Public reason of ban</span></td>
+		<td class="kcol-addban-right">
+			<textarea class=" required" name="public_reason" id="public_reason" ><?php //echo ; ?></textarea>
+		</td>
 	</tr>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('Private Reason'); ?></b><br /><span class="ks">Admin viewable Private reason of ban</span></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><textarea class="required" name="signature"
-		id="" style="width:60%;height:5em;"><?php
-			echo kunena_htmlspecialchars ( $this->profile->signature );
-			?></textarea></td>
+		<td class="kcol-addban-left"><b><?php echo JText::_('Private Reason'); ?></b><br /><span class="ks">Admin viewable Private reason of ban</span></td>
+		<td class="kcol-addban-right">
+			<textarea class="required" name="private_reason" id="private_reason"><?php //echo ; ?></textarea>
+		</td>
 	</tr>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('On Profile ?'); ?></b><br /><span class="ks">Show banlevel info or image on user profile page?</span></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><input id="konprofile_keep" type="checkbox" name="onprofile" value="keep" /></td>
+		<td class="kcol-addban-left"><b><?php echo JText::_('On Profile ?'); ?></b><br /><span class="ks">Show banlevel info or image on user profile page?</span></td>
+		<td class="kcol-addban-right"><input id="konprofile_keep" type="checkbox" name="onprofile" value="keep" /></td>
 	</tr>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('On Message ?'); ?></b><br /><span class="ks">Show banlevel info or image on user message?</span></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><input id="konmessage_keep" type="checkbox" name="onmessage" value="keep" /></td>
+		<td class="kcol-addban-left"><b><?php echo JText::_('On Message ?'); ?></b><br /><span class="ks">Show banlevel info or image on user message?</span></td>
+		<td class="kcol-addban-right"><input id="konmessage_keep" type="checkbox" name="onmessage" value="keep" /></td>
 	</tr>
-	<!--<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('IP History'); ?></b></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"> dragan </td>
-	</tr>-->
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('Follow IPs'); ?></b><br /><span class="ks">Find other users that might use the same IP</span></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><ul>
+		<td class="kcol-addban-left"><b><?php echo JText::_('Follow IPs'); ?></b><br /><span class="ks">Find other users that might use the same IP</span></td>
+		<td class="kcol-addban-right">
+			<ul>
 				<?php
 				$usernames = array();
 				foreach ($iplist as $ip) {
@@ -118,38 +107,41 @@ JHTML::_('behavior.calendar');
 
 					if (!empty($useriplist[$ip->ip])) {
 				?>
-				<li><span><a href="http://ws.arin.net/whois/?queryinput=<?php echo $ip->ip; ?>" target="_blank"><?php echo $ip->ip; ?></a></span> (<?php echo JText::_('COM_KUNENA_MODERATE_OTHER_USERS_WITH_IP'); ?>: <?php echo $username; ?>)</li>
+				<li><span>
+					<a href="http://ws.arin.net/whois/?queryinput=<?php echo $ip->ip; ?>" target="_blank"><?php echo $ip->ip; ?></a>
+					</span> (<?php echo JText::_('COM_KUNENA_MODERATE_OTHER_USERS_WITH_IP'); ?>: <?php echo $username; ?>)
+				</li>
 				<?php
 					} else {
 					?>
-				<li><span><a href="http://ws.arin.net/whois/?queryinput=<?php echo $ip->ip; ?>" target="_blank"><?php echo $ip->ip; ?></a></span> (<?php echo JText::_('COM_KUNENA_MODERATE_OTHER_USERS_WITH_IP'); ?>: <?php echo JText::_('COM_KUNENA_MODERATION_USER_NONE_IPS'); ?>)</li>
+				<li><span>
+					<a href="http://ws.arin.net/whois/?queryinput=<?php echo $ip->ip; ?>" target="_blank"><?php echo $ip->ip; ?></a>
+					</span> (<?php echo JText::_('COM_KUNENA_MODERATE_OTHER_USERS_WITH_IP'); ?>: <?php echo JText::_('COM_KUNENA_MODERATION_USER_NONE_IPS'); ?>)
+				</li>
 				<?php
 					}
-				} ?></ul>
+				} ?>
+			</ul>
 		</td>
 	</tr>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_BAD_SIGNATURE'); ?></b></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><input type="checkbox" id="ban-delsignature" name="delsignature" value="delsignature" class="" /></td>
-	</tr>
-	<!--<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('COM_KUNENA_MODERATE_BAN_USER'); ?></b></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><input type="checkbox" id="ban-user" name="banuser" value="banuser" class="" /></td>
-	</tr>-->
-	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_BAD_AVATAR'); ?></b></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><input type="checkbox" id="ban-delavatar" name="delavatar" value="delavatar" class="" /></td>
+		<td class="kcol-addban-left"><b><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_BAD_SIGNATURE'); ?></b></td>
+		<td class="kcol-addban-right"><input type="checkbox" id="ban-delsignature" name="delsignature" value="delsignature" class="" /></td>
 	</tr>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_BAD_PROFILEINFO'); ?></b></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><input type="checkbox" id="ban-delprofileinfo" name="delprofileinfo" value="delprofileinfo" class="" /></td>
+		<td class="kcol-addban-left"><b><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_BAD_AVATAR'); ?></b></td>
+		<td class="kcol-addban-right"><input type="checkbox" id="ban-delavatar" name="delavatar" value="delavatar" /></td>
 	</tr>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:left;" width="40%"><b><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_ALL_POSTS'); ?></b></td>
-		<td style="text-align:left;padding: 1 10px;" width="64%"><input type="checkbox" id="ban-delposts" name="bandelposts" value="bandelposts" class="" /></td>
+		<td class="kcol-addban-left"><b><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_BAD_PROFILEINFO'); ?></b></td>
+		<td class="kcol-addban-right"><input type="checkbox" id="ban-delprofileinfo" name="delprofileinfo" value="delprofileinfo" /></td>
 	</tr>
 	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
-		<td style="text-align:center;" width="100%" colspan="2">
+		<td class="kcol-addban-left"><b><?php echo JText::_('COM_KUNENA_MODERATE_DELETE_ALL_POSTS'); ?></b></td>
+		<td class="kcol-addban-right"><input type="checkbox" id="ban-delposts" name="bandelposts" value="bandelposts" /></td>
+	</tr>
+	<tr class="ksectiontableentry<?php echo ($i^=1)+1;?>">
+		<td class="kcol-addban-center" style="text-align:center;" width="100%" colspan="2">
 			<input class="kbutton kbutton ks" type="submit" value="<?php echo JText::_('Add ban'); ?>" name="Submit" />
 			<input type="hidden" name="option" value="com_kunena" />
 			<input type="hidden" name="func" value="banactions" />

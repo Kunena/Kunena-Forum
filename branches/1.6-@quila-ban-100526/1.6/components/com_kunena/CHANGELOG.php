fcf514ace39d5b6b6d0246bcd70fe12ab09116b1
xillibit <xillibit@gmail.com>
2010-05-29 19:48:56 +0000

    # [#19244] Add upgrade things for kunena_banned_users table
    git-svn-id: file:///Users/oratzes/svn/kunena@2583 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6-@quila-ban-100526/1.6/components/com_kunena/CHANGELOG.php
+++ branches/1.6-@quila-ban-100526/1.6/components/com_kunena/CHANGELOG.php
@@ -37,6 +37,7 @@ Kunena 1.6.0-ALPHA
 29-May-2010 Xillibit
 # [#19244] Escape all database column, little change on database structure
 # [#19244] Add configuration setting to display ban reaon in profile, set ban rank when an user is banned
+# [#19244] Add upgrade things for kunena_banned_users table
 
 28-May-2010 @quila
 ^ [#19244] Moderation feature: cleanup ban manager, ban history and add ban pages

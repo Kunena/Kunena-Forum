14eefbc888cba65e76db98fdc23fd48101df9602
xillibit <xillibit@gmail.com>
2010-06-01 14:40:34 +0000

    # [#20769] Queries for sample datas during installation are broken
    git-svn-id: file:///Users/oratzes/svn/kunena@2606 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.5-xillibit-fixing/components/com_kunena/CHANGELOG.php
+++ branches/1.5-xillibit-fixing/components/com_kunena/CHANGELOG.php
@@ -35,6 +35,7 @@ Kunena 1.5.12-DEV
 
 01-Jun-2010 Xillibit
 # [#20759] Fix some syntax errors
+# [#20769] Queries for sample datas during installation are broken
 
 31-May-2010 Xillibit
 # [#20108] When you try to change the username with one already existing you have a blank page

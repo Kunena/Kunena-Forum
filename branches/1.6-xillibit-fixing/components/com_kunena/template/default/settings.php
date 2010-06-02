c95b9b754e14ca0774490496a6080d1438619c0a
xillibit <xillibit@gmail.com>
2010-06-02 19:36:07 +0000

    [#18627] Create branch 1.6-xillibit-fixing
    git-svn-id: file:///Users/oratzes/svn/kunena@2613 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ branches/1.6-xillibit-fixing/components/com_kunena/template/default/settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* @version $Id$
+* Kunena Component
+* @package Kunena
+*
+* @Copyright (C) 2010 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+**/
+
+$this->avatarSizes = array (
+// 'name' => array(width, height)
+'thumb' => array(36, 36),
+'lastpost' => array(36, 36),
+'welcome' => array(72, 72),
+'reply' => array(144, 144),
+'profile' => array(200, 200),
+);

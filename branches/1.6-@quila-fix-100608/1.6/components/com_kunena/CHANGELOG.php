dcf8f63b7c6ff6ea4e3a62ce3f0f4ab8a9b7b292
dragan <dmilanovich@gmail.com>
2010-06-08 14:47:08 +0000

    # [#19288] Fix regression - subject message suffix wrong for new post.
    git-svn-id: file:///Users/oratzes/svn/kunena@2677 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6-@quila-fix-100608/1.6/components/com_kunena/CHANGELOG.php
+++ branches/1.6-@quila-fix-100608/1.6/components/com_kunena/CHANGELOG.php
@@ -34,6 +34,9 @@ Legend:
 
 Kunena 1.6.0-ALPHA2
 
+8-June-2010 @quila
+# [#19288] Fix regression - subject message suffix wrong for new post.
+
 7-June-2010 Matias
 ^ [#15886] Merged revisions 2634-2659 from /branches/1.6-xillibit with changes
 

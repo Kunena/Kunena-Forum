08756c4f4dfd8e9f1ff5a847ad0a52901251a60c
dragan <dmilanovich@gmail.com>
2010-06-24 10:43:12 +0000

    + [#18267]  Custom 1.6 branches
    git-svn-id: file:///Users/oratzes/svn/kunena@2830 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ branches/1.6-dragan-fix-20100624/1.6/administrator/components/com_kunena/libraries/factory.php
@@ -0,0 +1,152 @@
+<?php
+/**
+* @version $Id$
+* Kunena Component - Kunena Factory
+* @package Kunena
+*
+* @Copyright (C) 2009 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+**/
+
+// Dont allow direct linking
+defined( '_JEXEC' ) or die('Restricted access');
+
+abstract class KunenaFactory {
+	static $session = null;
+
+	/**
+	 * Get a Kunena configuration object
+	 *
+	 * Returns the global {@link CKunenaConfig} object, only creating it if it doesn't already exist.
+	 *
+	 * @return object CKunenaConfig
+	 */
+	public static function getConfig()
+	{
+		require_once(KPATH_SITE.'/lib/kunena.config.class.php');
+		return CKunenaConfig::getInstance();
+	}
+
+	/**
+	 * Get a Kunena template object
+	 *
+	 * Returns the global {@link KunenaTemplate} object, only creating it if it doesn't already exist.
+	 *
+	 * @return object KunenaTemplate
+	 */
+	public static function getTemplate($name = null)
+	{
+		kimport('template');
+		return KunenaTemplate::getInstance($name);
+	}
+
+	/**
+	 * Get Kunena user object
+	 *
+	 * Returns the global {@link KunenaUser} object, only creating it if it doesn't already exist.
+	 *
+	 * @param	int	$id	The user to load - Can be an integer or string - If string, it is converted to Id automatically.
+	 *
+	 * @return object KunenaUser
+	 */
+	static public function getUser($id = null, $reload = false) {
+		kimport('user');
+		return KunenaUser::getInstance($id, $reload);
+	}
+
+	/**
+	 * Get Kunena session object
+	 *
+	 * Returns the global {@link KunenaSession} object, only creating it if it doesn't already exist.
+	 *
+	 * @param array An array containing session options
+	 * @return object KunenaSession
+	 */
+	public static function getSession($update = false)
+	{
+		kimport('session');
+		if (!is_object(KunenaFactory::$session)) {
+			KunenaFactory::$session = KunenaSession::getInstance($update);
+		}
+		return KunenaFactory::$session;
+	}
+
+	/**
+	 * Get Kunena login object
+	 *
+	 * Returns the global {@link KunenaLogin} object, only creating it if it doesn't already exist.
+	 *
+	 * @return object KunenaLogin
+	 */
+	public static function getLogin()
+	{
+		kimport('integration.login');
+		return KunenaLogin::getInstance();
+	}
+
+	/**
+	 * Get Kunena avatar integration object
+	 *
+	 * Returns the global {@link KunenaAvatar} object, only creating it if it doesn't already exist.
+	 *
+	 * @return object KunenaAvatar
+	 */
+	public static function getAvatarIntegration()
+	{
+		kimport('integration.avatar');
+		return KunenaAvatar::getInstance();
+	}
+
+	/**
+	 * Get Kunena private message system integration object
+	 *
+	 * Returns the global {@link KunenaPrivate} object, only creating it if it doesn't already exist.
+	 *
+	 * @return object KunenaPrivate
+	 */
+	public static function getPrivateMessaging()
+	{
+		kimport('integration.private');
+		return KunenaPrivate::getInstance();
+	}
+
+	/**
+	 * Get Kunena activity integration object
+	 *
+	 * Returns the global {@link KunenaActivity} object, only creating it if it doesn't already exist.
+	 *
+	 * @return object KunenaActivity
+	 */
+	public static function getActivityIntegration()
+	{
+		kimport('integration.activity');
+		return KunenaActivity::getInstance();
+	}
+
+	/**
+	 * Get Kunena profile integration object
+	 *
+	 * Returns the global {@link KunenaProfile} object, only creating it if it doesn't already exist.
+	 *
+	 * @return object KunenaProfile
+	 */
+	public static function getProfile()
+	{
+		kimport('integration.profile');
+		return KunenaProfile::getInstance();
+	}
+
+	/**
+	 * Get Kunena access control object
+	 *
+	 * Returns the global {@link KunenaAccess} object, only creating it if it doesn't already exist.
+	 *
+	 * @return object KunenaAccess
+	 */
+	public static function getAccessControl()
+	{
+		kimport('integration.access');
+		return KunenaAccess::getInstance();
+	}
+}
---
+++ branches/1.6-dragan-fix-20100624/1.6/components/com_kunena/template/default/view/message.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * @version $Id$
+ * Kunena Component
+ * @package Kunena
+ *
+ * @Copyright (C) 2008 - 2010 Kunena Team All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.com
+ *
+ **/
+
+// Dont allow direct linking
+defined ( '_JEXEC' ) or die ();
+
+if ($this->params->get('avatarPosition') == 'top') : ?>
+
+<table <?php echo $this->class ?>>
+	<thead>
+		<tr class="ksth">
+			<th colspan="2" class="kview-th ksectiontableheader">
+				<a name="<?php echo $this->id ?>"></a>
+				<?php echo $this->numLink ?>
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td valign="top" class="kprofile-top">
+				<?php $this->displayProfile() ?>
+			</td>
+		</tr>
+		<tr>
+			<td class="kmessage-top">
+				<?php $this->displayContents() ?>
+			</td>
+		</tr>
+		<tr>
+			<td class="kbuttonbar-top">
+				<?php $this->displayActions() ?>
+				<?php $this->displayThankyou() ?>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+<?php elseif ($this->params->get('avatarPosition') == 'bottom') : ?>
+
+<table <?php echo $this->class ?>>
+	<thead>
+		<tr class="ksth">
+			<th colspan="2" class="kview-th ksectiontableheader">
+				<a name="<?php echo $this->id ?>"></a>
+				<?php echo $this->numLink ?>
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="kmessage-bottom">
+				<?php $this->displayContents() ?>
+			</td>
+		</tr>
+		<tr>
+			<td class="kbuttonbar-bottom">
+				<?php $this->displayActions() ?>
+				<?php $this->displayThankyou() ?>
+			</td>
+		</tr>
+		<tr>
+			<td valign="top" class="kprofile-bottom">
+				<?php $this->displayProfile() ?>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+<?php elseif ($this->params->get('avatarPosition') == 'left') : ?>
+
+<table <?php echo $this->class ?>>
+	<thead>
+		<tr class="ksth">
+			<th colspan="2" class="kview-th ksectiontableheader">
+				<a name="<?php echo $this->id ?>"></a>
+				<?php echo $this->numLink ?>
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td rowspan="2" valign="top" class="kprofile-left">
+				<?php $this->displayProfile() ?>
+			</td>
+			<td class="kmessage-left">
+				<?php $this->displayContents() ?>
+			</td>
+		</tr>
+		<tr>
+			<td class="kbuttonbar-left">
+				<?php $this->displayActions() ?>
+				<?php $this->displayThankyou() ?>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+<?php else : ?>
+
+<table <?php echo $this->class ?>>
+	<thead>
+		<tr class="ksth">
+			<th colspan="2" class="kview-th ksectiontableheader">
+				<a name="<?php echo $this->id ?>"></a>
+				<?php echo $this->numLink ?>
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="kmessage-right">
+				<?php $this->displayContents() ?>
+			</td>
+			<td rowspan="2" class="kprofile-right">
+				<?php $this->displayProfile() ?>
+			</td>
+		</tr>
+		<tr>
+			<td class="kbuttonbar-right">
+				<?php $this->displayActions() ?>
+				<?php $this->displayThankyou() ?>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+<?php endif ?>
+
+<!-- Begin: Message Module Position -->
+<?php CKunenaTools::showModulePosition('kunena_msg_' . $this->mmm) ?>
+<!-- Finish: Message Module Position -->

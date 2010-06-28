6b68ca70e7e3037399300f13ca86df42efd13d08
mahagr <matias@kunena.org>
2010-06-28 02:32:32 +0000

    + [#19288] Added Spanish language (thanks Alakentu)
    git-svn-id: file:///Users/oratzes/svn/kunena@2863 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6/components/com_kunena/CHANGELOG.php
+++ trunk/1.6/components/com_kunena/CHANGELOG.php
@@ -34,9 +34,12 @@ Legend:
 
 Kunena 1.6.0-ALPHA3
 
+28-June-2010 Matias
++ [#19288] Added Spanish language (thanks Alakentu)
+
 28-June-2010 severdia
 # [#21206] Restyling attachment buttons
- 
+
 27-June-2010 fxstein
 # [#21203] Workaround for IE8 bug to enable size and color selection in bbcode editor
 ^ [#20833] Adjusted english language strings for Thank You feature

571ed50bce5bc7247fcd185c12d96fb7f2b06143
fxstein <oliver@kunena.org>
2010-07-03 23:52:16 +0000

    + [#19707] Add ant builder
    git-svn-id: file:///Users/oratzes/svn/kunena@2910 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/kunena.com/j15.template/kunena_j15/CHANGELOG.php
+++ trunk/kunena.com/j15.template/kunena_j15/CHANGELOG.php
@@ -38,6 +38,7 @@ Kunena Site Template based on Rocket Theme Afterburner
 ^ [#19707] Update template version info to 1.6.0
 - [#19707] Remove unsupported css color choices
 ^ [#19707] Move tenmplate files into a j15.template folder
++ [#19707] Add ant builder
 
 13-Feb-2010 fxstein
 + [#19707] New kunena style

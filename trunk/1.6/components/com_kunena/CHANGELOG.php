fdf2a471d8dfa6cd8ca7d4607018de135f0be73c
fxstein <oliver@kunena.org>
2010-07-10 04:54:47 +0000

    # [#19345] Updated credits page copyright years
    git-svn-id: file:///Users/oratzes/svn/kunena@2974 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6/components/com_kunena/CHANGELOG.php
+++ trunk/1.6/components/com_kunena/CHANGELOG.php
@@ -33,6 +33,9 @@ Legend:
 
 Kunena 1.6.0-BETA1
 
+10-July-2010 fxstein
+# [#19345] Updated credits page copyright years
+
 9-July-2010 Matias
 ^ [#19380] Update Mootools plugin to latest version, fixes bug with Window.onDomReady()
 # [#19288] Polls: Fix wrong URL when you change your vote (non-AJAX version)

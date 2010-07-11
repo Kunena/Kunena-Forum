05344f181c91722d7c80507cd817304829fab3e3
xillibit <xillibit@gmail.com>
2010-07-11 02:45:14 +0000

    [#18627] Create branch 1.6-xillibit-fixes-20100711
    git-svn-id: file:///Users/oratzes/svn/kunena@2989 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ branches/1.6-xillibit-fixes-20100711/administrator/components/com_kunena/install/router.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @version $Id$
+ * Kunena Component
+ * @package Kunena
+ *
+ * @Copyright (C) 2008 - 2010 Kunena Team All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.com
+ *
+ **/
+
+function KunenaBuildRoute(&$query) {
+	return array();
+}
+
+function KunenaParseRoute($segments) {
+	return array();
+}

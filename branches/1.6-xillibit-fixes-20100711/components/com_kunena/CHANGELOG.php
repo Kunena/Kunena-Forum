b2f7f2efb6452d8dec3fe4930b7ffd5517ca67a3
xillibit <xillibit@gmail.com>
2010-07-11 05:57:57 +0000

    + [#19288] Search filter in category manager with cat name or cat id
    git-svn-id: file:///Users/oratzes/svn/kunena@2997 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6-xillibit-fixes-20100711/components/com_kunena/CHANGELOG.php
+++ branches/1.6-xillibit-fixes-20100711/components/com_kunena/CHANGELOG.php
@@ -36,6 +36,7 @@ Kunena 1.6.0-BETA1
 11-July-2010 Xillibit
 # [#19288] Fix issue with polls stats which doesn't count the right number of poll votes
 # [#19288] Little change on thank you part of API to get correct datas
++ [#19288] Search filter in category manager with cat name or cat id
 
 10-July-2010 Matias
 # [#19288] Fix Regression: Some profile icons missing from profile page

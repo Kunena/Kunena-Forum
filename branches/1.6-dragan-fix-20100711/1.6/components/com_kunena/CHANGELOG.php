0f1c2472aea6ece01a415cee967956a9312b5ae1
dragan <dmilanovich@gmail.com>
2010-07-11 06:50:26 +0000

    # [#19288] Fix Regression: message body too height in IE, Chrome, Opera
    git-svn-id: file:///Users/oratzes/svn/kunena@2998 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6-dragan-fix-20100711/1.6/components/com_kunena/CHANGELOG.php
+++ branches/1.6-dragan-fix-20100711/1.6/components/com_kunena/CHANGELOG.php
@@ -49,6 +49,7 @@ Kunena 1.6.0-BETA1
 # [#19288] Fix Regression: icon show in profile page
 # [#19288] Fix Regression: alignement in credit page
 # [#19288] Fix Regression: width of search page when left module is active
+# [#19288] Fix Regression: message body too height in IE, Chrome, Opera
 
 10-July-2010 Matias
 # [#19288] Fix Regression: Some profile icons missing from profile page

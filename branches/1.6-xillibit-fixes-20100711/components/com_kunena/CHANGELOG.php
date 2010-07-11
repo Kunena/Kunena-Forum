bddd5752830f7b0c34a488d7ef6e7e696558738f
xillibit <xillibit@gmail.com>
2010-07-11 10:21:06 +0000

    # [#19288] Little change on latestx class due to a change on the kunena latest module
    git-svn-id: file:///Users/oratzes/svn/kunena@3001 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6-xillibit-fixes-20100711/components/com_kunena/CHANGELOG.php
+++ branches/1.6-xillibit-fixes-20100711/components/com_kunena/CHANGELOG.php
@@ -37,6 +37,7 @@ Kunena 1.6.0-BETA1
 # [#19288] Fix issue with polls stats which doesn't count the right number of poll votes
 # [#19288] Little change on thank you part of API to get correct datas
 + [#19288] Search filter in category manager with cat name or cat id
+# [#19288] Little change on latestx class due to a change on the kunena latest module
 
 10-July-2010 Matias
 # [#19288] Fix Regression: Some profile icons missing from profile page

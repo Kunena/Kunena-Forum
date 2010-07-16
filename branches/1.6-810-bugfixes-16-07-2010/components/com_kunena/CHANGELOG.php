a7250d26ba944a5668edf47bddeeaac82b2d0677
810 <schumacher1986@hotmail.com>
2010-07-16 14:25:09 +0000

    # [#19288] Fix select bug IE
    git-svn-id: file:///Users/oratzes/svn/kunena@3045 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6-810-bugfixes-16-07-2010/components/com_kunena/CHANGELOG.php
+++ branches/1.6-810-bugfixes-16-07-2010/components/com_kunena/CHANGELOG.php
@@ -33,6 +33,9 @@ Legend:
 
 Kunena 1.6.0-BETA1
 
+17-July-2010 810
+# [#19288] Fix select bug IE
+
 16-July-2010 Matias
 ^ [#15886] Merged revisions 3030-3037 from /branches/1.6-xillibit-fixes-20100712
 # [#19288] Fix online/offline status in userlist

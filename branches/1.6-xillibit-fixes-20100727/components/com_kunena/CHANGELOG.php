efdab65bbfd7757514efa0383d74bfede545e881
xillibit <xillibit@gmail.com>
2010-07-28 16:23:42 +0000

    # [#19288] Escape strings when create a menu because the query fail with commas in some language
    git-svn-id: file:///Users/oratzes/svn/kunena@3118 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6-xillibit-fixes-20100727/components/com_kunena/CHANGELOG.php
+++ branches/1.6-xillibit-fixes-20100727/components/com_kunena/CHANGELOG.php
@@ -35,6 +35,7 @@ Kunena 1.6.0-BETA2
 
 28-July-2010 Xillibit
 # [#19288] Re-write of review to use a class and put a new tab in profile
+# [#19288] Escape strings when create a menu because the query fail with commas in some language
 
 27-July-2010 Xillibit
 # [#19288] Update language file

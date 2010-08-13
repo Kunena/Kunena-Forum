66dd750b5daea5a02b0d3d901b44b655dfd5cf91
lorddarkangel <sven@schultschik.de>
2010-08-13 14:07:28 +0000

    [#21829] Auth check in profile thank you listing
    git-svn-id: file:///Users/oratzes/svn/kunena@3195 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6-LDAsvens-thankyou-20100813/1.6/components/com_kunena/CHANGELOG.php
+++ branches/1.6-LDAsvens-thankyou-20100813/1.6/components/com_kunena/CHANGELOG.php
@@ -33,6 +33,9 @@ Legend:
 
 Kunena 1.6.0-RC2
 
+13-August-2010 svens(LDA)
+* [#21829] Auth check in profile thank you listing
+
 13-August-2010 Matias
 ^ [#15886] Merged revisions 3087-3089 from /branches/1.6-b2-language-svens-20100721
 ^ [#15886] Merged revision 3166 from /branches/1.6-810-bugfixes-07-08-2010

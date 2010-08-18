0b41eefa28c5966339f162314e472a76662e12aa
fxstein <oliver@kunena.org>
2010-08-18 09:55:47 +0000

    [#21923] mod_kunenalatest 1.6.0RC1 build 116
    git-svn-id: file:///Users/oratzes/svn/kunena@3233 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ tags/mod_kunenalatest1.6.0rc1/mod_kunenalatest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @version $Id$
+ * KunenaLatest Module
+ * @package Kunena Latest
+ *
+ * @Copyright (C) 2010 www.kunena.com All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.com
+ */
+
+// no direct access
+defined ( '_JEXEC' ) or die ( '' );
+
+// Detect and load Kunena 1.6+
+$kunena_api = JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_kunena' . DS . 'api.php';
+if (! JComponentHelper::isEnabled ( 'com_kunena', true ) || ! is_file ( $kunena_api ))
+	return JError::raiseError ( JText::_ ( 'Kunena Error' ), JText::_ ( 'Kunena 1.6 is not installed on your system' ) );
+
+require_once ($kunena_api);
+
+$params = ( object ) $params;
+$klatest = new modKunenaLatest ( $params );
+
+class modKunenaLatest {
+	public function __construct($params) {
+		require_once (KUNENA_PATH_LIB . DS . 'kunena.link.class.php');
+		require_once (KUNENA_PATH_LIB . DS . 'kunena.image.class.php');
+		require_once (KUNENA_PATH_LIB . DS . 'kunena.timeformat.class.php');
+		require_once (JPATH_ADMINISTRATOR . '/components/com_kunena/libraries/html/parser.php');
+		$this->kunena_config = KunenaFactory::getConfig ();
+		$this->document = JFactory::getDocument ();
+		$this->document->addStyleSheet ( JURI::root () . 'modules/mod_kunenalatest/tmpl/klatest.css' );
+
+		$this->latestdo = null;
+
+		if ($params->get ( 'choosemodel' ) != 'latest') {
+			$this->latestdo = $params->get ( 'choosemodel' );
+		}
+
+		// Load topic icons
+		$this->topic_emoticons = array ();
+		$this->topic_emoticons [0] = JURI::root () . 'components/com_kunena/template/default/images/icons/topic-default.gif';
+		$this->topic_emoticons [1] = JURI::root () . 'components/com_kunena/template/default/images/icons/topic-exclamation.png';
+		$this->topic_emoticons [2] = JURI::root () . 'components/com_kunena/template/default/images/icons/topic-question.png';
+		$this->topic_emoticons [3] = JURI::root () . 'components/com_kunena/template/default/images/icons/topic-arrow.png';
+		$this->topic_emoticons [4] = JURI::root () . 'components/com_kunena/template/default/images/icons/topic-love.png';
+		$this->topic_emoticons [5] = JURI::root () . 'components/com_kunena/template/default/images/icons/topic-grin.png';
+		$this->topic_emoticons [6] = JURI::root () . 'components/com_kunena/template/default/images/icons/topic-shock.png';
+		$this->topic_emoticons [7] = JURI::root () . 'components/com_kunena/template/default/images/icons/topic-smile.png';
+
+		// Include the kunenalatest functions only once
+		require_once (dirname ( __FILE__ ) . '/helper.php');
+
+		$this->params = $params;
+		$this->klistpost = modKunenaLatestHelper::getKunenaLatestList ( $params );
+
+		require (JModuleHelper::getLayoutPath ( 'mod_kunenalatest' ));
+	}
+}
---
+++ tags/mod_kunenalatest1.6.0rc1/tmpl/default.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @version $Id$
+ * KunenaLatest Module
+ * @package Kunena latest
+ *
+ * @Copyright (C) 2010 www.kunena.com All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.com
+ */
+
+// no direct access
+defined ( '_JEXEC' ) or die ( '' );
+?>
+<div class="<?php echo $this->params->get ( 'moduleclass_sfx' )?> klatest <?php echo $this->params->get ( 'sh_moduleshowtype' )?>" id="klatestmodule">
+
+<ul id="klatest-items">
+<?php
+if (is_array ( $this->klistpost ) && !$this->kunena_config->board_offline) {
+	foreach ( $this->klistpost as $item ) {
+?>
+
+<li class="klatest-item">
+	<ul class="klatest-itemdetails">
+		<?php
+		if ( $this->params->get ( 'sh_topiciconoravatar' ) == 1) { ?>
+		<li class="klatest-avatar">
+			<?php echo modKunenaLatestHelper::userAvatar( $item->userid, $this->params ); ?>
+		</li>
+		<?php } elseif( $this->params->get ( 'sh_topiciconoravatar' ) == 0) {  ?>
+		<li class="klatest-avatar">
+		<?php echo '<img src="' . $this->topic_emoticons[$item->topic_emoticon]  . '" />'; ?>
+		</li>
+		<?php } ?>
+		<li class="klatest-subject">
+		<?php
+			if ($this->params->get ( 'sh_sticky' )) {
+				if ($item->ordering) {
+					echo '<img src="' . JURI::root () . 'modules/mod_kunenalatest/tmpl/sticky.png" alt="' . JText::_ ( 'MOD_KUNENALATEST_STICKY_TOPIC' ) . '" title="' . JText::_ ( 'MOD_KUNENALATEST_STICKY_TOPIC' ) . '" />';
+				}
+			}
+			echo CKunenaLink::GetThreadLink ( 'view', $item->catid, $item->id, substr ( htmlspecialchars ( $item->subject ), '0', $this->params->get ( 'titlelength' ) ), substr ( htmlspecialchars ( KunenaParser::stripBBCode($item->message) ), '0', $this->params->get ( 'messagelength' ) ), 'follow' );
+			if ($item->unread) {
+				echo $this->params->get ( 'unreadindicator' );
+			}
+			if ($this->params->get ( 'sh_favorite' )) {
+				if ($item->favcount) {
+					if ($item->myfavorite) {
+						echo '<img class="favoritestar" src="' . JURI::root () . 'components/com_kunena/template/default/images/icons/favoritestar.png"  alt="' . JText::_ ( 'MOD_KUNENALATEST_FAVORITE' ) . '" title="' . JText::_ ( 'MOD_KUNENALATEST_FAVORITE' ) . '" />';
+					} else {
+						echo '<img class="favoritestar-grey" src="' . JURI::root () . 'components/com_kunena/template/default/images/icons/favoritestar-grey.png"  alt="' . JText::_ ( 'MOD_KUNENALATEST_FAVORITE' ) . '" title="' . JText::_ ( 'MOD_KUNENALATEST_FAVORITE' ) . '" />';
+					}
+				}
+			}
+			if ($this->params->get ( 'sh_locked' )) {
+				if ($item->locked) {
+					echo '<img src="' . JURI::root () . 'components/com_kunena/template/default/images/icons/lock_sm.png"  alt="' . JText::_ ( 'MOD_KUNENALATEST_LOCKED_TOPIC' ) . '" title="' . JText::_ ( 'MOD_KUNENALATEST_LOCKED_TOPIC' ) . '" />';
+				}
+			}
+			?>
+		</li>
+		<?php if ($this->params->get ( 'sh_firstcontentcharacter' )) : ?>
+			<li class="klatest-preview-content"><?php echo substr(KunenaParser::stripBBCode($item->message), '0', $this->params->get ( 'lengthcontentcharacters' )); ?></li>
+		<?php endif; ?>
+		<li class="klatest-cat"><?php echo JText::_ ( 'MOD_KUNENALATEST_IN_CATEGORY' ).' '.CKunenaLink::GetCategoryLink ( 'showcat', $item->catid, $item->catname ); ?></li>
+		<li class="klatest-author"><?php echo JText::_ ( 'MOD_KUNENALATEST_LAST_POST_BY' ) .' '. CKunenaLink::GetProfileLink ( $item->userid, $item->name ); ?></li>
+		<li class="klatest-posttime"><?php echo JText::_ ( 'MOD_KUNENALATEST_POSTED_AT' ); ?> <?php echo CKunenaTimeformat::showDate($item->lasttime, $this->params->get ( 'dateformat' )); ?></li>
+	</ul>
+</li>
+<?php
+	} //end foreach
+?>
+</ul>
+<p id="klatest-more"><?php echo CKunenaLink::GetShowLatestLink ( JText::_ ( 'MOD_KUNENALATEST_MORE_LINK' ) , $this->latestdo ); ?></p>
+<?php
+} else {
+	echo JText::_('MOD_KUNENALATEST_OFFLINE');
+} ?>
+</div>

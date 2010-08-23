a44f5e781d870028c9c1c493c80816316600e4a9
mahagr <matias@kunena.org>
2010-08-23 09:40:02 +0000

    # [#19002] Minor improvements, had wrong name in the plugin
    git-svn-id: file:///Users/oratzes/svn/kunena@3272 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6-addons/plg_jomsocial_kunenamenu/kunenamenu.php
+++ trunk/1.6-addons/plg_jomsocial_kunenamenu/kunenamenu.php
@@ -11,43 +11,41 @@
 defined ( '_JEXEC' ) or die ();
 
 $path = JPATH_ROOT . '/components/com_community/libraries/core.php';
-if (!is_file($path)) return;
+if (! is_file ( $path ))
+	return;
+require_once $path;
 
-if (! class_exists ( 'plgCommunityMyKunenaToolbar' )) {
-	require_once $path;
+class plgCommunityKunenaMenu extends CApplications {
+	var $name = "My Kunena Menu";
+	var $_name = 'kunenamenu';
 
-	class plgCommunityKunenaMenu extends CApplications {
-		var $name = "My Kunena Toolbar";
-		var $_name = 'mykunenatoolbar';
+	function plgCommunityKunenaMenu(& $subject, $config) {
+		//Load Language file.
+		JPlugin::loadLanguage ( 'plg_community_kunenamenu', JPATH_ADMINISTRATOR );
 
-		function plgCommunityKunenaMenu(& $subject, $config) {
-			//Load Language file.
-			JPlugin::loadLanguage ( 'plg_community_kunenamenu', JPATH_ADMINISTRATOR );
-
-			// Kunena detection and version check
-			$minKunenaVersion = '1.6.0-RC2';
-			if (! class_exists ( 'Kunena' ) || Kunena::versionBuild () < 3251) {
-				return;
-			}
-			parent::__construct ( $subject, $config );
+		// Kunena detection and version check
+		$minKunenaVersion = '1.6.0-RC2';
+		if (! class_exists ( 'Kunena' ) || Kunena::versionBuild () < 3251) {
+			return;
 		}
+		parent::__construct ( $subject, $config );
+	}
 
-		function onSystemStart() {
-			//initialize the toolbar object
-			$toolbar = CFactory::getToolbar ();
+	function onSystemStart() {
+		//initialize the toolbar object
+		$toolbar = CFactory::getToolbar ();
 
-			//adding new 'tab' 'Forum Settings' to JomSocial toolbar
-			$toolbar->addGroup ( 'KUNENAMENU', JText::_ ( 'PLG_COMMUNITY_KUNENANENU_FORUM' ), KunenaRoute::_ ( 'index.php?option=com_kunena&func=myprofile' ) );
-			// Kunena online check
-			if (! Kunena::enabled ()) {
-				$toolbar->addItem ( 'KUNENAMENU', 'KUNENAMENU_OFFLINE', JText::_ ( 'PLG_COMMUNITY_KUNENAMENU_KUNENA_OFFLINE' ), KunenaRoute::_ ( 'index.php?option=com_kunena' ) );
-				return;
-			}
-			$toolbar->addItem ( 'KUNENAMENU', 'KUNENAMENU_EDITPROFILE', JText::_ ( 'PLG_COMMUNITY_KUNENAMENU_EDITPROFILE' ), KunenaRoute::_ ( 'index.php?option=com_kunena&func=myprofile&task=edit' ) );
-			$toolbar->addItem ( 'KUNENAMENU', 'KUNENAMENU_PROFILE', JText::_ ( 'PLG_COMMUNITY_KUNENAMENU_PROFILE' ), KunenaRoute::_ ( 'index.php?option=com_kunena&func=myprofile' ) );
-			$toolbar->addItem ( 'KUNENAMENU', 'KUNENAMENU_POSTS', JText::_ ( 'PLG_COMMUNITY_KUNENAMENU_POSTS' ), KunenaRoute::_ ( 'index.php?option=com_kunena&func=latest&do=userposts' ) );
-			$toolbar->addItem ( 'KUNENAMENU', 'KUNENAMENU_SUBSCRIBES', JText::_ ( 'PLG_COMMUNITY_KUNENAMENU_SUBSCRIBTIONS' ), KunenaRoute::_ ( 'index.php?option=com_kunena&func=latest&do=subscriptions' ) );
-			$toolbar->addItem ( 'KUNENAMENU', 'KUNENAMENU_FAVORITES', JText::_ ( 'PLG_COMMUNITY_KUNENAMENU_FAVORITES' ), KunenaRoute::_ ( 'index.php?option=com_kunena&func=latest&do=favorites' ) );
+		//adding new 'tab' 'Forum Settings' to JomSocial toolbar
+		$toolbar->addGroup ( 'KUNENAMENU', JText::_ ( 'PLG_COMMUNITY_KUNENANENU_FORUM' ), KunenaRoute::_ ( 'index.php?option=com_kunena&func=myprofile' ) );
+		// Kunena online check
+		if (! Kunena::enabled ()) {
+			$toolbar->addItem ( 'KUNENAMENU', 'KUNENAMENU_OFFLINE', JText::_ ( 'PLG_COMMUNITY_KUNENAMENU_KUNENA_OFFLINE' ), KunenaRoute::_ ( 'index.php?option=com_kunena' ) );
+			return;
 		}
+		$toolbar->addItem ( 'KUNENAMENU', 'KUNENAMENU_EDITPROFILE', JText::_ ( 'PLG_COMMUNITY_KUNENAMENU_EDITPROFILE' ), KunenaRoute::_ ( 'index.php?option=com_kunena&func=myprofile&task=edit' ) );
+		$toolbar->addItem ( 'KUNENAMENU', 'KUNENAMENU_PROFILE', JText::_ ( 'PLG_COMMUNITY_KUNENAMENU_PROFILE' ), KunenaRoute::_ ( 'index.php?option=com_kunena&func=myprofile' ) );
+		$toolbar->addItem ( 'KUNENAMENU', 'KUNENAMENU_POSTS', JText::_ ( 'PLG_COMMUNITY_KUNENAMENU_POSTS' ), KunenaRoute::_ ( 'index.php?option=com_kunena&func=latest&do=userposts' ) );
+		$toolbar->addItem ( 'KUNENAMENU', 'KUNENAMENU_SUBSCRIBES', JText::_ ( 'PLG_COMMUNITY_KUNENAMENU_SUBSCRIBTIONS' ), KunenaRoute::_ ( 'index.php?option=com_kunena&func=latest&do=subscriptions' ) );
+		$toolbar->addItem ( 'KUNENAMENU', 'KUNENAMENU_FAVORITES', JText::_ ( 'PLG_COMMUNITY_KUNENAMENU_FAVORITES' ), KunenaRoute::_ ( 'index.php?option=com_kunena&func=latest&do=favorites' ) );
 	}
 }

878da88800ae226296586b998443ed9ffe77c5a3
fxstein <oliver@kunena.org>
2010-08-24 09:00:54 +0000

    + [#20081] Add TODO list for outstanding work
    git-svn-id: file:///Users/oratzes/svn/kunena@3281 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6-addons/mod_kunenalatest/CHANGELOG.php
+++ trunk/1.6-addons/mod_kunenalatest/CHANGELOG.php
@@ -32,6 +32,7 @@ KunenaLatest 1.6.0-RC2
 
 24-Aug-2010 fxstein
 ^ [#20081] Update minimum required Kunena build number to sync up API
++ [#20081] Add TODO list for outstanding work
 
 23-Aug-2010 fxstein
 + [#20081] Load Kunena language file to have access to all Kunena strings in frontend
---
+++ trunk/1.6-addons/mod_kunenalatest/TODO.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @version $Id$
+* KunenaLatest Module
+* @package Kunena latest
+*
+* @Copyright (C) 2010 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+
+// no direct access
+die( '' );
+?>
+<!--
+TODO list
+------------
+.) parse message array for topic display mode
+.) add css styling for (NEW) indicator to mimic Kunena
+.) move name/realname setting out of module and leverage Kunena config setting
+.) Add option to include css in site template
+-->

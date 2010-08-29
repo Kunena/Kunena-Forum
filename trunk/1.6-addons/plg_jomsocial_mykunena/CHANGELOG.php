b6b5df572d6d6f392f9f010e7fc0b35dce3de1c6
mahagr <matias@kunena.org>
2010-08-29 20:08:37 +0000

    ^ [#19002] Major cleanup, use new API
    git-svn-id: file:///Users/oratzes/svn/kunena@3336 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/1.6-addons/plg_jomsocial_mykunena/CHANGELOG.php
@@ -0,0 +1,47 @@
+<?php
+/**
+* @version $Id$
+* MyKunena Plugin
+* @package plg_jomsocial_mykunena
+*
+* @Copyright (C) 2009 - 2010 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+die();
+?>
+<!--
+Changelog
+------------
+This is a non-exhaustive (but still near complete) changelog for
+the myKunena plugin for JomSocial, including beta and release candidate versions.
+Legend:
+
+* -> Security Fix
+# -> Bug Fix
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+MyKunena 1.6.0-RC1
+
+29-August-2010 Matias
+^ [#19002] Major cleanup, use new API
+
+27-June-2010 Severdia
+! [#19002] Forked from Kunena 1.5 version
+^ [#19002] Update for Kunena 1.6
+
+14-December-2009 fxstein
++ [#19002] Initial plugin structure and build setup
+
+13-December-2009 fxstein
+^ Redo plugin code based on latest JomSocial release
+
+15-Februray-2009 fxstein
++ Initial plugin structure
+
+ -->
---
+++ trunk/1.6-addons/plg_jomsocial_mykunena/README.txt
@@ -0,0 +1,20 @@
+/**
+* @version $Id$
+* Kunena Component
+* @package Kunena
+* @Copyright (C) 2008-2009 Kunena All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+**/
+
+Kunena README
+
+PLEASE READ THIS ENTIRE FILE BEFORE INSTALLING myKunena @kunenaversion@!
+
+KNOWN ISSUES
+============
+
+TBD
+
+END OF README
+=============

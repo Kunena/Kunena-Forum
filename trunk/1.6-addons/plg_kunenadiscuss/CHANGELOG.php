65a350bd96cce7f68a70755b1061e057d0620dbe
fxstein <oliver@kunena.org>
2010-08-31 19:47:06 +0000

    ^ [#20084] Updated language strings to 'Comments'
    git-svn-id: file:///Users/oratzes/svn/kunena@3376 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6-addons/plg_kunenadiscuss/CHANGELOG.php
+++ trunk/1.6-addons/plg_kunenadiscuss/CHANGELOG.php
@@ -34,6 +34,7 @@ Kunena Discuss Plugin 1.6.0-RC2
 31-Aug-2010 fxstein
 ^ [#20084] Load kunena language through API
 ^ [#20084] Updated plugin description
+^ [#20084] Updated language strings to 'Comments'
 
 31-Aug-2010 @quila
 # [#20084] Removed table in message.php

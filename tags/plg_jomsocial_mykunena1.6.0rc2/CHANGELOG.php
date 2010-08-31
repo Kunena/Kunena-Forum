918001f84d4544e2d87cab7f6782b27e119e6b87
fxstein <oliver@kunena.org>
2010-08-31 22:48:40 +0000

    [#19002] plg_jomsocial_mykunena 1.6.0 RC2 build 138
    git-svn-id: file:///Users/oratzes/svn/kunena@3395 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ tags/plg_jomsocial_mykunena1.6.0rc2/CHANGELOG.php
@@ -0,0 +1,54 @@
+<?php
+/**
+* @version $Id$
+* MyKunena Plugin
+* @package plg_jomsocial_mykunena
+*
+* @Copyright (C) 2009 - 2010 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+die();
+?>
+<!--
+Changelog
+------------
+This is a non-exhaustive (but still near complete) changelog for
+the myKunena plugin for JomSocial, including beta and release candidate versions.
+Legend:
+
+* -> Security Fix
+# -> Bug Fix
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+MyKunena 1.6.0-RC2
+
+31-Aug-2010 fxstein
+^ [#19002] Updated build files for RC2 release
+
+31-August-2010 Matias
+# [#19002] Fix white page in JomSocial when Kunena is not fully installed
+# [#19002] Language file was not loaded -- PLG_COMMUNITY_MYKUNENA_NO_POSTS shown insted of text
+
+29-August-2010 Matias
+^ [#19002] Major cleanup, use new API
+
+27-June-2010 Severdia
+! [#19002] Forked from Kunena 1.5 version
+^ [#19002] Update for Kunena 1.6
+
+14-December-2009 fxstein
++ [#19002] Initial plugin structure and build setup
+
+13-December-2009 fxstein
+^ Redo plugin code based on latest JomSocial release
+
+15-Februray-2009 fxstein
++ Initial plugin structure
+
+ -->

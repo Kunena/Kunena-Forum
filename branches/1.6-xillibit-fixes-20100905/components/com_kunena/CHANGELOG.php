2bcc7f55974a7acc29d2adcea8da26a9bd697b07
xillibit <xillibit@gmail.com>
2010-09-05 19:47:06 +0000

    # [#22279] Thank you list in profile is showed even for visitors
    git-svn-id: file:///Users/oratzes/svn/kunena@3424 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6-xillibit-fixes-20100905/components/com_kunena/CHANGELOG.php
+++ branches/1.6-xillibit-fixes-20100905/components/com_kunena/CHANGELOG.php
@@ -37,6 +37,7 @@ Kunena 1.6.0-RC3
 # [#22254] Configuration report settings : check that the filed are right of type char, vachar, text... (Part 2)
 # [#22258] Change logic for save updated polls during edit
 # [#22278] User type need to follow translation in category manager
+# [#22279] Thank you list in profile is showed even for visitors
 
 4-September-2010 Severdia
 # [#19356] Fixed table alignments in admin

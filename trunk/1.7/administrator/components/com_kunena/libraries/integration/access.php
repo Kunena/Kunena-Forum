4ca0f1ef6cd3f36d8f91b0953f5ceeeede1dc167
mahagr <matias@kunena.org>
2010-09-21 16:15:48 +0000

    + [#22534] Create trunk for K1.7
    git-svn-id: file:///Users/oratzes/svn/kunena@3542 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/1.7/administrator/components/com_kunena/libraries/integration/access.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @version $Id$
+ * Kunena Component
+ * @package Kunena
+ *
+ * @Copyright (C) 2008 - 2010 Kunena Team All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.com
+ *
+ **/
+//
+// Dont allow direct linking
+defined ( '_JEXEC' ) or die ( '' );
+
+kimport ( 'integration.integration' );
+
+abstract class KunenaAccess {
+	public $priority = 0;
+
+	protected static $instance = false;
+
+	abstract public function __construct();
+
+	static public function getInstance($integration = null) {
+		if (self::$instance === false) {
+			$config = KunenaFactory::getConfig ();
+			if (! $integration)
+				$integration = $config->integration_access;
+			self::$instance = KunenaIntegration::initialize ( 'access', $integration );
+		}
+		return self::$instance;
+	}
+
+	function getAllowedHold($user, $catid, $string=true) {
+		// hold = 0: normal
+		// hold = 1: unapproved
+		// hold = 2: deleted
+		$user = KunenaFactory::getUser($user);
+		$config = KunenaFactory::getConfig ();
+
+		$hold [] = 0;
+		if ($this->isModerator ( $user->userid, $catid )) {
+			$hold [] = 1;
+		}
+		if ($this->isAdmin ( $user->userid, $catid )
+			|| ($config->mod_see_deleted && $this->isModerator( $user->userid, $catid ))) {
+			$hold [] = 2;
+			$hold [] = 3;
+	}
+		if ($string) $hold = implode ( ',', $hold );
+		return $hold;
+	}
+
+	abstract function isAdmin($uid = null, $catid = 0);
+	abstract function isModerator($uid = null, $catid = 0);
+	abstract function getAllowedCategories($userid);
+	abstract function getSubscribers($catid, $thread, $subscriptions = false, $moderators = false, $admins = false, $excludeList = '0');
+}

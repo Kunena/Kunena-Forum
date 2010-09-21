aefc18c4d25d6ed4d2cd70dc914e470ec7c8aedc
mahagr <matias@kunena.org>
2010-09-21 20:10:20 +0000

    + [#22541] Initial version of JomSocial plugin for Kunena Groups integration
    git-svn-id: file:///Users/oratzes/svn/kunena@3548 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/1.7-addons/plg_jomsocial_kunenagroups/CHANGELOG.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* @version $Id$
+* Kunena Groups Support for JomSocial
+* @package plg_jomsocial_kunenagroups
+*
+* @Copyright (C) 2009-2010 www.kunena.com All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+die();
+?>
+<!--
+
+Changelog
+------------
+This is a non-exhaustive (but still near complete) changelog for
+the Kunena Groups plugin for JomSocial, including beta and release candidate versions.
+
+Legend:
+
+* -> Security Fix
+# -> Bug Fix
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Kunena Groups 1.7.0-DEV
+
++ [#22541] Initial version of JomSocial plugin for Kunena Groups integration
+
+-->

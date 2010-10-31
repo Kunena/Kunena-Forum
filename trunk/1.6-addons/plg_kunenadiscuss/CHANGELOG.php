7b8453f76eafd6f9bd042913e12b1045ea78e576
fxstein <oliver@kunena.org>
2010-10-31 04:23:00 +0000

    ^ [#20081] Language string updated to reflect 1.6.0-RC3
    git-svn-id: file:///Users/oratzes/svn/kunena@3819 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6-addons/plg_kunenadiscuss/CHANGELOG.php
+++ trunk/1.6-addons/plg_kunenadiscuss/CHANGELOG.php
@@ -35,6 +35,7 @@ Kunena Discuss Plugin 1.6.0-RC3
 + [#20084] Added missing language string in english
 + [#20081] Links to individual messages
 + [#20081] Topic and Read More links added
+^ [#20081] Language string updated to reflect 1.6.0-RC3
 
 30-October-2010 Xillibit
 ^ [#20084] When the kunena reference isn't yet created and the quick form is disabled the user will informed that there is no reference

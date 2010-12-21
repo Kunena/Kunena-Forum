73c2696cc4a3a057b713a030b046e1dc4c6d4c21
mahagr <matias@kunena.org>
2010-12-21 20:51:17 +0000

    ^ [#22541] Backport KunenaGroups JomSocial plugin to Kunena 1.6.3 # [#22541] Fix a bug in version detection # [#22541] Fix Warning: array_merge(): Argument #2 is not an array # [#22541] Fix a bug where category were added into root instead under specified category
    git-svn-id: file:///Users/oratzes/svn/kunena@4055 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ trunk/1.6-addons/plg_jomsocial_kunenagroups/COPYRIGHT.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* @version $Id: COPYRIGHT.php 3548 2010-09-21 20:10:20Z mahagr $
+* KunenaGroups Plugin for JomSocial
+* @package plg_jomsocial_kunenagroups
+*
+* @Copyright (C) 2009-2010 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+die();
+?>
+<!--
+
+Kunena derives from copyrighted works licensed under the GNU General
+Public License.  This version has been modified pursuant to the
+GNU General Public License as of September 15, 2005, and as distributed,
+it includes or is derivative of works licensed under the GNU General
+Public License or other free or open source software licenses.
+
+-->

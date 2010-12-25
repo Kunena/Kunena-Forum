8caa6c5e9980b59bc985b10655b45bf49087d356
fxstein <oliver@kunena.org>
2010-12-25 05:44:49 +0000

    ^ [#22073] Update version info to 1.6.2 stable
    git-svn-id: file:///Users/oratzes/svn/kunena@4082 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6-addons/mod_kunenalogin/CHANGELOG.php
+++ trunk/1.6-addons/mod_kunenalogin/CHANGELOG.php
@@ -28,6 +28,9 @@ Legend:
 
 KunenaLogin 1.6.2
 
+25-December-2010 fxstein
+^ [#22073] Update version info to 1.6.2 stable
+
 24-December-2010 fxstein
 ^ [#22073] Merged revisions 3803-4058 from /branches/1.6-addons-LDAsvens-language-20101030
 

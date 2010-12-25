65be006c34fccc832375a1dabe69e15062a6a4b2
fxstein <oliver@kunena.org>
2010-12-25 05:49:56 +0000

    ^ [#20138] Updated version info 1.6.2 stable
    git-svn-id: file:///Users/oratzes/svn/kunena@4084 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6-addons/plg_kunenasearch/CHANGELOG.php
+++ trunk/1.6-addons/plg_kunenasearch/CHANGELOG.php
@@ -26,7 +26,11 @@ Legend:
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-KunenaSearch 1.6.0-RC2
+KunenaSearch 1.6.2
+
+25-December-2010 fxstein
+^ [#20138] Updated version info 1.6.2 stable
+
 12-Nov-2010 svens(LDA)
 + [#23293] add de-DE (thanks rich)
 + [#23293] added missing xml entry for nb-NO

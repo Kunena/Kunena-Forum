39d372a02b740b65a5022c3cabba9c38a360ec20
fxstein <oliver@kunena.org>
2010-12-25 05:46:47 +0000

    ^ [#20095] Updated version info to 1.6.2 stable
    git-svn-id: file:///Users/oratzes/svn/kunena@4083 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- trunk/1.6-addons/mod_kunenastats/CHANGELOG.php
+++ trunk/1.6-addons/mod_kunenastats/CHANGELOG.php
@@ -30,6 +30,9 @@ Legend:
 
 KunenaStats 1.6.2
 
+25-December-2010 fxstein
+^ [#20095] Updated version info to 1.6.2 stable
+
 24-December-2010 fxstein
 ^ [#20095] Merged revisions 3803-4058 from /branches/1.6-addons-LDAsvens-language-20101030
 

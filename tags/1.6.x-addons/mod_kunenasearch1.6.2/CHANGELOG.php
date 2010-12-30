4eee5d0f703cfa3fb683cf4e320d2d551c3f99e6
fxstein <oliver@kunena.org>
2010-12-30 05:55:32 +0000

    [#22592] mod_kunenasearch 1.6.2 build 47
    git-svn-id: file:///Users/oratzes/svn/kunena@4112 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ tags/1.6.x-addons/mod_kunenasearch1.6.2/CHANGELOG.php
@@ -0,0 +1,65 @@
+<?php
+/**
+* @version $Id: CHANGELOG.php 3904 2010-11-15 16:21:58Z mahagr $
+* KunenaSearch Module
+* @package Kunena Search
+*
+* @Copyright (C) 2009 www.kunena.org All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.org
+*/
+
+// no direct access
+defined( '_VALID_MOS' ) or die( 'Restricted access' );
+?>
+<!--
+Changelog
+------------
+This is a non-exhaustive (but still near complete) changelog for
+the KunenaSearch module, including beta and release candidate versions.
+Legend:
+
+* -> Security Fix
+# -> Bug Fix
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+KunenaSearch 1.6.2
+
+25-December-2010 fxstein
+^ [#22592] Update version info to 1.6.2 stable
++ [#22592] Add version info to module description
++ [#23293] Add version info auto expansion to builder
++ [#23293] Add min Kunena version requirements (1.6.2)
+# [#23293] JURI::root() instead of ::Root
+# [#23293] $ksearch_width instead of $ksearch-width
+# [#23293] Fixed all code warnings
+
+21-December-2010 Severdia
+^ [#23900] Made file naming and folder structure consistent among Kunena modules
+# [#23900] Made CSS class names consistent with prefix
+
+KunenaSearch 1.6.0-RC2
+
+20-December-2010 Xillibit
++ [#22592] Add build directory with files, changelog and readme files
+^ [#22592] Missing build directory with files
++ [#22592] Put all language strings into the file en-GB.mod_kunenasearch.ini
+
+20-December-2010 Severdia
+^ [#23899] Update search module naming
+
+15-November-2010 Matias
++ [#23293] Update languages
+
+31-October-2010 Matias
+# [#20084] Fix message ordering to obey configuration
+
+25-September-2010 Severdia
++ [#22592] Added Kunena Search Module
+
+-->
---
+++ tags/1.6.x-addons/mod_kunenasearch1.6.2/helper.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* @package		Kunena Search
+* @copyright	(C) 2010 Kunena Project. All rights reserved.
+* @license		GNU/GPL
+*/
+
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+

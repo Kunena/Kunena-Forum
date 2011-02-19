8510cbbd65c28ee18cf8c3e29cc21bc1bea84b30
810 <schumacher1986@hotmail.com>
2011-02-19 22:38:23 +0000

    # [#24364] CSS fix (Search)
    git-svn-id: file:///Users/oratzes/svn/kunena@4463 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/2.0-810-mvc-backend-17-02-2011/components/com_kunena/CHANGELOG.php
+++ branches/2.0-810-mvc-backend-17-02-2011/components/com_kunena/CHANGELOG.php
@@ -34,6 +34,7 @@ Kunena 2.0.0-DEV
 18-February-2011 810
 # [#24364] CSS fix (announcement)
 # [#24364] CSS fix (Profile)
+# [#24364] CSS fix (Search)
 
 18-February-2011 810
 ^ [#24395] Backend in MVC (stats part2)

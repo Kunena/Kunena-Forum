cb81de3634bb6a27d97c5c4b9550f5fcbed770fc
lorddarkangel <sven@schultschik.de>
2011-02-27 22:41:43 +0000

    + [#24847] add cs-CZ (thanks David Mara) ^ [#24847] updated el-GR (thanks Chrysovalantis Mochlas) ^ [#24847] updated da-DK (thanks Lars Westermann) ^ [#24847] updated ru-RU (thanks Zarkos) ^ [#24847] updated nl-NL (thanks weenkbuul) ^ [#24847] updated es-ES (thanks Kunena Spanish Team)
    git-svn-id: file:///Users/oratzes/svn/kunena@4518 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

--- branches/1.6.4-LDA(svens)-language-2011-02-23/components/com_kunena/CHANGELOG.php
+++ branches/1.6.4-LDA(svens)-language-2011-02-23/components/com_kunena/CHANGELOG.php
@@ -32,6 +32,14 @@ Legend:
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Kunena 1.6.4-DEV
 
+27-February-2011 svens(LDA)
++ [#24847] add cs-CZ (thanks David Mara)
+^ [#24847] updated el-GR (thanks Chrysovalantis Mochlas)
+^ [#24847] updated da-DK (thanks Lars Westermann)
+^ [#24847] updated ru-RU (thanks Zarkos)
+^ [#24847] updated nl-NL (thanks weenkbuul)
+^ [#24847] updated es-ES (thanks Kunena Spanish Team)
+
 23-February-2011 Matias
 ^ [#15886] Merged revisions 4401-4471 from /branches/1.6.3-language-LDAsvens-2011-01-22
 ^ [#15886] Merged revisions 4458-4462 from /branches/1.6-xillibit-19-02-2011

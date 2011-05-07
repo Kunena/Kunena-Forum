7bf09dc0c05c14921f6f424d76a8855b760a4e9c
fxstein <oliver@kunena.org>
2011-05-07 15:26:46 +0000

    [#24415] 1.6.4 build 289
    git-svn-id: file:///Users/oratzes/svn/kunena@4830 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ tags/1.6.x-addons/mod_kunenalatest1.6.4/mod_kunenalatest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @version $Id$
+ * KunenaLatest Module
+ * @package Kunena Latest
+ *
+* @Copyright (C)2010-2011 www.kunena.org. All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.org
+ **/
+
+// no direct access
+defined ( '_JEXEC' ) or die ( '' );
+
+// Kunena detection and version check
+$minKunenaVersion = '1.6.3';
+if (!class_exists('Kunena') || Kunena::versionBuild() < 4344) {
+	echo JText::sprintf ( 'MOD_KUNENALATEST_KUNENA_NOT_INSTALLED', $minKunenaVersion );
+	return;
+}
+// Kunena online check
+if (!Kunena::enabled()) {
+	echo JText::_ ( 'MOD_KUNENALATEST_KUNENA_OFFLINE' );
+	return;
+}
+
+// Include the kunenalatest functions only once
+require_once (dirname ( __FILE__ ) . '/class.php');
+
+$params = ( object ) $params;
+
+// Add basic caching for visitors (3 minutes)
+$user = JFactory::getUser();
+if (!$user->id) {
+	$cache = JFactory::getCache('mod_kunenalatest', 'output');
+	$cache->setLifeTime(180);
+	if ($cache->start(md5(serialize($params)), 'mod_kunenalatest')) return;
+}
+$klatest = new modKunenaLatest ( $params );
+if (!$user->id) {
+	$cache->end();
+}

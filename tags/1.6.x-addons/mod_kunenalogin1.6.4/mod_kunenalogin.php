548a5476d3d0f47dc1a7f4521f2ed5af0fa4c09f
fxstein <oliver@kunena.org>
2011-05-08 06:04:53 +0000

    [#19002] 1.6.4 Build 247
    git-svn-id: file:///Users/oratzes/svn/kunena@4835 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ tags/1.6.x-addons/mod_kunenalogin1.6.4/mod_kunenalogin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @version $Id$
+ * Kunena Login Module
+ * @package Kunena login
+ *
+ * @Copyright (C)2010-2011 Kunena Team. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.org
+ */
+defined ( '_JEXEC' ) or die ();
+
+// Kunena detection and version check
+$minKunenaVersion = '1.6.3';
+if (! class_exists ( 'Kunena' ) || Kunena::versionBuild () < 4344) {
+	echo JText::sprintf ( 'MOD_KUNENALOGIN_KUNENA_NOT_INSTALLED', $minKunenaVersion );
+	return;
+}
+// Kunena online check
+if (! Kunena::enabled ()) {
+	echo JText::_ ( 'MOD_KUNENALOGIN_KUNENA_OFFLINE' );
+	return;
+}
+
+require_once (dirname ( __FILE__ ) . DS . 'class.php');
+
+$params = ( object ) $params;
+$modKunenaLogin = new ModKunenaLogin ( $params );
+$modKunenaLogin->display();

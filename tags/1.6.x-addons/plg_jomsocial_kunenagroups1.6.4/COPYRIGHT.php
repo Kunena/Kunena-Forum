da3d3a8cf2483c4e135603b6d373cc8a9cc4b216
fxstein <oliver@kunena.org>
2011-05-12 19:10:04 +0000

    [#22541] 1.6.4 Build 125
    git-svn-id: file:///Users/oratzes/svn/kunena@4855 daf2aa89-cb5b-0410-b8ee-fe93ebc9b27d

---
+++ tags/1.6.x-addons/plg_jomsocial_kunenagroups1.6.4/COPYRIGHT.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* @version $Id$
+* KunenaGroups Plugin for JomSocial
+* @package plg_jomsocial_kunenagroups
+*
+* @Copyright (C) 2009-2010 Kunena Team All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @link http://www.kunena.com
+*/
+die();
+?>
+<!--
+
+Kunena derives from copyrighted works licensed under the GNU General
+Public License.  This version has been modified pursuant to the
+GNU General Public License as of September 15, 2005, and as distributed,
+it includes or is derivative of works licensed under the GNU General
+Public License or other free or open source software licenses.
+
+-->

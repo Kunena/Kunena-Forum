7646051d2b74d0365fcf248152ac2b1851941a18
Oliver Ratzesberger <oliver@kunena.org>
2011-06-30 17:06:33 -0700

    [#1] Cleanup of master part 1

--- custom/matias-1.0/components/com_kunena/template/default/plugin/announcement/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*-----------------------------------------------
-this is a dummy file and intentionally left blank
-do not remove from ditribution package
-/*----------------------------------------------*/
-?>

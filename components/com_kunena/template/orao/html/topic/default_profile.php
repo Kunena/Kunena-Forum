924da2a693c7af5ebd31eb93218f5a93220216f5
draganz <info@megasystem.ch>
2011-08-02 16:37:18 +0200

    [#54] More code cleanup

--- components/com_kunena/template/orao/html/topic/default_profile.php
+++ components/com_kunena/template/orao/html/topic/default_profile.php
@@ -19,14 +19,6 @@ defined ( '_JEXEC' ) or die ();
 							</li>
 							<?php endif; ?>
 							<?php if ($this->profile->exists()): ?>
-							<?php /*?>
-							<li class="kpost-user-status" style="overflow: auto;">
-								<span class="kicon-button kbuttononline-<?php echo $this->profile->isOnline('yes', 'no') ?>">
-									<span class="online-<?php echo $this->profile->isOnline('yes', 'no') ?>">
-										<span><?php echo $this->profile->isOnline(JText::_('COM_KUNENA_ONLINE'), JText::_('COM_KUNENA_OFFLINE')) ?></span>
-									</span>
-								</span>
-							</li><?php */?>
 							<?php if (!empty($this->userranktitle)) : ?><li class="kpost-user-rank"><?php echo $this->escape($this->userranktitle) ?></li><?php endif ?>
 							<?php if (!empty($this->userrankimage)) : ?><li class="kpost-user-rank-img"><?php echo $this->userrankimage ?></li><?php endif ?>
 							<?php if ($this->userposts) : ?><li class="kpost-user-posts"><?php echo JText::_('COM_KUNENA_POSTS') .' '. intval($this->userposts) ?></li><?php endif ?>
@@ -43,49 +35,10 @@ defined ( '_JEXEC' ) or die ();
 								<?php echo $this->profile->profileIcon('birthdate') ?>
 								<?php echo $this->profile->profileIcon('location') ?>
 								<?php echo $this->profile->profileIcon('website') ?>
-								<?php /*if ($this->me->userid != $this->profile->userid && $this->params->get('uddeimPopup') == '1') : ?>
-									<span class="kicon-profile" style="background-image:none;"><a class="tk-mb-profile-pmlink" style="color:#fff;" href="#mb_inline-<?php echo intval($this->message->id);?>" rel="lightbox[pm-<?php echo intval($this->message->id);?> 400 200]"></a></span>
-								<?php else:*/?>
 								<?php echo $this->profile->profileIcon('private'); ?>
-								<?php //endif; ?>
 								<?php echo $this->profile->profileIcon('email') ?>
 							</li>
 							<?php if (!empty($this->personalText)) : ?><li class="kpost-user-perstext"><?php echo $this->personalText ?></li><?php endif ?>
 							<?php if (!empty($this->ipLink)) : ?><li class=""><?php echo $this->ipLink ?></li><?php endif ?>
 							<?php endif ?>
 						</ul>
-
-			<div id="mb_inline-<?php echo intval($this->message->id);?>" style="display: none;color:#999;background:#fff;position:relative;">
-				<div class="tk-mb-header-pm">
-					<span class="tk-mb-first"><?php echo JText::_('COM_KUNENA_TEMPLATE_CONTACT'); ?> <?php echo $this->escape($this->message->name);?></span>
-				</div>
-				<div class="" style="border:0px;padding:5px;">
-					<form method="post" name='sendeform' action="<?php echo JRoute::_( 'index.php?option=com_uddeim' ); ?>" enctype="multipart/form-data">
-						<div class="tk-mb-avatar" style="float:left;">
-							<?php echo $this->profile->getLink($avatar); ?>
-						</div>
-						<div class="" style="float:left;margin-left:10px;">
-							<div class="" style="">
-								<input type="text" style="border:1px solid #dfdfdf;padding: 4px;background: #FAFAFA;margin:0;" name="to_name" class="inputbox" readonly="readonly" size="35" value="<?php echo $this->escape($this->message->name);?>"/>
-							</div>
-							<div class="" style="padding-top:5px;">
-								<textarea name="pmessage" class="inputbox required" rows="5" cols="37" style="border:1px solid #dfdfdf;"> <?php echo JText::_('COM_KUNENA_TEMPLATE_WRITE_HERE'); ?></textarea>
-							</div>
-							<div class="" style="border:0px;float:left;margin-top: 10px;">
-								<span class="tk-mb-submit">
-									<a style="text-decoration:none;" href="javascript:this.onclick(Mediabox.open('#mb_pmresult', '', '400 80'));">
-										<input type="submit" class="tk-mb-submit" name="reply" value="Send" />
-									</a>
-								</span>
-								<span class="tk-mb-submit">
-									<input type="reset" class="tk-mb-cancel" name="cancel" value="Cancel" onclick="Mediabox.close();" />
-								</span>
-							</div>
-						</div>
-						<span class="tk-date1" style="color:#fff;font-size: 12px;float: left;line-height: 5.5em;"><?php $now = &JFactory::getDate(); echo $now->toFormat(); ?></span>
-						<input type="hidden"  name="to_id" value="<?php $this->profile->userid ?>" />
-						<input type="hidden"  name="task" value="save" />
-						<?php echo JHTML::_ ( 'form.token' ); ?>
-					</form>
-				</div>
-			</div>

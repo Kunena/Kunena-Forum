3c6a7fd96fb4fd28b5dc0559fafc16d3888f9054
Jelle Kok <schumacher1986@hotmail.com>
2011-08-13 20:34:27 +0300

    delete line: <div id="kunena">

--- components/com_kunena/template/default20/html/announcement/edit.php
+++ components/com_kunena/template/default20/html/announcement/edit.php
@@ -22,7 +22,6 @@ $document->addScriptDeclaration('// <![CDATA[
 }
 // ]]>');
 ?>
-<div id="kunena">
 		<div id="kannouncement">
 			<h2 class="kheader">
 				<a title="<?php echo JText::_('COM_KUNENA_ANN_ANNOUNCEMENTS') ?>" rel="kannouncement-detailsbox">

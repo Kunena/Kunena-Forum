d3634f629cea45a72b8910c348afb40f0a23692d
Matias Griese <matias@kunena.org>
2011-12-15 09:46:30 +0200

    Move kunena.j16.xml rename to another location

--- administrator/components/com_kunena/install/model.php
+++ administrator/components/com_kunena/install/model.php
@@ -543,10 +543,6 @@ class KunenaModelInstall extends JModel {
 
 		// Cleanup directory structure
 		if (!KunenaForum::isSVN()) {
-			if ( version_compare(JVERSION, '1.6', '>') && JFile::exists(KPATH_ADMIN . '/kunena.j16.xml')) {
-				if ( JFile::exists(KPATH_ADMIN . '/kunena.xml')) JFile::delete(KPATH_ADMIN . '/kunena.xml');
-				JFile::move(KPATH_ADMIN . '/kunena.j16.xml', KPATH_ADMIN . '/kunena.xml');
-			}
 			if ( JFolder::exists(KPATH_ADMIN . '/language') ) JFolder::delete(KPATH_ADMIN . '/language');
 			if ( JFolder::exists(KPATH_SITE . '/language') ) JFolder::delete(KPATH_SITE . '/language');
 		}

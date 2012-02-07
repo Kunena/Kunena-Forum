f24d9cbe77250f5b973de3d53b8118a9e1092ac8
coder4life <coder4life.dev@gmail.com>
2012-02-06 23:45:52 -0800

    CSS override fixes.

--- components/com_kunena/template/mirage/html/category/list_section.php
+++ components/com_kunena/template/mirage/html/category/list_section.php
@@ -36,16 +36,16 @@ defined ( '_JEXEC' ) or die ();
 									<dd class="category-icon">
 									</dd>
 									<dd class="category-subject">
-										<span><?php echo JText::_('Subject') ?></<span>
+										<span><?php echo JText::_('Subject') ?></span>
 									</dd>
 									<dd class="category-topics">
-										<span><?php echo JText::_('COM_KUNENA_GEN_TOPICS') ?></<span>
+										<span><?php echo JText::_('COM_KUNENA_GEN_TOPICS') ?></span>
 									</dd>
 									<dd class="category-replies">
-										<span><?php echo JText::_('COM_KUNENA_GEN_REPLIES') ?></<span>
+										<span><?php echo JText::_('COM_KUNENA_GEN_REPLIES') ?></span>
 									</dd>
 									<dd class="category-lastpost">
-										<span><?php echo JText::_('Last Post') ?></<span>
+										<span><?php echo JText::_('Last Post') ?></span>
 									</dd>
 								</dl>
 							</li>

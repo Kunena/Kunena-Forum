48f5c0096c92bc9c79dcd6989adebb1e2fc6bc74
Sven Schultschik <sven@schultschik.de>
2012-03-08 23:33:45 +0100

    [#404] Add hook to BBCode editor initializer to add buttons.

--- administrator/components/com_kunena/libraries/bbcode/editor.php
+++ administrator/components/com_kunena/libraries/bbcode/editor.php
@@ -19,6 +19,11 @@ defined ( '_JEXEC' ) or die ();
  * @version		2.0
  */
 class KunenaBbcodeEditor {
+
+	protected $_insert = array( "after" => array(),
+								"before" => array(),
+								"afterall" => array());
+
 	function __construct($config = array()) {
 		$this->config = $config;
 	}
@@ -31,6 +36,24 @@ class KunenaBbcodeEditor {
 		return $instance;
 	}
 
+	/**
+	 * @param SimpleXMLElement $button
+	 * @param null $pos
+	 * @param $where
+	 */
+	public function addButton(SimpleXMLElement $button, $pos=NULL , $where) {
+		switch ($pos) {
+			case 'after':
+				$this->_insert['after'][$where][] = $button;
+				break;
+			case 'before':
+				$this->_insert['before'][$where][] = $button;
+				break;
+			default:
+				$this->_insert['afterall'][] = $button;
+		}
+	}
+
 	public function initialize($identifier ='class') {
 		$js = "window.addEvent('domready', function() {
 	kbbcode = new kbbcode('kbbcode-message', 'kbbcode-toolbar', {
@@ -39,41 +62,79 @@ class KunenaBbcodeEditor {
 		interceptTab: true
 });\n";
 		$editor = simplexml_load_file(dirname(__FILE__).'/editor.xml');
+		//Hook to manipulate the Editor XML like adding buttons
+		$dispatcher = JDispatcher::getInstance();
+		JPluginHelper::importPlugin('kunena');
+		$dispatcher->trigger( 'onKunenaBbcodeEditorInit', array ( $this ) );
+
 		foreach ($editor as $item) {
-			if ($item['disabled'] == 'disabled') continue;
-			switch ($item->getName()) {
-				case 'button':
-					// <button tag="i" name="italic" title="COM_KUNENA_EDITOR_ITALIC" alt="COM_KUNENA_EDITOR_HELPLINE_ITALIC">
-					if ($item['config']) {
-						$cfgVariable = (string) $item['config'];
-						$cfgValue = intval($cfgVariable[0] != '!');
-						if (!$cfgValue) $cfgVariable = substr($cfgVariable, 1);
-						if (KunenaFactory::getConfig()->$cfgVariable != $cfgValue) continue;
-					}
-					$name = $item['name'] ? $item['name'] : ($item['tag'] ? $item['tag'] : '#');
-					$class = $item['class'] ? $item['class'] : "kbbcode-{$name}-button";
-					$js .= "\nkbbcode.addFunction('{$name}', function() {";
-					$js .= $this->editorAction($name, $item);
-					$js .= "\n}, {";
-					foreach (array('title', 'alt') as $type) {
-						if (isset($item[$type])) {
-							$value = JText::_($item[$type], true);
-							$js .= "\n	'{$type}': '{$value}',";
-						}
-					}
-					if ($item['class']) {
-						$js .= "\n	'class': '{$class}'";
-					} else {
-							$js .= "\n	'{$identifier}': '{$class}'";
-					}
-					$js .= "\n});\n";
-					break;
+			if ( array_key_exists($item['name'], $this->_insert['before'])) {
+				foreach ( $this->_insert['before'][$item['name']] as $insert_item ) {
+					$js .= $this->editorItem( $insert_item, $identifier);
+				}
+				unset ( $this->_insert['before'][$item['name']] );
+			}
+
+			$js .= $this->editorItem($item, $identifier);
+
+			if ( array_key_exists($item['name'], $this->_insert['after'])) {
+				foreach ( $this->_insert['after'][$item['name']] as $insert_item ) {
+					$js .= $this->editorItem( $insert_item, $identifier);
+				}
+				unset ( $this->_insert['after'][$item['name']] );
 			}
 		}
+		foreach ( $this->_insert['afterall'] as $insert_item ) {
+			$js .= $this->editorItem( $insert_item, $identifier);
+		}
+		$this->_insert['afterall'] = array();
+
 		$js .= "});\n";
 		$template = KunenaTemplate::getInstance();
 		$template->addScript('js/editor.js');
 		JFactory::getDocument()->addScriptDeclaration( "// <![CDATA[\n{$js}\n// ]]>");
+	}
+
+	/**
+	 * Generate the addFunction call
+	 *
+	 * @param $item SimpleXMLElement which describes the functionality
+	 * @param $identifier
+	 * @return null|string JavaScript that needs to be added
+	 */
+	protected function editorItem($item, $identifier) {
+		$js = '';
+		if ($item['disabled'] == 'disabled') return null;
+		switch ($item->getName()) {
+			case 'button':
+				// <button tag="i" name="italic" title="COM_KUNENA_EDITOR_ITALIC" alt="COM_KUNENA_EDITOR_HELPLINE_ITALIC">
+				if ($item['config']) {
+					$cfgVariable = (string) $item['config'];
+					$cfgValue = intval($cfgVariable[0] != '!');
+					if (!$cfgValue) $cfgVariable = substr($cfgVariable, 1);
+					if (KunenaFactory::getConfig()->$cfgVariable != $cfgValue) continue;
+				}
+				$name = $item['name'] ? $item['name'] : ($item['tag'] ? $item['tag'] : '#');
+				$class = $item['class'] ? $item['class'] : "kbbcode-{$name}-button";
+				$js .= "\nkbbcode.addFunction('{$name}', function() {";
+				$js .= $this->editorAction($name, $item);
+				$js .= "\n}, {";
+				foreach (array('title', 'alt') as $type) {
+					if (isset($item[$type])) {
+						$value = JText::_($item[$type], true);
+						$js .= "\n	'{$type}': '{$value}',";
+					}
+				}
+				if ($item['class']) {
+					$js .= "\n	'class': '{$class}'";
+				} else {
+						$js .= "\n	'{$identifier}': '{$class}'";
+				}
+				$js .= "\n});\n";
+				break;
+		}
+
+		return $js;
 	}
 
 	protected function editorAction($name, $item) {

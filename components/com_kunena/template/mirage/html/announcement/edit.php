cfbf537e47c14e12e4d471f2044607b18e5bbe40
Matias Griese <matias@kunena.org>
2012-03-10 12:55:11 +0200

    Simplify announcement layouts

--- components/com_kunena/template/mirage/html/announcement/edit.php
+++ components/com_kunena/template/mirage/html/announcement/edit.php
@@ -22,7 +22,7 @@ $this->document->addScriptDeclaration('// <![CDATA[
 				<div class="header fl">
 					<h2 class="header">
 						<a class="link" title="<?php echo JText::_('COM_KUNENA_ANN_ANNOUNCEMENTS') ?>" rel="kannouncement-detailsbox">
-							<?php echo JText::_('COM_KUNENA_ANN_ANNOUNCEMENTS'); ?>: <?php echo $this->announcement->id ? JText::_('COM_KUNENA_ANN_EDIT') : JText::_('COM_KUNENA_ANN_ADD'); ?>
+							<?php echo JText::_('COM_KUNENA_ANN_ANNOUNCEMENTS') ?>: <?php echo $this->announcement->exists() ? JText::_('COM_KUNENA_ANN_EDIT') : JText::_('COM_KUNENA_ANN_ADD') ?>
 						</a>
 					</h2>
 				</div>
@@ -33,80 +33,74 @@ $this->document->addScriptDeclaration('// <![CDATA[
 			<div class="detailsbox-wrapper">
 				<div class="announcement-details detailsbox box-border box-border_radius box-shadow" >
 					<form action="<?php echo KunenaRoute::_('index.php?option=com_kunena&view=announcement') ?>" class="form-validate" method="post" name="editform" onsubmit="return kunenaValidate(this);">
-						<input type="hidden" name="view" value="announcement" />
 						<input type="hidden" name="task" value="save" />
-						<input type="hidden" name="id" value="<?php echo intval($this->announcement->id) ;?>" />
-						<?php echo JHtml::_( 'form.token' ); ?>
+						<?php echo $this->displayInput('id') ?>
+						<?php echo JHtml::_( 'form.token' ) ?>
+
 							<ul class="list-unstyled kform announcement-list clearfix">
 								<li class="announcement-row box-hover box-hover_list-row">
 									<div class="form-label">
 										<label>
-											<?php echo JText::_('COM_KUNENA_ANN_TITLE'); ?>:
+											<?php echo JText::_('COM_KUNENA_ANN_TITLE') ?>:
 										</label>
 									</div>
 									<div class="form-field">
-										<input class="box-width inputbox required" type="text" name="title" value="<?php echo $this->escape($this->announcement->title) ;?>"/>
+										<?php echo $this->displayInput('title', 'class="box-width inputbox required"') ?>
 									</div>
 								</li>
 								<li class="announcement-row box-hover box-hover_list-row">
 									<div class="form-label">
 										<label>
-											<?php echo JText::_('COM_KUNENA_ANN_SORTTEXT'); ?>:
+											<?php echo JText::_('COM_KUNENA_ANN_SORTTEXT') ?>:
 										</label>
 									</div>
 									<div class="form-field">
-										<textarea class="box-width inputbox required" rows="80" cols="16" name="sdescription"><?php echo $this->escape($this->announcement->sdescription); ?></textarea>
+										<?php echo $this->displayInput('sdescription', 'class="box-width textbox required" rows="80" cols="4"') ?>
 									</div>
 								</li>
 								<li class="announcement-row box-hover box-hover_list-row">
 									<div class="form-label">
 										<label>
-											<?php echo JText::_('COM_KUNENA_ANN_LONGTEXT'); ?>:
+											<?php echo JText::_('COM_KUNENA_ANN_LONGTEXT') ?>:
 										</label>
 									</div>
 									<div class="form-field">
-										<textarea class="box-width textbox" rows="80" cols="16" name="description"><?php echo $this->escape($this->announcement->description); ?></textarea>
+										<?php echo $this->displayInput('description', 'class="box-width textbox" rows="80" cols="16"') ?>
 									</div>
 								</li>
 								<li class="announcement-row box-hover box-hover_list-row">
 									<div class="form-label">
 										<label>
-											<?php echo JText::_('COM_KUNENA_ANN_DATE'); ?>:
+											<?php echo JText::_('COM_KUNENA_ANN_DATE') ?>:
 										</label>
 									</div>
 									<div class="form-field">
-										<?php echo JHTML::_('calendar', $this->escape($this->announcement->created), 'created', 'addcreated');?>
+										<?php echo $this->displayInput('created', 'addcreated') ?>
 									</div>
 								</li>
 								<li class="announcement-row box-hover box-hover_list-row">
 									<div class="form-label">
 										<label>
-											<?php echo JText::_('COM_KUNENA_ANN_SHOWDATE'); ?>:
+											<?php echo JText::_('COM_KUNENA_ANN_SHOWDATE') ?>:
 										</label>
 									</div>
 									<div class="form-field">
-										<select name="showdate">
-											<option value="1" <?php echo ($this->announcement->showdate == 1 ? 'selected="selected"' : ''); ?>><?php echo JText::_('COM_KUNENA_ANN_YES'); ?></option>
-											<option value="0" <?php echo ($this->announcement->showdate == 0 ? 'selected="selected"' : ''); ?>><?php echo JText::_('COM_KUNENA_ANN_NO'); ?></option>
-										</select>
+										<?php echo $this->displayInput('showdate') ?>
 									</div>
 								</li>
 								<li class="announcement-row box-hover box-hover_list-row">
 									<div class="form-label">
 										<label>
-											<?php echo JText::_('COM_KUNENA_ANN_PUBLISH'); ?>:
+											<?php echo JText::_('COM_KUNENA_ANN_PUBLISH') ?>:
 										</label>
 									</div>
 									<div class="form-field">
-										<select name="published">
-											<option value="1" <?php echo ($this->announcement->published == 1 ? 'selected="selected"' : ''); ?>><?php echo JText::_('COM_KUNENA_ANN_YES'); ?></option>
-											<option value="0" <?php echo ($this->announcement->published == 0 ? 'selected="selected"' : ''); ?>><?php echo JText::_('COM_KUNENA_ANN_NO'); ?></option>
-										</select>
+										<?php echo $this->displayInput('published') ?>
 									</div>
 								</li>
 							</ul>
-						<input name="submit" class="kbutton" type="submit" value="<?php echo JText::_('COM_KUNENA_ANN_SAVE'); ?>"/>
-						<input onclick="javascript:window.history.back();" name="cancel" class="kbutton" type="button" value="<?php echo JText::_('COM_KUNENA_ANN_CANCEL'); ?>"/>
+						<input name="submit" class="kbutton" type="submit" value="<?php echo JText::_('COM_KUNENA_ANN_SAVE') ?>"/>
+						<input onclick="javascript:window.history.back();" name="cancel" class="kbutton" type="button" value="<?php echo JText::_('COM_KUNENA_ANN_CANCEL') ?>"/>
 					</form>
 				</div>
 			</div>

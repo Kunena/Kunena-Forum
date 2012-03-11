7abada7449db7bffb6bc5c69913ff10dad5dc540
Matias Griese <matias@kunena.org>
2012-03-11 17:09:19 +0200

    Improve Kunena menu module (use Kunena library, enable in J!1.5)

--- administrator/components/com_kunena/install/modules/mod_kunenamenu/mod_kunenamenu.php
+++ administrator/components/com_kunena/install/modules/mod_kunenamenu/mod_kunenamenu.php
@@ -1,10 +1,13 @@
 <?php
 /**
- * @package		Joomla.Site
- * @subpackage	mod_menu
- * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
- */
+ * Kunena Menu Module
+ * @package Kunena.Modules
+ * @subpackage Menu
+ *
+ * @copyright (C) 2008 - 2012 Kunena Team. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.org
+ **/
 defined('_JEXEC') or die;
 
 // Kunena detection and version check
@@ -13,19 +16,9 @@ if (!class_exists('KunenaForum') || !KunenaForum::installed() || !KunenaForum::i
 	return;
 }
 
-// Include the syndicate functions only once
-require_once dirname(__FILE__).'/helper.php';
+// Include the class only once
+require_once dirname ( __FILE__ ) . '/class.php';
 
 $params = (object) $params;
-$list	= modKunenamenuHelper::getList($params);
-$app	= JFactory::getApplication();
-$menu	= $app->getMenu();
-$active	= $menu->getActive();
-$active_id = isset($active) ? $active->id : $menu->getDefault()->id;
-$path	= isset($active) ? $active->tree : array();
-$showAll	= $params->get('showAllChildren');
-$class_sfx	= htmlspecialchars($params->get('class_sfx'));
-
-if(count($list)) {
-	require JModuleHelper::getLayoutPath('mod_kunenamenu', $params->get('layout', 'default'));
-}
+$menu = new modKunenaMenu($params);
+$menu->display();

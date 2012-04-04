60e999d01a5272ece310c1dd4ffe41f435e8971c
coder4life <coder4life.dev@gmail.com>
2012-04-04 01:50:57 -0700

    Mirage: Implement profile position for left and right.

--- components/com_kunena/template/mirage/html/topic/default.php
+++ components/com_kunena/template/mirage/html/topic/default.php
@@ -13,4 +13,42 @@ defined ( '_JEXEC' ) or die ();
 $this->displayPoll();
 $this->getModulePosition( 'kunena_poll' );
 //$this->displayTopicActions(0);
-$this->displayTemplateFile('topic', 'default', 'list');
+//$this->displayTemplateFile('topic', 'default', 'list');
+
+?>
+
+<div class="kmodule topic-default_list">
+	<div class="kbox-wrapper kbox-full">
+		<div class="topic-default_list-kbox kbox kbox-full kbox-color kbox-border kbox-border_radius kbox-border_radius-vchild kbox-shadow">
+			<div class="headerbox-wrapper kbox-full">
+				<div class="header">
+					<!-- a href="#" title="Topic RSS Feed"><span class="krss-icon">Topic RSS Feed</span></a -->
+					<!-- a href="#" title="View Subscribers of this Topic" class="ktopic-subsc">4 Subscribers</a -->
+					<h2 class="header link-header2"><?php echo JText::_('COM_KUNENA_TOPIC') ?> <a class="section" rel="topic-detailsbox"><?php echo $this->displayTopicField('subject') ?></a></h2>
+					<?php if ( $this->config->keywords ) : ?>
+						<ul class="list-unstyled topic-taglist">
+							<?php if (!empty($this->keywords)) : ?>
+								<li class="topic-taglist-title"><?php echo JText::sprintf('COM_KUNENA_TOPIC_TAGS', $this->keywords) ?></li>
+							<?php else: ?>
+								<li class="topic-taglist-title"><?php echo JText::_('COM_KUNENA_TOPIC_NO_TAGS') ?></li>
+							<?php endif ?>
+							<?php if ( $this->me->userid == $this->topic->first_post_userid || intval($this->me->isModerator('global')) ): ?><li class="topic-taglist-edit"><a id="edit_keywords" class="link"><?php echo JText::_('COM_KUNENA_TOPIC_TAGS_ADD_EDIT') ?></a></li><?php endif ?>
+						</ul>
+					<?php endif ?>
+				</div>
+			</div>
+			<?php echo $this->displayTopicActions(0); ?>
+			<div class="innerbox-wrapper innerspacer">
+				<?php echo $this->getPagination(4); ?>
+			</div>
+			<div class="innerbox-wrapper">
+				<div class="topic-detailsbox detailsbox">
+					<ul class="list-unstyled topic-posts">
+						<?php $this->displayMessages() ?>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="spacer"></div>
--- components/com_kunena/template/mirage/html/topic/default_message.php
+++ components/com_kunena/template/mirage/html/topic/default_message.php
@@ -10,72 +10,37 @@
  **/
 defined ( '_JEXEC' ) or die ();
 ?>
-<li class="topic-row">
-	<dl class="list-unstyled">
-		<dd class="kbox-border kbox-border_radius kbox-shadow">
-			<ul class="list-unstyled message-list">
-				<li class="header">
-					<a id="<?php echo $this->displayMessageField('id') ?>"></a>
-					<dl class="list-unstyled">
-						<dd class="kposthead-replytitle"><h3>RE: <?php echo $this->displayMessageField('subject') ?></h3></dd>
-						<dd class="kposthead-postid" ><?php echo $this->numLink ?></dd>
-						<?php if (!empty($this->ipLink)) : ?><dd class="kposthead-postip"><?php echo $this->ipLink ?></dd><?php endif ?>
-						<dd class="kposthead-posttime">Posted [K=DATE:<?php echo $this->message->time ?>]</dd>
-					</dl>
-				</li>
-			</ul>
-			[K=MESSAGE_PROFILE]
-			<div class="kpost-container kbox-hover">
-				<ul class="list-unstyled kpost-post-body">
-					<li class="kpost-body">
-						<?php echo $this->displayMessageField('message') ?>
-					</li>
-					<li class="clr"></li>
-					<?php if (!empty($this->attachments)) : ?>
-						<li class="kpost-body-attach">
-							<span class="kattach-title"><?php echo JText::_('COM_KUNENA_ATTACHMENTS') ?></span>
-							<ul class="list-unstyled">
-								<?php foreach($this->attachments as $attachment) : ?>
-								<li class="kattach-details">
-									<?php echo $attachment->getThumbnailLink(); ?> <span><?php echo $attachment->getTextLink(); ?></span>
-								</li>
-								<?php endforeach; ?>
-							</ul>
-							<div class="clr"></div>
-						</li>
-						<?php endif; ?>
 
-						<?php $this->displayTemplateFile('topic', 'default', 'quickreply') ?>
-
-						<?php if ($this->config->editmarkup && $this->message->modified_by) : ?>
-							<li class="kpost-body-lastedit">
-								<?php echo JText::_('COM_KUNENA_EDITING_LASTEDIT') . ": [K=DATE:{$this->message->modified_time}] " . JText::_('COM_KUNENA_BY') . ' ' . $this->message->getModifier()->getLink() . '.'; ?>
-							</li>
-						<?php if ($this->message->modified_reason) : ?>
-							<li class="kpost-body-editreason">
-								<?php echo JText::_('COM_KUNENA_REASON') . ': ' . $this->escape ( $this->message->modified_reason ); ?>
-							</li>
-						<?php endif ?>
-					<?php endif ?>
-					<?php if (!empty($this->signatureHtml)) : ?>
-						<li class="kpost-body-sig"><?php echo $this->signatureHtml ?></li>
-					<?php endif ?>
-				</ul>
-				<?php if(!empty($this->thankyou)): ?>
-				<div class="buttonbar">
-					<ul class="list-unstyled buttons-message innerblock">
-					<?php
-					echo JText::_('COM_KUNENA_THANKYOU').': ';
-					echo implode(', ', $this->thankyou);
-					if (count($this->thankyou) > 9) echo '...';
-					?>
-					</ul>
-				</div>
-				<?php endif ?>
-				[K=MESSAGE_ACTIONS]
-			</div>
-			<div class="clr"></div>
-		</dd>
-	</dl>
+<li class="kpost-body">
+	<?php echo $this->displayMessageField('message') ?>
 </li>
-<?php if ($this->isModulePosition('kunena_msg_' . $this->mmm)) : ?><li class="kmodules"><?php $this->getModulePosition('kunena_msg_' . $this->mmm) ?></li><?php endif ?>
+<li class="clr"></li>
+<?php if (!empty($this->attachments)) : ?>
+	<li class="kpost-body-attach">
+		<span class="kattach-title"><?php echo JText::_('COM_KUNENA_ATTACHMENTS') ?></span>
+		<ul class="list-unstyled">
+			<?php foreach($this->attachments as $attachment) : ?>
+			<li class="kattach-details">
+				<?php echo $attachment->getThumbnailLink(); ?> <span><?php echo $attachment->getTextLink(); ?></span>
+			</li>
+			<?php endforeach; ?>
+		</ul>
+		<div class="clr"></div>
+	</li>
+	<?php endif; ?>
+
+	<?php $this->displayTemplateFile('topic', 'default', 'quickreply') ?>
+
+	<?php if ($this->config->editmarkup && $this->message->modified_by) : ?>
+		<li class="kpost-body-lastedit">
+			<?php echo JText::_('COM_KUNENA_EDITING_LASTEDIT') . ": [K=DATE:{$this->message->modified_time}] " . JText::_('COM_KUNENA_BY') . ' ' . $this->message->getModifier()->getLink() . '.'; ?>
+		</li>
+	<?php if ($this->message->modified_reason) : ?>
+		<li class="kpost-body-editreason">
+			<?php echo JText::_('COM_KUNENA_REASON') . ': ' . $this->escape ( $this->message->modified_reason ); ?>
+		</li>
+	<?php endif ?>
+<?php endif ?>
+<?php if (!empty($this->signatureHtml)) : ?>
+	<li class="kpost-body-sig"><?php echo $this->signatureHtml ?></li>
+<?php endif ?>

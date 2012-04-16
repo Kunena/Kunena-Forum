7c59c3ac035d3a4b76b15daabdcab4cd7c969045
Florian DAL FITTO <xillibit@gmail.com>
2012-04-16 22:40:27 +0200

    #555 New emoticons: icon preview is broken

--- administrator/components/com_kunena/views/smilies/tmpl/add.php
+++ administrator/components/com_kunena/views/smilies/tmpl/add.php
@@ -16,7 +16,7 @@ if (JFactory::getLanguage()->isRTL()) $document->addStyleSheet ( JURI::base().'c
 $document->addScriptDeclaration('function update_smiley(newimage)
 			{
 				document.smiley_image.src = "'.
-				$this->escape(KURL_SITE . $this->smileypath).'" + newimage;
+				$this->escape(JURI::root() . $this->smileypath).'" + newimage;
 			}');
 ?>
 <div id="kadmin">

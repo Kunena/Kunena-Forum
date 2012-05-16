0a95d6d838d84e1a39d010b04528f40d74178543
Matias Griese <matias@kunena.org>
2012-05-17 00:30:06 +0300

    Fix misc bugs

--- administrator/components/com_kunena/libraries/html/pagination.php
+++ administrator/components/com_kunena/libraries/html/pagination.php
@@ -220,12 +220,14 @@ class KunenaHtmlPagination extends JPagination
 
 		// Build the additional URL parameters string.
 		$uri = KunenaRoute::normalize($this->_uri, true);
-		$uri->delVar('start');
-		$uri->delVar('limitstart');
-		$uri->delVar('limit');
-		if (!empty($this->_additionalUrlParams)) {
-			foreach($this->_additionalUrlParams as $key => $value) {
-				$uri->setVar($key, $value);
+		if ($uri) {
+			$uri->delVar('start');
+			$uri->delVar('limitstart');
+			$uri->delVar('limit');
+			if (!empty($this->_additionalUrlParams)) {
+				foreach($this->_additionalUrlParams as $key => $value) {
+					$uri->setVar($key, $value);
+				}
 			}
 		}
 

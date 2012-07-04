f7035a8307300921b490af0abc2197eb6cd467c6
Matias Griese <matias@kunena.org>
2012-07-04 23:50:56 +0300

    Joomla 2.5: Fix warning in error page if debug was turned on

--- administrator/components/com_kunena/libraries/error.php
+++ administrator/components/com_kunena/libraries/error.php
@@ -142,6 +142,7 @@ function kunenaShutdownHandler($debug) {
 	if ($error && in_array ( $error ['type'], $types )) {
 		header('HTTP/1.1 500 Internal Server Error');
 		while(@ob_end_clean());
+		ob_start();
 		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb" lang="en-gb">
 <head>

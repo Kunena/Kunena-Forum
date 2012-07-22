57fa27ee302c5ba9cf8174e70d365b1683b54982
Rouven Weßling <me@rouvenwessling.de>
2012-07-22 20:22:07 +0200

    Remove some uses of the deprecated JViewLegacy::assign() and JViewLegacy::assignRef().

--- administrator/components/com_kunena/liveupdate/classes/view.php
+++ administrator/components/com_kunena/liveupdate/classes/view.php
@@ -25,12 +25,12 @@ class LiveUpdateView extends JView
 			$url = JURI::base().'/components/'.JRequest::getCmd('option','').'/liveupdate/assets/liveupdate.css';
 			$document->addStyleSheet($url, 'text/css');
 		}
-		
+
 		$requeryURL = 'index.php?option='.JRequest::getCmd('option','').'&view='.JRequest::getCmd('view','liveupdate').'&force=1';
 		$this->assign('requeryURL', $requeryURL);
-		
+
 		$model = $this->getModel();
-		
+
 		$extInfo = (object)$config->getExtensionInformation();
 		JToolBarHelper::title($extInfo->title.' &ndash; '.JText::_('LIVEUPDATE_TASK_OVERVIEW'),'liveupdate');
 		if(version_compare(JVERSION,'1.6.0','ge')) {
@@ -39,20 +39,20 @@ class LiveUpdateView extends JView
 			$msg = 'Back';
 		}
 		JToolBarHelper::back($msg, 'index.php?option='.JRequest::getCmd('option',''));
-		
+
 		switch(JRequest::getCmd('task','default'))
 		{
 			case 'startupdate':
 				$this->setLayout('startupdate');
 				$this->assign('url','index.php?option='.JRequest::getCmd('option','').'&view='.JRequest::getCmd('view','liveupdate').'&task=download');
 				break;
-				
+
 			case 'install':
 				$this->setLayout('install');
 
 				// Get data from the model
 				$state		= $this->get('State');
-		
+
 				// Are there messages to display ?
 				$showMessage	= false;
 				if ( is_object($state) )
@@ -61,31 +61,31 @@ class LiveUpdateView extends JView
 					$message2		= $state->get('extension.message');
 					$showMessage	= ( $message1 || $message2 );
 				}
-		
-				$this->assign('showMessage',	$showMessage);
-				$this->assignRef('state',		$state);
-				
+
+				$this->showMessage = $showMessage;
+				$this->state = &$state;
+
 				break;
-				
+
 			case 'nagscreen':
 				$this->setLayout('nagscreen');
 				$this->assign('updateInfo', LiveUpdate::getUpdateInformation());
 				$this->assign('runUpdateURL','index.php?option='.JRequest::getCmd('option','').'&view='.JRequest::getCmd('view','liveupdate').'&task=startupdate&skipnag=1');
 				break;
-			
+
 			case 'overview':
 			default:
 				$this->setLayout('overview');
-				
+
 				$force = JRequest::getInt('force',0);
 				$this->assign('updateInfo', LiveUpdate::getUpdateInformation($force));
 				$this->assign('runUpdateURL','index.php?option='.JRequest::getCmd('option','').'&view='.JRequest::getCmd('view','liveupdate').'&task=startupdate');
-				
+
 				$needsAuth = !($config->getAuthorization()) && ($config->requiresAuthorization());
-				$this->assign('needsAuth', $needsAuth); 
+				$this->assign('needsAuth', $needsAuth);
 				break;
 		}
-		
+
 		parent::display($tpl);
 	}
 }

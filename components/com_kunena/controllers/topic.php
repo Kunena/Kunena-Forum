e80f517e623ec42451b233e2bbfd77afeb3b99e7
Matias Griese <matias@kunena.org>
2012-09-01 00:29:17 +0300

    Fix #1051: If message has no IP address, wrong IP gets displayed

--- components/com_kunena/controllers/topic.php
+++ components/com_kunena/controllers/topic.php
@@ -102,8 +102,11 @@ class KunenaControllerTopic extends KunenaController {
 			$topic->icon_id = $fields['icon_id'];
 		}
 
-		$message->removeIPTracking();
-
+		// Remove IP address
+		// TODO: Add administrator tool to remove all tracked IP addresses (from the database)
+		if (!$this->config->iptracking) {
+			$message->ip = '';
+		}
 		// If requested: Make message to be anonymous
 		if ($fields['anonymous'] && $message->getCategory()->allow_anonymous) {
 			$message->makeAnonymous();

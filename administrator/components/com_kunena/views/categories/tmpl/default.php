bf4d00b293f988169db63f5b312c18744605c309
810 <schumacher1986@hotmail.com>
2012-12-28 04:45:00 +0100

    K3.0 Backend: Add Bootstrap template (part 3)

--- administrator/components/com_kunena/views/categories/tmpl/default.php
+++ administrator/components/com_kunena/views/categories/tmpl/default.php
@@ -16,32 +16,40 @@ JHtml::_('formbehavior.chosen', 'select');
 
 $changeOrder 	= ($this->state->get('list.ordering') == 'ordering' && $this->state->get('list.direction') == 'asc');
 ?>
-<!-- Main page container -->
+<script type="text/javascript">
+	Joomla.orderTable = function() {
+		table = document.getElementById("sortTable");
+		direction = document.getElementById("directionTable");
+		order = table.options[table.selectedIndex].value;
+		if (order != '<?php echo $listOrder; ?>') {
+			dirn = 'asc';
+		} else {
+			dirn = direction.options[direction.selectedIndex].value;
+		}
+		Joomla.tableOrdering(order, dirn, '');
+	}
+</script>
+ <form action="<?php echo KunenaRoute::_('administrator/index.php?option=com_kunena&view=categories'); ?>" method="post" name="adminForm" id="adminForm">
+ <!-- Main page container -->
 <div class="container-fluid">
 <div class="row-fluid">
  <div class="span2">
 	<div><?php include KPATH_ADMIN.'/views/common/tmpl/menu.php'; ?></div>
+    <div class="clr">&nbsp;</div>
+    <div><?php echo $this->sidebar; ?></div>
 		<!-- Right side -->
  </div>
  <div class="span10">
  <div class="well well-small" style="min-height:120px;">
                        <div class="nav-header">Categories</div>
                          <div class="row-striped">
-                         <br />
- <form action="<?php echo KunenaRoute::_('administrator/index.php?option=com_kunena&view=categories') ?>" method="post" id="adminForm" name="adminForm">
- 		    <input type="hidden" name="task" value="" />
-			<input type="hidden" name="filter_order" value="<?php echo intval ( $this->state->get('list.ordering') ) ?>" />
-			<input type="hidden" name="filter_order_Dir" value="<?php echo $this->escape ($this->state->get('list.direction')) ?>" />
-			<input type="hidden" name="limitstart" value="<?php echo intval ( $this->navigation->limitstart ) ?>" />
-			<input type="hidden" name="boxchecked" value="0" />
-		<?php echo JHTML::_( 'form.token' ); ?>
-			
+                         <br />			
 			<div class="filter-search btn-group pull-left">
 				<label for="filter_search" class="element-invisible"><?php echo 'Search in';?></label>
 				<input type="text" name="filter_search" id="filter_search" placeholder="<?php echo 'Search categorie'; ?>" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo 'Search categorie'; ?>" />
 			</div>
 			<div class="btn-group pull-left">
-				<button class="btn" rel="tooltip" type="submit" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>"><i class="icon-search"></i></button>
+				<button class="btn" rel="tooltip" type="submit" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>" onclick="document.id('filter_search').value='';this.form.submit();"><i class="icon-search"></i></button>
 				<button class="btn" rel="tooltip" type="button" title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>" onclick="document.id('filter_search').value='';this.form.submit();"><i class="icon-remove"></i></button>
 			</div>
 			<div class="btn-group pull-right hidden-phone">
@@ -60,10 +68,11 @@ $changeOrder 	= ($this->state->get('list.ordering') == 'ordering' && $this->stat
 				<label for="sortTable" class="element-invisible"><?php echo JText::_('JGLOBAL_SORT_BY');?></label>
 				<select name="sortTable" id="sortTable" class="input-medium" onchange="Joomla.orderTable()">
 					<option value=""><?php echo JText::_('JGLOBAL_SORT_BY');?></option>
-					<?php echo  $this->navigation->getLimitBox (); ?>
+                    <option value="type" <?php if ($listDirn == 'type') echo 'selected="selected"'; ?>><?php echo JText::_('Type');?></option>
 				</select>
 			</div>
-		<div class="clr">&nbsp;</div>
+		
+        
 		<table class="table table-striped adminlist">
 			<thead>
 				<tr>
@@ -92,7 +101,8 @@ $changeOrder 	= ($this->state->get('list.ordering') == 'ordering' && $this->stat
 			</thead>
 			<tfoot>
 				<tr>
-					<td colspan="7">
+					<td colspan="6">
+                    
 								<div class="pagination"><?php echo $this->navigation->getPagesLinks (); ?></div>
 					</td>
 				</tr>
@@ -164,6 +174,11 @@ $changeOrder 	= ($this->state->get('list.ordering') == 'ordering' && $this->stat
 		</table>
 		
 	</div>
+    <input type="hidden" name="task" value="" />
+		<input type="hidden" name="boxchecked" value="0" />
+		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
+		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
+		<?php echo JHtml::_('form.token'); ?>
 </form>
 	</div>
 
--- administrator/components/com_kunena/views/categories/view.html.php
+++ administrator/components/com_kunena/views/categories/view.html.php
@@ -58,5 +58,92 @@ class KunenaAdminViewCategories extends KunenaView {
 		JToolBarHelper::editList ();
 		JToolBarHelper::deleteList ();
 		//JToolBarHelper::back ( JText::_ ( 'Home' ), 'index.php?option=com_kunena' );
+
+		JHtmlSidebar::setAction('index.php?option=com_kunena&view=categories');
+
+		JHtmlSidebar::addFilter(
+				JText::_('JOPTION_SELECT_PUBLISHED'),
+				'jgrid.published',
+				JHtml::_('select.options', PluginsHelper::publishedOptions(), 'value', 'text', $this->state->get('jgrid.published'), true)
+		);
+
+		JHtmlSidebar::addFilter(
+				JText::_('- Select Type -'),
+				'filter_type',
+				JHtml::_('select.options', PluginsHelper::typeOptions(), 'value', 'text', $this->state->get('filter.type'))
+		);
+
+		JHtmlSidebar::addFilter(
+				JText::_('JOPTION_SELECT_ACCESS'),
+				'filter_access',
+				JHtml::_('select.options', JHtml::_('access.assetgroups'), 'value', 'text', $this->state->get('filter.access'))
+		);
+
+		$this->sidebar = JHtmlSidebar::render();
 	}
+}
+class PluginsHelper
+{
+	public static $extension = 'com_kunena';
+
+	/**
+	 * Configure the Linkbar.
+	 *
+	 * @param	string	The name of the active view.
+	 */
+	public static function addSubmenu($vName)
+	{
+		// No submenu for this component.
+	}
+
+	/**
+	 * Gets a list of the actions that can be performed.
+	 *
+	 * @return	JObject
+	 */
+	public static function getActions()
+	{
+		$user		= JFactory::getUser();
+		$result		= new JObject;
+		$assetName	= 'com_kunena';
+
+		$actions = JAccess::getActions($assetName);
+
+		foreach ($actions as $action) {
+			$result->set($action->name,	$user->authorise($action->name, $assetName));
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Returns an array of standard published state filter options.
+	 *
+	 * @return	string			The HTML code for the select tag
+	 */
+	public static function publishedOptions()
+	{
+		// Build the active state filter options.
+		$options	= array();
+		$options[]	= JHtml::_('select.option', '1', 'JENABLED');
+		$options[]	= JHtml::_('select.option', '0', 'JDISABLED');
+
+		return $options;
+	}
+
+	/**
+	 * Returns an array of standard published state filter options.
+	 *
+	 * @return	string			The HTML code for the select tag
+	 */
+	public static function typeOptions()
+	{
+		// Build the active state filter options.
+		$options	= array();
+		$options[]	= JHtml::_('select.option', '1', 'Sections');
+		$options[]	= JHtml::_('select.option', '0', 'Categories');
+
+		return $options;
+	}
+
 }

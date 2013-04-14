c2fb383d3cc4bdb347e56371fd2dd01cf42a94c9
Matias Griese <matias@kunena.org>
2013-04-14 13:19:01 +0300

    Translate the new installer

--- administrator/components/com_kunena/install/tmpl/default.php
+++ administrator/components/com_kunena/install/tmpl/default.php
@@ -13,23 +13,23 @@ JHtml::_('behavior.framework', true);
 ?>
 <div id="kunena" style="max-width:530px">
 	<div id="kunena-install">
-		<h2>Installing Kunena, please wait...</h2>
+		<h2><?php echo JText::_('COM_KUNENA_INSTALL_PLEASE_WAIT'); ?></h2>
 		<div>
-			<div id="kunena-description">Preparing installation...</div>
+			<div id="kunena-description"><?php echo JText::_('COM_KUNENA_INSTALL_PREPARING'); ?></div>
 			<div class="progress progress-striped active">
 				<div id="kunena-progress" class="bar" style="width: 0%;"></div>
 			</div>
 		</div>
 	</div>
 	<div>
-		<button id="kunena-toggle" class="btn" style="float: left;">Details</button>
+		<button id="kunena-toggle" class="btn" style="float: left;"><?php echo JText::_('COM_KUNENA_INSTALL_DETAILS'); ?></button>
 		<div class="pull-right">
-			<button id="kunena-component" class="btn kunena-close" disabled="disabled">To Kunena</button>
-			<button id="kunena-installer" class="btn kunena-close" disabled="disabled" data-dismiss="modal" aria-hidden="true">Close</button>
+			<button id="kunena-component" class="btn kunena-close" disabled="disabled"><?php echo JText::_('COM_KUNENA_INSTALL_TO_KUNENA'); ?></button>
+			<button id="kunena-installer" class="btn kunena-close" disabled="disabled" data-dismiss="modal" aria-hidden="true"><?php echo JText::_('COM_KUNENA_INSTALL_CLOSE'); ?></button>
 		</div>
 		<div id="kunena-container" class="hidden">
 			<p class="clr clearfix"></p>
-			<div id="kunena-details" class="well well-small"><h4>Installation Details</h4><div>Preparing installation...</div></div>
+			<div id="kunena-details" class="well well-small"><h4><?php echo JText::_('COM_KUNENA_INSTALL_DETAILS'); ?></h4><div><?php echo JText::_('COM_KUNENA_INSTALL_PREPARING'); ?></div></div>
 		</div>
 	</div>
 </div>
@@ -63,20 +63,20 @@ window.kunenainstall = function() {
 					window.kunenainstall();
 					return;
 				} else {
-					kunenaInstall.getElement('h2').set('text', 'Kunena has been successfully installed!')
+					kunenaInstall.getElement('h2').set('text', '<?php echo JText::_('COM_KUNENA_INSTALL_SUCCESS_MESSAGE', true); ?>')
 					kunenaProgress.getParent().removeClass('active');
 				}
 			} else {
-				kunenaInstall.set('html', '<h2>Kunena Installation Failed!</h2><div>Sorry, installation failed with following error:</div><div>' + responseJSON.html + '</div>');
+				kunenaInstall.set('html', '<h2><?php echo JText::_('COM_KUNENA_INSTALL_ERROR_MESSAGE', true); ?></h2><div><?php echo JText::_('COM_KUNENA_INSTALL_ERROR_DETAILS', true); ?></div><div>' + responseJSON.html + '</div>');
 			}
 			$$('.kunena-close').removeProperty('disabled');
 		},
 		onError: function(responseText) {
-			kunenaInstall.set('html', '<h2>Kunena Installation Failed!</h2><div>Sorry, installation failed with following error:</div><div>' + responseText + '</div>');
+			kunenaInstall.set('html', '<h2><?php echo JText::_('COM_KUNENA_INSTALL_ERROR_MESSAGE', true); ?></h2><div><?php echo JText::_('COM_KUNENA_INSTALL_ERROR_DETAILS', true); ?></div><div>' + responseText + '</div>');
 			$$('.kunena-close').removeProperty('disabled');
 		},
 		onFailure: function() {
-			kunenaDescription.set('text', 'Sorry, installation failed on network error!');
+			kunenaDescription.set('text', '<?php echo JText::_('COM_KUNENA_INSTALL_ERROR_FATAL', true); ?>');
 			$$('.kunena-close').removeProperty('disabled');
 		}
 	});

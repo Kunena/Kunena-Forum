e9b0b8d20212132a85288b3c1183a3f80a61b221
Matias Griese <matias@kunena.org>
2013-04-30 21:57:14 +0300

    Misleading language on Recent Topics vs Replied Topics

--- components/com_kunena/template/crypsis/html/topics/default_embed.php
+++ components/com_kunena/template/crypsis/html/topics/default_embed.php
@@ -29,7 +29,7 @@ defined ( '_JEXEC' ) or die ();
 				<table class="table table-striped table-condensed">
 					<?php if (empty ( $this->topics ) && empty ( $this->subcategories )) : ?>
 						<tr class="krow2">
-							<td class="kcol-first"><?php echo JText::_('COM_KUNENA_VIEW_NO_POSTS') ?></td>
+							<td class="kcol-first"><?php echo JText::_('COM_KUNENA_VIEW_NO_TOPICS') ?></td>
 						</tr>
 					<?php else : ?>
 						<?php $this->displayRows (); ?>

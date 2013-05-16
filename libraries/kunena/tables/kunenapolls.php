067a04e53213016084b7eff640495e7dc0550f36
Matias Griese <matias@kunena.org>
2013-05-16 20:42:36 +0300

    Fix #1535 Update Kunena Repository Directory Structure, #1536 Broken GIT install

---
+++ libraries/kunena/tables/kunenapolls.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Kunena Component
+ * @package Kunena.Framework
+ * @subpackage Tables
+ *
+ * @copyright (C) 2008 - 2013 Kunena Team. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.org
+ **/
+defined ( '_JEXEC' ) or die ();
+
+require_once(__DIR__ . '/kunena.php');
+/**
+ * Kunena Polls
+ * Provides access to the #__kunena_polls table
+ */
+class TableKunenaPolls extends KunenaTable {
+	public $id = null;
+	public $title = null;
+	public $threadid = null;
+	public $polltimetolive = null;
+
+	public function __construct($db) {
+		parent::__construct ( '#__kunena_polls', 'id', $db );
+	}
+}

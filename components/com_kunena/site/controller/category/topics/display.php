3e74d83427af77266b5b392565b96fdc58376584
Matias Griese <matias@kunena.org>
2013-09-27 13:28:31 +0300

    Improve last post fetching

--- components/com_kunena/site/controller/category/topics/display.php
+++ components/com_kunena/site/controller/category/topics/display.php
@@ -99,7 +99,7 @@ class ComponentKunenaControllerCategoryTopicsDisplay extends KunenaControllerDis
 			$lastreadlist = KunenaForumTopicHelper::fetchNewStatus($this->topics);
 
 			// Fetch last / new post positions when user can see unapproved or deleted posts
-			if (($lastpostlist || $lastreadlist) && ($this->me->isAdmin() || KunenaAccess::getInstance()->getModeratorStatus())) {
+			if ($lastreadlist || $this->me->isAdmin() || KunenaAccess::getInstance()->getModeratorStatus()) {
 				KunenaForumMessageHelper::loadLocation($lastpostlist + $lastreadlist);
 			}
 		}

1fb03b8e5b1510c102c73debca028085eccdd7bd
Matias Griese <matias@kunena.org>
2013-11-15 22:09:02 +0200

    Administrator: Implement level filter into category manager

--- components/com_kunena/admin/template/joomla30/categories/default.php
+++ components/com_kunena/admin/template/joomla30/categories/default.php
@@ -80,6 +80,13 @@ $filterItem = $this->escape($this->state->get('item.id'));
 						<?php echo JHtml::_('select.options', $this->sortFields, 'value', 'text', $this->listOrdering);?>
 					</select>
 				</div>
+				<div class="btn-group pull-right">
+					<label for="sortTable" class="element-invisible"><?php echo JText::_('JGLOBAL_SORT_BY');?></label>
+					<select name="levellimit" id="sortTable" class="input-medium" onchange="Joomla.orderTable()">
+						<option value=""><?php echo JText::_('JOPTION_SELECT_MAX_LEVELS');?></option>
+						<?php echo JHtml::_('select.options', $this->levelFields, 'value', 'text', $this->filterLevels);?>
+					</select>
+				</div>
 				<div class="clearfix"></div>
 			</div>
 

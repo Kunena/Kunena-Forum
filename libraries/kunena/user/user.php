7cc3306e3dea7de7c8bc6f24482783e58421e6df
Matias Griese <matias@kunena.org>
2014-01-30 14:11:10 +0200

    Improve user profile authorization, return 401 code for login prompt

--- libraries/kunena/user/user.php
+++ libraries/kunena/user/user.php
@@ -170,19 +170,19 @@ class KunenaUser extends JObject {
 			case 'read' :
 				if (!isset($this->registerDate) || (!$user->exists() && !$config->pubprofile))
 				{
-					$exception = new KunenaExceptionAuthorise(JText::_('COM_KUNENA_PROFILEPAGE_NOT_ALLOWED_FOR_GUESTS'), 403);
+					$exception = new KunenaExceptionAuthorise(JText::_('COM_KUNENA_PROFILEPAGE_NOT_ALLOWED_FOR_GUESTS'), $user->exists() ? 403 : 401);
 				}
 				break;
 			case 'edit' :
 				if (!isset($this->registerDate) || !$this->isMyself())
 				{
-					$exception = new KunenaExceptionAuthorise(JText::sprintf('COM_KUNENA_VIEW_USER_EDIT_AUTH_FAILED', $this->getName()), 403);
+					$exception = new KunenaExceptionAuthorise(JText::sprintf('COM_KUNENA_VIEW_USER_EDIT_AUTH_FAILED', $this->getName()), $user->exists() ? 403 : 401);
 				}
 				break;
 			case 'ban' :
 				$banInfo = KunenaUserBan::getInstanceByUserid($this->userid, true);
 				if (!$banInfo->canBan()) {
-					$exception =  new KunenaExceptionAuthorise($banInfo->getError(), 403);
+					$exception =  new KunenaExceptionAuthorise($banInfo->getError(), $user->exists() ? 403 : 401);
 				}
 				break;
 			default :

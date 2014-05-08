2c21f2c1f9f7c04fd8250efb53b78c9d9f269057
xillibit <xillibit@gmail.com>
2014-05-08 10:50:49 +0200

    Implement methods to check if TFA is enabled and secret key is valid

--- libraries/kunena/login.php
+++ libraries/kunena/login.php
@@ -113,4 +113,75 @@ class KunenaLogin {
 		}
 		return null;
 	}
+
+	/**
+	 * Checks if the Two Factor Authentication method is globally enabled and if the
+	 * user has enabled a specific TFA method on their account. Only if both conditions
+	 * are met will this method return true;
+	 *
+	 * @param   integer  $userId  The user ID to check. Skip to use the current user.
+	 *
+	 * @return boolean True if TFA is enabled for this user
+	 */
+	public function isTFAEnabled($userId = null)
+	{
+		if ( !version_compare(JVERSION, '3.2', '>') )
+		{
+			return false;
+		}
+
+		// Include the necessary user model and helper
+		require_once JPATH_ADMINISTRATOR . '/components/com_users/helpers/users.php';
+		require_once JPATH_ADMINISTRATOR . '/components/com_users/models/user.php';
+
+		// Is TFA globally turned off?
+		$twoFactorMethods = UsersHelper::getTwoFactorMethods();
+
+		if (count($twoFactorMethods) <= 1)
+		{
+			return false;
+		}
+
+		// Do we need to get the User ID?
+		if (empty($userId))
+		{
+			$userId = JFactory::getUser()->id;
+		}
+
+		// Has this user turned on TFA on their account?
+		$model = new UsersModelUser;
+		$otpConfig = $model->getOtpConfig($userId);
+
+		return !(empty($otpConfig->method) || ($otpConfig->method == 'none'));
+	}
+
+	/**
+	 * Checks if the provided secret code is a valid two factor authentication
+	 * code for the user whose $userId is provided. If TFA is disabled globally
+	 * or for the specific user you will receive true.
+	 *
+	 * @param   string   $code    The secret code to check
+	 * @param   integer  $userId  The user ID to check. Skip to use the current user.
+	 *
+	 * @return boolean True if you should accept the code
+	 */
+	public function isValidTFA($code, $userId = null)
+	{
+		// Include the necessary user model
+		require_once JPATH_ADMINISTRATOR . '/components/com_users/models/user.php';
+
+		// Do we need to get the User ID?
+		if (empty($userId))
+		{
+			$userId = JFactory::getUser()->id;
+		}
+
+		// Check the secret code
+		$model = new UsersModelUser;
+		$options = array(
+				'warn_if_not_req'	=> false,
+		);
+
+		return $model->isValidSecretKey($userId, $code, $options);
+	}
 }

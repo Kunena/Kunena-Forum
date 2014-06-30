8769017e035745216df49e6e4d257a4ba9e79a2e
Matt Jaworski <matt@ijoomla.com>
2014-06-30 16:55:14 +0800

    New version of community (jomsocial) plugin: initial commit

--- components/com_kunena/admin/install/plugins/plg_kunena_community/private.php
+++ components/com_kunena/admin/install/plugins/plg_kunena_community/private.php
@@ -1,48 +1,50 @@
 <?php
+
 /**
  * Kunena Plugin
  * @package Kunena.Plugins
  * @subpackage Community
  *
- * @copyright (C) 2008 - 2014 Kunena Team. All rights reserved.
- * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @copyright (C) 2013 iJoomla, Inc. - All rights reserved. Forked from Kunena Team
+ * @license GNU General Public License, version 2 (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @author iJoomla.com <webmaster@ijoomla.com>
+ * @url https://www.jomsocial.com/license-agreement
  * @link http://www.kunena.org
- **/
-defined ( '_JEXEC' ) or die ();
+ * The PHP code portions are distributed under the GPL license. If not otherwise stated, all images, manuals, cascading style sheets, and included JavaScript *are NOT GPL, and are released under the IJOOMLA Proprietary Use License v1.0
+ * More info at https://www.jomsocial.com/license-agreement
+ */
+defined('_JEXEC') or die('Restricted access');
 
 class KunenaPrivateCommunity extends KunenaPrivate {
-	protected $loaded = false;
-	protected $params = null;
 
-	public function __construct($params) {
-		$this->params = $params;
-		CFactory::load('libraries', 'messaging');
-	}
+    protected $loaded = false;
+    protected $params = null;
 
-	protected function getOnClick($userid)
-	{
-		if (!$this->loaded) {
-			// PM popup requires JomSocial css to be loaded from selected template
-			$cconfig = CFactory::getConfig ();
-			$document = JFactory::getDocument ();
-			$document->addStyleSheet ( 'components/com_community/assets/window.css' );
-			$document->addStyleSheet ( 'components/com_community/templates/' . $cconfig->get ( 'template' ) . '/css/style.css' );
-			$this->loaded = true;
-		}
-		return ' onclick="'.CMessaging::getPopup($userid).'"';
-	}
+    public function __construct($params) {
+        $this->params = $params;
+        CFactory::load('libraries', 'messaging');
+    }
 
-	protected function getURL($userid)
-	{
-		return "javascript:void(0)";
-	}
+    protected function getOnClick($userid) {
+        if (!$this->loaded) {
+            // PM popup requires JomSocial css to be loaded from selected template
+            $cconfig = CFactory::getConfig();
+            $document = JFactory::getDocument();
+            $document->addStyleSheet('components/com_community/assets/window.css');
+            $document->addStyleSheet('components/com_community/templates/' . $cconfig->get('template') . '/css/style.css');
+            $this->loaded = true;
+        }
+        return ' onclick="' . CMessaging::getPopup($userid) . '"';
+    }
 
-	public function getInboxLink ($text) {
-		if (!$text) $text = JText::_('COM_KUNENA_PMS_INBOX');
-		return '<a href="'. CRoute::_('index.php?option=com_community&view=inbox').'" rel="follow">'. $text.'</a>';
-	}
+    protected function getURL($userid) {
+        return "javascript:void(0)";
+    }
 
-	public function getInboxURL () {
-		return CRoute::_('index.php?option=com_community&view=inbox');
-	}
+    public function getInboxLink($text) {
+        if (!$text)
+            $text = JText::_('COM_KUNENA_PMS_INBOX');
+        return '<a href="' . CRoute::_('index.php?option=com_community&view=inbox') . '" rel="follow">' . $text . '</a>';
+    }
+
 }
--- components/com_kunena/admin/install/plugins/plg_kunena_community/profile.php
+++ components/com_kunena/admin/install/plugins/plg_kunena_community/profile.php
@@ -1,50 +1,57 @@
 <?php
+
 /**
  * Kunena Plugin
  * @package Kunena.Plugins
  * @subpackage Community
  *
- * @copyright (C) 2008 - 2014 Kunena Team. All rights reserved.
- * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @copyright (C) 2013 iJoomla, Inc. - All rights reserved. Forked from Kunena Team
+ * @license GNU General Public License, version 2 (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @author iJoomla.com <webmaster@ijoomla.com>
+ * @url https://www.jomsocial.com/license-agreement
  * @link http://www.kunena.org
- **/
-defined ( '_JEXEC' ) or die ();
+ * The PHP code portions are distributed under the GPL license. If not otherwise stated, all images, manuals, cascading style sheets, and included JavaScript *are NOT GPL, and are released under the IJOOMLA Proprietary Use License v1.0
+ * More info at https://www.jomsocial.com/license-agreement
+ */
+defined('_JEXEC') or die('Restricted access');
 
 class KunenaProfileCommunity extends KunenaProfile {
-	protected $params = null;
 
-	public function __construct($params) {
-		$this->params = $params;
-	}
+    protected $params = null;
 
-	public function getUserListURL($action='', $xhtml = true) {
-		$config = KunenaFactory::getConfig ();
-		$my = JFactory::getUser();
-		if ( $config->userlist_allowed == 1 && $my->id == 0  ) return false;
-		return CRoute::_('index.php?option=com_community&view=search&task=browse', $xhtml);
-	}
+    public function __construct($params) {
+        $this->params = $params;
+    }
 
-	public function getProfileURL($userid, $task='', $xhtml = true) {
-		$do = $task ? '&task='.$task : '';
-		return CRoute::_('index.php?option=com_community&view=profile&userid='.$userid.$do, $xhtml);
-	}
+    public function getUserListURL($action = '', $xhtml = true) {
+        $config = KunenaFactory::getConfig();
+        $my = JFactory::getUser();
+        if ($config->userlist_allowed == 1 && $my->id == 0)
+            return false;
+        return CRoute::_('index.php?option=com_community&view=search&task=browse', $xhtml);
+    }
 
-	public function _getTopHits($limit=0) {
-		$db = JFactory::getDBO ();
-		$query = "SELECT cu.userid AS id, cu.view AS count
-			FROM #__community_users AS cu
-			INNER JOIN #__users AS u ON u.id=cu.userid
-			WHERE cu.view>0
-			ORDER BY cu.view DESC";
-		$db->setQuery ( $query, 0, $limit );
-		$top = (array) $db->loadObjectList ();
-		KunenaError::checkDatabaseError();
-		return $top;
-	}
+    public function getProfileURL($userid, $task = '', $xhtml = true) {
+        if ($userid == 0)
+            return false;
+        // Get CUser object
+        $user = CFactory::getUser($userid);
+        if ($user === null)
+            return false;
+        return CRoute::_('index.php?option=com_community&view=profile&userid=' . $userid, $xhtml);
+    }
 
-	public function showProfile($view, &$params) {}
+    public function _getTopHits($limit = 0) {
+        $db = JFactory::getDBO();
+        $query = "SELECT userid AS id, view AS count FROM #__community_users WHERE view>0 ORDER BY view DESC";
+        $db->setQuery($query, 0, $limit);
+        $top = (array) $db->loadObjectList();
+        KunenaError::checkDatabaseError();
+        return $top;
+    }
 
-	public function getEditProfileURL($userid, $xhtml = true) {
-		return $this->getProfileURL($userid, 'edit', $xhtml);
-	}
+    public function showProfile($view, &$params) {
+
+    }
+
 }

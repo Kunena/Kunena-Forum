08b7098512ee7ccabd936d0365cf208a762597a2
Jelle Kok <schumacher1986@hotmail.com>
2014-10-29 00:50:36 +0100

    Fix auto embed youtube

--- libraries/kunena/bbcode/bbcode.php
+++ libraries/kunena/bbcode/bbcode.php
@@ -1331,7 +1331,7 @@ class KunenaBbcodeLibrary extends BBCodeLibrary {
 			return true;
 		}
 
-		if (KunenaFactory::getConfig()->autolink_disable) {
+		if (KunenaFactory::getConfig()->autoembedyoutube) {
 			$bbcode->autolink_disable--;
 			return '';
 		}

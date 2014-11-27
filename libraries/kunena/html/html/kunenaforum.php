6a791902365b245b351281db23a89047c02132bf
Obsidev <jerome@obsidev.com>
2014-11-27 14:18:20 +0100

    avoid code duplication in categorylist function

--- libraries/kunena/html/html/kunenaforum.php
+++ libraries/kunena/html/html/kunenaforum.php
@@ -38,33 +38,26 @@ abstract class JHtmlKunenaForum {
 		}
 		$channels = array();
 		if (!isset($categories)) {
-			if(is_array($parent)) {
-				$categories = array();
-				$channels = array();
-				foreach($parent as $p) {
-					$category = KunenaForumCategoryHelper::get($p);
-					$children = KunenaForumCategoryHelper::getChildren($p, $levels, $params);
-					if ($params['action'] == 'topic.create') {
-						$channels_local = $category->getChannels();
-						if (empty($children) && !isset($channels_local[$category->id])) $category = KunenaForumCategoryHelper::get();
-						foreach ($channels_local as $id=>$channel) {
-							if (!$id || $category->id == $id || isset($children[$id]) || !$channel->authorise ($action)) unset ($channels_local[$id]);
-						}
+			if(!is_array($parent)) {
+				$parent = array($parent);
+			}
+			$categories = array();
+			$channels = array();
+			foreach($parent as $p) {
+				$channels_local = array();
+				$category = KunenaForumCategoryHelper::get($p);
+				$children = KunenaForumCategoryHelper::getChildren($p, $levels, $params);
+				if ($params['action'] == 'topic.create') {
+					$channels_local = $category->getChannels();
+					if (empty($children) && !isset($channels_local[$category->id])) $category = KunenaForumCategoryHelper::get();
+					foreach ($channels_local as $id=>$channel) {
+						if (!$id || $category->id == $id || isset($children[$id]) || !$channel->authorise ($action)) unset ($channels_local[$id]);
 					}
-					$categories += $category->id > 0 ? array($category->id=>$category)+$children : $children;
+				}
+				$categories += $category->id > 0 ? array($category->id=>$category)+$children : $children;
+				if(!empty($channels_local)) {
 					$channels += $channels_local;
 				}
-			} else {
-				$category = KunenaForumCategoryHelper::get($parent);
-				$children = KunenaForumCategoryHelper::getChildren($parent, $levels, $params);
-				if ($params['action'] == 'topic.create') {
-					$channels = $category->getChannels();
-					if (empty($children) && !isset($channels[$category->id])) $category = KunenaForumCategoryHelper::get();
-					foreach ($channels as $id=>$channel) {
-						if (!$id || $category->id == $id || isset($children[$id]) || !$channel->authorise ($action)) unset ($channels[$id]);
-					}
-				}
-				$categories = $category->id > 0 ? array($category->id=>$category)+$children : $children;
 			}
 			if ($hide_lonely && count($categories)+count($channels) <= 1) return;
 		}

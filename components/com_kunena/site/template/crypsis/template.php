bfffa6b09845f9b0bda26a2d6110d9e1b5cab846
Jelle Kok <schumacher1986@hotmail.com>
2015-04-17 22:28:23 +0200

    [K4.0] Crypsis: Improve stats and whoisonline widgets

--- components/com_kunena/site/template/crypsis/template.php
+++ components/com_kunena/site/template/crypsis/template.php
@@ -111,8 +111,8 @@ EOF;
 		if ($iconcolor) {
 			$styles .= <<<EOF
 		.layout#kunena [class*="category"] i,
-		.layout#kunena #kwho i.icon-big,
-		.layout#kunena#kstats i.icon-big { color: {$iconcolor}; }
+		.layout#kunena #kwho i.icon-users,
+		.layout#kunena#kstats i.icon-bars { color: {$iconcolor}; }
 EOF;
 		}
 

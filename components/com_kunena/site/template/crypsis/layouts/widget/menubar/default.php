c0c0d2f343ac0665134c0c6c81a12e9db7f7aa05
Jelle Kok <schumacher1986@hotmail.com>
2015-05-07 02:08:11 +0200

    Crypsis: Update less IE menu

--- components/com_kunena/site/template/crypsis/layouts/widget/menubar/default.php
+++ components/com_kunena/site/template/crypsis/layouts/widget/menubar/default.php
@@ -18,7 +18,7 @@ defined('_JEXEC') or die;
 		</div>
 		<div class="hidden-desktop">
 			<ul class="nav navbar-nav pull-left">
-				<div><a class="btn btn-link" data-toggle="collapse" data-target=".knav-collapse"> <i class="icon-large icon-list"></i> <b class="caret"></b> </a></div>
+				<div><a class="btn btn-link" data-toggle="collapse" data-target=".knav-collapse"><i class="icon-large icon-list"></i> <b class="caret"></b> </a></div>
 				<div class="knav-collapse"><?php echo $this->subRequest('Widget/Menu'); ?></div>
 			</ul>
 			<?php echo $this->subRequest('Widget/Login'); ?>

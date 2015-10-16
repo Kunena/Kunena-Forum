a9f9f50d37adb610b65788674e2e72b8a4e3bedb
Mark Lee <mark@stackideas.com>
2015-10-16 10:40:29 +0800

    Added EasySocial plugin for Kunena

---
+++ components/com_kunena/admin/install/plugins/plg_kunena_easysocial/avatar.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* @package		EasySocial
+* @copyright	Copyright (C) 2010 - 2014 Stack Ideas Sdn Bhd. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* EasySocial is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+defined( '_JEXEC' ) or die( 'Unauthorized Access' );
+
+
+class KunenaAvatarEasySocial extends KunenaAvatar {
+	protected $params = null;
+
+	public function __construct($params) {
+		$this->params = $params;
+	}
+
+	public function load($userlist)
+	{
+		KUNENA_PROFILER ? KunenaProfiler::instance()->start('function '.__CLASS__.'::'.__FUNCTION__.'()') : null;
+		if (class_exists('CFactory') && method_exists('CFactory', 'loadUsers')) CFactory::loadUsers($userlist);
+		KUNENA_PROFILER ? KunenaProfiler::instance()->stop('function '.__CLASS__.'::'.__FUNCTION__.'()') : null;
+	}
+
+	public function getEditURL()
+	{
+		return FRoute::profile( array( 'layout' => 'edit' ) );
+	}
+
+	protected function _getURL($user, $sizex, $sizey)
+	{
+		$user = KunenaFactory::getUser($user);
+
+		$user 	= FD::user( $user->userid );
+
+		$avatar	= $user->getAvatar( SOCIAL_AVATAR_LARGE );
+
+		return $avatar;
+	}
+}

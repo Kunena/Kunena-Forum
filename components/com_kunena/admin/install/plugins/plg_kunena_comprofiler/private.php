612b95ce5c79b7a499ada995f1cbaf4941d5ab99
Jelle Kok <schumacher1986@hotmail.com>
2015-11-29 12:34:15 +0100

    Fix Pm on CB Plugin

--- components/com_kunena/admin/install/plugins/plg_kunena_comprofiler/private.php
+++ components/com_kunena/admin/install/plugins/plg_kunena_comprofiler/private.php
@@ -5,7 +5,7 @@
  * @package       Kunena.Plugins
  * @subpackage    Comprofiler
  *
- * @copyright (C) 2008 - 2014 Kunena Team. All rights reserved.
+ * @copyright (C) 2008 - 2015 Kunena Team. All rights reserved.
  * @license       http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link          http://www.kunena.org
  **/
@@ -38,10 +38,13 @@ class KunenaPrivateComprofiler extends KunenaPrivate
 
 		outputCbTemplate($_CB_framework->getUi());
 		$resultArray = $_CB_PMS->getPMSlinks($userid, $myid, '', '', 1);
-		$html        = '';
+		$url = 'index.php?option=com_comprofiler&task=userProfile&user=' . $userid . getCBprofileItemid();
+		$html        = '<a href="' . $url . '" title="'.JText::_('COM_KUNENA_VIEW_PMS').'"><span class="kicon-profile kicon-profile-pm" alt="' .JText::_('COM_KUNENA_VIEW_PMS'). '"></span></a>';
+
 		if (count($resultArray) > 0)
 		{
 			$linkItem = '<span class="pm" alt="' . JText::_('COM_KUNENA_VIEW_PMS') . '" />';
+
 			foreach ($resultArray as $res)
 			{
 				if (is_array($res))
@@ -49,6 +52,32 @@ class KunenaPrivateComprofiler extends KunenaPrivate
 					$html .= '<a href="' . cbSef($res["url"]) . '" title="' . getLangDefinition($res["tooltip"]) . '">' . $linkItem . '</a> ';
 				}
 			}
+		}
+
+		return $html;
+	}
+
+	public function shownewIcon($userid, $class='btn btn-small', $icon='icon icon-comments-2')
+	{
+		global $_CB_framework, $_CB_PMS;
+
+		$myid = $_CB_framework->myId();
+
+		// Don't send messages from/to anonymous and to yourself
+		if ($myid == 0 || $userid == 0)
+		{
+			return '';
+		}
+
+		$url = 'index.php?option=com_comprofiler&task=userProfile&user=' . $userid . getCBprofileItemid();
+		$html        = '<a class="' . $class . '" href="' . $url . '" title="'.JText::_('COM_KUNENA_VIEW_PMS').'"><i class="' . $icon . '"></i>' . JText::_('COM_KUNENA_PM_WRITE') . '</a>';
+
+		if ($userid == $myid)
+		{
+			$this->pmCount = $this->getUnreadCount($myid);
+			$text = $this->pmCount ? JText::sprintf('COM_KUNENA_PMS_INBOX_NEW', $this->pmCount) : JText::_('COM_KUNENA_PMS_INBOX');
+			$url = $this->getInboxURL();
+			return '<a class="' . $class . '" href="' . $url . '"><i class="' . $icon . '"></i>' . $text . '</a>';
 		}
 
 		return $html;

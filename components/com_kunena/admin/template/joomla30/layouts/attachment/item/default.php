69d40a97461fa19f2f369ea30f636a56d744707d
xillibit <xillibit@gmail.com>
2016-01-23 19:04:13 +0100

    When Direct Component Access and Protect Attachments is active, Images will not appear in the backend #3635

--- components/com_kunena/admin/template/joomla30/layouts/attachment/item/default.php
+++ components/com_kunena/admin/template/joomla30/layouts/attachment/item/default.php
@@ -12,11 +12,26 @@ defined ( '_JEXEC' ) or die ();
 
 /** @var KunenaAttachment $attachment */
 $attachment = $this->attachment;
+
+if ($attachment->protected)
+{
+	$url_href = $attachment->getUrl();
+}
+else
+{
+	$url_href = JUri::root() . $attachment->getUrl();
+}
+
 ?>
-<a href="<?php echo JUri::root() . $attachment->getUrl(); ?>" title="<?php echo $attachment->getFilename(); ?>">
-	<?php if ($attachment->isImage())
+<a href="<?php echo $url_href; ?>" title="<?php echo $attachment->getFilename(); ?>">
+	<?php
+	if ($attachment->isImage() && !$attachment->protected)
 	{
 		echo '<img src="' . JUri::root() . $attachment->getUrl(true) . ' " height="40" width="40" />';
+	}
+	elseif ($attachment->isImage() && $attachment->protected)
+	{
+		echo '<img src="' . $attachment->getUrl(true) . ' " height="40" width="40" />';
 	}
 	else
 	{

d57095c065e7aca78f6224a4da20a89a734b0716
Jelle Kok <info@jellekoksolutions.nl>
2016-02-26 22:40:38 +0100

    Add utf8mb4 Support

--- components/com_kunena/admin/install/schema.php
+++ components/com_kunena/admin/install/schema.php
@@ -740,12 +740,20 @@ class KunenaModelSchema extends JModelLegacy
 						}
 					}
 					$collation = $this->db->getCollation();
-					if (!strstr($collation, 'utf8'))
+					if (strstr($collation, 'utf8mb4'))
+					{
+						$collation = 'utf8mb4_general_ci';
+						$str .= implode(",\n", $fields) . " ) DEFAULT CHARACTER SET utf8mb4 COLLATE {$collation};";
+					}
+					if (!strstr($collation, 'utf8') && !strstr($collation, 'utf8mb4'))
 					{
 						$collation = 'utf8_general_ci';
 					}
-					$str .= implode(",\n", $fields) . " ) DEFAULT CHARACTER SET utf8 COLLATE {$collation};";
-					break;
+					if (!strstr($collation, 'utf8mb4'))
+					{
+						$str .= implode(",\n", $fields) . " ) DEFAULT CHARACTER SET utf8 COLLATE {$collation};";
+					}
+				break;
 				default:
 					echo("Kunena Installer: Unknown action $tablename.$action on xml file<br />");
 			}

788ba771c7ab598e8b22b71d453b9cf41f7c60ef
Jelle Kok <info@jellekoksolutions.nl>
2016-04-09 22:46:32 +0200

    Fix avatar

--- src/components/com_kunena/template/crypsis/layouts/category/list/row/default.php
+++ src/components/com_kunena/template/crypsis/layouts/category/list/row/default.php
@@ -14,7 +14,7 @@ defined('_JEXEC') or die;
  * @var KunenaForumTopic $topic
  */
 $topic = $this->category->getLastTopic();
-$avatar = $this->config->avataroncat ? $topic->getAuthor()->getAvatarImage('img-thumbnail', 'thumb') : null;
+$avatar = $this->config->avataroncat ? $topic->getAuthor()->getAvatarImage(KunenaFactory::getTemplate()->params->get('avatarType'), 'thumb') : null;
 ?>
 
 <tr>

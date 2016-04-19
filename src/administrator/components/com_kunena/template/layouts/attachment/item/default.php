e886d56a7357091bff4c8e67348577496f18fa72
Jelle Kok <info@jellekoksolutions.nl>
2016-04-20 01:12:14 +0200

    When Direct Component Access and Protect Attachments is active, Images #3647

--- src/administrator/components/com_kunena/template/layouts/attachment/item/default.php
+++ src/administrator/components/com_kunena/template/layouts/attachment/item/default.php
@@ -11,17 +11,33 @@
 defined('_JEXEC') or die();
 
 // @var KunenaAttachment $attachment
-
 $attachment = $this->attachment;
-?>
-<a href="<?php echo JUri::root() . $attachment->getUrl(); ?>" title="<?php echo $attachment->getFilename(); ?>">
-	<?php if ($attachment->isImage())
+$config = KunenaFactory::getConfig();
+
+if ($config->access_component)
+{
+	?>
+	<a href="<?php echo $attachment->getUrl(); ?>" title="<?php echo $attachment->getFilename(); ?>">
+		<?php if ($attachment->isImage())
+		{
+			echo '<img src="' . JUri::root() . $attachment->getUrl(true) . ' " height="40" width="40" />';
+		}
+		else
+		{
+			echo '<i class="icon-flag-2 icon-big"></i>';
+		}
+		?>
+	</a>
+	<?php
+}
+else
+{
+	if ($attachment->isImage())
 	{
-		echo '<img src="' . JUri::root() . $attachment->getUrl(true) . ' " height="40" width="40" />';
+		echo '<i class="icon-picture icon-big"></i>';
 	}
 	else
 	{
 		echo '<i class="icon-flag-2 icon-big"></i>';
 	}
-	?>
-</a>
+} ?>

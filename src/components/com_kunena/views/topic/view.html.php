2f049cd98690bc20f0f15ac61a2c2dccc178da84
xillibit <xillibit@gmail.com>
2016-04-19 18:57:14 +0200

    Need to keep view.html.php under /views/topic/ to be overwrote on update

---
+++ src/components/com_kunena/views/topic/view.html.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Kunena Component
+ *
+ * @package       Kunena.Site
+ * @subpackage    Views
+ *
+ * @copyright (C) 2008 - 2016 Kunena Team. All rights reserved.
+ * @license       http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link          https://www.kunena.org
+ **/
+defined('_JEXEC') or die();
+/**
+ * Topic View
+ */
+class KunenaViewTopic extends KunenaView
+{
+	
+}

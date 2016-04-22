650fcba3027c0d777c36e8536ba7202cb666b226
Jelle Kok <info@jellekoksolutions.nl>
2016-04-22 20:29:25 +0200

    Fix codestyle

--- src/components/com_kunena/template/crypsis/layouts/page/default.php
+++ src/components/com_kunena/template/crypsis/layouts/page/default.php
@@ -45,18 +45,17 @@ $config = KunenaFactory::getTemplate()->params;
 
 	if ($config->get('displayBreadcrumb'))
 	{
-	echo $this->subLayout('Widget/Breadcrumb')->set('breadcrumb', $this->breadcrumb);
+		echo $this->subLayout('Widget/Breadcrumb')->set('breadcrumb', $this->breadcrumb);
 	}
-
 
 	if ($config->get('displayModule'))
 	{
-	echo $this->subLayout('Widget/Module')->set('position', 'kunena_bottom');
+		echo $this->subLayout('Widget/Module')->set('position', 'kunena_bottom');
 	}
 
 	if ($config->get('displayFooter'))
 	{
-	echo $this->subLayout('Widget/Footer');
+		echo $this->subLayout('Widget/Footer');
 	}
 ?>
 </div>

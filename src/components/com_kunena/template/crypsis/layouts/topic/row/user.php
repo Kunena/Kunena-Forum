cedfd3f2bcd13fa558b4758005c7c7824dcf0c0c
Jelle Kok <schumacher1986@hotmail.com>
2016-06-03 21:07:22 +0200

    Stars for voting are messy #3974 (#3983)

--- src/components/com_kunena/template/crypsis/layouts/topic/row/user.php
+++ src/components/com_kunena/template/crypsis/layouts/topic/row/user.php
@@ -80,7 +80,7 @@ if (!empty($this->spacing)) : ?>
 				echo $this->getTopicLink($topic, null, null, null, 'hasTooltip topictitle');
 			}
 			?>
-			<div class="pull-right"><?php echo $this->subLayout('Widget/Rating')->set('config', $config)->set('category', $category)->set('topic', $this->topic)->setLayout('default'); ?></div>
+			<?php echo $this->subLayout('Widget/Rating')->set('config', $config)->set('category', $category)->set('topic', $this->topic)->setLayout('default'); ?>
 		</div>
 
 

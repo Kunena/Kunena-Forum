35d3f620140661f6cbefd201015abbe6325a7639
Jelle Kok <info@jellekoksolutions.nl>
2016-06-10 02:58:30 +0200

    Disable team member label

--- src/components/com_kunena/template/crypsis/layouts/user/profile/default.php
+++ src/components/com_kunena/template/crypsis/layouts/user/profile/default.php
@@ -42,9 +42,9 @@ if ($show)
 		<?php if (isset($this->topic_starter) && $this->topic_starter) : ?>
 				<span class="topic-starter <?php if (KunenaFactory::getTemplate()->params->get('avatarType') == 'img-circle') {echo 'topic-starter-circle';};?>"><?php echo JText::_('COM_KUNENA_TOPIC_AUTHOR') ?></span>
 		<?php endif;?>
-		<?php if ($user->isModerator()) : ?>
-			<span class="<?php if (KunenaFactory::getTemplate()->params->get('avatarType') == 'img-circle') {echo 'topic-moderator-circle';};?> topic-moderator"><?php echo JText::_('COM_KUNENA_TEAM_MEMBER') ?></span>
-		<?php endif;?>
+		<?php /*if ($user->isModerator()) : */?><!--
+			<span class="<?php /*if (KunenaFactory::getTemplate()->params->get('avatarType') == 'img-circle') {echo 'topic-moderator-circle';};*/?> topic-moderator"><?php /*echo JText::_('COM_KUNENA_TEAM_MEMBER') */?></span>
+		--><?php /*endif;*/?>
 	</li>
 	<?php endif; ?>
 

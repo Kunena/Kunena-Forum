023bd000978815557fbe36eb387ffcda603791c1
Jelle Kok <schumacher1986@hotmail.com>
2016-06-15 23:20:42 +0200

    Add option to see full actions (#4029)

--- src/components/com_kunena/template/crypsis/layouts/message/item/actions/default.php
+++ src/components/com_kunena/template/crypsis/layouts/message/item/actions/default.php
@@ -11,53 +11,40 @@
 defined('_JEXEC') or die;
 
 $config = KunenaConfig::getInstance();
+$this->ktemplate = KunenaFactory::getTemplate();
+$fullactions = $this->ktemplate->params->get('fullactions');
 ?>
 
+<?php if (!$fullactions) : ?>
+
 <?php if (empty($this->message_closed)) : ?>
-<div class="btn-toolbar btn-marging kmessagepadding">
+<div class="kmessagepadding">
 	<?php if($this->quickreply) : ?>
 		<a href="#kreply<?php echo $this->message->displayField('id'); ?>_form" role="button" class="btn openmodal"
 		   data-toggle="modal" rel="nofollow"><i class="icon-undo"></i> <?php echo JText::_('COM_KUNENA_MESSAGE_ACTIONS_LABEL_QUICK_REPLY'); ?>
 		</a>
 	<?php endif; ?>
-	<div class="btn-group">
-		<button class="btn" data-toggle="dropdown">
-			<i class="icon-pencil"></i> <?php echo JText::_('COM_KUNENA_MESSAGE_ACTIONS_LABEL_ACTION'); ?>
-		</button>
-		<button class="btn dropdown-toggle" data-toggle="dropdown">
-			<span class="caret"></span>
-		</button>
-		<ul class="dropdown-menu">
-			<li><?php echo $this->messageButtons->get('reply'); ?></li>
-			<li><?php echo $this->messageButtons->get('quote'); ?></li>
-			<li><?php echo $this->messageButtons->get('edit'); ?></li>
-			<?php if ($config->userdeletetmessage > 0) : ?>
-			<li><?php echo $this->messageButtons->get('delete'); ?></li>
-			<?php endif; ?>
-		</ul>
-	</div>
+
+		<?php echo $this->messageButtons->get('reply'); ?>
+		<?php echo $this->messageButtons->get('quote'); ?>
+		<?php echo $this->messageButtons->get('edit'); ?>
+		<?php if ($config->userdeletetmessage > 0) : ?>
+			<br />
+			<?php echo $this->messageButtons->get('delete'); ?>
+		<?php endif; ?>
+		<?php echo $this->messageButtons->get('thankyou'); ?>
+		<?php echo $this->messageButtons->get('unthankyou'); ?>
 
 	<?php if ($this->messageButtons->get('moderate')) : ?>
-	<div class="btn-group">
-		<button class="btn" data-toggle="dropdown">
-			<i class="icon-shuffle"></i> <?php echo JText::_('COM_KUNENA_MESSAGE_ACTIONS_LABEL_MODERATE'); ?>
-		</button>
-		<button class="btn dropdown-toggle" data-toggle="dropdown">
-			<span class="caret"></span>
-		</button>
-		<ul class="dropdown-menu">
-			<li><?php echo $this->messageButtons->get('moderate'); ?></li>
-			<li><?php echo $this->messageButtons->get('delete'); ?></li>
-			<li><?php echo $this->messageButtons->get('undelete'); ?></li>
-			<li><?php echo $this->messageButtons->get('permdelete'); ?></li>
-			<li><?php echo $this->messageButtons->get('publish'); ?></li>
-			<li><?php echo $this->messageButtons->get('spam'); ?></li>
-		</ul>
-	</div>
+		<br />
+		<br />
+			<?php echo $this->messageButtons->get('moderate'); ?>
+			<?php echo $this->messageButtons->get('delete'); ?>
+			<?php echo $this->messageButtons->get('undelete'); ?>
+			<?php echo $this->messageButtons->get('permdelete'); ?>
+			<?php echo $this->messageButtons->get('publish'); ?>
+			<?php echo $this->messageButtons->get('spam'); ?>
 	<?php endif; ?>
-
-	<?php echo $this->messageButtons->get('thankyou'); ?>
-	<?php echo $this->messageButtons->get('unthankyou'); ?>
 </div>
 
 <?php else : ?>
@@ -66,3 +53,61 @@ $config = KunenaConfig::getInstance();
 	<?php echo $this->message_closed; ?>
 </div>
 <?php endif;
+	 endif; ?>
+
+<?php if ($fullactions) : ?>
+
+	<?php if (empty($this->message_closed)) : ?>
+		<div class="btn-toolbar btn-marging kmessagepadding">
+			<?php if($this->quickreply) : ?>
+				<a href="#kreply<?php echo $this->message->displayField('id'); ?>_form" role="button" class="btn openmodal"
+					data-toggle="modal" rel="nofollow"><i class="icon-undo"></i> <?php echo JText::_('COM_KUNENA_MESSAGE_ACTIONS_LABEL_QUICK_REPLY'); ?>
+				</a>
+			<?php endif; ?>
+			<div class="btn-group">
+				<button class="btn" data-toggle="dropdown">
+					<i class="icon-pencil"></i> <?php echo JText::_('COM_KUNENA_MESSAGE_ACTIONS_LABEL_ACTION'); ?>
+				</button>
+				<button class="btn dropdown-toggle" data-toggle="dropdown">
+					<span class="caret"></span>
+				</button>
+				<ul class="dropdown-menu">
+					<li><?php echo $this->messageButtons->get('reply'); ?></li>
+					<li><?php echo $this->messageButtons->get('quote'); ?></li>
+					<li><?php echo $this->messageButtons->get('edit'); ?></li>
+					<?php if ($config->userdeletetmessage > 0) : ?>
+						<li><?php echo $this->messageButtons->get('delete'); ?></li>
+					<?php endif; ?>
+				</ul>
+			</div>
+
+			<?php if ($this->messageButtons->get('moderate')) : ?>
+				<div class="btn-group">
+					<button class="btn" data-toggle="dropdown">
+						<i class="icon-shuffle"></i> <?php echo JText::_('COM_KUNENA_MESSAGE_ACTIONS_LABEL_MODERATE'); ?>
+					</button>
+					<button class="btn dropdown-toggle" data-toggle="dropdown">
+						<span class="caret"></span>
+					</button>
+					<ul class="dropdown-menu">
+						<li><?php echo $this->messageButtons->get('moderate'); ?></li>
+						<li><?php echo $this->messageButtons->get('delete'); ?></li>
+						<li><?php echo $this->messageButtons->get('undelete'); ?></li>
+						<li><?php echo $this->messageButtons->get('permdelete'); ?></li>
+						<li><?php echo $this->messageButtons->get('publish'); ?></li>
+						<li><?php echo $this->messageButtons->get('spam'); ?></li>
+					</ul>
+				</div>
+			<?php endif; ?>
+
+			<?php echo $this->messageButtons->get('thankyou'); ?>
+			<?php echo $this->messageButtons->get('unthankyou'); ?>
+		</div>
+
+	<?php else : ?>
+
+		<div class="kreplymessage">
+			<?php echo $this->message_closed; ?>
+		</div>
+	<?php endif;
+endif; ?>

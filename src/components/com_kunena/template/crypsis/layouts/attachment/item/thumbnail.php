af0a86ae83032ea7822a5c923293fd43774ae2df
Jelle Kok <info@jellekoksolutions.nl>
2016-07-08 02:10:49 +0200

    With "Adds Suffix to URL = Yes" no attachments and images are available in the forum #4017

--- src/components/com_kunena/template/crypsis/layouts/attachment/item/thumbnail.php
+++ src/components/com_kunena/template/crypsis/layouts/attachment/item/thumbnail.php
@@ -21,19 +21,29 @@ $config = KunenaConfig::getInstance();
 
 $attributesLink = $attachment->isImage() && $config->lightbox ? ' class="fancybox-button" rel="fancybox-button"' : '';
 $attributesImg  = ' style="max-height: ' . (int) $config->thumbheight . 'px;"';
+$name = preg_replace('/.html/', '', $attachment->getUrl());
+
+if (JApplicationCms::getInstance('site')->get('sef_suffix'))
+{
+	$name = preg_replace('/.html/', '', $attachment->getUrl());
+}
+else
+{
+	$name = $attachment->getUrl();
+}
 
 if ($attachment->isImage())
-{ 
+{
 	?>
-	<a href="<?php echo $attachment->getUrl(); ?>" title="<?php echo $attachment->getShortName($config->attach_start, $config->attach_end); ?>"<?php echo $attributesLink; ?>>
-		<img src="<?php echo $attachment->getUrl(); ?>"<?php echo $attributesImg; ?> width="<?php echo $config->thumbheight ;?>" height="<?php echo $config->thumbheight ;?>" alt=""  />
+	<a href="<?php echo $name; ?>" title="<?php echo $attachment->getShortName($config->attach_start, $config->attach_end); ?>"<?php echo $attributesLink; ?>>
+		<img src="<?php echo $name; ?>"<?php echo $attributesImg; ?> width="<?php echo $config->thumbheight ;?>" height="<?php echo $config->thumbheight ;?>" alt=""  />
 	</a>
 <?php
 }
 else
 {
 	?>
-	<a href="<?php echo $attachment->getUrl(); ?>" title="<?php echo $attachment->getShortName($config->attach_start, $config->attach_end); ?>"<?php echo $attributesLink; ?>>
+	<a href="<?php echo $name; ?>" title="<?php echo $attachment->getShortName($config->attach_start, $config->attach_end); ?>"<?php echo $attributesLink; ?>>
 		<i class="large-kicon icon-file"></i>
 	</a>
 <?php
--- src/components/com_kunena/template/crypsis/layouts/message/item/default.php
+++ src/components/com_kunena/template/crypsis/layouts/message/item/default.php
@@ -52,42 +52,6 @@ $list = array();
 			endif;?>
 		</p>
 	</div>
-	<?php if (!empty($attachments)) : ?>
-		<div class="kattach">
-			<h5> <?php echo JText::_('COM_KUNENA_ATTACHMENTS'); ?> </h5>
-			<ul class="thumbnails">
-				<?php foreach ($attachments as $attachment) : ?>
-					<li class="span3 center">
-						<div class="thumbnail"> <?php echo $attachment->getLayout()->render('thumbnail'); ?> <?php echo $attachment->getLayout()->render('textlink'); ?> </div>
-					</li>
-				<?php endforeach; ?>
-			</ul>
-		</div>
-	<?php elseif ($attachs->total > 0  && !$this->me->exists()) :
-		if ($attachs->image > 0 && !$this->config->showimgforguest)
-		{
-			if ($attachs->image > 1)
-			{
-				echo KunenaLayout::factory('BBCode/Image')->set('title', JText::_('COM_KUNENA_SHOWIMGFORGUEST_HIDEIMG_MULTIPLES'))->setLayout('unauthorised');
-			}
-			else
-			{
-				echo KunenaLayout::factory('BBCode/Image')->set('title', JText::_('COM_KUNENA_SHOWIMGFORGUEST_HIDEIMG_SIMPLE'))->setLayout('unauthorised');
-			}
-		}
-
-		if ($attachs->file > 0 && !$this->config->showfileforguest)
-		{
-			if ($attachs->file > 1)
-			{
-				echo KunenaLayout::factory('BBCode/Image')->set('title', JText::_('COM_KUNENA_SHOWIMGFORGUEST_HIDEFILE_MULTIPLES'))->setLayout('unauthorised');
-			}
-			else
-			{
-				echo KunenaLayout::factory('BBCode/Image')->set('title', JText::_('COM_KUNENA_SHOWIMGFORGUEST_HIDEFILE_SIMPLE'))->setLayout('unauthorised');
-			}
-		}
-	endif; ?>
 	<?php if ($signature) : ?>
 		<div class="ksig">
 			<hr>
@@ -105,7 +69,42 @@ $list = array();
 		<?php endif; ?>
 	<?php endif; ?>
 </div>
+<?php if (!empty($attachments)) : ?>
+	<div class="kattach">
+		<h5> <?php echo JText::_('COM_KUNENA_ATTACHMENTS'); ?> </h5>
+		<ul class="thumbnails">
+			<?php foreach ($attachments as $attachment) : ?>
+				<li class="span3 center">
+					<div class="thumbnail"> <?php echo $attachment->getLayout()->render('thumbnail'); ?> <?php echo $attachment->getLayout()->render('textlink'); ?> </div>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
+<?php elseif ($attachs->total > 0  && !$this->me->exists()) :
+	if ($attachs->image > 0 && !$this->config->showimgforguest)
+	{
+		if ($attachs->image > 1)
+		{
+			echo KunenaLayout::factory('BBCode/Image')->set('title', JText::_('COM_KUNENA_SHOWIMGFORGUEST_HIDEIMG_MULTIPLES'))->setLayout('unauthorised');
+		}
+		else
+		{
+			echo KunenaLayout::factory('BBCode/Image')->set('title', JText::_('COM_KUNENA_SHOWIMGFORGUEST_HIDEIMG_SIMPLE'))->setLayout('unauthorised');
+		}
+	}
 
+	if ($attachs->file > 0 && !$this->config->showfileforguest)
+	{
+		if ($attachs->file > 1)
+		{
+			echo KunenaLayout::factory('BBCode/Image')->set('title', JText::_('COM_KUNENA_SHOWIMGFORGUEST_HIDEFILE_MULTIPLES'))->setLayout('unauthorised');
+		}
+		else
+		{
+			echo KunenaLayout::factory('BBCode/Image')->set('title', JText::_('COM_KUNENA_SHOWIMGFORGUEST_HIDEFILE_SIMPLE'))->setLayout('unauthorised');
+		}
+	}
+endif; ?>
 <?php if ($message->modified_by && $this->config->editmarkup) :
 $dateshown = $datehover = '';
 if ($message->modified_time) {

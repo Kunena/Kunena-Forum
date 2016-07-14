3528ce63a538eab7e503bcfa68c2269dcc9e094c
Florian DAL FITTO <xillibit@gmail.com>
2016-07-14 16:57:19 +0200

    Bbcode size tools are not translated #4069 (#4128)

--- src/components/com_kunena/template/crypsisb3/assets/js/markitup.editor.js
+++ src/components/com_kunena/template/crypsisb3/assets/js/markitup.editor.js
@@ -1,23 +1,23 @@
 bbcodeSettings = {
 		previewParserPath:	'',
 		markupSet: [{className: 'boldbutton',name: 'Bold',key: 'B',openWith: '[b]',closeWith: '[/b]'},{className: 'italicbutton',name: 'Italic',key: 'I',openWith: '[i]',closeWith: '[/i]'},{className: 'underlinebutton',name: 'Underline',key: 'U',openWith: '[u]',closeWith: '[/u]'},{className: 'strokebutton',name: 'Stroke',key: 'T',openWith: '[strike]',closeWith: '[/strike]'},{className: 'subscriptbutton',name: 'Subscript',key: 'T',openWith: '[sub]',closeWith: '[/sub]'},{className: 'supscriptbutton',name: 'Supscript',key: 'T',openWith: '[sup]',closeWith: '[/sup]'},{className: 'sizebutton', name:'Size', key:'S', openWith:'[size=[![Text size]!]]', closeWith:'[/size]',	dropMenu :[
-						{name:'Very very small', openWith:'[size=1]', 	closeWith:'[/size]' },
-						{name:'Very Small', openWith:'[size=2]', 	closeWith:'[/size]' },
-						{name:'Small', openWith:'[size=3]', closeWith:'[/size]' },
-						{name:'Normal', openWith:'[size=4]', closeWith:'[/size]' },
-						{name:'Big', openWith:'[size=5]', closeWith:'[/size]' },
-						{name:'Super Bigger', openWith:'[size=6]', closeWith:'[/size]' }
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_SIZE_VERY_VERY_SMALL'), openWith:'[size=1]', 	closeWith:'[/size]' },
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_SIZE_VERY_SMALL'), openWith:'[size=2]', 	closeWith:'[/size]' },
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_SIZE_SMALL'), openWith:'[size=3]', closeWith:'[/size]' },
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_SIZE_NORMAL'), openWith:'[size=4]', closeWith:'[/size]' },
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_SIZE_BIG'), openWith:'[size=5]', closeWith:'[/size]' },
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_SIZE_SUPER_BIGGER'), openWith:'[size=6]', closeWith:'[/size]' }
 						]},{className: 'colors', name:'Colors', key:'', openWith:'[color=[![Color]!]]', closeWith:'[/color]',dropMenu: [
-						{name:'Black',	openWith:'[color=black]', 	closeWith:'[/color]', className:'col1-1' },
-						{name:'Orange',	openWith:'[color=orange]', 	closeWith:'[/color]', className:'col1-2' },
-						{name:'Red', 	openWith:'[color=red]', 	closeWith:'[/color]', className:'col1-3' },
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_COLOR_BLACK'),	openWith:'[color=black]', 	closeWith:'[/color]', className:'col1-1' },
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_COLOR_ORANGE'),	openWith:'[color=orange]', 	closeWith:'[/color]', className:'col1-2' },
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_COLOR_RED'), 	openWith:'[color=red]', 	closeWith:'[/color]', className:'col1-3' },
 
-						{name:'Blue', 	openWith:'[color=blue]', 	closeWith:'[/color]', className:'col2-1' },
-						{name:'Purple', openWith:'[color=purple]', 	closeWith:'[/color]', className:'col2-2' },
-						{name:'Green', 	openWith:'[color=green]', 	closeWith:'[/color]', className:'col2-3' },
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_COLOR_BLUE'), 	openWith:'[color=blue]', 	closeWith:'[/color]', className:'col2-1' },
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_COLOR_PURPLE'), openWith:'[color=purple]', 	closeWith:'[/color]', className:'col2-2' },
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_COLOR_GREEN'), 	openWith:'[color=green]', 	closeWith:'[/color]', className:'col2-3' },
 
-						{name:'White', 	openWith:'[color=white]', 	closeWith:'[/color]', className:'col3-1' },
-						{name:'Gray', 	openWith:'[color=gray]', 	closeWith:'[/color]', className:'col3-2' }
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_COLOR_WHITE'), 	openWith:'[color=white]', 	closeWith:'[/color]', className:'col3-1' },
+						{name: Joomla.JText._('COM_KUNENA_EDITOR_COLOR_GRAY'), 	openWith:'[color=gray]', 	closeWith:'[/color]', className:'col3-2' }
 						]},{separator:'|' },{className: 'bulletedlistbutton',name: 'Unordered List',openWith: '[ul]\n  [li]',closeWith: '[/li]\n  [li][/li]\n[/ul]'},{className: 'numericlistbutton',name: 'Ordered List',openWith: '[ol]\n  [li]',closeWith: '[/li]\n  [li][/li]\n[/ol]'},{className: 'listitembutton',name: 'Li',openWith: '\n  [li]',closeWith: '[/li]'},{className: 'hrbutton',name: 'HR',openWith: '[hr]'},{className: 'alignleftbutton',name: 'Left',openWith: '[left]',closeWith: '[/left]'},{className: 'centerbutton',name: 'Center',openWith: '[center]',closeWith: '[/center]'},{className: 'alignrightbutton',name: 'Right',openWith: '[right]',closeWith: '[/right]'},{separator:'|' },{className: 'quotebutton',name: 'Quote',openWith: '[quote]',closeWith: '[/quote]'},{className: 'codesimplebutton',name: 'Code',openWith: '[code]',closeWith: '[/code]'},{name:'code', className: 'codemodalboxbutton', beforeInsert:function() {
 						jQuery('#code-modal-submit').click(function(event) {
 							event.preventDefault();

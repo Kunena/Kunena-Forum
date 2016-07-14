972e59a1e88172752306085c11942452b14ee5df
Florian DAL FITTO <xillibit@gmail.com>
2016-07-14 23:27:47 +0200

    Error in Duplicated Alias for Categories (convert js to jQuery) #3999 (#4129)

--- src/administrator/components/com_kunena/template/categories/edit.php
+++ src/administrator/components/com_kunena/template/categories/edit.php
@@ -16,41 +16,43 @@ JHtml::_('behavior.tooltip');
 JHtml::_('behavior.multiselect');
 JHtml::_('dropdown.init');
 JHtml::_('behavior.tabstate');
+
+JText::script('COM_KUNENA_CATEGORIES_ERROR_CHOOSE_ANOTHER_ALIAS');
+
 JFactory::getDocument()->addScriptDeclaration('
-window.addEvent("domready", function(){
-var box = $("aliascheck");
-var cun = document.id("jform_aliases");
-$(cun).addEvent("blur", function(){
-    if ($(cun).value.length > 0){
-        var url = "index.php?option=com_kunena&view=categories&format=raw&task=chkAliases&alias="+$(cun).value;
-        box.style.display = "inline-block";
-        box.set("html", " Check in progress...");
-        var a = new Request.JSON({
-            url: url,
-            onComplete: function(response){
-                 box.set("html", response);
-                 if (response){
-                   var el = $(box);
-                   (function(){
-                   el.className = "invalid icon icon-remove";
-                   el.set("html", " Please choose other aliasname");
-                   cun.className = "inputbox invalid-border";
-                 }).delay(100);
-                 }
-                 else {
-                 var el = $(box);
-                 (function(){
-                   el.className = "valid icon icon-ok";
-                   el.set("html", "");
-                   cun.className = "inputbox";
-                 }).delay(100);
-                 }
-            }
-        });
-        a.get();
-      }
-    });
-   });
+jQuery(document).ready(function($) {
+	var input_alias = $("#jform_aliases");
+	var box = $("#aliascheck");
+
+	input_alias.on(\'input\', function() {
+	$.ajax({
+		dataType: "json",
+		url: "index.php?option=com_kunena&view=categories&format=raw&layout=chkAliases&alias="+input_alias.val(),
+		}).done(function(response) {
+			if (!response.msg){
+				input_alias.addClass("inputbox invalid-border");
+
+				if (box.length)
+				{
+					box.removeClass("valid icon-ok");			
+					box.addClass("invalid icon icon-remove");
+					box.html(Joomla.JText._(\'COM_KUNENA_CATEGORIES_ERROR_CHOOSE_ANOTHER_ALIAS\'));
+				}
+			}
+			else 
+			{
+				input_alias.addClass("inputbox"); 
+				input_alias.removeClass("invalid-border");
+
+				if (box.length)
+				{
+					box.addClass("valid icon icon-ok");
+					box.html("");
+				}
+			} 
+		});
+	});
+});
 ');
 ?>
 

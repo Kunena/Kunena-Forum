c58257d4475e150d9758177fc8f0a732e1f18cc1
xillibit <xillibit@gmail.com>
2016-08-13 20:41:49 +0200

    Move correctImageOrientation into KunenaImage class

--- src/libraries/kunena/upload/upload.php
+++ src/libraries/kunena/upload/upload.php
@@ -20,95 +20,7 @@ class KunenaUpload
 	protected $validExtensions = array();
 
 	protected $filename;
-
-	/**
-	 * Correct Image Orientation
-	 *
-	 * @since  K5.0
-	 *
-	 * @param   $filename
-	 */
-	function correctImageOrientation($filename)
-	{
-		$testForJpg = @getimagesize($filename);
-
-		if ($testForJpg[2] == 2)
-		{
-			if (function_exists('exif_read_data'))
-			{
-				$deg  = 0;
-				$exif = @exif_read_data($filename);
-				$flip = '';
-				$img  = '';
-
-				if ($exif && isset($exif['Orientation']))
-				{
-					$orientation = $exif['Orientation'];
-
-					if ($orientation != 1)
-					{
-						$img = @imagecreatefromjpeg($filename);
-
-						switch ($orientation)
-						{
-							case 1: // nothing
-									$deg  = 0;
-									$flip = 0;
-									break;
-							case 2: // horizontal flip
-									$deg  = 0;
-									$flip = 1;
-								break;
-							case 3: // 180 rotate left
-									$deg  = 180;
-									$flip = 0;
-									break;
-							case 4: // vertical flip
-									$deg  = 0;
-									$flip = 2;
-									break;
-							case 5: // vertical flip + 90 rotate
-									$deg  = 90;
-									$flip = 2;
-									break;
-							case 6: // 270 rotate left
-									$deg  = 270;
-									$flip = 0;
-									break;
-							case 7: // horizontal flip + 90 rotate
-									$deg  = 90;
-									$flip = 1;
-									break;
-							case 8: // 90 rotate left
-									$deg  = 90;
-									$flip = 0;
-									break;
-						}
-					}
-				}
-
-				if ($deg > 0)
-				{
-					$img = @imagerotate($img, $deg, 0);
-				}
-
-				if ($flip != 0)
-				{
-					if ($flip == 1)
-					{
-						@imageflip($img, IMG_FLIP_HORIZONTAL);
-					}
-					else
-					{
-						@imageflip($img, IMG_FLIP_VERTICAL);
-					}
-				}
-
-				@imagejpeg($img, $filename, 95);
-			}
-		}
-	}
-
+	
 	/**
 	 * Get new instance of upload class.
 	 *
@@ -436,7 +348,7 @@ class KunenaUpload
 				// Get filename from stream
 				$meta_data = stream_get_meta_data($out);
 				$filename  = $meta_data['uri'];
-				$this->correctImageOrientation($filename);
+				KunenaImage::correctImageOrientation($filename);
 			}
 		}
 		catch (Exception $exception)
@@ -740,8 +652,8 @@ class KunenaUpload
 				}
 			}
 		}
-
-		$this->correctImageOrientation($file->tmp_name);
+		
+		KunenaImage::correctImageOrientation($file->tmp_name);
 
 		if (!KunenaFile::copy($file->tmp_name, $file->destination))
 		{

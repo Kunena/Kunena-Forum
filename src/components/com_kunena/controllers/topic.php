dccf84ba4f13073a952641f70594791e0513c4f0
xillibit <xillibit@gmail.com>
2016-08-17 10:48:07 +0200

    Thumbnail image is broken when protect attachments = yes #4329

--- src/components/com_kunena/controllers/topic.php
+++ src/components/com_kunena/controllers/topic.php
@@ -65,7 +65,16 @@ class KunenaControllerTopic extends KunenaController
 			$object->folder  = $attach->folder;
 			$object->caption = $attach->caption;
 			$object->type    = $attach->filetype;
-			$object->path    = JURI::root(true) . '/' . $attach->getUrl();
+			
+			if ($attach->protected)
+			{
+				$object->path    = $attach->getUrl();
+			}
+			else
+			{
+				$object->path    = JURI::root(true) . '/' . $attach->getUrl();
+			}
+			
 			$object->image   = $attach->isImage();
 			$list['files'][] = $object;
 		}

d477232214b263bd192f36ae027211f9e121a1c4
xillibit <xillibit@gmail.com>
2016-08-20 17:09:47 +0200

    Name field is optional when reply a topic (Guests) #4351 (change in crypsis b3)

--- src/components/com_kunena/template/crypsisb3/layouts/message/edit/quickreply.php
+++ src/components/com_kunena/template/crypsisb3/layouts/message/edit/quickreply.php
@@ -106,7 +106,7 @@ $quick = $template->params->get('quick');
 							<label class="col-md-12 control-label" style="padding:0;">
 								<?php echo JText::_('COM_KUNENA_GEN_NAME'); ?>:
 							</label>
-							<input type="text" name="authorname" class="form-control" maxlength="35" placeholder="<?php echo JText::_('COM_KUNENA_GEN_NAME'); ?>" value="" />
+							<input type="text" name="authorname" class="form-control" maxlength="35" placeholder="<?php echo JText::_('COM_KUNENA_GEN_NAME'); ?>" value="" required />
 						</div>
 					<?php endif; ?>
 

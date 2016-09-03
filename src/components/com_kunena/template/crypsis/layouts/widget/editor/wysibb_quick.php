4e139a319c98df9c5c1de7c78390eabe7abaa1b0
Jelle Kok <info@jellekoksolutions.nl>
2016-09-03 04:09:56 +0200

    Fix multiple wysibb on quickreply

--- src/components/com_kunena/template/crypsis/layouts/widget/editor/wysibb_quick.php
+++ src/components/com_kunena/template/crypsis/layouts/widget/editor/wysibb_quick.php
@@ -21,11 +21,11 @@ $this->addStyleSheet('assets/css/jquery.atwho.css');
 ?>
 <script>
 	jQuery(function($) {
-		$("#editor").wysibb();
+		$('[id^=editor-]').wysibb();
 	})
 </script>
 
-<textarea class="span12" name="message" id="editor" rows="12" tabindex="7" required="required"></textarea>
+<textarea class="span12" name="message" id="editor-<?php echo $this->message->id ;?>" rows="12" tabindex="7" required="required"></textarea>
 
 <!-- Bootstrap modal to be used with bbcode editor -->
 <div id="modal-map" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">

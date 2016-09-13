131e77c1acf2dc5b4a0804bed375e43580b2761e
Jelle Kok <info@jellekoksolutions.nl>
2016-09-14 01:59:02 +0200

    Html tags in topic title have a negative impact #4427 #4456 port K50

--- src/components/com_kunena/template/crypsis/layouts/topic/row/default.php
+++ src/components/com_kunena/template/crypsis/layouts/topic/row/default.php
@@ -56,10 +56,10 @@ if (!empty($this->spacing)) : ?>
 <tr class="category<?php echo $this->escape($category->class_sfx) . $txt; ?>">
 	<?php if ($topic->unread) : ?>
 <td class="hidden-phone span1 center topic-item-unread">
-<?php echo $this->getTopicLink($topic, 'unread', $topic->getIcon($topic->getCategory()->iconset), $topic->subject, 'hasTooltip', $category, true, true); ?>
+<?php echo $this->getTopicLink($topic, 'unread', $topic->getIcon($topic->getCategory()->iconset), $this->escape($topic->subject), 'hasTooltip', $category, true, true); ?>
 <?php else : ?>
 	<td class="hidden-phone span1 center">
-		<?php echo $this->getTopicLink($topic, null, $topic->getIcon($topic->getCategory()->iconset), $topic->subject, 'hasTooltip', $category, true, false); ?>
+		<?php echo $this->getTopicLink($topic, null, $topic->getIcon($topic->getCategory()->iconset), $this->escape($topic->subject), 'hasTooltip', $category, true, false); ?>
 		<?php endif; ?>
 	</td>
 	<td class="span<?php echo $cols ?>">
@@ -72,7 +72,7 @@ if (!empty($this->spacing)) : ?>
 
 			if ($topic->unread)
 			{
-				echo $this->getTopicLink($topic, 'unread', $topic->subject . '<sup class="knewchar" dir="ltr">(' . (int) $topic->unread .
+				echo $this->getTopicLink($topic, 'unread', $this->escape($topic->subject) . '<sup class="knewchar" dir="ltr">(' . (int) $topic->unread .
 					' ' . JText::_('COM_KUNENA_A_GEN_NEWCHAR') . ')</sup>', null, 'hasTooltip', $category, true, true);
 			}
 			else

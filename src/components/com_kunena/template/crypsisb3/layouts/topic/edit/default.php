7e69a21fb3239fbb11f4be83f031dae4eecd7215
Jelle Kok <info@jellekoksolutions.nl>
2016-09-14 15:34:36 +0200

    Missing buttons in crypsis b3 : insert all and remove all #4460

--- src/components/com_kunena/template/crypsisb3/layouts/topic/edit/default.php
+++ src/components/com_kunena/template/crypsisb3/layouts/topic/edit/default.php
@@ -231,11 +231,19 @@ if (KunenaFactory::getTemplate()->params->get('formRecover'))
 
 									<!-- The fileinput-button span is used to style the file input field as button -->
 									<span class="btn btn-primary fileinput-button">
-											<?php echo KunenaIcons::plus(); ?>
+										<?php echo KunenaIcons::plus(); ?>
 										<span><?php echo JText::_('COM_KUNENA_UPLOADED_LABEL_ADD_FILES_BUTTON') ?></span>
 										<!-- The file input field used as target for the file upload widget -->
-											<input id="fileupload" type="file" name="file" multiple>
-										</span>
+										<input id="fileupload" type="file" name="file" multiple>
+									</span>
+									<button id="insert-all" class="btn btn-primary" type="submit">
+										<?php echo KunenaIcons::upload();?>
+										<span><?php echo JText::_('COM_KUNENA_UPLOADED_LABEL_INSERT_ALL_BUTTON') ?></span>
+									</button>
+									<button id="remove-all" class="btn btn-danger" type="submit">
+										<?php echo KunenaIcons::cancel();?>
+										<span><?php echo JText::_('COM_KUNENA_UPLOADED_LABEL_REMOVE_ALL_BUTTON') ?></span>
+									</button>
 
 									<!-- The container for the uploaded files -->
 									<div id="files" class="files"></div>

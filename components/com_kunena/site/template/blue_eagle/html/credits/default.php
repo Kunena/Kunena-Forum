1c603745449c71ede1adca03ff0763b26738b980
Jelle Kok <schumacher1986@hotmail.com>
2016-09-29 02:48:56 +0200

    K4.0: Add donation on frontend (#4614)

--- components/com_kunena/site/template/blue_eagle/html/credits/default.php
+++ components/com_kunena/site/template/blue_eagle/html/credits/default.php
@@ -27,6 +27,17 @@ defined ( '_JEXEC' ) or die ();
 						<a href="<?php echo $member['url'] ?>" target="_blank" rel="follow"><?php echo $this->escape($member['name']) ?></a> - <?php echo $member['title'] ?>
 					</li>
 					<?php endforeach ?>
+					<hr class="hr-condensed">
+					<dt><a><?php echo JText::_('COM_KUNENA_DONATE');?></a></dt>
+					<dd>
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+							<input name="cmd" type="hidden" value="_s-xclick">
+							<input name="hosted_button_id" type="hidden" value="TPKVQFBQPFSLU">
+							<input name="submit" type="image" alt="PayPal - The safer, easier way to pay online!" src="https://www.paypalobjects.com/en_US/NL/i/btn/btn_donateCC_LG.gif" border="0">
+							<img width="1" height="1" alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" border="0">
+						</form>
+					</dd>
+					<hr class="hr-condensed">
 				</ul>
 			</div>
 			<div class="kcredits-language">

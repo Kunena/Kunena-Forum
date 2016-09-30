6fead06b8ffd014c1a32bd246b97cf7063215b46
Jelle Kok <info@jellekoksolutions.nl>
2016-10-01 01:04:46 +0200

    part2

---
+++ src/components/com_kunena/template/system/layouts/bbcode/attachment/audio.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Kunena Component
+ * @package         Kunena.Template.Crypsis
+ * @subpackage      BBCode
+ *
+ * @copyright       Copyright (C) 2008 - 2016 Kunena Team. All rights reserved.
+ * @license         http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            https://www.kunena.org
+ **/
+defined('_JEXEC') or die();
+
+// @var KunenaAttachment $attachment
+
+$attachment = $this->attachment;
+?>
+<div class="kmsgaudio">
+	<?php echo $this->subLayout('Attachment/Item')->set('attachment', $attachment); ?>
+</div>

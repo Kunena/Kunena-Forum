3badc863e79032b8a4a2b5834d88429d9c4affeb
Jelle Kok <schumacher1986@hotmail.com>
2016-10-04 02:04:05 +0200

    crypsisb3: minor fixes on stats/whoisonline widgets #4650 Port K50 (#4681)

--- src/components/com_kunena/template/crypsisb3/layouts/widget/statistics/default.php
+++ src/components/com_kunena/template/crypsisb3/layouts/widget/statistics/default.php
@@ -1,12 +1,13 @@
 <?php
 /**
  * Kunena Component
- * @package         Kunena.Template.Crypsis
- * @subpackage      Layout.Widget
  *
- * @copyright       Copyright (C) 2008 - 2016 Kunena Team. All rights reserved.
- * @license         http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * @link            https://www.kunena.org
+ * @package     Kunena.Template.Crypsis
+ * @subpackage  Layout.Statistics
+ *
+ * @copyright   (C) 2008 - 2016 Kunena Team. All rights reserved.
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link        https://www.kunena.org
  **/
 defined('_JEXEC') or die;
 ?>
@@ -14,69 +15,79 @@ defined('_JEXEC') or die;
 <div class="kfrontend">
 	<div class="btn-toolbar pull-right">
 		<div class="btn-group">
-			<div class="btn btn-default btn-small <?php echo KunenaIcons::collapse(); ?>" data-toggle="collapse" data-target="#kstats"></div>
+			<div class="btn btn-default btn-sm <?php echo KunenaIcons::collapse();?>" data-toggle="collapse" data-target="#kwho"></div>
 		</div>
 	</div>
 	<h2 class="btn-link">
-		<?php if ($this->statisticsUrl) : ?>
-			<a href="<?php echo $this->statisticsUrl; ?>">
-				<?php echo JText::_('COM_KUNENA_STATISTICS'); ?>
+		<?php if ($this->usersUrl) : ?>
+			<a href="<?php echo $this->usersUrl; ?>">
+				<?php echo JText::_('COM_KUNENA_MEMBERS'); ?>
 			</a>
 		<?php else : ?>
-			<?php echo JText::_('COM_KUNENA_STATISTICS'); ?>
+			<?php echo JText::_('COM_KUNENA_MEMBERS'); ?>
 		<?php endif; ?>
 	</h2>
-	<div class="row collapse in" id="kstats">
-		<div class="well-small">
-			<ul class="unstyled col-md-1 btn-link">
-				<?php echo KunenaIcons::stats(); ?>
-			</ul>
-			<ul class="unstyled col-md-3">
-				<li>
-					<?php echo JText::_('COM_KUNENA_STAT_TOTAL_MESSAGES'); ?>:
-					<strong><?php echo (int) $this->messageCount; ?></strong>
-				</li>
-				<li>
-					<?php echo JText::_('COM_KUNENA_STAT_TOTAL_SECTIONS'); ?>:
-					<strong><?php echo (int) $this->sectionCount; ?></strong>
-				</li>
-				<li>
-					<?php echo JText::_('COM_KUNENA_STAT_TODAY_OPEN_THREAD'); ?>:
-					<strong><?php echo (int) $this->todayTopicCount; ?></strong>
-				</li>
-				<li>
-					<?php echo JText::_('COM_KUNENA_STAT_TODAY_TOTAL_ANSWER'); ?>:
-					<strong><?php echo (int) $this->todayReplyCount; ?></strong>
-				</li>
-			</ul>
-			<ul class="unstyled col-md-3">
-				<li>
-					<?php echo JText::_('COM_KUNENA_STAT_TOTAL_SUBJECTS'); ?>:
-					<strong><?php echo (int) $this->topicCount; ?></strong>
-				</li>
-				<li>
-					<?php echo JText::_('COM_KUNENA_STAT_TOTAL_CATEGORIES'); ?>:
-					<strong><?php echo (int) $this->categoryCount; ?></strong>
-				</li>
-				<li>
-					<?php echo JText::_('COM_KUNENA_STAT_YESTERDAY_OPEN_THREAD'); ?>:
-					<strong><?php echo (int) $this->yesterdayTopicCount; ?></strong>
-				</li>
-				<li>
-					<?php echo JText::_('COM_KUNENA_STAT_YESTERDAY_TOTAL_ANSWER'); ?>:
-					<strong><?php echo (int) $this->yesterdayReplyCount; ?></strong>
-				</li>
-			</ul>
-			<ul class="unstyled col-md-3">
-				<li>
-					<?php echo JText::_('COM_KUNENA_STAT_TOTAL_USERS'); ?>:
-					<strong><?php echo $this->memberCount; ?></strong>
-				</li>
-				<li>
-					<?php echo JText::_('COM_KUNENA_STAT_LATEST_MEMBERS'); ?>:
-					<strong><?php echo $this->latestMemberLink; ?></strong>
-				</li>
-			</ul>
+
+	<div class="collapse in" id="kwho">
+		<div class="well well-sm">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-1">
+						<ul class="list-unstyled">
+							<li class="btn-link">
+								<?php echo KunenaIcons::members(); ?>
+							</li>
+						</ul>
+					</div>
+
+					<div class="col-md-11">
+						<ul class="list-unstyled">
+							<span>
+								<?php echo JText::sprintf('COM_KUNENA_VIEW_COMMON_WHO_TOTAL', $this->membersOnline); ?>
+							</span>
+							<?php
+							$template = KunenaTemplate::getInstance();
+							$direction = $template->params->get('whoisonlineName');
+
+							if ($direction == 'both') : ?>
+								<div><?php echo $this->setLayout('both'); ?></div>
+								<?php
+							elseif ($direction == 'avatar') : ?>
+								<div><?php echo $this->setLayout('avatar'); ?></div>
+							<?php else : ?>
+								<div><?php echo $this->setLayout('name'); ?></div>
+								<?php
+							endif;
+							?>
+
+							<?php if (!empty($this->onlineList)) : ?>
+								<div>
+									<span><?php echo JText::_('COM_KUNENA_LEGEND'); ?>:</span>
+									<span class="kwho-admin">
+										<?php echo KunenaIcons::user(); ?> <?php echo JText::_('COM_KUNENA_COLOR_ADMINISTRATOR'); ?>
+									</span>
+									<span class="kwho-globalmoderator">
+										<?php echo KunenaIcons::user(); ?> <?php echo JText::_('COM_KUNENA_COLOR_GLOBAL_MODERATOR'); ?>
+									</span>
+									<span class="kwho-moderator">
+										<?php echo KunenaIcons::user(); ?> <?php echo JText::_('COM_KUNENA_COLOR_MODERATOR'); ?>
+									</span>
+									<span class="kwho-banned">
+										<?php echo KunenaIcons::user(); ?> <?php echo JText::_('COM_KUNENA_COLOR_BANNED'); ?>
+									</span>
+									<span class="kwho-user">
+										<?php echo KunenaIcons::user(); ?> <?php echo JText::_('COM_KUNENA_COLOR_USER'); ?>
+									</span>
+									<span class="kwho-guest">
+										<?php echo KunenaIcons::user(); ?> <?php echo JText::_('COM_KUNENA_COLOR_GUEST'); ?>
+									</span>
+								</div>
+							<?php endif; ?>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>

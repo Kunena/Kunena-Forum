d4310140de2f2e8bda2b019efc84582cefc95e75
betteryouthanme <betteryouthanme@hotmail.de>
2016-10-04 15:30:27 +0200

    crypsisb3: minor fixes on index page (#4686)

--- src/libraries/kunena/icons/icons.php
+++ src/libraries/kunena/icons/icons.php
@@ -50,7 +50,7 @@ class KunenaIcons
 	}
 
 	/**
-	 * Return the arrwo up icon
+	 * Return the arrow up icon
 	 *
 	 * @return string
 	 *
@@ -139,6 +139,66 @@ class KunenaIcons
 		}
 	}
 
+	/**
+	 * Return the lock icon
+	 *
+	 * @return string
+	 *
+	 * @since K5.0
+	 */
+	static public function lock()
+	{
+		$ktemplate  = KunenaFactory::getTemplate();
+		$topicicontype    = $ktemplate->params->get('topicicontype');
+
+		if ($topicicontype == 'fa')
+		{
+			return '<i class="fa fa-lock" aria-hidden="true"></i>';
+		}
+		elseif ($topicicontype == 'B2')
+		{
+			return '<span class="icon icon-lock"></span>';
+		}
+		elseif ($topicicontype == 'B3')
+		{
+			return '<span class="glyphicon glyphicon-lock"></span>';
+		}
+		else
+		{
+			return '<span class="icon icon-lock"></span>';
+		}
+	}
+	
+	/**
+	 * Return the shield icon (reviewed)
+	 *
+	 * @return string
+	 *
+	 * @since K5.0
+	 */
+	static public function shield()
+	{
+		$ktemplate  = KunenaFactory::getTemplate();
+		$topicicontype    = $ktemplate->params->get('topicicontype');
+
+		if ($topicicontype == 'fa')
+		{
+			return '<i class="fa fa-shield" aria-hidden="true"></i>';
+		}
+		elseif ($topicicontype == 'B2')
+		{
+			return '<span class="icon icon-eye-open"></span>';
+		}
+		elseif ($topicicontype == 'B3')
+		{
+			return '<span class="glyphicon glyphicon-eye-open"></span>';
+		}
+		else
+		{
+			return '<span class="icon icon-eye-open"></span>';
+		}
+	}
+	
 	/**
 	 * Return the stats icon
 	 *

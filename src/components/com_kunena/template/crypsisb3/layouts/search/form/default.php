a3a59f2e589f7ad176eff80c881299e6e8b030f7
betteryouthanme <betteryouthanme@hotmail.de>
2016-10-08 14:38:07 +0200

    crysisb3 update search layout

--- src/components/com_kunena/template/crypsisb3/layouts/search/form/default.php
+++ src/components/com_kunena/template/crypsisb3/layouts/search/form/default.php
@@ -37,7 +37,7 @@ $this->addScript('assets/js/bootstrap.datepicker.js');
 
 	<div class="btn-toolbar pull-right">
 		<div class="btn-group">
-			<button class="btn btn-default btn-small <?php echo KunenaIcons::collapse();?>" type="button" data-toggle="collapse" data-target="#search" aria-expanded="false" aria-controls="search"></button>
+			<button class="btn btn-default btn-sm <?php echo KunenaIcons::collapse();?>" type="button" data-toggle="collapse" data-target="#search" aria-expanded="false" aria-controls="search"></button>
 		</div>
 	</div>
 	<h1>
@@ -45,22 +45,24 @@ $this->addScript('assets/js/bootstrap.datepicker.js');
 	</h1>
 
 	<div class="collapse in" id="search">
-	<div class="well">
+		<div class="well">
 			<div class="row">
 				<fieldset class="col-md-6">
 					<legend>
 						<?php echo JText::_('COM_KUNENA_SEARCH_SEARCHBY_KEYWORD'); ?>
 					</legend>
+					
 					<div class="col-md-6">
-						<label>
-							<?php echo JText::_('COM_KUNENA_SEARCH_KEYWORDS'); ?>:
-							<input type="text" name="query" class="form-control"
-							       value="<?php echo $this->escape($this->state->get('searchwords')); ?>" />
-						</label>
+							<div class="form-group">
+								<input type="text" name="query" class="form-control"
+							       value="<?php echo $this->escape($this->state->get('searchwords')); ?>" placeholder="<?php echo JText::_('COM_KUNENA_SEARCH_KEYWORDS'); ?>" />		   
+							</div>
 					</div>
+					
 					<div class="col-md-6">
-						<br>
-						<?php $this->displayModeList('mode'); ?>
+							<div class="form-group">
+								<?php $this->displayModeList('mode'); ?>
+							</div>
 					</div>
 				</fieldset>
 
@@ -69,38 +71,38 @@ $this->addScript('assets/js/bootstrap.datepicker.js');
 					<legend>
 						<?php echo JText::_('COM_KUNENA_SEARCH_SEARCHBY_USER'); ?>
 					</legend>
-					<label>
-						<?php echo JText::_('COM_KUNENA_SEARCH_UNAME'); ?>:
-						<input id="kusersearch" type="text" name="searchuser" class="form-control"
-						       value="<?php echo $this->escape($this->state->get('query.searchuser')); ?>" />
-					</label>
-					<br>
-					<label>
-						<?php echo JText::_('COM_KUNENA_SEARCH_EXACT'); ?>:
-						<input type="checkbox" name="exactname" value="1"
-							<?php if ($this->state->get('query.exactname')) { echo $this->checked; } ?> />
-					</label>
+					
+						<div class="form-group">
+							<input id="kusersearch" type="text" name="searchuser" class="form-control"
+								value="<?php echo $this->escape($this->state->get('query.searchuser')); ?>" placeholder="<?php echo JText::_('COM_KUNENA_SEARCH_UNAME'); ?>" />			
+							<div class="checkbox">
+								<label>
+									<input type="checkbox" name="exactname" value="1"
+										<?php if ($this->state->get('query.exactname')) { echo $this->checked; } ?>  />
+										<?php echo JText::_('COM_KUNENA_SEARCH_EXACT'); ?>
+								</label>
+							</div>	
+						</div>		
 				</fieldset>
 				<?php endif; ?>
 			</div>
 		</div>
 
-		<button type="button" class="btn btn-default btn-small pull-right  <?php echo KunenaIcons::collapse();?>" data-toggle="collapse" data-target="#search-options" aria-expanded="false" aria-controls="search-options"></button>
-		<h2>
-			<?php echo JText::_('COM_KUNENA_SEARCH_OPTIONS'); ?>
-		</h2>
+		<button type="button" class="btn btn-default btn-sm pull-right  <?php echo KunenaIcons::collapse();?>" data-toggle="collapse" data-target="#search-options" aria-expanded="false" aria-controls="search-options"></button>
+			<h2>
+				<?php echo JText::_('COM_KUNENA_SEARCH_OPTIONS'); ?>
+			</h2>
 
 		<div class="collapse in" id="search-options">
 			<div class="well">
 				<div class="row">
+				
 					<fieldset class="col-md-6">
 						<legend>
 							<?php echo JText::_('COM_KUNENA_SEARCH_FIND_POSTS'); ?>
 						</legend>
-						<div class="col-md-6">
+						<div class="form-group">	
 							<?php $this->displayDateList('date'); ?>
-						</div>
-						<div class="col-md-6">
 							<?php $this->displayBeforeAfterList('beforeafter'); ?>
 						</div>
 					</fieldset>
@@ -109,10 +111,8 @@ $this->addScript('assets/js/bootstrap.datepicker.js');
 						<legend>
 							<?php echo JText::_('COM_KUNENA_SEARCH_SORTBY'); ?>
 						</legend>
-						<div class="col-md-6">
+						<div class="form-group">
 						<?php $this->displaySortByList('sort'); ?>
-						</div>
-						<div class="col-md-6">
 						<?php $this->displayOrderList('order'); ?>
 						</div>
 					</fieldset>
@@ -121,9 +121,13 @@ $this->addScript('assets/js/bootstrap.datepicker.js');
 						<legend>
 							<?php echo JText::_('COM_KUNENA_SEARCH_AT_A_SPECIFIC_DATE'); ?>
 						</legend>
-						<div class="col-md-6" id="searchatdate">
-							<div class="input-group date">
-								<input class="form-control" type="text" name="searchatdate" data-date-format="mm/dd/yyyy" value="<?php echo JFactory::getDate()->format('m/d/Y'); ?>"> <span class="input-group-addon"><i class="glyphicon glyphicon-th"></i></span>
+						<div class="col-md-6">
+							<div class="form-group" id="searchatdate">
+								<div class="input-group date">
+									<input class="form-control" type="text" name="searchatdate" data-date-format="mm/dd/yyyy" 
+										value="<?php echo JFactory::getDate()->format('m/d/Y'); ?>"> 
+										<span class="input-group-addon"><?php echo KunenaIcons::calendar();?></span>
+								</div>
 							</div>
 						</div>
 					</fieldset>
@@ -131,10 +135,11 @@ $this->addScript('assets/js/bootstrap.datepicker.js');
 
 				<div class="row">
 					<div class="col-md-6">
-						<fieldset>
+						<fieldset class="form-group">
 							<legend>
 								<?php echo JText::_('COM_KUNENA_SEARCH_START'); ?>
 							</legend>
+							
 							<div class="col-md-6">
 							<input type="text" name="limitstart" class="form-control"
 							       value="<?php echo $this->escape($this->state->get('list.start')); ?>" size="5" />
@@ -149,24 +154,32 @@ $this->addScript('assets/js/bootstrap.datepicker.js');
 							<legend>
 								<?php echo JText::_('COM_KUNENA_SEARCH_SHOW'); ?>
 							</legend>
-							<label class="radio">
-								<input type="radio" name="show" value="0"
-									<?php if ($this->state->get('query.show') == 0) { echo 'checked="checked"'; } ?> />
-								<?php echo JText::_('COM_KUNENA_SEARCH_SHOW_NORMAL'); ?>
-							</label>
-							<label class="radio">
-								<input type="radio" name="show" value="1"
-									<?php if ($this->state->get('query.show') == 1) { echo 'checked="checked"'; } ?> />
-								<?php echo JText::_('COM_KUNENA_SEARCH_SHOW_UNAPPROVED'); ?>
-							</label>
-							<label class="radio">
-								<input type="radio" name="show" value="2"
-									<?php if ($this->state->get('query.show') == 2) { echo 'checked="checked"'; } ?> />
-								<?php echo JText::_('COM_KUNENA_SEARCH_SHOW_TRASHED'); ?>
-							</label>
+							
+							<div class="radio">
+								<label>
+									<input type="radio" name="show" value="0"
+										<?php if ($this->state->get('query.show') == 0) { echo 'checked="checked"'; } ?> />
+									<?php echo JText::_('COM_KUNENA_SEARCH_SHOW_NORMAL'); ?>
+								</label>
+							</div>
+							
+							<div class="radio">
+								<label>
+									<input type="radio" name="show" value="1"
+										<?php if ($this->state->get('query.show') == 1) { echo 'checked="checked"'; } ?> />
+									<?php echo JText::_('COM_KUNENA_SEARCH_SHOW_UNAPPROVED'); ?>
+								</label>
+							</div>	
+								
+							<div class="radio">	
+								<label>
+									<input type="radio" name="show" value="2"
+										<?php if ($this->state->get('query.show') == 2) { echo 'checked="checked"'; } ?> />
+									<?php echo JText::_('COM_KUNENA_SEARCH_SHOW_TRASHED'); ?>
+								</label>
+							</div>
 						</fieldset>
 						<?php endif; ?>
-
 					</div>
 
 					<fieldset class="col-md-6">
@@ -184,7 +197,7 @@ $this->addScript('assets/js/bootstrap.datepicker.js');
 			</div>
 		</div>
 
-		<div class="center">
+		<div class="text-center">
 			<button type="submit" class="btn btn-primary">
 				<?php echo KunenaIcons::search();?><?php echo(' ' . JText::_('COM_KUNENA_SEARCH_SEND') . ' '); ?>
 			</button>
@@ -195,3 +208,4 @@ $this->addScript('assets/js/bootstrap.datepicker.js');
 	</div>
 </form>
 <div class="clearfix"></div>
+

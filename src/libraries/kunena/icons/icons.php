e4e97e0294aadf63b19d1c28249e3d7553269d6b
Jelle Kok <info@jellekoksolutions.nl>
2016-11-12 02:20:04 +0100

    Category icons are not shown #4868 #4870 port K50

--- src/libraries/kunena/icons/icons.php
+++ src/libraries/kunena/icons/icons.php
@@ -1133,43 +1133,7 @@ class KunenaIcons
 		}
 		else
 		{
-			if ($topicicontype == 'fa')
-			{
-				if (!empty($caticon))
-				{
-					$bigicon = ' ';
-				}
-				else
-				{
-					$bigicon = ' fa-folder-open ';
-				}
-			}
-			elseif ($topicicontype == 'B2')
-			{
-				if (!empty($caticon))
-				{
-					$bigicon = ' ';
-				}
-				else
-				{
-					$bigicon = ' icon-folder ';
-				}
-			}
-			elseif ($topicicontype == 'B3')
-			{
-				if (!empty($caticon))
-				{
-					$bigicon = ' ';
-				}
-				else
-				{
-					$bigicon = ' glyphicon-folder-open ';
-				}
-			}
-			else
-			{
-				$bigicon = ' ';
-			}
+			$bigicon = ' ';
 		}
 
 		if ($topicicontype == 'fa')

70709771b77d1f3146395f0e2589c1b35b6237ff
Jelle Kok <schumacher1986@hotmail.com>
2016-11-12 01:47:47 +0100

    Revert models user: broke select user ajax search #4843  Port K50 (#4946)

--- src/components/com_kunena/models/user.php
+++ src/components/com_kunena/models/user.php
@@ -196,10 +196,11 @@ class KunenaModelUser extends KunenaModel
 			$where  = $this->getQueryWhere();
 			$search = $this->getQuerySearch();
 			$query  = $db->getQuery(true);
-			$query->select('COUNT(*)')->from($db->quoteName('#__users', 'u')
-				->join('left', $db->quoteName('#__kunena_users', 'ku') . ' ON (' . $db->quoteName('ku.userid') . ' = ' . $db->quoteName('u.id') . ')')
-				->where("{$where} {$search}")
-			);
+			$query  = "SELECT COUNT(*)  
+						FROM #__users AS u  
+						LEFT JOIN #__kunena_users AS ku ON ku.userid = u.id  
+						WHERE {$where} {$search}";
+			$db->setQuery($query);
 
 			try
 			{
@@ -264,11 +265,11 @@ class KunenaModelUser extends KunenaModel
 			$where  = $this->getQueryWhere();
 			$search = $this->getQuerySearch();
 			$query  = $db->getQuery(true);
-			$query->select($db->quoteName('u.id'))->from($db->quoteName('#__users', 'u')
-				->join('left', $db->quoteName('#__kunena_users', 'ku') . ' ON (' . $db->quoteName('ku.userid') . ' = ' . $db->quoteName('u.id') . ')')
-				->where("{$where} {$search}")
-				->order($db->quoteName("{$where}") . " {$search}")
-			);
+			$query  = "SELECT u.id  
+				FROM #__users AS u  
+				LEFT JOIN #__kunena_users AS ku ON ku.userid = u.id  
+				WHERE {$where} {$search}";
+			$query .= " ORDER BY {$orderby} {$direction}";
 			$db->setQuery($query, $limitstart, $limit);
 
 			try

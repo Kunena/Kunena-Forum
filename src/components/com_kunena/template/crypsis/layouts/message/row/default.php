39df8d1198d562523cd44d0d7b7e86cf4d56d119
Jelle Kok <info@jellekoksolutions.nl>
2016-11-12 02:13:46 +0100

    Can't see post author and/or content in unapproved view #4848 #4866  Port K50

--- src/components/com_kunena/template/crypsis/layouts/message/row/default.php
+++ src/components/com_kunena/template/crypsis/layouts/message/row/default.php
@@ -77,7 +77,7 @@ if ($topic->moved_id > 0)
 			}
 			else
 			{
-				echo $this->getTopicLink($topic, null, null, null, 'hasTooltip topictitle', $category, true, false);
+				echo $this->getTopicLink($topic, $this->message, null, null, 'hasTooltip topictitle', $category, true, false);
 			}
 
 			if ($topic->getUserTopic()->favorite)

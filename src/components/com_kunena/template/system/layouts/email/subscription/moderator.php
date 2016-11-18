9cfe8ce9828a5ed18ddb561667a1d19634a5f76c
810 <schumacher1986@hotmail.com>
2016-11-18 15:19:22 +0100

    Review posts : Mods and admins get mails with wrong text #4939 (#4977) Port K50

--- src/components/com_kunena/template/system/layouts/email/subscription/moderator.php
+++ src/components/com_kunena/template/system/layouts/email/subscription/moderator.php
@@ -2,8 +2,8 @@
 /**
  * Kunena Component
  *
- * @package         Kunena.Template.Crypsis
- * @subpackage      Layout.Email
+ * @package     Kunena.Template
+ * @subpackage  Layout.Email
  *
  * @copyright   (C) 2008 - 2016 Kunena Team. All rights reserved.
  * @license         http://www.gnu.org/copyleft/gpl.html GNU/GPL
@@ -19,8 +19,8 @@ $author  = $this->message->getAuthor();
 $config  = KunenaConfig::getInstance();
 $subject = $this->message->subject ? $this->message->subject : $this->message->getTopic()->subject;
 
-$msg1 = $this->message->parent ? JText::_('COM_KUNENA_POST_EMAIL_NOTIFICATION1') : JText::_('COM_KUNENA_POST_EMAIL_NOTIFICATION1_CAT');
-$msg2 = $this->message->parent ? JText::_('COM_KUNENA_POST_EMAIL_NOTIFICATION2') : JText::_('COM_KUNENA_POST_EMAIL_NOTIFICATION2_CAT');
+$msg1 = JText::_('COM_KUNENA_POST_EMAIL_MOD1');
+$msg2 = JText::_('COM_KUNENA_POST_EMAIL_MOD2');
 $more = ($this->once ?
 	JText::_(
 		$this->message->parent ? 'COM_KUNENA_POST_EMAIL_NOTIFICATION_MORE_READ' :

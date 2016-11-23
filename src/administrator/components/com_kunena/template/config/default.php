cf2358b73467e73d64c5301dd3bd66efec9f412e
sshcli <sshcli@users.noreply.github.com>
2016-11-23 09:50:40 -0400

    Typo: shorthen / shorten (#5042)

--- src/administrator/components/com_kunena/template/config/default.php
+++ src/administrator/components/com_kunena/template/config/default.php
@@ -376,17 +376,17 @@ JHtml::_('behavior.tabstate');
 												<td align="left" valign="top"><?php echo JText::_('COM_KUNENA_CONFIGURATION_RATING_DESC') ?></td>
 											</tr>
 											<tr>
-												<td><?php echo JText::_('COM_KUNENA_CONFIG_ATTACHMENT_SHORTHEN_NAME_START') ?></td>
+												<td><?php echo JText::_('COM_KUNENA_CONFIG_ATTACHMENT_SHORTEN_NAME_START') ?></td>
 												<td>
 													<input type="text" name="cfg_attach_start" class="ksm-field" value="<?php echo $this->escape($this->config->attach_start) ?>" />
 												</td>
-												<td><?php echo JText::_('COM_KUNENA_CONFIG_ATTACHMENT_SHORTHEN_NAME_START_DESC') ?></td>
+												<td><?php echo JText::_('COM_KUNENA_CONFIG_ATTACHMENT_SHORTEN_NAME_START_DESC') ?></td>
 											</tr><tr>
-												<td><?php echo JText::_('COM_KUNENA_CONFIG_ATTACHMENT_SHORTHEN_NAME_END') ?></td>
+												<td><?php echo JText::_('COM_KUNENA_CONFIG_ATTACHMENT_SHORTEN_NAME_END') ?></td>
 												<td>
 													<input type="text" name="cfg_attach_end" class="ksm-field" value="<?php echo $this->escape($this->config->attach_end) ?>" />
 												</td>
-												<td><?php echo JText::_('COM_KUNENA_CONFIG_ATTACHMENT_SHORTHEN_NAME_END_DESC') ?></td>
+												<td><?php echo JText::_('COM_KUNENA_CONFIG_ATTACHMENT_SHORTEN_NAME_END_DESC') ?></td>
 											</tr>
 											</tbody>
 										</table>

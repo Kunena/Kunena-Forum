9d1b0145b45ea7b09905819f8fd556c26821955e
810 <schumacher1986@hotmail.com>
2016-11-25 00:30:22 +0100

    Email: hide bbcodes on email plain

--- src/components/com_kunena/template/system/layouts/email/report/default.php
+++ src/components/com_kunena/template/system/layouts/email/report/default.php
@@ -218,7 +218,7 @@ $alt = <<<EOS
 
 {$this->text('COM_KUNENA_REPORT_POST_MESSAGE')}
 -----
-{$this->message->displayField('message')}
+{$this->message->displayField('message', false)}
 -----
 
 {$this->text('COM_KUNENA_REPORT_POST_LINK')} {$this->messageLink}

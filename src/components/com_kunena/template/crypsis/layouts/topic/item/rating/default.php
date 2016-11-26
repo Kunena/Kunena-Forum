7a1eddc0de57a90726af9e683f395dfe933d717d
810 <schumacher1986@hotmail.com>
2016-11-26 04:06:15 +0100

    Fixed the wrong URL: Rating

--- src/components/com_kunena/template/crypsis/layouts/topic/item/rating/default.php
+++ src/components/com_kunena/template/crypsis/layouts/topic/item/rating/default.php
@@ -16,7 +16,10 @@ $this->addStyleSheet('assets/css/rating.css');
 ?>
 
 <?php if ($this->category->allow_ratings && $this->config->ratingenabled): ?>
-<input id="topic_id" type="hidden" value="<?php echo $this->topicid ?>"/>
-<input type="hidden" id="krating_submit_url" name="url" value="<?php echo 'index.php?option=com_kunena&view=topic&layout=rate&topic_id=' . $this->topicid . '&format=raw'; ?>" />
-<div id="krating"></div>
+	<input id="topic_id" type="hidden" value="<?php echo $this->topicid ?>"/>
+	<input type="hidden" id="krating_url" name="krating_url"
+	       value="<?php echo KunenaRoute::_('index.php?option=com_kunena&view=topic&layout=getrate&format=raw'); ?>"/>
+	<input type="hidden" id="krating_submit_url" name="url"
+	       value="<?php echo KunenaRoute::_('index.php?option=com_kunena&view=topic&layout=rate&topic_id=' . $this->topicid . '&format=raw'); ?>"/>
+	<div id="krating"></div>
 <?php endif; ?>

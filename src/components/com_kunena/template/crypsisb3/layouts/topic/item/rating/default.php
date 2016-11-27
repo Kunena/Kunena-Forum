907b1772cfdb516f468317915128256bd6dced0a
810 <schumacher1986@hotmail.com>
2016-11-27 21:27:26 +0100

    Microdata on ratings

--- src/components/com_kunena/template/crypsisb3/layouts/topic/item/rating/default.php
+++ src/components/com_kunena/template/crypsisb3/layouts/topic/item/rating/default.php
@@ -22,4 +22,11 @@ $this->addStyleSheet('assets/css/rating.css');
 	<input type="hidden" id="krating_submit_url" name="url"
 	       value="<?php echo KunenaRoute::_('index.php?option=com_kunena&view=topic&layout=rate&topic_id=' . $this->topicid . '&format=raw'); ?>"/>
 	<div id="krating"></div>
+
+	<div itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
+		<meta itemprop="worstRating" content="1">
+		<meta itemprop="bestRating" content="5">
+		<meta itemprop="reviewCount" content="<?php echo KunenaForumTopicRate::getTotalUsers($this->topicid);?>">
+		<meta itemprop="ratingValue" content="<?php echo KunenaForumTopicRateHelper::getSelected($this->topicid); ?>">
+	</div>
 <?php endif; ?>

b8a9ccd8f2fc663392feb936bd201228f1563269
Jelle Kok <schumacher1986@hotmail.com>
2017-02-18 00:08:45 +0100

    Incorrect path for attachments - The requested page can't be found. #5081 (#5196)

--- src/administrator/components/com_kunena/template/layouts/attachment/item/default.php
+++ src/administrator/components/com_kunena/template/layouts/attachment/item/default.php
@@ -17,7 +17,7 @@ $config = KunenaFactory::getConfig();
 if ($config->access_component)
 {
 	?>
-	<a href="<?php echo $attachment->getUrl(); ?>" title="<?php echo $attachment->getFilename(); ?>">
+	<a href="<?php echo JUri::root() . $attachment->getUrl(); ?>" title="<?php echo $attachment->getFilename(); ?>">
 		<?php if ($attachment->isImage())
 		{
 			echo '<img src="' . JUri::root() . $attachment->getUrl(true) . ' " height="40" width="40" />';

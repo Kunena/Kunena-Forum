5fa6937c49a64ecd36096af15a22f0de8f628583
sshcli <sshcli@users.noreply.github.com>
2017-02-18 11:58:36 -0400

    Migrate from http to https

--- src/components/com_kunena/template/crypsis/pages/search/default/default.php
+++ src/components/com_kunena/template/crypsis/pages/search/default/default.php
@@ -5,7 +5,7 @@
  * @subpackage  Pages.Search
  *
  * @copyright   (C) 2008 - 2017 Kunena Team. All rights reserved.
- * @license     http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link        https://www.kunena.org
  **/
 defined('_JEXEC') or die;

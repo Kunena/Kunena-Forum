5571f997836ae044fadc2317f57ae57ff26dbc73
sshcli <sshcli@users.noreply.github.com>
2017-02-18 11:21:55 -0400

    Migrate from http to https

--- COPYRIGHT.php
+++ COPYRIGHT.php
@@ -4,7 +4,7 @@
  * @package Kunena.Site
  *
  * @copyright (C) 2008 - 2017 Kunena Team. All rights reserved.
- * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @license https://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link https://www.kunena.org
  **/
 die ();
@@ -21,34 +21,34 @@ licenses, including works distributed under the following copyright notices:
 Joomla
 ------
 Copyright:	2005 - 2014 Open Source Matters, Inc. All rights reserved.
-License:	GPLv2 or later; http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+License:	GPLv2 or later; https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
 Bootstrap
 ---------
 Copyright:	Copyright 2012 Twitter, Inc.
-License:	Apache License v2.0: http://www.apache.org/licenses/LICENSE-2.0
+License:	Apache License v2.0: https://www.apache.org/licenses/LICENSE-2.0
 
 mooboo
 ------
 Copyright:	Copyright 2012 Twitter, Inc.
 			Modified for MooTools by GP Technology Solutions Pty Ltd, darrennolan
-License:	Apache License v2.0: http://www.apache.org/licenses/LICENSE-2.0
+License:	Apache License v2.0: https://www.apache.org/licenses/LICENSE-2.0
 
 Nawte
 -----
 Copyright:	2008 Jean-Nicolas Jolivet (www.silverscripting.com)
-License:	MIT license; http://www.opensource.org/licenses/mit-license.php
+License:	MIT license; https://www.opensource.org/licenses/mit-license.php
 
 NBBC
 ----
 Copyright:	2008-2009, the Phantom Inker.  All rights reserved.
 			Portions Copyright (c) 2004-2008 AddedBytes.com
-License:	BSD license; http://www.opensource.org/licenses/bsd-license.php
+License:	BSD license; https://www.opensource.org/licenses/bsd-license.php
 
 mediaboxAdvanced
 ----------------
 Copyright:	2007-2011 John Einselen - http://iaian7.com
 			2007-2008 Christophe Beyls - http://www.digitalia.be
-License:	MIT license; http://www.opensource.org/licenses/mit-license.php
+License:	MIT license; https://www.opensource.org/licenses/mit-license.php
 
  -->

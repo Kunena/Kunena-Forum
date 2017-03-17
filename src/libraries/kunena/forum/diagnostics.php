0c57780d485e5a759c7f86d644f0290217205313
Jelle Kok <schumacher1986@hotmail.com>
2017-03-17 23:20:35 +0100

    Fix empty categories channels on sample (#5248)

--- src/libraries/kunena/forum/diagnostics.php
+++ src/libraries/kunena/forum/diagnostics.php
@@ -1188,7 +1188,7 @@ abstract class KunenaForumDiagnostics
 	 */
 	protected static function query_ratingOrphaned()
 	{
-		// Query to find user read which do not belong in any existing topic
+		// Query to find orphaned ratings
 		$query = new KunenaDatabaseQuery();
 		$query->from("#__kunena_rate AS r")->leftJoin("#__kunena_topics AS t ON t.id=r.topic_id")->where("t.id IS NULL");
 
@@ -1225,7 +1225,7 @@ abstract class KunenaForumDiagnostics
 	 */
 	protected static function query_channelOrphaned()
 	{
-		// Query to find user read which do not belong in any existing topic
+		// Query to find orphaned categories channels
 		$query = new KunenaDatabaseQuery();
 		$query->from("#__kunena_categories")->where("channels IS NULL OR 'none'");
 

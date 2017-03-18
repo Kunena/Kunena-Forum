38d4656917d02bb33b415079e5a6b1c0ad65766a
Jelle Kok <jelle@kunena.org>
2017-03-18 01:05:21 +0100

    Avatar: save uppercase ext

--- src/libraries/kunena/upload/upload.php
+++ src/libraries/kunena/upload/upload.php
@@ -587,6 +587,7 @@ class KunenaUpload
 		if ($file->isAvatar)
 		{
 			$a = array('gif', 'jpeg', 'jpg', 'png');
+			$file->ext = strtolower($file->ext);
 
 			if (!in_array($file->ext, $a, true))
 			{

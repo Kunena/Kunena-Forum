5089c3f4c262c1af50cbd9284f29565e5680f2b0
Jelle Kok <schumacher1986@hotmail.com>
2017-03-21 02:35:52 +0100

    Fix get params robots (#5263)

--- src/components/com_kunena/controller/topic/list/recent/display.php
+++ src/components/com_kunena/controller/topic/list/recent/display.php
@@ -307,9 +307,8 @@ class ComponentKunenaControllerTopicListRecentDisplay extends ComponentKunenaCon
 		$app = JFactory::getApplication();
 		$menu_item   = $app->getMenu()->getActive();
 
-		$config = JFactory::getApplication('site');
-		$componentParams = $config->getParams('com_config');
-		$robots = $componentParams->get('robots');
+		$config = JFactory::getConfig();
+		$robots = $config->get('robots');
 
 		if ($robots == '')
 		{

d3513b418ddacec9103dcf000f6ce0067378800a
sshcli <sshcli@users.noreply.github.com>
2017-06-29 11:50:20 -0400

    Add posted prefix to unapproved / my topics

--- src/components/com_kunena/template/crypsis/layouts/topic/row/user.php
+++ src/components/com_kunena/template/crypsis/layouts/topic/row/user.php
@@ -105,7 +105,8 @@ if (!empty($this->spacing)) : ?>
 			<span class="ktopic-category"> <?php echo JText::sprintf('COM_KUNENA_CATEGORY_X', $this->getCategoryLink($this->topic->getCategory(), null, $this->topic->getCategory()->description, KunenaTemplate::getInstance()->tooltips())) ?></span>
 			<?php if ($config->post_dateformat != 'none') : ?>
 			<br />
-				<?php echo $topic->getFirstPostTime()->toKunena('config_post_dateformat'); ?>
+			<?php echo JText::_('COM_KUNENA_POSTED')?>
+			<?php echo $topic->getFirstPostTime()->toKunena('config_post_dateformat'); ?>
 			<?php endif; ?>
 			<div class="pull-right hidden-phone">
 				<?php /** TODO: New Feature - LABELS

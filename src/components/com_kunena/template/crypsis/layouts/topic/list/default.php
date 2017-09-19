5ca64b766e418261a7ecf57ebe62b3bf3575afab
Ruud <info@onlinecommunityhub.nl>
2017-09-19 10:35:08 +0200

    Add socials buttons to list view

--- src/components/com_kunena/template/crypsis/layouts/topic/list/default.php
+++ src/components/com_kunena/template/crypsis/layouts/topic/list/default.php
@@ -14,12 +14,19 @@ $cols = !empty($this->actions) ? 6 : 5;
 $colspan = !empty($this->actions) ? 4 : 3;
 $view = JFactory::getApplication()->input->getWord('view');
 $this->ktemplate = KunenaFactory::getTemplate();
+$social = $this->ktemplate->params->get('socialshare');
 
 $this->addStyleSheet('assets/css/rating.css');
 ?>
 <div><?php echo $this->subLayout('Widget/Module')->set('position', 'kunena_list_top'); ?></div>
 <div class="row-fluid">
 	<div class="span12">
+	<?php if ($social == 1) : ?>
+		<div><?php echo $this->subLayout('Widget/Social'); ?></div>
+	<?php endif; ?>
+	<?php if ($social == 2) : ?>
+		<div><?php echo $this->subLayout('Widget/Socialcustomtag'); ?></div>
+	<?php endif; ?>
 		<div class="pull-left">
 			<h1>
 				<?php echo $this->escape($this->headerText); ?>

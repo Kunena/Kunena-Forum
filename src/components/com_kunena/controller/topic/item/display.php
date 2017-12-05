d1da1f3dc976e2ada018a004d3be5016f026486a
Jelle Kok <schumacher1986@hotmail.com>
2017-12-06 00:14:39 +0100

    Add support for twitter cards #5445

--- src/components/com_kunena/controller/topic/item/display.php
+++ src/components/com_kunena/controller/topic/item/display.php
@@ -469,6 +469,10 @@ class ComponentKunenaControllerTopicItemDisplay extends KunenaControllerDisplay
 		$doc->setMetaData('og:image', $image, 'property');
 		$doc->setMetaData('article:published_time', $this->topic->getFirstPostTime()->toKunena('config_post_dateformat'), 'property');
 		$doc->setMetaData('article:section', $this->topic->getCategory()->name, 'property');
+		$doc->setMetaData('twitter:card', $this->topic->first_post_message, 'name');
+		$doc->setMetaData('twitter:title', $this->topic->displayField('subject'), 'name');
+		$doc->setMetaData('twitter:image', $image, 'property');
+		$doc->setMetaData('twitter:description', $this->topic->first_post_message);
 
 		$config = \Joomla\CMS\Factory::getConfig();
 		$robots = $config->get('robots');

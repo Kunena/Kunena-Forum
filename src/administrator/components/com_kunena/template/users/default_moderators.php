d52904dce81dd11098c8083e63ab4de2a26bb092
Jelle Kok <schumacher1986@hotmail.com>
2017-12-28 04:41:41 +0100

    improve backend modals

--- src/administrator/components/com_kunena/template/users/default_moderators.php
+++ src/administrator/components/com_kunena/template/users/default_moderators.php
@@ -12,24 +12,28 @@
 defined('_JEXEC') or die;
 ?>
 <div class="modal hide fade" id="moderateModal">
-	<div class="modal-header">
-		<button type="button" role="presentation" class="close" data-dismiss="modal">x</button>
-		<h3><?php echo JText::_('COM_KUNENA_BATCH_USERS_OPTIONS'); ?></h3>
-	</div>
-	<div class="modal-body">
-		<p><?php echo JText::_('COM_KUNENA_BATCH_USERS_TIP'); ?></p>
-		<div class="control-group">
-			<div class="controls">
-				<?php echo $this->modcatlist; ?>
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" role="presentation" class="close" data-dismiss="modal">x</button>
+				<h3><?php echo JText::_('COM_KUNENA_BATCH_USERS_OPTIONS'); ?></h3>
+			</div>
+			<div class="modal-body span11">
+				<p><?php echo JText::_('COM_KUNENA_BATCH_USERS_TIP'); ?></p>
+				<div class="control-group">
+					<div class="controls">
+						<?php echo $this->modcatlist; ?>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button class="btn" type="button" onclick="document.id('catid').value='';" data-dismiss="modal">
+					<?php echo JText::_('JCANCEL'); ?>
+				</button>
+				<button class="btn btn-primary" type="submit" onclick="Joomla.submitbutton('batch_moderators');">
+					<?php echo JText::_('JSUBMIT'); ?>
+				</button>
 			</div>
 		</div>
-	</div>
-	<div class="modal-footer">
-		<button class="btn" type="button" onclick="document.id('catid').value='';" data-dismiss="modal">
-			<?php echo JText::_('JCANCEL'); ?>
-		</button>
-		<button class="btn btn-primary" type="submit" onclick="Joomla.submitbutton('batch_moderators');">
-			<?php echo JText::_('JSUBMIT'); ?>
-		</button>
 	</div>
 </div>

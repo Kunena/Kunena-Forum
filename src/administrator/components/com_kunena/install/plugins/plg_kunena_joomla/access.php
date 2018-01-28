19601e0411ca6f3e56802059d39c701a41eb433a
Jelle Kok <schumacher1986@hotmail.com>
2018-01-28 20:31:53 +0100

    By editing category permissions you can't select more than one group  #2488 (#5711)

--- src/administrator/components/com_kunena/install/plugins/plg_kunena_joomla/access.php
+++ src/administrator/components/com_kunena/install/plugins/plg_kunena_joomla/access.php
@@ -73,7 +73,7 @@ class KunenaAccessJoomla
 			$accessname = JText::sprintf($category->pub_recurse ? 'COM_KUNENA_A_GROUP_X_PLUS' : 'COM_KUNENA_A_GROUP_X_ONLY', $groupname ? JText::_($groupname) : JText::_('COM_KUNENA_NOBODY'));
 
 			$list["joomla.group.{$category->pub_access}"] = array('type'  => 'joomla.group', 'id' => $category->pub_access, 'alias' => $accessname,
-																  'title' => $accessname, );
+			                                                      'title' => $accessname, );
 
 			$groupname = $this->getGroupName($category->accesstype, $category->admin_access);
 
@@ -81,14 +81,14 @@ class KunenaAccessJoomla
 			{
 				$accessname                                     = JText::sprintf($category->admin_recurse ? 'COM_KUNENA_A_GROUP_X_PLUS' : 'COM_KUNENA_A_GROUP_X_ONLY', JText::_($groupname));
 				$list["joomla.group.{$category->admin_access}"] = array('type'  => 'joomla.group', 'id' => $category->admin_access, 'alias' => $accessname,
-																		'title' => $accessname, );
+				                                                        'title' => $accessname, );
 			}
 		}
 		else
 		{
 			$groupname                                = $this->getGroupName($category->accesstype, $category->access);
 			$list["joomla.level.{$category->access}"] = array('type'  => 'joomla.level', 'id' => $category->access, 'alias' => $groupname,
-															  'title' => $groupname, );
+			                                                  'title' => $groupname, );
 		}
 
 		return $list;
@@ -191,8 +191,9 @@ class KunenaAccessJoomla
 				'desc'  => JText::_('PLG_KUNENA_JOOMLA_ACCESS_GROUP_PRIMARY_DESC') . '<br /><br />' .
 					JText::_('PLG_KUNENA_JOOMLA_ACCESS_GROUP_PRIMARY_DESC2') . '<br /><br />' .
 					JText::_('PLG_KUNENA_JOOMLA_ACCESS_GROUP_PRIMARY_DESC_J25'),
-				'input' => JHtml::_('access.usergroup', 'pub_access', $category->pub_access, 'class="inputbox" size="10"', false),
-			);
+				'input' => JHtml::_('access.usergroup', 'params-joomla-group[pub_access][]',
+					$category->params->get('pub_access', array($category->pub_access)), 'multiple="multiple" class="inputbox" size="10"', false
+				));
 			$html ['joomla-group']['pub_recurse']   = array(
 				'title' => JText::_('PLG_KUNENA_JOOMLA_ACCESS_GROUP_PRIMARY_CHILDS_TITLE'),
 				'desc'  => JText::_('PLG_KUNENA_JOOMLA_ACCESS_GROUP_PRIMARY_CHILDS_DESC'),
@@ -203,8 +204,9 @@ class KunenaAccessJoomla
 				'desc'  => JText::_('PLG_KUNENA_JOOMLA_ACCESS_GROUP_SECONDARY_DESC') . '<br /><br />' .
 					JText::_('PLG_KUNENA_JOOMLA_ACCESS_GROUP_SECONDARY_DESC2') . '<br /><br />' .
 					JText::_('PLG_KUNENA_JOOMLA_ACCESS_GROUP_SECONDARY_DESC_J25'),
-				'input' => JHtml::_('access.usergroup', 'admin_access', $category->admin_access, 'class="inputbox" size="10"', false),
-			);
+				'input' => JHtml::_('access.usergroup', 'params-joomla-group[admin_access][]',
+					$category->params->get('admin_access', array($category->pub_access)), 'multiple="multiple" class="inputbox" size="10"', false
+				));
 			$html ['joomla-group']['admin_recurse'] = array(
 				'title' => JText::_('PLG_KUNENA_JOOMLA_ACCESS_GROUP_SECONDARY_CHILDS_TITLE'),
 				'desc'  => JText::_('PLG_KUNENA_JOOMLA_ACCESS_GROUP_SECONDARY_CHILDS_DESC'),

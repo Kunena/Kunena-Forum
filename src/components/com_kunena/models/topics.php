47fbfc0d542a72f890db7745c096f3d0bed27b6e
Jelle Kok <schumacher1986@hotmail.com>
2018-03-04 03:53:48 +0100

    remove checktoken request

--- src/components/com_kunena/models/topics.php
+++ src/components/com_kunena/models/topics.php
@@ -169,7 +169,7 @@ class KunenaModelTopics extends KunenaModel
 		if (\Joomla\CMS\Factory::getDocument()->getType() != 'feed')
 		{
 			// Selection time from user state / menu item / url parameter / configuration.
-			if (!\Joomla\CMS\Session\Session::checkToken('request') || !$this->me->exists() || $this->me->exists() && $this->me->userListtime == -2)
+			if (!$this->me->exists() || $this->me->exists() && $this->me->userListtime == -2)
 			{
 				$value = $this->getUserStateFromRequest("com_kunena.topics_{$active}_{$layout}_{$mode}_{$userid}_{$catid}_list_time", 'sel', $params->get('topics_time', $this->config->show_list_time), 'int');
 				$this->setState('list.time', (int) $value);

e24193eb72a92daf66103f35dd1850f92d506858
Jelle Kok <schumacher1986@hotmail.com>
2018-04-13 22:45:22 +0200

    Fix J4 issue loading error

--- src/libraries/kunena/user/user.php
+++ src/libraries/kunena/user/user.php
@@ -2137,13 +2137,16 @@ class KunenaUser extends JObject
 				return $this->userid;
 		}
 
-		$trace = debug_backtrace();
-		trigger_error(
-			'Undefined property via __get(): ' . $name .
-			' in ' . $trace[0]['file'] .
-			' on line ' . $trace[0]['line'],
-			E_USER_NOTICE
-		);
+		if (version_compare(JVERSION, '4.0', '<'))
+		{
+			$trace = debug_backtrace();
+			trigger_error(
+				'Undefined property via __get(): ' . $name .
+				' in ' . $trace[0]['file'] .
+				' on line ' . $trace[0]['line'],
+				E_USER_NOTICE
+			);
+		}
 
 		return;
 	}

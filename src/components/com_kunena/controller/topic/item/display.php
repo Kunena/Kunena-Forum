eca225489c3ed002880b54d290065670d4d9c03f
Jelle Kok <schumacher1986@hotmail.com>
2018-05-02 21:59:43 +0200

    Twitter card not loading  #5887

--- src/components/com_kunena/controller/topic/item/display.php
+++ src/components/com_kunena/controller/topic/item/display.php
@@ -491,7 +491,7 @@ class ComponentKunenaControllerTopicItemDisplay extends KunenaControllerDisplay
 		$doc->setMetaData('og:image', $image, 'property');
 		$doc->setMetaData('article:published_time', $this->topic->getFirstPostTime()->toKunena('config_post_dateformat'), 'property');
 		$doc->setMetaData('article:section', $this->topic->getCategory()->name, 'property');
-		$doc->setMetaData('twitter:card', $first, 'name');
+		$doc->setMetaData('twitter:card', 'summary', 'name');
 		$doc->setMetaData('twitter:title', $this->topic->displayField('subject'), 'name');
 		$doc->setMetaData('twitter:image', $image, 'property');
 		$doc->setMetaData('twitter:description', $first);

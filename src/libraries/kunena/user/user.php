5118253a9f1a1bca08e60522cc95f648666eb063
sshcli <sshcli@users.noreply.github.com>
2018-05-16 11:46:30 -0400

    Remove blank space

--- src/libraries/kunena/user/user.php
+++ src/libraries/kunena/user/user.php
@@ -2130,7 +2130,7 @@ class KunenaUser extends JObject
 						'telegram'   => array('url' => 'https://t.me/##VALUE##', 'title' => JText::_('COM_KUNENA_MYPROFILE_TELEGRAM'), 'nourl' => '0'),
 						'apple'      => array('url' => '##VALUE##', 'title' => JText::_('COM_KUNENA_MYPROFILE_APPLE'), 'nourl' => '1'),
 						'whatsapp'   => array('url' => '##VALUE##', 'title' => JText::_('COM_KUNENA_MYPROFILE_WHATSAPP'), 'nourl' => '1'),
-						'youtube'    => array('url' => 'https://www.youtube.com/##VALUE##', 'title' => JText::_(' COM_KUNENA_MYPROFILE_YOUTUBE'), 'nourl' => '0'),
+						'youtube'    => array('url' => 'https://www.youtube.com/##VALUE##', 'title' => JText::_('COM_KUNENA_MYPROFILE_YOUTUBE'), 'nourl' => '0'),
 						'ok'         => array('url' => 'https://ok.ru/##VALUE##', 'title' => JText::_('COM_KUNENA_MYPROFILE_OK'), 'nourl' => '0'),
 		);
 

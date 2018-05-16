257da7e9d886d72310aee411644b093dcba72be0
sshcli <sshcli@users.noreply.github.com>
2018-05-16 16:44:27 -0400

    Outdated FA URL

--- src/administrator/components/com_kunena/views/user/view.html.php
+++ src/administrator/components/com_kunena/views/user/view.html.php
@@ -39,7 +39,7 @@ class KunenaAdminViewUser extends KunenaView
 
 		if (KunenaFactory::getTemplate()->params->get('fontawesome'))
 		{
-			Factory::getDocument()->addScript('https://use.fontawesome.com/releases/v5.0.6/js/all.js', array(), array('defer' => true));
+			Factory::getDocument()->addScript('https://use.fontawesome.com/releases/v5.0.13/js/all.js', array(), array('defer' => true));
 		}
 
 		// Make the select list for the moderator flag

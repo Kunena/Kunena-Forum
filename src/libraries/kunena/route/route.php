3efeb2d401f069a9bef3e51aac78a67fb807bd45
Jelle Kok <schumacher1986@hotmail.com>
2018-05-28 13:34:21 +0200

    remove routefix

--- src/libraries/kunena/route/route.php
+++ src/libraries/kunena/route/route.php
@@ -1107,11 +1107,6 @@ abstract class KunenaRoute
 		KUNENA_PROFILER ? KunenaProfiler::instance()->start('function ' . __CLASS__ . '::' . __FUNCTION__ . '()') : null;
 		$uri = self::prepare($uri);
 
-		if (!$uri)
-		{
-			$uri->setVar('Itemid', self::fixMissingItemID());
-		}
-
 		if (!$uri->getVar('Itemid'))
 		{
 			self::setItemID($uri);

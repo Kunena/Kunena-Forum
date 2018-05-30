3cb2600a5a7900074759f2d1d8453fc10394bc3a
sshcli <sshcli@users.noreply.github.com>
2018-05-30 13:34:55 -0400

    Update some references (#5959)

--- src/components/com_kunena/template/crypsis/layouts/category/manage/default.php
+++ src/components/com_kunena/template/crypsis/layouts/category/manage/default.php
@@ -6,7 +6,7 @@
  * @subpackage      Layout.Category
  *
  * @copyright   (C) 2008 - 2016 Kunena Team. All rights reserved.
- * @license         http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link            https://www.kunena.org
  **/
 defined('_JEXEC') or die;

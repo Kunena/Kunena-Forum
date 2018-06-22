136a981885b8d6874b02623ee36b4dbe26a64b3b
sshcli <sshcli@users.noreply.github.com>
2018-06-22 13:35:33 -0400

    Update FA to 5.1.0 (#6007)

--- src/administrator/components/com_kunena/views/users/view.html.php
+++ src/administrator/components/com_kunena/views/users/view.html.php
@@ -50,7 +50,7 @@ class KunenaAdminViewUsers extends KunenaView
 
 		if (KunenaFactory::getTemplate()->params->get('fontawesome'))
 		{
-			Factory::getDocument()->addScript('https://use.fontawesome.com/releases/v5.0.13/js/all.js', array(), array('defer' => true));
+			Factory::getDocument()->addScript('https://use.fontawesome.com/releases/v5.1.0/js/all.js', array(), array('defer' => true));
 		}
 
 		$this->display();

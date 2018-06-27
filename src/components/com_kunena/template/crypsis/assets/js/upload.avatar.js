db80df5a74c54648129a1fbb9b1d7e2b1f505083
Jelle Kok <schumacher1986@hotmail.com>
2018-06-27 03:03:23 +0200

    Edit Avatar - text in dropzone and icon in button #5987

--- src/components/com_kunena/template/crypsis/assets/js/upload.avatar.js
+++ src/components/com_kunena/template/crypsis/assets/js/upload.avatar.js
@@ -8,7 +8,7 @@ jQuery(function ($) {
 	var removeButton = $('<button/>')
 		.addClass('btn btn-danger')
 		.attr('type', 'button')
-		.html('<i class="icon-trash"></i> ' + Joomla.JText._('COM_KUNENA_GEN_REMOVE_AVATAR'))
+		.html(Joomla.getOptions('com_kunena.avatar_delete') + ' ' + Joomla.JText._('COM_KUNENA_GEN_REMOVE_AVATAR'))
 		.on('click', function () {
 			var $this = $(this),
 				data = $this.data();

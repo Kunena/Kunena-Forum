f88573bfb44a119f450b4532f2a7e218169fff40
sshcli <sshcli@users.noreply.github.com>
2018-09-16 14:01:42 -0400

    Add module position in custom content (#6113)

--- src/components/com_kunena/template/crypsis/layouts/widget/custom/default.php
+++ src/components/com_kunena/template/crypsis/layouts/widget/custom/default.php
@@ -11,4 +11,4 @@
 defined('_JEXEC') or die;
 ?>
 <h1><?php echo $this->header; ?></h1>
-<div class="well well-small"><?php echo $this->body; ?></div>
+<div class="well well-small"><div class="kcustom-top"><?php echo $this->subLayout('Widget/Module')->set('position', 'kunena_custom_top'); ?></div><?php echo $this->body; ?><div class="kcustom-bottom"><?php echo $this->subLayout('Widget/Module')->set('position', 'kunena_custom_bottom'); ?></div></div>
--- src/components/com_kunena/template/crypsisb3/layouts/widget/custom/default.php
+++ src/components/com_kunena/template/crypsisb3/layouts/widget/custom/default.php
@@ -11,4 +11,4 @@
 defined('_JEXEC') or die;
 ?>
 <h3><?php echo $this->header; ?></h3>
-<div class="well well-small"><?php echo $this->body; ?></div>
+<div class="well well-small"><div class="kcustom-top"><?php echo $this->subLayout('Widget/Module')->set('position', 'kunena_custom_top'); ?></div><?php echo $this->body; ?><div class="kcustom-bottom"><?php echo $this->subLayout('Widget/Module')->set('position', 'kunena_custom_bottom'); ?></div></div>

3f509deec3449fc1fa7d79598f2d8b48ef7883e0
sshcli <sshcli@users.noreply.github.com>
2018-11-05 23:08:15 -0400

    Update FA to 5.5.0 (#6148)

--- src/components/com_kunena/template/crypsisb3/template.php
+++ src/components/com_kunena/template/crypsisb3/template.php
@@ -136,8 +136,8 @@ class KunenaTemplateCrypsisb3 extends KunenaTemplate
 
 		if ($fontawesome)
 		{
-			$this->addScript('https://use.fontawesome.com/releases/v5.4.2/js/all.js', array(), array('defer' => true));
-			$this->addScript('https://use.fontawesome.com/releases/v5.4.2/js/v4-shims.js', array(), array('defer' => true));
+			$this->addScript('https://use.fontawesome.com/releases/v5.5.0/js/all.js', array(), array('defer' => true));
+			$this->addScript('https://use.fontawesome.com/releases/v5.5.0/js/v4-shims.js', array(), array('defer' => true));
 		}
 
 		$icons = $this->ktemplate->params->get('icons');

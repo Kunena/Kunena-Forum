5f5429590d3fab1145287600134ac8f708df6b91
Jelle Kok <schumacher1986@hotmail.com>
2018-12-07 03:29:34 +0100

    [DOM] Found 2 elements with non-unique id #6176

--- src/components/com_kunena/template/system/layouts/widget/login/login/login.php
+++ src/components/com_kunena/template/system/layouts/widget/login/login/login.php
@@ -30,8 +30,8 @@ use Joomla\CMS\Language\Text;
 							<?php echo Text::_('JGLOBAL_USERNAME'); ?>
 						</label>
 					</span>
-					<input id="klogin-username" type="text" name="username" class="input-small" tabindex="1"
-					       size="18" placeholder="<?php echo Text::_('JGLOBAL_USERNAME'); ?>"/>
+					<input id="klogin-username-full" type="text" name="username" class="input-small" tabindex="1"
+					       size="18" autocomplete="username" placeholder="<?php echo Text::_('JGLOBAL_USERNAME'); ?>"/>
 				</div>
 			</div>
 		</div>
@@ -45,8 +45,8 @@ use Joomla\CMS\Language\Text;
 							<?php echo Text::_('JGLOBAL_PASSWORD'); ?>
 						</label>
 					</span>
-					<input id="klogin-passwd" type="password" name="password" class="input-small" tabindex="2"
-					       size="18" placeholder="<?php echo Text::_('JGLOBAL_PASSWORD'); ?>"/>
+					<input id="klogin-passwd-full" type="password" name="password" class="input-small" tabindex="2"
+					       size="18" autocomplete="current-password" placeholder="<?php echo Text::_('JGLOBAL_PASSWORD'); ?>"/>
 				</div>
 			</div>
 		</div>

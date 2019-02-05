32a944c4325be74ba8a9abbcf6e5b64322991031
Jelle Kok <schumacher1986@hotmail.com>
2019-02-05 23:29:06 +0100

    Users can change the complete title in a topic #6177

--- src/components/com_kunena/controllers/topic.php
+++ src/components/com_kunena/controllers/topic.php
@@ -865,7 +865,7 @@ class KunenaControllerTopic extends KunenaController
 		}
 
 		// Check if we are editing first post and update topic if we are!
-		if ($topic->first_post_id == $message->id || KunenaConfig::getInstance()->allow_change_subject || KunenaUserHelper::getMyself()->isModerator())
+		if ($topic->first_post_id == $message->id || KunenaConfig::getInstance()->allow_change_subject && $topic->first_post_userid == $message->userid || KunenaUserHelper::getMyself()->isModerator())
 		{
 			$topic->subject = $fields['subject'];
 		}

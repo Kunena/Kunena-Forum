776063972937d8af0e3ac6a2faf77abb828cdb19
Jelle Kok <schumacher1986@hotmail.com>
2019-04-30 16:40:51 +0200

    Backend: template fix menu

--- src/administrator/components/com_kunena/template/j4/templates/editless.php
+++ src/administrator/components/com_kunena/template/j4/templates/editless.php
@@ -16,39 +16,41 @@ use Joomla\CMS\Language\Text;
 ?>
 <div id="kunena" class="container-fluid">
 	<div class="row">
-		<div class="col-md-2 d-none d-md-block sidebar">
-			<div id="sidebar">
-				<nav class="sidebar-nav"><?php include KPATH_ADMIN . '/template/j4/common/menu.php'; ?></nav>
-			</div>
-		</div>
-		<div id="j-main-container" class="col-md-10" role="main">
-			<form action="<?php echo KunenaRoute::_('administrator/index.php?option=com_kunena&view=templates') ?>"
-				  method="post" id="adminForm"
-				  name="adminForm">
-				<input type="hidden" name="task" value=""/>
-				<input type="hidden" name="id" value="<?php echo $this->escape($this->templatename); ?>"/>
-				<input type="hidden" name="cid[]" value="<?php echo $this->escape($this->templatename); ?>"/>
-				<input type="hidden" name="filename" value="<?php echo $this->escape($this->filename); ?>"/>
-				<?php echo HTMLHelper::_('form.token'); ?>
+		<div id="j-main-container" class="col-md-12" role="main">
+			<div class="card card-block bg-faded p-2">
+				<div class="module-title nav-header">
+					<i class="icon-color-palette"></i>
+					<?php echo Text::_('COM_KUNENA_A_TEMPLATE_MANAGER') ?>
+				</div>
+				<hr class="hr-condensed">
+				<form action="<?php echo KunenaRoute::_('administrator/index.php?option=com_kunena&view=templates') ?>"
+					  method="post" id="adminForm"
+					  name="adminForm">
+					<input type="hidden" name="task" value=""/>
+					<input type="hidden" name="id" value="<?php echo $this->escape($this->templatename); ?>"/>
+					<input type="hidden" name="cid[]" value="<?php echo $this->escape($this->templatename); ?>"/>
+					<input type="hidden" name="filename" value="<?php echo $this->escape($this->filename); ?>"/>
+					<?php echo HTMLHelper::_('form.token'); ?>
 
-				<?php // TODO: redo FTP protection fields ?>
-				<fieldset>
-					<legend><?php echo Text::_('COM_KUNENA_A_TEMPLATE_MANAGER_EDIT_LESS_TEMPLATE'); ?></legend>
-					<table class="table table-bordered table-striped">
-						<tr>
-							<th>
-								<?php echo $this->escape($this->less_path); ?>
-							</th>
-						</tr>
-						<tr>
-							<td>
-								<textarea class="input-xxlarge" cols="110" rows="25"
-										  name="filecontent"><?php echo $this->content; ?></textarea>
-							</td>
-						</tr>
-					</table>
-				</fieldset>
-			</form>
+					<?php // TODO: redo FTP protection fields ?>
+					<fieldset>
+						<legend><?php echo Text::_('COM_KUNENA_A_TEMPLATE_MANAGER_EDIT_LESS_TEMPLATE'); ?></legend>
+						<table class="table table-bordered table-striped">
+							<tr>
+								<th>
+									<?php echo $this->escape($this->less_path); ?>
+								</th>
+							</tr>
+							<tr>
+								<td>
+									<textarea class="input-xxlarge" cols="110" rows="25"
+											  name="filecontent"><?php echo $this->content; ?></textarea>
+								</td>
+							</tr>
+						</table>
+					</fieldset>
+				</form>
+			</div>
 		</div>
 	</div>
 	<div class="pull-right small">

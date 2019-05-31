705723392f19a49e79c43813781ccc4bac7cf11f
Florian DAL FITTO <xillibit@gmail.com>
2019-05-31 17:04:13 +0200

    User Ban Error | Exception throw at line 419 in file... #6377 #2165 (#6395)

---
+++ src/administrator/components/com_kunena/install/sql/updates/php/6.0.0-2019-05-31_banneddatetimedefault.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Kunena Component
+ *
+ * @package        Kunena.Installer
+ *
+ * @copyright      Copyright (C) 2008 - 2019 Kunena Team. All rights reserved.
+ * @license        https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link           https://www.kunena.org
+ **/
+defined('_JEXEC') or die();
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+
+// Kunena 6.0.0: Update default value for banned users
+/**
+ * @param $parent
+ *
+ * @return array
+ * @throws Exception
+ * @since Kunena
+ */
+function kunena_600_2019_31_banneddatetimedefault($parent)
+{
+	$db  = Factory::getDbo();
+
+	$query    = "UPDATE `#___kunena_users` SET banned='9999-12-31 23:59:59' WHERE banned='0000-00-00 00:00:00'";
+	$db->setQuery((string) $query);
+
+	try
+	{
+		$db->execute();
+	}
+	catch (Exception $e)
+	{
+		throw new KunenaInstallerException($e->getMessage(), $e->getCode());
+	}
+
+	return array('action' => '', 'name' => Text::_('COM_KUNENA_INSTALL_600_BANNED_DATETIME_DEFAULT'), 'success' => true);
+}

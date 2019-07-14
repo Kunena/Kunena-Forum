02b940be430624232fe571dbcbdef5614b60d8d2
xillibit <xillibit@gmail.com>
2019-07-14 16:42:06 +0200

    Plugins | Notice: Trying to get property 'input' of non-object #6499

--- src/administrator/components/com_kunena/controllers/plugins.php
+++ src/administrator/components/com_kunena/controllers/plugins.php
@@ -43,7 +43,6 @@ class KunenaAdminControllerPlugins extends KunenaController
 	public function __construct($config = array())
 	{
 		$this->option = 'com_kunena';
-		$this->input  = $this->app->input;
 
 		parent::__construct($config);
 		$this->baseurl     = 'administrator/index.php?option=com_kunena&view=plugins';

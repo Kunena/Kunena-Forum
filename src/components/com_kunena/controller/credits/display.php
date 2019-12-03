770d590601e3399ad6e369a28ed972077d0388ec
xillibit <xillibit@gmail.com>
2019-12-03 22:24:57 +0100

    Issue since last J! change can't access property params in menu (Part 3)

--- src/components/com_kunena/controller/credits/display.php
+++ src/components/com_kunena/controller/credits/display.php
@@ -125,7 +125,7 @@ class ComponentKunenaControllerCreditsDisplay extends KunenaControllerDisplay
 
 		if ($menu_item)
 		{
-			$params             = $menu_item->params;
+			$params             = $menu_item->getParams();
 			$params_title       = $params->get('page_title');
 			$params_keywords    = $params->get('menu-meta_keywords');
 			$params_description = $params->get('menu-meta_description');
--- src/components/com_kunena/controller/statistics/general/display.php
+++ src/components/com_kunena/controller/statistics/general/display.php
@@ -83,7 +83,7 @@ class ComponentKunenaControllerStatisticsGeneralDisplay extends KunenaController
 
 		if ($menu_item)
 		{
-			$params             = $menu_item->params;
+			$params             = $menu_item->getParams();
 			$params_title       = $params->get('page_title');
 			$params_keywords    = $params->get('menu-meta_keywords');
 			$params_description = $params->get('menu-meta_description');

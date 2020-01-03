d036f93025bd66da664b7f4f4df01bc609a2375d
Florian DAL FITTO <xillibit@gmail.com>
2020-01-03 18:14:27 +0100

    Make ranks title translatable (#6699)

--- src/administrator/components/com_kunena/install/sql/install/php/sampledata.php
+++ src/administrator/components/com_kunena/install/sql/install/php/sampledata.php
@@ -80,16 +80,16 @@ function installSampleData()
 
 	$query = "INSERT INTO `#__kunena_ranks`
 	(`rank_id`, `rank_title`, `rank_min`, `rank_special`, `rank_image`) VALUES
-	(1, {$db->quote(KText::_('COM_KUNENA_SAMPLEDATA_RANK1'))}, 0, 0, 'rank1.gif'),
-	(2, {$db->quote(KText::_('COM_KUNENA_SAMPLEDATA_RANK2'))}, 20, 0, 'rank2.gif'),
-	(3, {$db->quote(KText::_('COM_KUNENA_SAMPLEDATA_RANK3'))}, 40, 0, 'rank3.gif'),
-	(4, {$db->quote(KText::_('COM_KUNENA_SAMPLEDATA_RANK4'))}, 80, 0, 'rank4.gif'),
-	(5, {$db->quote(KText::_('COM_KUNENA_SAMPLEDATA_RANK5'))}, 160, 0, 'rank5.gif'),
-	(6, {$db->quote(KText::_('COM_KUNENA_SAMPLEDATA_RANK6'))}, 320, 0, 'rank6.gif'),
-	(7, {$db->quote(KText::_('COM_KUNENA_SAMPLEDATA_RANK_ADMIN'))}, 0, 1, 'rankadmin.gif'),
-	(8, {$db->quote(KText::_('COM_KUNENA_SAMPLEDATA_RANK_MODERATOR'))}, 0, 1, 'rankmod.gif'),
-	(9, {$db->quote(KText::_('COM_KUNENA_SAMPLEDATA_RANK_SPAMMER'))}, 0, 1, 'rankspammer.gif'),
-	(10, {$db->quote(KText::_('COM_KUNENA_SAMPLEDATA_RANK_BANNED'))}, 0, 1, 'rankbanned.gif');";
+	(1, {$db->quote('COM_KUNENA_SAMPLEDATA_RANK1')}, 0, 0, 'rank1.gif'),
+	(2, {$db->quote('COM_KUNENA_SAMPLEDATA_RANK2')}, 20, 0, 'rank2.gif'),
+	(3, {$db->quote('COM_KUNENA_SAMPLEDATA_RANK3')}, 40, 0, 'rank3.gif'),
+	(4, {$db->quote('COM_KUNENA_SAMPLEDATA_RANK4')}, 80, 0, 'rank4.gif'),
+	(5, {$db->quote('COM_KUNENA_SAMPLEDATA_RANK5')}, 160, 0, 'rank5.gif'),
+	(6, {$db->quote('COM_KUNENA_SAMPLEDATA_RANK6')}, 320, 0, 'rank6.gif'),
+	(7, {$db->quote('COM_KUNENA_SAMPLEDATA_RANK_ADMIN')}, 0, 1, 'rankadmin.gif'),
+	(8, {$db->quote('COM_KUNENA_SAMPLEDATA_RANK_MODERATOR')}, 0, 1, 'rankmod.gif'),
+	(9, {$db->quote('COM_KUNENA_SAMPLEDATA_RANK_SPAMMER')}, 0, 1, 'rankspammer.gif'),
+	(10, {$db->quote('COM_KUNENA_SAMPLEDATA_RANK_BANNED')}, 0, 1, 'rankbanned.gif');";
 
 	$queries[] = array('kunena_ranks', $query);
 
---
+++ src/administrator/components/com_kunena/install/sql/updates/php/6.0.0-2020-01-03_updaterankstitle.php
@@ -0,0 +1,269 @@
+<?php
+/**
+ * Kunena Component
+ *
+ * @package        Kunena.Installer
+ *
+ * @copyright      Copyright (C) 2008 - 2020 Kunena Team. All rights reserved.
+ * @license        https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link           https://www.kunena.org
+ **/
+defined('_JEXEC') or die();
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+
+// Kunena 6.0.0: Update ranks title value to have string which can be translated
+/**
+ * @param $parent
+ *
+ * @return array
+ * @since Kunena
+ * @throws Exception
+ */
+function kunena_600_2020_01_03_updaterankstitle($parent)
+{
+	$db = Factory::getDbo();
+
+	$query = $db->getQuery(true);
+
+	// Fields to update.
+	$fields = array(
+		$db->quoteName('title') . ' = ' . $db->quote('COM_KUNENA_SAMPLEDATA_RANK1')
+	);
+
+	// Conditions for which records should be updated.
+	$conditions = array(
+		$db->quoteName('id') . ' = 1'
+	);
+
+	$query->update($db->quoteName('#__kunena_ranks'))->set($fields)->where($conditions);
+	$db->setQuery($query);
+
+	try
+	{
+		$db->execute();
+	}
+	catch (Exception $e)
+	{
+		throw new KunenaInstallerException($e->getMessage(), $e->getCode());
+	}
+
+	$query = $db->getQuery(true);
+	
+	// Fields to update.
+	$fields = array(
+		$db->quoteName('title') . ' = ' . $db->quote('COM_KUNENA_SAMPLEDATA_RANK2')
+	);
+	
+	// Conditions for which records should be updated.
+	$conditions = array(
+		$db->quoteName('id') . ' = 2'
+	);
+	
+	$query->update($db->quoteName('#__kunena_ranks'))->set($fields)->where($conditions);
+	$db->setQuery($query);
+	
+	try
+	{
+		$db->execute();
+	}
+	catch (Exception $e)
+	{
+		throw new KunenaInstallerException($e->getMessage(), $e->getCode());
+	}
+	
+	$query = $db->getQuery(true);
+	
+	// Fields to update.
+	$fields = array(
+		$db->quoteName('title') . ' = ' . $db->quote('COM_KUNENA_SAMPLEDATA_RANK3')
+	);
+	
+	// Conditions for which records should be updated.
+	$conditions = array(
+		$db->quoteName('id') . ' = 3'
+	);
+	
+	$query->update($db->quoteName('#__kunena_ranks'))->set($fields)->where($conditions);
+	$db->setQuery($query);
+	
+	try
+	{
+		$db->execute();
+	}
+	catch (Exception $e)
+	{
+		throw new KunenaInstallerException($e->getMessage(), $e->getCode());
+	}
+	
+	$query = $db->getQuery(true);
+	
+	// Fields to update.
+	$fields = array(
+		$db->quoteName('title') . ' = ' . $db->quote('COM_KUNENA_SAMPLEDATA_RANK4')
+	);
+	
+	// Conditions for which records should be updated.
+	$conditions = array(
+		$db->quoteName('id') . ' = 4'
+	);
+	
+	$query->update($db->quoteName('#__kunena_ranks'))->set($fields)->where($conditions);
+	$db->setQuery($query);
+	
+	try
+	{
+		$db->execute();
+	}
+	catch (Exception $e)
+	{
+		throw new KunenaInstallerException($e->getMessage(), $e->getCode());
+	}
+	
+	$query = $db->getQuery(true);
+	
+	// Fields to update.
+	$fields = array(
+		$db->quoteName('title') . ' = ' . $db->quote('COM_KUNENA_SAMPLEDATA_RANK5')
+	);
+	
+	// Conditions for which records should be updated.
+	$conditions = array(
+		$db->quoteName('id') . ' = 5'
+	);
+	
+	$query->update($db->quoteName('#__kunena_ranks'))->set($fields)->where($conditions);
+	$db->setQuery($query);
+	
+	try
+	{
+		$db->execute();
+	}
+	catch (Exception $e)
+	{
+		throw new KunenaInstallerException($e->getMessage(), $e->getCode());
+	}
+	
+	$query = $db->getQuery(true);
+	
+	// Fields to update.
+	$fields = array(
+		$db->quoteName('title') . ' = ' . $db->quote('COM_KUNENA_SAMPLEDATA_RANK6')
+	);
+	
+	// Conditions for which records should be updated.
+	$conditions = array(
+		$db->quoteName('id') . ' = 6'
+	);
+	
+	$query->update($db->quoteName('#__kunena_ranks'))->set($fields)->where($conditions);
+	$db->setQuery($query);
+	
+	try
+	{
+		$db->execute();
+	}
+	catch (Exception $e)
+	{
+		throw new KunenaInstallerException($e->getMessage(), $e->getCode());
+	}
+	
+	$query = $db->getQuery(true);
+	
+	// Fields to update.
+	$fields = array(
+		$db->quoteName('title') . ' = ' . $db->quote('COM_KUNENA_SAMPLEDATA_RANK_ADMIN')
+	);
+	
+	// Conditions for which records should be updated.
+	$conditions = array(
+		$db->quoteName('id') . ' = 7'
+	);
+	
+	$query->update($db->quoteName('#__kunena_ranks'))->set($fields)->where($conditions);
+	$db->setQuery($query);
+	
+	try
+	{
+		$db->execute();
+	}
+	catch (Exception $e)
+	{
+		throw new KunenaInstallerException($e->getMessage(), $e->getCode());
+	}
+	
+	$query = $db->getQuery(true);
+	
+	// Fields to update.
+	$fields = array(
+		$db->quoteName('title') . ' = ' . $db->quote('COM_KUNENA_SAMPLEDATA_RANK_MODERATOR')
+	);
+	
+	// Conditions for which records should be updated.
+	$conditions = array(
+		$db->quoteName('id') . ' = 8'
+	);
+	
+	$query->update($db->quoteName('#__kunena_ranks'))->set($fields)->where($conditions);
+	$db->setQuery($query);
+	
+	try
+	{
+		$db->execute();
+	}
+	catch (Exception $e)
+	{
+		throw new KunenaInstallerException($e->getMessage(), $e->getCode());
+	}
+	
+	$query = $db->getQuery(true);
+	
+	// Fields to update.
+	$fields = array(
+		$db->quoteName('title') . ' = ' . $db->quote('COM_KUNENA_SAMPLEDATA_RANK_SPAMMER')
+	);
+	
+	// Conditions for which records should be updated.
+	$conditions = array(
+		$db->quoteName('id') . ' = 9'
+	);
+	
+	$query->update($db->quoteName('#__kunena_ranks'))->set($fields)->where($conditions);
+	$db->setQuery($query);
+	
+	try
+	{
+		$db->execute();
+	}
+	catch (Exception $e)
+	{
+		throw new KunenaInstallerException($e->getMessage(), $e->getCode());
+	}
+	
+	$query = $db->getQuery(true);
+	
+	// Fields to update.
+	$fields = array(
+		$db->quoteName('title') . ' = ' . $db->quote('COM_KUNENA_SAMPLEDATA_RANK_BANNED')
+	);
+	
+	// Conditions for which records should be updated.
+	$conditions = array(
+		$db->quoteName('id') . ' = 10'
+	);
+	
+	$query->update($db->quoteName('#__kunena_ranks'))->set($fields)->where($conditions);
+	$db->setQuery($query);
+	
+	try
+	{
+		$db->execute();
+	}
+	catch (Exception $e)
+	{
+		throw new KunenaInstallerException($e->getMessage(), $e->getCode());
+	}
+
+	return array('action' => '', 'name' => Text::_('COM_KUNENA_INSTALL_600_RANKS_TITLE_VALUE_IN_TABLES'), 'success' => true);
+}

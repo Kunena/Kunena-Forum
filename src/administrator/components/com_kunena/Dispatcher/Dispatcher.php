856039e0b45e3f07a94403e98e4309c842c876fd
xillibit <xillibit@gmail.com>
2020-01-22 18:54:50 +0100

    Add dispatcher and remove useless file kunena.php at root of admin

---
+++ src/administrator/components/com_kunena/Dispatcher/Dispatcher.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Kunena Component
+ *
+ * @package         Kunena.Administrator
+ * @subpackage      Dispatcher
+ *
+ * @copyright       Copyright (C) 2008 - 2020 Kunena Team. All rights reserved.
+ * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            https://www.kunena.org
+ **/
+
+namespace Kunena\Forum\Administrator\Dispatcher;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Dispatcher\ComponentDispatcher;
+
+/**
+ * ComponentDispatcher class for com_kunena
+ *
+ * @since  6.0
+ */
+class Dispatcher extends ComponentDispatcher
+{
+	/**
+	 * Kunena have to check for extension permission
+	 *
+	 * @return  void
+	 */
+	protected function checkAccess()
+	{
+		if ($this->app->isClient('administrator') && !$this->app->getIdentity()->authorise('core.manage', 'com_kunena'))
+		{
+			throw new \KunenaExceptionAuthorise($this->app->getLanguage()->_('COM_KUNENA_NO_ACCESS'), 401);
+		}
+	}
+}

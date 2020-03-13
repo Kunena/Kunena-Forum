468a86ff80d44acb1df1d2586b9c191dc9d74546
Florian DAL FITTO <xillibit@gmail.com>
2020-03-13 22:45:30 +0100

    Try to fix Kunena install (#6762)

--- src/administrator/components/com_kunena/services/provider.php
+++ src/administrator/components/com_kunena/services/provider.php
@@ -12,14 +12,14 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Component\Router\RouterFactoryInterface;
 use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
+use Joomla\CMS\Component\Router\RouterFactoryInterface;
 use Joomla\CMS\Extension\ComponentInterface;
 use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
 use Joomla\CMS\Extension\Service\Provider\MVCFactory;
-use Joomla\CMS\Extension\Service\Provider\RouterFactory;
 use Joomla\CMS\HTML\Registry;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\CMS\Extension\Service\Provider\RouterFactory;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Kunena\Forum\Administrator\Extension\ForumComponent;

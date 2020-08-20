3e552367c49c8ab818f5c57fe4ec9d782064e5b4
Florian DAL FITTO <xillibit@gmail.com>
2020-08-20 15:26:21 +0200

    [K5.2] Create a widget to render CSS ranks in templates to reduce code (#7458)

---
+++ src/components/com_kunena/template/crypsisb4/layouts/widget/ranks/default.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Kunena Component
+ * @package         Kunena.Template.Crypsisb4
+ * @subpackage      Layout.Widget
+ *
+ * @copyright       Copyright (C) 2008 - 2020 Kunena Team. All rights reserved.
+ * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            https://www.kunena.org
+ **/
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+
+if ($this->topicicontype == 'fa'):
+?>
+<li class="kwho-<?php echo $this->type ?>" alt="<?php echo Text::_($this->rank->rank_title) ?>">
+	<i class="fas fa-circle"></i><i class="fas fa-circle" <?php echo $this->stylesecond ?>></i><i class="fas fa-circle" <?php echo $this->stylethird ?>></i><i class="fas fa-circle" <?php echo $this->stylefourth ?>></i><i class="fas fa-circle" <?php echo $this->stylelast ?>></i><?php echo $this->rank->rank_special ? '<i class="fas fa-circle"></i>' : '' ?>
+</li>
+<?php else: ?>
+<li class="kwho-<?php echo $this->type ?>" alt="<?php echo Text::_($this->rank->rank_title) ?>">
+	<span class="glyphicon glyphicon-one-fine-dot"></span><span class="glyphicon glyphicon-one-fine-dot" <?php echo $this->stylesecond ?>></span><span class="glyphicon glyphicon-one-fine-dot" <?php echo $this->stylethird ?>></span><span class="glyphicon glyphicon-one-fine-dot" <?php echo $this->stylefourth ?>></span><span class="glyphicon glyphicon-one-fine-dot" <?php echo $this->stylelast ?>></span><?php echo $this->rank->rank_special ? '<span class="glyphicon glyphicon-one-fine-dot"></span>' : '' ?>
+</li>
+<?php endif; ?>

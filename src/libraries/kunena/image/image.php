3839f7d2713b4e1211aa86d797bf18cac2ad11ca
xillibit <xillibit@gmail.com>
2020-09-07 13:10:24 +0200

    [K5.2] Backport from 6.0 : handle image orientation not only for jpg

--- src/libraries/kunena/image/image.php
+++ src/libraries/kunena/image/image.php
@@ -10,6 +10,8 @@
  **/
 defined('_JEXEC') or die;
 
+use Joomla\Image\Image;
+
 define('MIME_GIF', 'image/gif');
 define('MIME_PNG', 'image/png');
 
@@ -17,7 +19,7 @@ define('MIME_PNG', 'image/png');
  * Helper class for image manipulation.
  * @since Kunena
  */
-class KunenaImage extends \Joomla\Image\Image
+class KunenaImage extends Image
 {
 	/**
 	 * Copies a rectangular portion of an image to another image. Used when imagecopyresampled isn't available
@@ -99,88 +101,84 @@ class KunenaImage extends \Joomla\Image\Image
 	 */
 	public static function correctImageOrientation($filename)
 	{
-		$testForJpg = @getimagesize($filename);
+		// TODO: need to check here if the file given is right an image ?
 
-		if ($testForJpg[2] == 2)
+		if (function_exists('exif_read_data'))
 		{
-			if (function_exists('exif_read_data'))
+			$angle  = 0;
+			$exif   = @exif_read_data($filename);
+			$flip   = '';
+			$img    = '';
+
+			if ($exif && isset($exif['Orientation']))
 			{
-				$deg  = 0;
-				$exif = @exif_read_data($filename);
-				$flip = '';
-				$img  = '';
+				$orientation = $exif['Orientation'];
 
-				if ($exif && isset($exif['Orientation']))
+				if ($orientation != 1)
 				{
-					$orientation = $exif['Orientation'];
+					$img = new Image();
+					$img->loadFile($filename);
 
-					if ($orientation != 1)
+					switch ($orientation)
 					{
-						$img = @imagecreatefromjpeg($filename);
+						case 1: // Nothing
+							$angle  = 0;
+							$flip = 0;
+							break;
 
-						switch ($orientation)
-						{
-							case 1: // Nothing
-								$deg  = 0;
-								$flip = 0;
-								break;
+						case 2: // Horizontal flip
+							$angle  = 0;
+							$flip = 1;
+							break;
 
-							case 2: // Horizontal flip
-								$deg  = 0;
-								$flip = 1;
-								break;
+						case 3: // 180 rotate left
+							$angle  = 180;
+							$flip = 0;
+							break;
 
-							case 3: // 180 rotate left
-								$deg  = 180;
-								$flip = 0;
-								break;
+						case 4: // Vertical flip
+							$angle  = 0;
+							$flip = 2;
+							break;
 
-							case 4: // Vertical flip
-								$deg  = 0;
-								$flip = 2;
-								break;
+						case 5: // Vertical flip + 90 rotate
+							$angle  = 90;
+							$flip = 2;
+							break;
 
-							case 5: // Vertical flip + 90 rotate
-								$deg  = 90;
-								$flip = 2;
-								break;
+						case 6: // 270 rotate left
+							$angle  = 270;
+							$flip = 0;
+							break;
 
-							case 6: // 270 rotate left
-								$deg  = 270;
-								$flip = 0;
-								break;
+						case 7: // Horizontal flip + 90 rotate
+							$angle  = 90;
+							$flip = 1;
+							break;
 
-							case 7: // Horizontal flip + 90 rotate
-								$deg  = 90;
-								$flip = 1;
-								break;
-
-							case 8: // 90 rotate left
-								$deg  = 90;
-								$flip = 0;
-								break;
-						}
+						case 8: // 90 rotate left
+							$angle  = 90;
+							$flip = 0;
+							break;
 					}
 				}
+			}
 
-				if ($deg > 0)
+			if ($angle > 0)
+			{
+				$img->rotate($angle, -1, false);
+			}
+
+			if ($flip != 0)
+			{
+				if ($flip == 1)
 				{
-					$img = @imagerotate($img, $deg, 0);
+					$img->flip(IMG_FLIP_HORIZONTAL, false);
 				}
-
-				if ($flip != 0)
+				else
 				{
-					if ($flip == 1)
-					{
-						@imageflip($img, IMG_FLIP_HORIZONTAL);
-					}
-					else
-					{
-						@imageflip($img, IMG_FLIP_VERTICAL);
-					}
+					$img->flip(IMG_FLIP_VERTICAL, false);
 				}
-
-				@imagejpeg($img, $filename, 95);
 			}
 		}
 	}

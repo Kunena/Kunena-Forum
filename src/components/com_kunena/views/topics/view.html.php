a6e23985e776144ba82c51e232e6cd8cb5beb926
xillibit <xillibit@gmail.com>
2020-11-05 19:09:59 +0100

    [K5.2] make changes on preg_replace_callback  work with php 7.3 (part2)

--- src/components/com_kunena/views/topics/view.html.php
+++ src/components/com_kunena/views/topics/view.html.php
@@ -209,7 +209,7 @@ class KunenaViewTopics extends KunenaView
 
 			if ($usertype != 'guest')
 			{
-				$contents = preg_replace_callback('|\[K=(\w+)(?:\:([\w_-]+))?\]|', array($this, 'fillTopicInfo'), $contents);
+			    $contents = preg_replace_callback('|\[K=(\w\+)(?:\:([\w\_-]+))?\]|', array($this, 'fillTopicInfo'), $contents);
 			}
 
 			echo $contents;

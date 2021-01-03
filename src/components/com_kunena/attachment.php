301996c908bac66acd7b04a4b1affff7d0780973
Florian DAL FITTO <xillibit@gmail.com>
2021-01-03 17:23:54 +0100

    [K5.2] Remove code for old Php versions not need now (#7835)

--- src/components/com_kunena/attachment.php
+++ src/components/com_kunena/attachment.php
@@ -13,11 +13,6 @@
  * A light application to serve attachments to the users. Will only partially initialize Joomla to gain some speed.
  */
 
-if (version_compare(PHP_VERSION, '5.3.1', '<'))
-{
-	die('Your host needs to use PHP 5.3.1 or higher to run this version of Joomla!');
-}
-
 /*
  * Constant that is checked in included files to prevent direct access.
  */

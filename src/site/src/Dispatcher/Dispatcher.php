153c82bfaef59d1ac67872c9b6fcd4199dc0d4c1
Jelle Kok <schumacher1986@hotmail.com>
2021-01-17 00:24:34 +0100

    [K6.0] Fix category index page

---
+++ src/site/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_content
+ *
+ * @copyright   (C) 2017 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Kunena\Forum\Site\Dispatcher;
+
+\defined('JPATH_PLATFORM') or die;
+
+use Joomla\CMS\Dispatcher\ComponentDispatcher;
+use Joomla\CMS\Language\Text;
+
+/**
+ * ComponentDispatcher class for com_kunena
+ *
+ * @since  4.0.0
+ */
+class Dispatcher extends ComponentDispatcher
+{
+	/**
+	 * Dispatch a controller task. Redirecting the user if appropriate.
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	public function dispatch()
+	{
+		parent::dispatch();
+	}
+}

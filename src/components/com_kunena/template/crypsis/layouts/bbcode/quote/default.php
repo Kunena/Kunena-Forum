ba63669dd76564efdeadfbf04f52d9e0b46f7ee6
Florian DAL FITTO <xillibit@gmail.com>
2021-03-28 17:21:21 +0200

    [K5.2] Improve display of quote in message and move quote in template (#7981)

---
+++ src/components/com_kunena/template/crypsis/layouts/bbcode/quote/default.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Kunena Component
+ * @package         Kunena.Template.Crypsis
+ * @subpackage      Layout.BBCode
+ *
+ * @copyright       Copyright (C) 2008 - 2021 Kunena Team. All rights reserved.
+ * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            https://www.kunena.org
+ **/
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+// [quote ="username post=messageid"]text content of the quote[/quote]
+
+// Display message quoted by user.
+?>
+<blockquote style="margin: .75em 0;background: #f9fafa;border: 1px solid #e0e0e0;border-left: 2px solid #1f85bd;"><p class="kmsgtext-quote">
+<?php if ($this->username)
+{ ?>
+
+	<div style="padding: 12px 1px;font-size: 1.3rem;background: #fff;">
+		<a href="<?php echo $this->msglink ?>"> <?php echo $this->username   . " " . Text::_('COM_KUNENA_POST_WROTE'); ?>: <i class="fas fa-arrow-circle-up"></i></a>
+		</div>
+<?php } 
+
+echo $this->content ?></p></blockquote>
+

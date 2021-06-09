95001af2ae7e61994902f3fb40185d75517c3d9b
Jelle Kok <schumacher1986@hotmail.com>
2021-06-09 17:21:57 +0200

    [K6.0] Fix W3C Warning for breadcrumbs: role="navigation"

--- src/site/template/aurelia/layouts/widget/breadcrumb/default.php
+++ src/site/template/aurelia/layouts/widget/breadcrumb/default.php
@@ -19,28 +19,28 @@ $item     = array_shift($pathway);
 $position = 2;
 
 if ($item)
-:
+	:
 	?>
-	<nav role="navigation" aria-label="breadcrumbs" class="pt-4">
-		<ol class="mod-kunena-breadcrumbs breadcrumb" itemtype="https://schema.org/BreadcrumbList" itemscope="">
-			<li class="mod-kunena-breadcrumbs__item breadcrumb-item active" aria-current="page"
-				itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+    <nav class="pagination-container-links pt-4" aria-label="breadcrumbs">
+        <ol class="mod-kunena-breadcrumbs breadcrumb" itemtype="https://schema.org/BreadcrumbList" itemscope="">
+            <li class="mod-kunena-breadcrumbs__item breadcrumb-item active" aria-current="page"
+                itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
 				<?php echo KunenaIcons::home(); ?>
-				<a itemprop="item" href="<?php echo $item->link; ?>"><span
-							itemprop="name"><?php echo $this->escape($item->name); ?></span></a>
-				<meta itemprop="position" content="1"/>
-			</li>
+                <a itemprop="item" href="<?php echo $item->link; ?>"><span
+                            itemprop="name"><?php echo $this->escape($item->name); ?></span></a>
+                <meta itemprop="position" content="1"/>
+            </li>
 
 			<?php foreach ($pathway as $item)
-			:
+				:
 				?>
-				<li class="mod-kunena-breadcrumbs__item breadcrumb-item" itemtype="https://schema.org/ListItem"
-					itemscope="" itemprop="itemListElement">
-					<a itemprop="item" href="<?php echo $item->link; ?>"><span
-								itemprop="name"><?php echo $this->escape($item->name); ?></span></a>
-					<meta itemprop="position" content="<?php echo $position++; ?>"/>
-				</li>
+                <li class="mod-kunena-breadcrumbs__item breadcrumb-item" itemtype="https://schema.org/ListItem"
+                    itemscope="" itemprop="itemListElement">
+                    <a itemprop="item" href="<?php echo $item->link; ?>"><span
+                                itemprop="name"><?php echo $this->escape($item->name); ?></span></a>
+                    <meta itemprop="position" content="<?php echo $position++; ?>"/>
+                </li>
 			<?php endforeach; ?>
-		</ol>
-	</nav>
+        </ol>
+    </nav>
 <?php endif;

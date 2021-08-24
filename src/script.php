b99a26d91c8bbfa9a2b9e0e5bf845b2dd21b8fc2
Florian DAL FITTO <xillibit@gmail.com>
2021-08-24 22:24:25 +0200

    [K6.0] The Kunenamenu will not created on a installation #8165 (#8352)

--- src/script.php
+++ src/script.php
@@ -10,17 +10,22 @@
  **/
 defined('_JEXEC') or die();
 
+use Joomla\CMS\Application\ApplicationHelper;
 use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Filesystem\File;
 use Joomla\CMS\Filesystem\Folder;
 use Joomla\CMS\Installer\Adapter\ComponentAdapter;
 use Joomla\CMS\Installer\InstallerScript;
+use Joomla\CMS\Language\LanguageHelper;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Menu\AbstractMenu;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Table\Table;
 use Kunena\Forum\Libraries\Forum\KunenaForum;
 use Kunena\Forum\Libraries\Install\KunenaInstallerException;
+use Joomla\Registry\Registry;
 
 /**
  * Kunena package installer script.
@@ -369,6 +374,8 @@ class Pkg_KunenaInstallerScript extends InstallerScript
 			File::delete($app->get('tmp_path') . '/pkg_kunena_v' . $version . '_' . $date . '.zip');
 		}
 
+		$this->buildMenu();
+
 		return true;
 	}
 
@@ -666,5 +673,232 @@ class Pkg_KunenaInstallerScript extends InstallerScript
 		$query = $db->getQuery(true)->delete($db->quoteName('#__update_sites_extensions'))->where($db->quoteName('update_site_id') . 'IN (' . $ids . ')');
 		$db->setQuery($query);
 		$db->execute();
+	}
+
+	/**
+	 * Create the Kunena menus
+	 * 
+	 * @throws KunenaInstallerException
+	 * @return boolean
+	 * 
+	 * @since   Kunena 6.0
+	 */
+	private function buildMenu()
+	{
+		$menu = ['name' => Text::_('COM_KUNENA_MENU_ITEM_FORUM'), 'alias' => ApplicationHelper::stringURLSafe(Text::_('COM_KUNENA_MENU_FORUM_ALIAS'), 'forum'),
+				'link' => 'index.php?option=com_kunena&view=home', 'access' => 1, 'params' => ['catids' => 0], ];
+
+		$component_id = (int) ComponentHelper::getComponent('com_kunena')->id;
+
+		$languages = LanguageHelper::getLanguages('default');
+		$langCode = $languages[0]->lang_code;
+
+		// First fix all broken menu items
+		$db    = Factory::getDbo();
+		$query = $db->getQuery(true)
+			->update($db->quoteName('#__menu'))
+			->set($db->quoteName('component_id') . ' = ' . $component_id)
+			->where("link LIKE '%option=com_kunena%'")
+			->andWhere('type = "component"');
+		$db->setQuery($query);
+
+		try
+		{
+			$db->execute();
+		}
+		catch (Exception $e)
+		{
+			throw new KunenaInstallerException($e->getMessage(), $e->getCode());
+		}
+
+		$table = Table::getInstance('MenuType');
+		$data  = [
+			'menutype'    => 'kunenamenu',
+			'title'       => Text::_('COM_KUNENA_MENU_TITLE'),
+			'description' => Text::_('COM_KUNENA_MENU_TITLE_DESC'),
+		];
+
+		if (!$table->bind($data) || !$table->check())
+		{
+			// Menu already exists, do nothing
+			return true;
+		}
+
+		if (!$table->store())
+		{
+			throw new KunenaInstallerException($table->getError());
+		}
+
+		$table = Table::getInstance('menu');
+		$table->load(['menutype' => 'kunenamenu', 'link' => $menu ['link']]);
+		$paramdata = ['menu-anchor_title'     => '',
+			'menu-anchor_css'       => '',
+			'menu_image'            => '',
+			'menu_text'             => 1,
+			'page_title'            => '',
+			'show_page_heading'     => 0,
+			'page_heading'          => '',
+			'pageclass_sfx'         => '',
+			'menu-meta_description' => '',
+			'robots'                => '',
+			'secure'                => 0, ];
+
+		$gparams = new Registry($paramdata);
+
+		$params = clone $gparams;
+		$params->loadArray($menu['params']);
+		$data = [
+			'menutype'     => 'kunenamenu',
+			'title'        => $menu ['name'],
+			'alias'        => $menu ['alias'],
+			'link'         => $menu ['link'],
+			'type'         => 'component',
+			'published'    => 1,
+			'parentid'     => 1,
+			'component_id' => $component_id,
+			'access'       => $menu ['access'],
+			'params'       => (string) $params,
+			'home'         => 0,
+			'language'     => '*',
+			'client_id'    => 0,
+		];
+		$table->setLocation(1, 'last-child');
+
+		if (!$table->bind($data) || !$table->check() || !$table->store())
+		{
+			$table->alias = 'kunena';
+
+			if (!$table->check() || !$table->store())
+			{
+				// Menu already exists, do nothing
+				return true;
+			}
+		}
+
+		$parent      = $table;
+		$defaultmenu = 0;
+
+		$lang = Factory::getApplication()->getLanguage();
+		$lang->load('com_kunena.install', JPATH_ADMINISTRATOR . '/components/com_kunena', $langCode);
+
+		$submenu = [
+			'index'     => ['name' => Text::_('COM_KUNENA_MENU_ITEM_INDEX'), 'alias' => ApplicationHelper::stringURLSafe(Text::_('COM_KUNENA_MENU_INDEX_ALIAS'), 'index'),
+				'link' => 'index.php?option=com_kunena&view=category&layout=list', 'access' => 1, 'default' => 'categories', 'params' => [], ],
+			'recent'    => ['name' => Text::_('COM_KUNENA_MENU_ITEM_RECENT'), 'alias' => ApplicationHelper::stringURLSafe(Text::_('COM_KUNENA_MENU_RECENT_ALIAS'), 'recent'),
+				'link' => 'index.php?option=com_kunena&view=topics&mode=replies', 'access' => 1, 'default' => 'recent', 'params' => ['topics_catselection' => '', 'topics_categories' => '', 'topics_time' => ''], ],
+			'unread'    => ['name' => Text::_('COM_KUNENA_MENU_ITEM_UNREAD'), 'alias' => ApplicationHelper::stringURLSafe(Text::_('COM_KUNENA_MENU_UNREAD_ALIAS'), 'unread'),
+				'link' => 'index.php?option=com_kunena&view=topics&layout=unread', 'access' => 2, 'params' => [], ],
+			'newtopic'  => ['name' => Text::_('COM_KUNENA_MENU_ITEM_NEWTOPIC'), 'alias' => ApplicationHelper::stringURLSafe(Text::_('COM_KUNENA_MENU_NEWTOPIC_ALIAS'), 'newtopic'),
+				'link' => 'index.php?option=com_kunena&view=topic&layout=create', 'access' => 2, 'params' => [], ],
+			'noreplies' => ['name' => Text::_('COM_KUNENA_MENU_ITEM_NOREPLIES'), 'alias' => ApplicationHelper::stringURLSafe(Text::_('COM_KUNENA_MENU_NOREPLIES_ALIAS'), 'noreplies'),
+				'link' => 'index.php?option=com_kunena&view=topics&mode=noreplies', 'access' => 2, 'params' => ['topics_catselection' => '', 'topics_categories' => '', 'topics_time' => ''], ],
+			'mylatest'  => ['name' => Text::_('COM_KUNENA_MENU_ITEM_MYLATEST'), 'alias' => ApplicationHelper::stringURLSafe(Text::_('COM_KUNENA_MENU_MYLATEST_ALIAS'), 'mylatest'),
+				'link' => 'index.php?option=com_kunena&view=topics&layout=user&mode=default', 'access' => 2, 'default' => 'my', 'params' => ['topics_catselection' => '2', 'topics_categories' => '0', 'topics_time' => ''], ],
+			'profile'   => ['name' => Text::_('COM_KUNENA_MENU_ITEM_PROFILE'), 'alias' => ApplicationHelper::stringURLSafe(Text::_('COM_KUNENA_MENU_PROFILE_ALIAS'), 'profile'),
+				'link' => 'index.php?option=com_kunena&view=user', 'access' => 2, 'params' => ['integration' => 1], ],
+			'help'      => ['name' => Text::_('COM_KUNENA_MENU_ITEM_HELP'), 'alias' => ApplicationHelper::stringURLSafe(Text::_('COM_KUNENA_MENU_HELP_ALIAS'), 'help'),
+				'link' => 'index.php?option=com_kunena&view=misc', 'access' => 3, 'params' => ['body' => Text::_('COM_KUNENA_MENU_HELP_BODY'), 'body_format' => 'bbcode'], ],
+			'search'    => ['name' => Text::_('COM_KUNENA_MENU_ITEM_SEARCH'), 'alias' => ApplicationHelper::stringURLSafe(Text::_('COM_KUNENA_MENU_SEARCH_ALIAS'), 'search'),
+				'link' => 'index.php?option=com_kunena&view=search', 'access' => 1, 'params' => [], ],
+		];
+
+		foreach ($submenu as $menuitem)
+		{
+			$params = clone $gparams;
+			$params->loadArray($menuitem['params']);
+			$table = Table::getInstance('menu');
+			$table->load(['menutype' => 'kunenamenu', 'link' => $menuitem ['link'], 'language' => $langCode]);
+			$data = [
+				'menutype'     => 'kunenamenu',
+				'title'        => $menuitem ['name'],
+				'alias'        => $menuitem ['alias'] . '-' . $langCode,
+				'link'         => $menuitem ['link'],
+				'type'         => 'component',
+				'published'    => 1,
+				'parentid'     => $parent->id,
+				'component_id' => $component_id,
+				'access'       => $menuitem ['access'],
+				'params'       => (string) $params,
+				'home'         => 0,
+				'language'     => $langCode,
+				'client_id'    => 0,
+			];
+			$table->setLocation($parent->id, 'last-child');
+
+			if (!$table->bind($data) || !$table->check() || !$table->store())
+			{
+				throw new KunenaInstallerException($table->getError());
+			}
+
+			/*if (!$defaultmenu || (isset($menuitem ['default']) && $config->defaultPage == $menuitem ['default']))
+			{
+				$defaultmenu = $table->id;
+			}*/
+		}
+
+		// Update forum menuitem to point into default page
+		$parent->link .= "&defaultmenu={$defaultmenu}";
+
+		if (!$parent->check() || !$parent->store())
+		{
+			throw new KunenaInstallerException($table->getError());
+		}
+
+		// Finally create alias
+		$defaultmenu = AbstractMenu::getInstance('site')->getDefault();
+
+		if (!$defaultmenu)
+		{
+			return true;
+		}
+
+		$table = Table::getInstance('menu');
+		$table->load(['menutype' => $defaultmenu->menutype, 'type' => 'alias', 'title' => Text::_('COM_KUNENA_MENU_ITEM_FORUM'), 'language' => $langCode]);
+
+		if (!$table->id)
+		{
+			$data = [
+				'menutype' => $defaultmenu->menutype,
+				'title' => Text::_('COM_KUNENA_MENU_ITEM_FORUM'),
+				'alias' => 'kunena-' . Factory::getDate()->format('Y-m-d'),
+				'note' => '',
+				'link' => 'index.php?Itemid=' . $parent->id,
+				'type' => 'alias',
+				'published' => 0,
+				'parent_id' => 1,
+				'component_id' => 0,
+				'checked_out' => null,
+				'checked_out_time' => null,
+				'browserNav' => 0,
+				'access' => 1,
+				'img' => '',
+				'template_style_id' => 0,
+				'params' => '{"aliasoptions":"' . (int) $parent->id . '","menu-anchor_title":"","menu-anchor_css":"","menu_image":""}',
+				'home' => 0,
+				'language' => '*',
+				'client_id' => 0
+			];
+
+			$table->setLocation(1, 'last-child');
+		}
+		else
+		{
+			$data = [
+				'alias'  => 'kunena-' . Factory::getDate()->format('Y-m-d'),
+				'link'   => 'index.php?Itemid=' . $parent->id,
+				'params' => '{"aliasoptions":"' . (int) $parent->id . '","menu-anchor_title":"","menu-anchor_css":"","menu_image":""}',
+			];
+		}
+
+		if (!$table->bind($data))
+		{
+			throw new KunenaInstallerException($table->getError());
+		}
+
+		if (!$table->check() || !$table->store())
+		{
+			// Menu already exists, do nothing
+			return true;
+		}
 	}
 }

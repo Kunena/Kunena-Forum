2cbf3772c8107445f052e2f4cce74f7c1013fc7d
xillibit <xillibit@gmail.com>
2021-10-03 15:09:55 +0200

    [K6.0] Typo in plugins

--- src/plugins/plg_system_kunena/kunena.php
+++ src/plugins/plg_system_kunena/kunena.php
@@ -58,7 +58,7 @@ class plgSystemKunena extends CMSPlugin
 		require_once $api;
 
 		// Do not load if Kunena version is not supported or Kunena is not installed
-		if (!(KunenaForum::isCompatible('4.0') && KunenaForum::installed()))
+		if (!(KunenaForum::isCompatible('6.0') && KunenaForum::installed()))
 		{
 			return;
 		}

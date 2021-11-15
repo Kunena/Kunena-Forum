0035e8894a8e06a36bd146fbab918c0ff7f9df73
xillibit <xillibit@gmail.com>
2021-11-15 22:05:33 +0100

    [K6.0] Cats cannot be deleted, the Delete button is not active #8544

--- src/admin/tmpl/categories/default_confirmdelete.php
+++ src/admin/tmpl/categories/default_confirmdelete.php
@@ -16,30 +16,19 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Language\Text;
 
 ?>
-<div class="modal hide fade" id="catconfirmdelete">
+<div class="modal hide fade" id="catconfirmdelete" tabindex="-1" aria-labelledby="catconfirmdelete" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<button type="button" role="presentation" class="close" data-bs-dismiss="modal">x</button>
-				<h3><?php echo Text::_('COM_KUNENA_CATEGORIES_CONFIRM_DELETE_TITLE_MODAL'); ?></h3>
+				<h5 class="modal-title" id="exampleModalLabel"><?php echo Text::_('COM_KUNENA_CATEGORIES_CONFIRM_DELETE_TITLE_MODAL'); ?></h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
-			<div class="modal-body span11">
+			<div class="modal-body">
 				<p><?php echo Text::_('COM_KUNENA_CATEGORIES_CONFIRM_DELETE_BODY_MODAL'); ?></p>
-				<div class="control-group">
-					<div class="controls">
-
-					</div>
-				</div>
 			</div>
 			<div class="modal-footer">
-				<button class="btn" type="button"
-						data-bs-dismiss="modal">
-					<?php echo Text::_('JCANCEL'); ?>
-				</button>
-				<button class="btn btn-outline-danger" type="submit"
-						onclick="Joomla.submitbutton('categories.remove');">
-					<?php echo Text::_('COM_KUNENA_CATEGORIES_DELETE_MODAL_BUTTON_LABEL'); ?>
-				</button>
+				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?php echo Text::_('JCANCEL'); ?></button>
+				<button type="button" class="btn btn-danger" onclick="Joomla.submitbutton('categories.remove');"><?php echo Text::_('COM_KUNENA_CATEGORIES_DELETE_MODAL_BUTTON_LABEL'); ?></button>
 			</div>
 		</div>
 	</div>

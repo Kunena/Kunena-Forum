9f14ba4e5ade4cd66a12b9d7991a1cd67fb84f46
Florian DAL FITTO <xillibit@gmail.com>
2021-12-05 20:32:09 +0100

    [K6.0] png and gif images will be changed to JPEG during upload #8531 (#8611)

--- src/libraries/kunena/src/Image/KunenaImage.php
+++ src/libraries/kunena/src/Image/KunenaImage.php
@@ -14,9 +14,6 @@ namespace Kunena\Forum\Libraries\Image;
 
 \defined('_JEXEC') or die;
 
-\define('MIME_GIF', 'image/gif');
-\define('MIME_PNG', 'image/png');
-
 use Exception;
 use Joomla\CMS\Image\Image;
 use RuntimeException;
@@ -37,7 +34,7 @@ class KunenaImage extends Image
 	 *
 	 * @since   Kunena 5.0
 	 */
-	public static function correctImageOrientation(string $filename): void
+	public static function correctImageOrientation($filename)
 	{
 		$img = new Image;
 
@@ -50,117 +47,77 @@ class KunenaImage extends Image
 			throw new RuntimeException($e->getMessage(), 500);
 		}
 
-		if (\function_exists('exif_read_data'))
+		$angle  = 0;
+		$flip   = 0;
+
+		$exif = exif_read_data($filename);
+
+		if ($exif && isset($exif['Orientation']))
 		{
-			$angle = 0;
+			$orientation = $exif['Orientation'];
 
-			try
+			if ($orientation != 1)
 			{
-				$exif = exif_read_data($filename);
-			}
-			catch (Exception $e)
-			{
-				$exif = false;
-			}
 
-			$flip = '';
-
-			if ($exif && isset($exif['Orientation']))
-			{
-				$orientation = $exif['Orientation'];
-
-				if ($orientation != 1)
+				switch ($orientation)
 				{
-					switch ($orientation)
-					{
-						case 1: // Nothing
-							$angle = 0;
-							$flip  = 0;
-							break;
+					case 1: // Nothing
+						$angle  = 0;
+						$flip = 0;
+						break;
 
-						case 2: // Horizontal flip
-							$angle = 0;
-							$flip  = 1;
-							break;
+					case 2: // Horizontal flip
+						$angle  = 0;
+						$flip = 1;
+						break;
 
-						case 3: // 180 rotate left
-							$angle = 180;
-							$flip  = 0;
-							break;
+					case 3: // 180 rotate left
+						$angle  = 180;
+						$flip = 0;
+						break;
 
-						case 4: // Vertical flip
-							$angle = 0;
-							$flip  = 2;
-							break;
+					case 4: // Vertical flip
+						$angle  = 0;
+						$flip = 2;
+						break;
 
-						case 5: // Vertical flip + 90 rotate
-							$angle = 90;
-							$flip  = 2;
-							break;
+					case 5: // Vertical flip + 90 rotate
+						$angle  = 90;
+						$flip = 2;
+						break;
 
-						case 6: // 270 rotate left
-							$angle = 270;
-							$flip  = 0;
-							break;
+					case 6: // 270 rotate left
+						$angle  = 270;
+						$flip = 0;
+						break;
 
-						case 7: // Horizontal flip + 90 rotate
-							$angle = 90;
-							$flip  = 1;
-							break;
+					case 7: // Horizontal flip + 90 rotate
+						$angle  = 90;
+						$flip = 1;
+						break;
 
-						case 8: // 90 rotate left
-							$angle = 90;
-							$flip  = 0;
-							break;
-					}
+					case 8: // 90 rotate left
+						$angle  = 90;
+						$flip = 0;
+						break;
 				}
 			}
+		}
 
-			if ($angle > 0)
-			{
-				try
-				{
-					$img->rotate($angle, -1, false);
-				}
-				catch (Exception $e)
-				{
-					throw new RuntimeException($e->getMessage(), 500);
-				}
-			}
+		if ($angle > 0)
+		{
+			$img->rotate($angle, -1, false);
+		}
 
-			if ($flip > 0)
+		if ($flip != 0)
+		{
+			if ($flip == 1)
 			{
-				if ($flip == 1)
-				{
-					try
-					{
-						$img->flip(IMG_FLIP_HORIZONTAL, false);
-					}
-					catch (Exception $e)
-					{
-						throw new RuntimeException($e->getMessage(), 500);
-					}
-				}
-				else
-				{
-					try
-					{
-						$img->flip(IMG_FLIP_VERTICAL, false);
-					}
-					catch (Exception $e)
-					{
-						throw new RuntimeException($e->getMessage(), 500);
-					}
-				}
+				$img->flip(IMG_FLIP_HORIZONTAL, false);
 			}
-
-			try
+			else
 			{
-				$img->toFile($filename);
-			}
-			catch (Exception $e)
-			{
-				throw new RuntimeException($e->getMessage(), 500);
+				$img->flip(IMG_FLIP_VERTICAL, false);
 			}
 		}
 	}

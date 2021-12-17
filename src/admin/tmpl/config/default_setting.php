7a64451772d635c6f44d500ac75b0ec86ea6a789
Florian DAL FITTO <xillibit@gmail.com>
2021-12-17 23:38:16 +0100

    [K6.0] Configuration: The button Restore Default is inactive #8570 (#8643)

--- src/admin/tmpl/config/default_setting.php
+++ src/admin/tmpl/config/default_setting.php
@@ -2,8 +2,8 @@
 /**
  * Kunena Component
  *
- * @package         Kunena.Administrator.Users
- * @subpackage      Categories
+ * @package         Kunena.Administrator.Template
+ * @subpackage      Configuration
  *
  * @copyright       Copyright (C) 2008 - 2021 Kunena Team. All rights reserved.
  * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
@@ -20,29 +20,25 @@ use Kunena\Forum\Libraries\Route\KunenaRoute;
 <form action="<?php echo KunenaRoute::_('administrator/index.php?option=com_kunena&view=config') ?>" method="post"
 	  name="settingFormModal"
 	  id="settingFormModal">
-	<div class="modal hide fade" id="settingModal">
+
+	<div class="modal fade" id="settingModal" tabindex="-1" aria-labelledby="settingModalLabel" aria-hidden="true">
 		<div class="modal-dialog">
 			<div class="modal-content">
-				<input type="hidden" name="task" value="setDefault"/>
-				<?php echo HTMLHelper::_('form.token') ?>
-
 				<div class="modal-header">
-					<button type="button" role="presentation" class="close" data-bs-dismiss="modal">x</button>
-					<h3><?php echo Text::_('COM_KUNENA_CONFIG_MODAL_TITLE'); ?></h3>
+					<h5 class="modal-title" id="exampleModalLabel"><?php echo Text::_('COM_KUNENA_CONFIG_MODAL_TITLE'); ?></h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 				</div>
-				<div class="modal-body span11">
+				<div class="modal-body">
 					<p><?php echo Text::_('COM_KUNENA_CONFIG_MODAL_DESCRIPTION'); ?></p>
 				</div>
 				<div class="modal-footer">
-					<button class="btn btn-outline-primary" type="button" data-bs-dismiss="modal">
-						<?php echo Text::_('JCANCEL'); ?>
-					</button>
-					<button class="btn btn-outline-primary" type="submit"
-							onclick="document.getElementById('settingFormModal').submit();">
-						<?php echo Text::_('JSUBMIT'); ?>
-					</button>
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?php echo Text::_('JCANCEL'); ?></button>
+					<button type="submit" class="btn btn-primary"
+					onclick="document.getElementById('settingFormModal').submit();"><?php echo Text::_('JSUBMIT'); ?></button>
 				</div>
 			</div>
 		</div>
 	</div>
+	<input type="hidden" name="task" value="setDefault"/>
+	<?php echo HTMLHelper::_('form.token') ?>
 </form>

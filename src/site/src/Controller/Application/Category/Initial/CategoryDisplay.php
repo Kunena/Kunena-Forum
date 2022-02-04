acc9aed0709aa8a3ef4e0c7ab5fbcb2e8de44351
xillibit <xillibit@gmail.com>
2022-02-04 22:49:10 +0100

    [K6.0] When clicking on a feed link kunena:View not found[name,type,p...

---
+++ src/site/src/Controller/Application/Category/Initial/CategoryDisplay.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * Kunena Component
+ *
+ * @package         Kunena.Site
+ * @subpackage      Controller.Application
+ *
+ * @copyright       Copyright (C) 2008 - 2022 Kunena Team. All rights reserved.
+ * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            https://www.kunena.org
+ **/
+
+namespace Kunena\Forum\Site\Controller\Application\Category\Initial;
+
+\defined('_JEXEC') or die();
+
+use Exception;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Kunena\Forum\Libraries\Controller\KunenaControllerDisplay;
+use Kunena\Forum\Libraries\Factory\KunenaFactory;
+use Kunena\Forum\Libraries\Forum\Category\KunenaCategoryHelper;
+use Kunena\Forum\Libraries\Html\KunenaParser;
+use Joomla\CMS\Date\Date;
+use Joomla\CMS\Document\Feed\FeedImage;
+use Joomla\CMS\Document\Feed\FeedItem;
+use Kunena\Forum\Site\Model\CategoryModel;
+
+/**
+ * CategoryDisplay
+ *
+ * @since   Kunena 6.0
+ */
+class CategoryDisplay extends KunenaControllerDisplay
+{
+	/**
+	 * Return true if layout exists.
+	 *
+	 * @return  boolean
+	 *
+	 * @throws  Exception
+	 * @since   Kunena 6.0
+	 */
+	public function exists()
+	{
+		return KunenaFactory::getTemplate()->isHmvc();
+	}
+
+	/**
+	 * Redirect unread layout to the page that contains the first unread message.
+	 *
+	 * @return  void
+	 *
+	 * @throws  null
+	 * @since   Kunena 6.0
+	 */
+	protected function before()
+	{
+	    $this->config = KunenaFactory::getConfig();
+	    $catid = $this->input->getInt('catid', 0);
+	    $model = new CategoryModel;
+	    $this->document = Factory::getApplication()->getDocument();
+
+	    if (!$this->config->enableRss)
+	    {
+	        throw new Exception(Text::_('COM_KUNENA_RSS_DISABLED'), 500);
+	    }
+	    
+	    KunenaParser::$relative = false;
+	    
+	    $this->category = KunenaCategoryHelper::get($catid);
+	    
+	    if (!$this->category->isAuthorised('read'))
+	    {
+	        throw new KunenaException($this->category->getError(), 404);
+	    }
+	    
+	    $this->topics = $model->getTopics();
+	    
+	    /*$title = Text::_('COM_KUNENA_THREADS_IN_FORUM') . ': ' . $this->category->name;
+	    $this->setTitle($title);
+	    
+	    $metaDesc = $this->document->getDescription() . '. ' . $this->escape("{$this->category->name} - {$this->config->boardTitle}");
+	    $this->document->setDescription($metaDesc);*/
+	    
+	    // Create image for feed
+	    /*$image                 = new FeedImage;
+	    $image->title          = $this->document->getTitle();
+	    $image->url            = $this->ktemplate->getImagePath('icons/rss.png');
+	    $image->description    = $this->document->getDescription();
+	    $this->document->image = $image;*/
+	    
+	    foreach ($this->topics as $topic)
+	    {
+	        if ($this->config->rss_type == 'topic')
+	        {
+	            $description = Text::sprintf('COM_KUNENA_RSS_TOPICS_CONTAINS_MESSAGES', $topic->posts) . ' - ' . Text::sprintf('COM_KUNENA_RSS_LAST_AUTHOR', KunenaFactory::getUser($topic->last_post_userid)->getName($topic->last_post_guest_name));
+	        }
+	        else
+	        {
+	            $description = $topic->last_post_message;
+	        }
+	        
+	        $date        = new Date($topic->last_post_time);
+	        $userid      = $topic->last_post_userid;
+
+	        $username    = KunenaFactory::getUser($userid)->getName($topic->last_post_guest_name);
+	        
+	        $title    = $topic->subject;
+	        $category = $topic->getCategory();
+	        $url      = $topic->getUrl($category, true, 'last');
+	        
+	        $this->createItem($title, $url, $description, $this->category->name, $date, $userid, $username);
+	    }
+
+	}
+
+	/**
+	 * Prepare document.
+	 *
+	 * @return void
+	 *
+	 * @since   Kunena 6.0
+	 */
+	protected function prepareDocument()
+	{
+		$this->setMetaData('robots', 'follow, noindex');
+	}
+	
+	public function createItem($title, $url, $description, $category, $date, $userid, $username)
+	{
+	    if ($this->config->rssAuthorInTitle)
+	    {
+	        // We want author in item titles
+	        $title .= ' - ' . Text::_('COM_KUNENA_RSS_BY') . ': ' . $username;
+	    }
+	    
+	    if ((int) $this->config->rssWordCount === -1)
+	    {
+	        $description = '';
+	    }
+	    else
+	    {
+	        $description = preg_replace('/\[confidential\](.*?)\[\/confidential\]/s', '', $description);
+	        $description = preg_replace('/\[hide\](.*?)\[\/hide\]/s', '', $description);
+	        $description = preg_replace('/\[spoiler\](.*?)\[\/spoiler\]/s', '', $description);
+	        $description = preg_replace('/\[code\](.*?)\[\/code]/s', '', $description);
+	        
+	        if ((bool) $this->config->rssAllowHtml)
+	        {
+	            $description = KunenaParser::parseBBCode($description, null, (int) $this->config->rssWordCount);
+	        }
+	        else
+	        {
+	            $description = KunenaParser::parseText($description, (int) $this->config->rssWordCount);
+	        }
+	    }
+	    
+	    // Assign values to feed item
+	    $item              = new FeedItem;
+	    $item->title       = $title;
+	    $item->link        = $url;
+	    $item->description = $description;
+	    $item->date        = $date->toSql();
+	    $item->author      = $username;
+	    
+	    // FIXME: inefficient to load users one by one -- also vulnerable to J! 2.5 user is NULL bug
+	    if ($this->config->rssAuthorFormat != 'name')
+	    {
+	        $item->authorEmail = Factory::getUser($userid)->email;
+	    }
+	    
+	    $item->category = $category;
+
+	    // Finally add item to feed
+	    $this->document->addItem($item);
+	}
+}

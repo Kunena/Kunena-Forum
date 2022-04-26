7118e5cf1973e2ec2170e195f05601a29ce99051
xillibit <xillibit@gmail.com>
2022-04-26 23:16:33 +0200

    [K6.0] Wrong strings in modal for assign moderators in users manager

--- src/admin/tmpl/users/default_moderators.php
+++ src/admin/tmpl/users/default_moderators.php
@@ -18,7 +18,7 @@ use Joomla\CMS\Language\Text;
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="exampleModalLabel"><?php echo Text::_('COM_KUNENA_CATEGORIES_CONFIRM_DELETE_TITLE_MODAL'); ?></h5>
+				<h5 class="modal-title" id="exampleModalLabel"><?php echo Text::_('COM_KUNENA_BATCH_USERS_OPTIONS'); ?></h5>
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
 			<div class="modal-body">
@@ -31,7 +31,7 @@ use Joomla\CMS\Language\Text;
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?php echo Text::_('JCANCEL'); ?></button>
-				<button type="button" class="btn btn-danger" onclick="Joomla.submitbutton('users.batchmoderators');"><?php echo Text::_('COM_KUNENA_CATEGORIES_DELETE_MODAL_BUTTON_LABEL'); ?></button>
+				<button type="button" class="btn btn-danger" onclick="Joomla.submitbutton('users.batchmoderators');"><?php echo Text::_('JSUBMIT'); ?></button>
 			</div>
 		</div>
 	</div>

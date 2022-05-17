7b6c414840636ca5c0c707b2c8f00956a14e187e
Ruud van Lent <info@onlinecommunityhub.nl>
2022-05-17 17:18:14 +0200

    replcae poovers with BS5 popovers

--- src/site/template/system/layouts/attachment/item/textlink.php
+++ src/site/template/system/layouts/attachment/item/textlink.php
@@ -24,9 +24,9 @@ $attachment = $this->attachment;
 $config = KunenaConfig::getInstance();
 ?>
 
-<a class="btn btn-small" rel="popover" data-placement="bottom" data-trigger="hover" target="_blank"
+<a class="btn btn-sm" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-trigger="hover" target="_blank"
    rel="noopener noreferrer"
-   data-content="Filesize: <?php echo number_format($attachment->size / 1024, 0, '', ',') . Text::_('COM_KUNENA_USER_ATTACHMENT_FILE_WEIGHT'); ?>
+   data-bs-content="Filesize: <?php echo number_format($attachment->size / 1024, 0, '', ',') . Text::_('COM_KUNENA_USER_ATTACHMENT_FILE_WEIGHT'); ?>
 " data-original-title="<?php echo $attachment->getShortName(); ?>" href="<?php echo $attachment->getUrl(); ?>"
    title="<?php echo KunenaAttachmentHelper::shortenFileName($attachment->getFilename(), (int) $config->attachStart, (int) $config->attachEnd); ?>">
 	<?php echo KunenaIcons::info(); ?>

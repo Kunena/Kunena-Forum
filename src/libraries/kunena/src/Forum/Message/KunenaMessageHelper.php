c47222762e04271ac655f0557fdc3f814f8243f2
xillibit <xillibit@gmail.com>
2022-09-04 11:41:49 +0200

    [K6.0] Clean things since the last commit on #9082

--- src/libraries/kunena/src/Forum/Message/KunenaMessageHelper.php
+++ src/libraries/kunena/src/Forum/Message/KunenaMessageHelper.php
@@ -771,13 +771,13 @@ abstract class KunenaMessageHelper
 
 		return $ip;
 	}
-	
+
 	/**
-	 * Get last IP address used by the user
+	 * Get the messages from users given
 	 *
 	 * @param   array  $users
 	 *
-	 * @return bool
+	 * @return object
 	 *
 	 * @since   Kunena 6.0
 	 *

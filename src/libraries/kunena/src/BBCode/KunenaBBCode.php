b50fe68436d3e9d44a4dca2dc3ace0a927062055
xillibit <xillibit@gmail.com>
2022-12-21 23:09:42 +0100

    [K6.0] Undefined array key 0 in /kunena/bbcode/bbcode.php on line 2863

--- src/libraries/kunena/src/BBCode/KunenaBBCode.php
+++ src/libraries/kunena/src/BBCode/KunenaBBCode.php
@@ -2638,15 +2638,20 @@ class KunenaBBCodeLibrary extends BBCodeLibrary
             }
         }
 
+        // Get the words set on alt params when alt is used like that : alt=my words; see issue #6751
+        $matches = array();
+        $altText = null;
         preg_match('/[img(\s*(?!alt)([\w\-\.]+\s*\/?]/', $params['_tag'], $matches);
-        $matches = rtrim($matches[0], "]");
+        if (count($matches) > 0) {
+            $altText = rtrim($matches[0], "]");
+        };
 
         $layout = KunenaLayout::factory('BBCode/Image')
             ->set('title', Text::_('COM_KUNENA_FILEATTACH'))
             ->set('url', null)
             ->set('filename', null)
             ->set('size', isset($params['size']) ? $params['size'] : 0)
-            ->set('alt', isset($params['alt']) ? $matches : 0)
+            ->set('alt', count($matches) > 0 ? $altText : 0)
             ->set('canLink', $bbcode->autoLink_disable == 0);
 
         if (Factory::getApplication()->getIdentity()->id == 0 && $this->config->showImgForGuest == 0) {

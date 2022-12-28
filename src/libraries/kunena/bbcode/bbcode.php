b6acc0e06d59e9292e3f5378b3e3a57104901d0d
xillibit <xillibit@gmail.com>
2022-12-28 11:08:34 +0100

    Warning:Undefined array key 1 in /kunena/bbcode/bbcode.php on line 237

--- src/libraries/kunena/bbcode/bbcode.php
+++ src/libraries/kunena/bbcode/bbcode.php
@@ -234,50 +234,53 @@ class KunenaBbcode extends Nbbc\BBCode
 			parse_str($params['query'], $query);
 			$path = explode('/', $params['path']);
 
-			if ($path[1] == 'itm')
+			if (is_array($path))
 			{
-				if (isset($path[3]) && is_numeric($path[3]))
+				if ($path[1] == 'itm')
 				{
-					$itemid = $path[3];
+					if (isset($path[3]) && is_numeric($path[3]))
+					{
+						$itemid = $path[3];
+					}
+					elseif (isset($path[2]) && is_numeric($path[2]))
+					{
+						$itemid = $path[2];
+					}
+
+					if (isset($itemid))
+					{
+						// Convert ebay item to embedded widget
+						return KunenaBbcodeLibrary::renderEbayLayout($itemid);
+					}
+
+					return;
 				}
-				elseif (isset($path[2]) && is_numeric($path[2]))
+
+				parse_str($params['query'], $query);
+
+				// FIXME: ebay search and seller listings are not supported.
+				if (isset($path[1]) && $path[1] == 'sch' && !empty($query['_nkw']))
 				{
-					$itemid = $path[2];
+					// Convert ebay search to embedded widget
+					KunenaBbcodeLibrary::renderEbayLayout($itemid);
+
+					// TODO: Remove in Kunena 4.0
+					return '<object width="355" height="300"><param name="movie" value="http://togo.ebay.com/togo/togo.swf?2008013100" /><param name="flashvars" value="base=http://togo.ebay.com/togo/&lang=' . $config->ebay_language . '&mode=search&query='
+						. urlencode($query['_nkw']) . '&campid=' . $config->ebay_affiliate_id . '" /><embed src="http://togo.ebay.com/togo/togo.swf?2008013100" type="application/x-shockwave-flash" width="355" height="300" flashvars="base=http://togo.ebay.com/togo/&lang='
+						. $config->ebay_language . '&mode=search&query=' . urlencode($query['_nkw']) . '&campid=' . $config->ebay_affiliate_id . '"> </embed></object>';
 				}
 
-				if (isset($itemid))
+				if (strstr($params['host'], 'myworld.') && !empty($path[1]))
 				{
-					// Convert ebay item to embedded widget
-					return KunenaBbcodeLibrary::renderEbayLayout($itemid);
+					// Convert seller listing to embedded widget
+
+					KunenaBbcodeLibrary::renderEbayLayout($itemid);
+
+					// TODO: Remove in Kunena 4.0
+					return '<object width="355" height="355"><param name="movie" value="http://togo.ebay.com/togo/seller.swf?2008013100" /><param name="flashvars" value="base=http://togo.ebay.com/togo/&lang='
+						. $config->ebay_language . '&seller=' . urlencode($path[1]) . '&campid=' . $config->ebay_affiliate_id . '" /><embed src="http://togo.ebay.com/togo/seller.swf?2008013100" type="application/x-shockwave-flash" width="355" height="355" flashvars="base=http://togo.ebay.com/togo/&lang='
+						. $config->ebay_language . '&seller=' . urlencode($path[1]) . '&campid=' . $config->ebay_affiliate_id . '"> </embed></object>';
 				}
-
-				return;
-			}
-
-			parse_str($params['query'], $query);
-
-			// FIXME: ebay search and seller listings are not supported.
-			if (isset($path[1]) && $path[1] == 'sch' && !empty($query['_nkw']))
-			{
-				// Convert ebay search to embedded widget
-				KunenaBbcodeLibrary::renderEbayLayout($itemid);
-
-				// TODO: Remove in Kunena 4.0
-				return '<object width="355" height="300"><param name="movie" value="http://togo.ebay.com/togo/togo.swf?2008013100" /><param name="flashvars" value="base=http://togo.ebay.com/togo/&lang=' . $config->ebay_language . '&mode=search&query='
-					. urlencode($query['_nkw']) . '&campid=' . $config->ebay_affiliate_id . '" /><embed src="http://togo.ebay.com/togo/togo.swf?2008013100" type="application/x-shockwave-flash" width="355" height="300" flashvars="base=http://togo.ebay.com/togo/&lang='
-					. $config->ebay_language . '&mode=search&query=' . urlencode($query['_nkw']) . '&campid=' . $config->ebay_affiliate_id . '"> </embed></object>';
-			}
-
-			if (strstr($params['host'], 'myworld.') && !empty($path[1]))
-			{
-				// Convert seller listing to embedded widget
-
-				KunenaBbcodeLibrary::renderEbayLayout($itemid);
-
-				// TODO: Remove in Kunena 4.0
-				return '<object width="355" height="355"><param name="movie" value="http://togo.ebay.com/togo/seller.swf?2008013100" /><param name="flashvars" value="base=http://togo.ebay.com/togo/&lang='
-					. $config->ebay_language . '&seller=' . urlencode($path[1]) . '&campid=' . $config->ebay_affiliate_id . '" /><embed src="http://togo.ebay.com/togo/seller.swf?2008013100" type="application/x-shockwave-flash" width="355" height="355" flashvars="base=http://togo.ebay.com/togo/&lang='
-					. $config->ebay_language . '&seller=' . urlencode($path[1]) . '&campid=' . $config->ebay_affiliate_id . '"> </embed></object>';
 			}
 		}
 

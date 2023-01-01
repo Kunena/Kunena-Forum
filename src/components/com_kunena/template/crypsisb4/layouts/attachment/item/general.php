2d58fd4d5217c7f5f335b9bb5d7a9d3997d78eba
Florian DAL FITTO <xillibit@gmail.com>
2023-01-01 18:07:42 +0100

    Set copyright date to 2023

--- src/components/com_kunena/template/crypsisb4/layouts/attachment/item/general.php
+++ src/components/com_kunena/template/crypsisb4/layouts/attachment/item/general.php
@@ -5,7 +5,7 @@
  * @package         Kunena.Template.Crypsisb4
  * @subpackage      BBCode
  *
- * @copyright       Copyright (C) 2008 - 2022 Kunena Team. All rights reserved.
+ * @copyright       Copyright (C) 2008 - 2023 Kunena Team. All rights reserved.
  * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link            https://www.kunena.org
  **/
--- src/components/com_kunena/template/system/layouts/email/subscription/default.php
+++ src/components/com_kunena/template/system/layouts/email/subscription/default.php
@@ -5,7 +5,7 @@
  * @package         Kunena.Template.Crypsis
  * @subpackage      Layout.Email
  *
- * @copyright   (C) 2008 - 2022 Kunena Team. All rights reserved.
+ * @copyright   (C) 2008 - 2023 Kunena Team. All rights reserved.
  * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link            https://www.kunena.org
  **/

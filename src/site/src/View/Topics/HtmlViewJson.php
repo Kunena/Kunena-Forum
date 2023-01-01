8fd126e118804f87e883124c13f4d5f612a1ffe6
Florian DAL FITTO <xillibit@gmail.com>
2023-01-01 17:53:30 +0100

    [K6.0] Change copyright date to 2023

--- src/site/src/View/Topics/HtmlViewJson.php
+++ src/site/src/View/Topics/HtmlViewJson.php
@@ -5,7 +5,7 @@
  *
  * @package       Kunena.json_kunenalatest
  *
- * @copyright (C) 2008 - 2022 Kunena Team. All rights reserved.
+ * @Copyright (C) 2008 - 2023 Kunena Team. All rights reserved.
  * @license       http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link          https://www.kunena.org
  **/

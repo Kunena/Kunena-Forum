7f7cb1f6c223d493aad59fa3a8845ca1cdedf2ad
Florian DAL FITTO <xillibit@gmail.com>
2023-03-27 21:32:37 +0200

    [K6.0] Code tag in text field if no template is set for a category #8971

--- src/site/template/aurelia/layouts/topic/edit/default.php
+++ src/site/template/aurelia/layouts/topic/edit/default.php
@@ -198,7 +198,7 @@ Text::script('COM_KUNENA_POLL_TITLE');
         <input id="kurl_topicons_request" type="hidden"
                value="<?php echo KunenaRoute::_('index.php?option=com_kunena&view=topic&layout=topicIcons&format=raw', false); ?>"/>
         <input id="kurl_category_template_text" type="hidden"
-               value="<?php echo KunenaRoute::_('index.php?option=com_kunena&view=ajax&display=topic&format=json&' . Session::getFormToken() . '=1', false); ?>"/>
+               value="<?php echo KunenaRoute::_('index.php?option=com_kunena&view=topic&task=categorytemplate&format=json&catid' .$this->message->id. '&' . Session::getFormToken() . '=1', false); ?>"/>
         <input id="kcategory_poll" type="hidden" name="kcategory_poll" value="<?php echo $this->message->catid; ?>"/>
         <input id="kpreview_url" type="hidden" name="kpreview_url"
                value="<?php echo KunenaRoute::_('index.php?option=com_kunena&view=topic&layout=edit&format=raw', false) ?>"/>

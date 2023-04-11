056b1b7615967c37e8b83b5ddc2b64abe8a7c510
Florian DAL FITTO <xillibit@gmail.com>
2023-04-11 22:33:04 +0200

    [K6.0] KunenaImage.php uses exif_read_data() is not available in #9506

--- src/libraries/kunena/src/Image/KunenaImage.php
+++ src/libraries/kunena/src/Image/KunenaImage.php
@@ -52,66 +52,70 @@ class KunenaImage extends Image
         $angle  = 0;
         $flip   = 0;
 
-        $exif = exif_read_data($filename);
-
-        if ($exif && isset($exif['Orientation'])) {
-            $orientation = $exif['Orientation'];
-
-            if ($orientation != 1) {
-                switch ($orientation) {
-                    case 1: // Nothing
-                        $angle  = 0;
-                        $flip   = 0;
-                        break;
-
-                    case 2: // Horizontal flip
-                        $angle  = 0;
-                        $flip   = 1;
-                        break;
-
-                    case 3: // 180 rotate left
-                        $angle  = 180;
-                        $flip   = 0;
-                        break;
-
-                    case 4: // Vertical flip
-                        $angle  = 0;
-                        $flip   = 2;
-                        break;
-
-                    case 5: // Vertical flip + 90 rotate
-                        $angle  = 90;
-                        $flip   = 2;
-                        break;
-
-                    case 6: // 270 rotate left
-                        $angle  = 270;
-                        $flip   = 0;
-                        break;
-
-                    case 7: // Horizontal flip + 90 rotate
-                        $angle  = 90;
-                        $flip   = 1;
-                        break;
-
-                    case 8: // 90 rotate left
-                        $angle  = 90;
-                        $flip   = 0;
-                        break;
+        if(function_exists('exif_read_data')) {
+            $exif = exif_read_data($filename);
+    
+            if ($exif && isset($exif['Orientation'])) {
+                $orientation = $exif['Orientation'];
+    
+                if ($orientation != 1) {
+                    switch ($orientation) {
+                        case 1: // Nothing
+                            $angle  = 0;
+                            $flip   = 0;
+                            break;
+    
+                        case 2: // Horizontal flip
+                            $angle  = 0;
+                            $flip   = 1;
+                            break;
+    
+                        case 3: // 180 rotate left
+                            $angle  = 180;
+                            $flip   = 0;
+                            break;
+    
+                        case 4: // Vertical flip
+                            $angle  = 0;
+                            $flip   = 2;
+                            break;
+    
+                        case 5: // Vertical flip + 90 rotate
+                            $angle  = 90;
+                            $flip   = 2;
+                            break;
+    
+                        case 6: // 270 rotate left
+                            $angle  = 270;
+                            $flip   = 0;
+                            break;
+    
+                        case 7: // Horizontal flip + 90 rotate
+                            $angle  = 90;
+                            $flip   = 1;
+                            break;
+    
+                        case 8: // 90 rotate left
+                            $angle  = 90;
+                            $flip   = 0;
+                            break;
+                    }
                 }
             }
-        }
-
-        if ($angle > 0) {
-            $img->rotate($angle, -1, false);
-        }
-
-        if ($flip != 0) {
-            if ($flip == 1) {
-                $img->flip(IMG_FLIP_HORIZONTAL, false);
-            } else {
-                $img->flip(IMG_FLIP_VERTICAL, false);
+    
+            if ($angle > 0) {
+                $img->rotate($angle, -1, false);
             }
+    
+            if ($flip != 0) {
+                if ($flip == 1) {
+                    $img->flip(IMG_FLIP_HORIZONTAL, false);
+                } else {
+                    $img->flip(IMG_FLIP_VERTICAL, false);
+                }
+            }
+        } else {
+            throw new RuntimeException('Not possible to correct image orientation becasue exif_read_data() method not available', 500);
         }
     }
 

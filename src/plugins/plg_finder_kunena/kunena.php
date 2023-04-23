863255a15abc27d0ab1917e9e41561e945f95cfd
Florian DAL FITTO <xillibit@gmail.com>
2023-04-23 15:31:06 +0200

    [K6.0] Smart Search - Kunena Forum rendering error KunenaLayout #9512

--- src/plugins/plg_finder_kunena/kunena.php
+++ src/plugins/plg_finder_kunena/kunena.php
@@ -474,7 +474,7 @@ class plgFinderKunena extends Adapter
 
         // Build the necessary url, route, path and alias information.
         $item->url   = $this->getUrl($message->id, $this->extension, $this->layout);
-        $item->route = $item->url . '&Itemid=' . KunenaRoute::getItemId($item->url);
+        $item->route = $item->url;
         $item->alias = KunenaRoute::stringURLSafe($message->subject);
 
         // Set body context.

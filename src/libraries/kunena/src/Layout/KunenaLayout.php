528d0c2b44b48be88b18955190d993077d6cbfb2
Florian DAL FITTO <xillibit@gmail.com>
2023-07-07 17:56:48 +0200

    [K6.1] stripBBCode(): Argument #1 ($txt) must be of type string, null

--- src/libraries/kunena/src/Layout/KunenaLayout.php
+++ src/libraries/kunena/src/Layout/KunenaLayout.php
@@ -333,6 +333,7 @@ class KunenaLayout extends KunenaBase
         }
 
         if ($title === null) {
+            $titleFirstPostCleaned = '';
             if (!empty($topic->first_post_message)) {
                 $titleFirstPostCleaned = KunenaParser::stripBBCode($topic->first_post_message, 200, false);
             }

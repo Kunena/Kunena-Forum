a6551246a1e9da14deff62b731659fd9624a7f90
Florian DAL FITTO <xillibit@gmail.com>
2023-08-26 17:25:04 +0200

    [K6.1] Missing changes on create tables on kunena_private_user_map

--- src/admin/sql/install/mysql/create_tables.utf8.sql
+++ src/admin/sql/install/mysql/create_tables.utf8.sql
@@ -313,9 +313,9 @@ CREATE TABLE IF NOT EXISTS `#__kunena_private_user_map`
 (
     `private_id` int(11)  NOT NULL,
     `user_id`    int(11)  NOT NULL,
-    `read_at`    datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-    `replied_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-    `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `read_at`    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `replied_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `deleted_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`private_id`, `user_id`),
     KEY `user_id` (`user_id`)
 )

2a76c69cb883d4c7b5b59e632575951c596befe1
Florian DAL FITTO <xillibit@gmail.com>
2023-11-10 15:57:46 +0100

    Add layouts to fix issues with Php 8.2.x

---
+++ src/site/src/Layout/Topic/Poll/TopicPollResults.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Kunena Component
+ *
+ * @package         Kunena.Site
+ * @subpackage      Layout.Topic.Item
+ *
+ * @copyright       Copyright (C) 2008 - 2023 Kunena Team. All rights reserved.
+ * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            https://www.kunena.org
+ **/
+
+namespace Kunena\Forum\Site\Layout\Topic\Poll;
+
+\defined('_JEXEC') or die;
+
+use Kunena\Forum\Libraries\Layout\KunenaLayout;
+
+/**
+ * KunenaLayoutTopicList
+ *
+ * @since   Kunena 6.2
+ */
+class TopicPollResults extends KunenaLayout
+{
+    public $intervalTimeToLive;
+}

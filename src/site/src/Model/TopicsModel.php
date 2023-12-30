7bda3d8204af0034973d07d5a2891b5ae7c9fbac
Florian DAL FITTO <xillibit@gmail.com>
2023-12-30 19:56:20 +0100

    [K6.2] Codestyle : missing some spaces

--- src/site/src/Model/TopicsModel.php
+++ src/site/src/Model/TopicsModel.php
@@ -615,7 +615,7 @@ class TopicsModel extends KunenaModel
                 $latestCategory   = $params->get('topics_categories', []);
                 $latestCategoryIn = $params->get('topics_catselection', '');
 
-                if (count($klatestCategory) ==1) {
+                if (count($klatestCategory) == 1) {
                     if ($klatestCategory[0] == '-1') {
                         $klatestCategory = [];
                     }
@@ -627,7 +627,7 @@ class TopicsModel extends KunenaModel
                  * From Kunena 6.1 in Kunena menus the default value of topics_catselection for option "Use Global" should be set to 2 instead of empty
                  */
                 if ((empty($latestCategoryIn) || $latestCategoryIn==2) || count($klatestCategory) == 0) {
-                    if($this->config->latestCategory==0) {
+                    if($this->config->latestCategory == 0) {
                         $latestCategory = false;
                     }
                     else {

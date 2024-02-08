1f77ea839fff6eab493e45b59fe9bfb2e1a5d62e
Florian DAL FITTO <xillibit@gmail.com>
2024-02-08 16:51:47 +0100

    [K6.2] Profile option Forum Settings - Select Subscription as default #9620

--- src/site/src/Controller/Topic/Form/Create/TopicFormCreateDisplay.php
+++ src/site/src/Controller/Topic/Form/Create/TopicFormCreateDisplay.php
@@ -234,7 +234,13 @@ class TopicFormCreateDisplay extends KunenaControllerDisplay
         $this->privateMessage->body = $saved ? $saved['private'] : $this->privateMessage->body;
 
         $this->postAnonymous        = $saved ? $saved['anonymous'] : !empty($this->category->postAnonymous);
-        $this->subscriptionsChecked = $saved ? $saved['subscribe'] : $this->config->subscriptionsChecked == 1;
+        
+        if ($this->me->canSubscribe == -1) {
+            $this->subscriptionsChecked = $saved ? $saved['subscribe'] : $this->config->subscriptionsChecked == 1;
+        } else {
+            $this->subscriptionsChecked = $this->me->canSubscribe;
+        }
+        
         $this->app->setUserState('com_kunena.postfields', null);
 
         $this->canSubscribe = $this->canSubscribe();

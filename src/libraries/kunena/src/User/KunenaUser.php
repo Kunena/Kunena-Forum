d19bfb292f4546f209c889b990da63732fb49052
Ruud <info@onlinecommunityhub.nl>
2024-02-21 08:14:45 +0100

    [K6.3] Fix: inconsistant language string naming > bsky_app - bluesky_social + link typo fix (#9623)

--- src/libraries/kunena/src/User/KunenaUser.php
+++ src/libraries/kunena/src/User/KunenaUser.php
@@ -499,7 +499,7 @@ class KunenaUser extends CMSObject
      * @since   Kunena 6.0
      */
     public $reddit;
-    
+
     /**
      * For bluesky social network
      *
@@ -738,7 +738,7 @@ class KunenaUser extends CMSObject
         switch ($action) {
             case 'read':
                 if (!isset($this->registerDate) || (!$user->exists() && !$config->pubProfile)) {
-                    $exception = new KunenaExceptionAuthorise(Text::_('COM_KUNENA_PROFILEPAGE_NOT_ALLOWED_FOR_GUESTS'), 403 );
+                    $exception = new KunenaExceptionAuthorise(Text::_('COM_KUNENA_PROFILEPAGE_NOT_ALLOWED_FOR_GUESTS'), 403);
                 }
                 break;
             case 'edit':
@@ -765,7 +765,7 @@ class KunenaUser extends CMSObject
 
         // Throw or return the exception.
         if ($throw && $exception) {
-             throw $exception;
+            throw $exception;
         }
 
         return $exception;
@@ -2234,7 +2234,7 @@ class KunenaUser extends CMSObject
             'ok'               => ['url' => 'https://ok.ru/##VALUE##', 'title' => Text::_('COM_KUNENA_MYPROFILE_OK'), 'nourl' => '0'],
             'pinterest'        => ['url' => 'https://pinterest.com/##VALUE##', 'title' => Text::_('COM_KUNENA_MYPROFILE_PINTEREST'), 'nourl' => '0'],
             'reddit'           => ['url' => 'https://www.reddit.com/user/##VALUE##', 'title' => Text::_('COM_KUNENA_MYPROFILE_REDDIT'), 'nourl' => '0'],
-            'bsky_app'         => ['url' => 'hhttps://bsky.app/profile/##VALUE##.bsky.social', 'title' => Text::_('COM_KUNENA_MYPROFILE_BLUESKY_SOCIAL'), 'nourl' => '0'],
+            'bsky_app'         => ['url' => 'https://bsky.app/profile/##VALUE##.bsky.social', 'title' => Text::_('COM_KUNENA_MYPROFILE_BSKY_APP'), 'nourl' => '0'],
         ];
     }
 

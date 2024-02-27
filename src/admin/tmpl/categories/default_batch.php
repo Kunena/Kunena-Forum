97d221735380c356cf12201876f3a00e4d494474
Ruud van Lent <info@onlinecommunityhub.nl>
2024-02-27 11:37:05 +0100

    Change: refactor toolbar

--- src/admin/tmpl/categories/default_batch.php
+++ src/admin/tmpl/categories/default_batch.php
@@ -17,38 +17,31 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Language\Text;
 
 ?>
-<div class="modal fade" id="batchCategories" tabindex="-1" aria-labelledby="batchCategories" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="batchCategoriesLabel"><?php echo Text::_('COM_KUNENA_BATCH_OPTIONS'); ?></h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
+<div class="p-3">
+    <div class="row">
         <p><?php echo Text::_('COM_KUNENA_BATCH_TIP'); ?></p>
-                <div class="control-group">
-                    <div class="controls">
-                        <label id="batch-choose-action-lbl" for="batch-category-id">
-                            <?php echo Text::_('COM_KUNENA_BATCH_CATEGORY_LABEL'); ?>
-                        </label>
-                        <fieldset id="batch-choose-action" class="combo">
-                            <?php echo $this->batchCategories; ?>
-                            <div id="batch-move-copy" class="control-group radio">
-                                <div class="controls">
-                                    <input type="radio" name="move_copy" value="move"/>
-                                    <label>
-                                        <?php echo Text::_('COM_KUNENA_BATCH_CATEGORY_MOVE') ?>
-                                    </label>
-                                </div>
-                            </div>
-                        </fieldset>
+        <div class="control-group">
+            <div class="controls">
+                <label id="batch-choose-action-lbl" for="batch-category-id">
+                    <?php echo Text::_('COM_KUNENA_BATCH_CATEGORY_LABEL'); ?>
+                </label>
+                <fieldset id="batch-choose-action" class="combo">
+                    <?php echo $this->batchCategories; ?>
+                    <div id="batch-move-copy" class="control-group radio">
+                        <div class="controls">
+                            <input type="radio" name="move_copy" value="move" />
+                            <label>
+                                <?php echo Text::_('COM_KUNENA_BATCH_CATEGORY_MOVE') ?>
+                            </label>
+                        </div>
                     </div>
-                </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?php echo Text::_('JCANCEL'); ?></button>
-        <button type="button" class="btn btn-primary"><?php echo Text::_('COM_KUNENA_BATCH_PROCESS'); ?></button>
-      </div>
+                </fieldset>
+            </div>
+        </div>
     </div>
-  </div>
+</div>
+<div class="btn-toolbar p-3">
+    <joomla-toolbar-button task="categories.batchCategories" class="ms-auto">
+        <button type="button" class="btn btn-success"><?php echo Text::_('COM_KUNENA_BATCH_PROCESS'); ?></button>
+    </joomla-toolbar-button>
 </div>

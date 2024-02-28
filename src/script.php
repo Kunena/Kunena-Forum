39a6f71ae68982edbb7765c53b327d084fbda81a
Florian DAL FITTO <xillibit@gmail.com>
2024-02-28 23:26:00 +0100

    [K6.2] Drop support of Joomla! 4.3.x

--- src/script.php
+++ src/script.php
@@ -40,7 +40,7 @@ class Pkg_KunenaInstallerScript extends InstallerScript
      * @var    string
      * @since  6.0.0
      */
-    protected $minimumJoomla = '4.3.4';
+    protected $minimumJoomla = '4.4.3';
 
     /**
      * List of supported versions. Newest version first!
@@ -83,8 +83,7 @@ class Pkg_KunenaInstallerScript extends InstallerScript
             '5.1' => '5.1-dev',
             '5.0' => '5.0.3',
             '4.4' => '4.4.3',
-            '4.3' => '4.4.3',
-            '0'   => '4.3.4', // Preferred version
+            '0'   => '4.4.3', // Preferred version
         ],
     ];
 

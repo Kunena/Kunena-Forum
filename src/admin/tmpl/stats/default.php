31c02915256b315ecabdbfa460022b06e03822c0
Ruud van Lent <info@onlinecommunityhub.nl>
2024-03-05 14:26:59 +0100

    Change: always show statistics (totals) in back-end

--- src/admin/tmpl/stats/default.php
+++ src/admin/tmpl/stats/default.php
@@ -18,84 +18,80 @@ use Kunena\Forum\Libraries\Version\KunenaVersion;
 
 ?>
 <div class="statistics">
-    <?php
-    if ($this->kunenaStats->showGenStats) :
-    ?>
-        <div class="card">
-            <h3 class="card-header">
-                <?php echo Text::_('COM_KUNENA_STATISTICS'); ?>
-            </h3>
+    <div class="card">
+        <h3 class="card-header">
+            <?php echo Text::_('COM_KUNENA_STATISTICS'); ?>
+        </h3>
 
-            <div class="card-body row">
-                <div class="col-md-4">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
-                            <div class="me-auto">
-                                <?php echo Text::_('COM_KUNENA_STAT_TOTAL_MESSAGES'); ?>:
-                            </div>
-                            <b><?php echo (int) $this->kunenaStats->messageCount; ?></b>
-                        </li>
-                        <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
-                            <div class="me-auto">
-                                <?php echo Text::_('COM_KUNENA_STAT_TOTAL_SECTIONS'); ?>:
-                            </div>
-                            <b><?php echo (int) $this->kunenaStats->sectionCount; ?></b>
-                        </li>
-                        <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
-                            <div class="me-auto">
-                                <?php echo Text::_('COM_KUNENA_STAT_TODAY_OPEN_THREAD'); ?>:
-                            </div>
-                            <b><?php echo (int) $this->kunenaStats->todayTopicCount; ?></b>
-                        </li>
-                        <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
-                            <div class="me-auto">
-                                <?php echo Text::_('COM_KUNENA_STAT_TODAY_TOTAL_ANSWER'); ?>:
-                            </div>
-                            <b><?php echo (int) $this->kunenaStats->todayReplyCount; ?></b>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
-                            <div class="me-auto">
-                                <?php echo Text::_('COM_KUNENA_STAT_TOTAL_SUBJECTS'); ?>:
-                            </div>
-                            <b><?php echo (int) $this->kunenaStats->topicCount; ?></b>
-                        </li>
-                        <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
-                            <div class="me-auto">
-                                <?php echo Text::_('COM_KUNENA_STAT_TOTAL_CATEGORIES'); ?>:
-                            </div>
-                            <b><?php echo (int) $this->kunenaStats->categoryCount; ?></b>
-                        </li>
-                        <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
-                            <div class="me-auto">
-                                <?php echo Text::_('COM_KUNENA_STAT_YESTERDAY_OPEN_THREAD'); ?>:
-                            </div>
-                            <b><?php echo (int) $this->kunenaStats->yesterdayTopicCount; ?></b>
-                        </li>
-                        <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
-                            <div class="me-auto">
-                                <?php echo Text::_('COM_KUNENA_STAT_YESTERDAY_TOTAL_ANSWER'); ?>:
-                            </div>
-                            <b><?php echo (int) $this->kunenaStats->yesterdayReplyCount; ?></b>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
-                            <div class="me-auto">
-                                <?php echo Text::_('COM_KUNENA_STAT_TOTAL_USERS'); ?>:
-                            </div>
-                            <b><?php echo (int) $this->kunenaStats->memberCount; ?></b>
-                        </li>
-                    </ul>
-                </div>
+        <div class="card-body row">
+            <div class="col-md-4">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
+                        <div class="me-auto">
+                            <?php echo Text::_('COM_KUNENA_STAT_TOTAL_MESSAGES'); ?>:
+                        </div>
+                        <b><?php echo (int) $this->kunenaStats->messageCount; ?></b>
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
+                        <div class="me-auto">
+                            <?php echo Text::_('COM_KUNENA_STAT_TOTAL_SECTIONS'); ?>:
+                        </div>
+                        <b><?php echo (int) $this->kunenaStats->sectionCount; ?></b>
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
+                        <div class="me-auto">
+                            <?php echo Text::_('COM_KUNENA_STAT_TODAY_OPEN_THREAD'); ?>:
+                        </div>
+                        <b><?php echo (int) $this->kunenaStats->todayTopicCount; ?></b>
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
+                        <div class="me-auto">
+                            <?php echo Text::_('COM_KUNENA_STAT_TODAY_TOTAL_ANSWER'); ?>:
+                        </div>
+                        <b><?php echo (int) $this->kunenaStats->todayReplyCount; ?></b>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
+                        <div class="me-auto">
+                            <?php echo Text::_('COM_KUNENA_STAT_TOTAL_SUBJECTS'); ?>:
+                        </div>
+                        <b><?php echo (int) $this->kunenaStats->topicCount; ?></b>
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
+                        <div class="me-auto">
+                            <?php echo Text::_('COM_KUNENA_STAT_TOTAL_CATEGORIES'); ?>:
+                        </div>
+                        <b><?php echo (int) $this->kunenaStats->categoryCount; ?></b>
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
+                        <div class="me-auto">
+                            <?php echo Text::_('COM_KUNENA_STAT_YESTERDAY_OPEN_THREAD'); ?>:
+                        </div>
+                        <b><?php echo (int) $this->kunenaStats->yesterdayTopicCount; ?></b>
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
+                        <div class="me-auto">
+                            <?php echo Text::_('COM_KUNENA_STAT_YESTERDAY_TOTAL_ANSWER'); ?>:
+                        </div>
+                        <b><?php echo (int) $this->kunenaStats->yesterdayReplyCount; ?></b>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item border-0 d-flex justify-content-between align-items-start">
+                        <div class="me-auto">
+                            <?php echo Text::_('COM_KUNENA_STAT_TOTAL_USERS'); ?>:
+                        </div>
+                        <b><?php echo (int) $this->kunenaStats->memberCount; ?></b>
+                    </li>
+                </ul>
             </div>
         </div>
-    <?php endif; ?>
+    </div>
     <?php foreach ($this->kunenaStats->top as $top) :
     ?>
         <div class="card">

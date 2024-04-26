5f4296f03a45f978d907ea5e094f24905de69478
xillibit <xillibit@gmail.com>
2024-04-26 18:14:05 +0200

    Fix deprecated issues with Php 8.2 when profile is set on top or bottom

---
+++ src/site/src/Layout/Message/Item/MessageItemTop.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Kunena Component
+ *
+ * @package         Kunena.Site
+ * @subpackage      Layout.Message.Item
+ *
+ * @copyright       Copyright (C) 2008 - @currentyear@ Kunena Team. All rights reserved.
+ * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            https://www.kunena.org
+ **/
+
+namespace Kunena\Forum\Site\Layout\Message\Item;
+
+\defined('_JEXEC') or die;
+
+use Kunena\Forum\Libraries\Layout\KunenaLayout;
+
+/**
+ * KunenaLayoutTopicList
+ *
+ * @since   Kunena 6.3
+ */
+class MessageItemTop extends KunenaLayout
+{
+    public $thankyou_delete;
+    
+    public $more_thankyou;
+    
+    public $numLink;
+    
+    public $total_thankyou;
+    
+    public $thankyou;
+    
+    public $captchaEnabled;
+    
+    public $candisplaymail;
+    
+    public $detail;
+    
+    public $location;
+    
+    public $ipLink;
+    
+    public $reportMessageLink;
+    
+    public $profile;
+}

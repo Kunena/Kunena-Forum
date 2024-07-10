664771dd295e4d8a14fe1b287d5e7663968b3cce
Florian DAL FITTO <xillibit@gmail.com>
2024-07-10 08:47:18 +0200

    [K6.3] Update Joomla! min requirements

--- src/script.php
+++ src/script.php
@@ -40,7 +40,7 @@ class Pkg_KunenaInstallerScript extends InstallerScript
      * @var    string
      * @since  6.0.0
      */
-    protected $minimumJoomla = '4.4.5';
+    protected $minimumJoomla = '4.4.6';
 
     /**
      * List of supported versions. Newest version first!
@@ -85,9 +85,9 @@ class Pkg_KunenaInstallerScript extends InstallerScript
             '0' => '10.8.6' // Preferred version
         ],
         'Joomla!' => [
-            '5.1' => '5.1.1',
+            '5.1' => '5.1.2',
             '5.0' => '5.0.3',
-            '4.4' => '4.4.5',
+            '4.4' => '4.4.6',
             '0' => '5.0.3',  // Preferred version
         ],
     ];

34384667868625683ad47873986b7587add5b5ce
xillibit <xillibit@gmail.com>
2024-07-21 22:58:16 +0200

    [K6.3] CKEditor language files are not loading anymore #9720

--- src/site/template/aurelia/layouts/widget/editor/ckeditor.php
+++ src/site/template/aurelia/layouts/widget/editor/ckeditor.php
@@ -28,7 +28,7 @@ $this->doc->addScriptOptions('com_kunena.ckeditor_skiname', $this->template->par
 
 $user = Factory::getApplication()->getIdentity();
 $userLanguage = $user->getParam('language', 'default');
-$joomlaLanguage = Factory::getApplication()->getLanguage()->getDefault();
+$joomlaLanguage = Factory::getApplication()->getLanguage()->getLanguage();
 if ($userLanguage != 'default' && $userLanguage != 'active') {
     $this->doc->addScriptOptions('com_kunena.ckeditor_userdefaultlanguage', substr($userLanguage, 0, 2));
 } else {

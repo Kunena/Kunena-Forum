64d1987840ef39deb5c56f5e0e15576855f980f5
xillibit <xillibit@gmail.com>
2024-08-14 12:05:28 +0200

    [K6.3] Protected Attachments shows only for Admins and Moderators #9697

---
+++ src/site/src/Layout/Bbcode/BbcodeImage.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Kunena Component
+ *
+ * @package         Kunena.Site
+ * @subpackage      Layout.Bbcode
+ *
+ * @copyright       Copyright (C) 2008 - @currentyear@ Kunena Team. All rights reserved.
+ * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            https://www.kunena.org
+ **/
+
+namespace Kunena\Forum\Site\Layout\Bbcode;
+
+\defined('_JEXEC') or die;
+
+use Kunena\Forum\Libraries\Layout\KunenaLayout;
+
+/**
+ * BbcodeImage
+ *
+ * @since   Kunena 6.3
+ */
+class BbcodeImage extends KunenaLayout
+{
+    public $title;
+}

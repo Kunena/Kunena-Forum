8e61e931c5520040ae420d99db2dbc348cf410e2
Florian DAL FITTO <xillibit@gmail.com>
2024-09-20 15:43:41 +0200

    [K6.4] Update minimum Joomla! requirements

--- src/script.php
+++ src/script.php
@@ -82,10 +82,10 @@ class Pkg_KunenaInstallerScript extends InstallerScript
             '0' => '10.8.6' // Preferred version
         ],
         'Joomla!' => [
-            '5.2' => '5.2.0-beta1',
-            '5.1' => '5.1.3',
+            '5.2' => '5.2.0-beta3',
+            '5.1' => '5.1.4',
             '5.0' => '5.0.3',
-            '0' => '5.1.3',  // Preferred version
+            '0' => '5.1.4',  // Preferred version
         ],
     ];
 

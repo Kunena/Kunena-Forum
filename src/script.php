0923d13e34750b4fc423edc72a31eec31d2435fd
xillibit <xillibit@gmail.com>
2025-03-11 18:54:06 +0100

    [K6.4] Update Joomla! requirements

--- src/script.php
+++ src/script.php
@@ -40,7 +40,7 @@ class Pkg_KunenaInstallerScript extends InstallerScript
      * @var    string
      * @since  6.0.0
      */
-    protected $minimumJoomla = '5.2.4';
+    protected $minimumJoomla = '5.2.5';
 
     /**
      * List of supported versions. Newest version first!
@@ -87,10 +87,10 @@ class Pkg_KunenaInstallerScript extends InstallerScript
             '0' => '10.8.6', // Preferred version
         ],
         'Joomla!' => [
-            '5.2' => '5.2.4',
+            '5.2' => '5.2.5',
             '5.1' => '5.1.4',
             '5.0' => '5.0.3',
-            '0' => '5.2.4',  // Preferred version
+            '0' => '5.2.5',  // Preferred version
         ],
     ];
 

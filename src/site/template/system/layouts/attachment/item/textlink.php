b2a9b65f0896ebb2f6ce253ff427f2570a49cdff
Ruud <info@onlinecommunityhub.nl>
2025-04-30 18:12:42 +0200

    [K6.4] Change: Added filesize formatted unit converter method (#9866)

--- src/site/template/system/layouts/attachment/item/textlink.php
+++ src/site/template/system/layouts/attachment/item/textlink.php
@@ -15,7 +15,7 @@ namespace Kunena\Forum\Site;
 
 \defined('_JEXEC') or die();
 
-use Joomla\CMS\Language\Text;
+use Joomla\CMS\HTML\HTMLHelper;
 use Kunena\Forum\Libraries\Attachment\KunenaAttachmentHelper;
 use Kunena\Forum\Libraries\Config\KunenaConfig;
 use Kunena\Forum\Libraries\Icons\KunenaIcons;
@@ -26,9 +26,9 @@ $config = KunenaConfig::getInstance();
 ?>
 
 <a class="btn btn-sm" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-trigger="hover" target="_blank"
-   rel="noopener noreferrer"
-   data-bs-content="Filesize: <?php echo number_format($attachment->size / 1024, 0, '', ',') . Text::_('COM_KUNENA_USER_ATTACHMENT_FILE_WEIGHT'); ?>
+    rel="noopener noreferrer"
+    data-bs-content="Filesize: <?php echo HTMLHelper::_('kunenaforum.formatfilesize', $attachment->size, 2); ?>
 " data-original-title="<?php echo $attachment->getShortName(); ?>" href="<?php echo $attachment->getUrl(); ?>"
-   title="<?php echo KunenaAttachmentHelper::shortenFileName($attachment->getFilename(), (int) $config->attachStart, (int) $config->attachEnd); ?>">
+    title="<?php echo KunenaAttachmentHelper::shortenFileName($attachment->getFilename(), (int) $config->attachStart, (int) $config->attachEnd); ?>">
     <?php echo KunenaIcons::info(); ?>
 </a>

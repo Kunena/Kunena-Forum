8076ff917d94c076f21e0219c7235bbef7e8df2d
Florian DAL FITTO <xillibit@gmail.com>
2025-07-10 17:00:30 +0200

    [K6.5] onAfterInitialise() doesn't work on Joomla! 6 without the compat6 plugin

--- src/plugins/plg_system_kunena/kunena.php
+++ src/plugins/plg_system_kunena/kunena.php
@@ -119,7 +119,8 @@ class PlgSystemKunena extends CMSPlugin
     public function onAfterInitialise()
     {
         // Add ban check
-        if (!$this->app->isClient('administrator') && !$this->app->isClient('api')) {
+        TODO : in Joomla! 6.0 with plugin compat6 disabled $this->app is null
+        /*if (!$this->app->isClient('administrator') && !$this->app->isClient('api')) {
             $timestamp = time();
             $lastCheck = $this->params->get('ban_check_last', 0);
             
@@ -147,7 +148,7 @@ class PlgSystemKunena extends CMSPlugin
                     $this->app->enqueueMessage($e->getMessage(), 'error');
                 }
             }
-        }
+        }*/
     }
     /**
      * Clean expired bans from the system

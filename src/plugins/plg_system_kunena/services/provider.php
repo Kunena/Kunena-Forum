a347188c9ba2916dd0d211c743d014e8d7fa9c06
Ruud van Lent <info@onlinecommunityhub.nl>
2025-07-14 17:58:14 +0200

    Fixed: header for provider.php

--- src/plugins/plg_system_kunena/services/provider.php
+++ src/plugins/plg_system_kunena/services/provider.php
@@ -1,17 +1,15 @@
 <?php
-// HEADER_START
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// [___HEADER_PHP___]
+
+/**
+ * Kunena System Plugin
+ *
+ * @package         Kunena.Plugins
+ * @subpackage      System
+ *
+ * @copyright       Copyright (C) 2008 - @currentyear@ Kunena Team. All rights reserved.
+ * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            https://www.kunena.org
+ **/
 
 \defined('_JEXEC') or die;
 

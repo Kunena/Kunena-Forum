a7de0a1a386eb85582aaa585c7721c46ee83855b
xillibit <xillibit@gmail.com>
2025-07-27 19:28:44 +0200

    [K6.5] Remove unused namespaces in plg_kunena_kunena

--- src/plugins/plg_kunena_kunena/src/Extension/Kunena.php
+++ src/plugins/plg_kunena_kunena/src/Extension/Kunena.php
@@ -15,22 +15,13 @@ namespace Kunena\Forum\Plugin\Kunena\Kunena\Extension;
 
 \defined('_JEXEC') or die();
 
-use Joomla\CMS\Component\ComponentHelper;
-use Joomla\CMS\Date\Date;
-use Joomla\CMS\Event\Application\AfterInitialiseEvent;
-use Joomla\CMS\Event\User\AfterSaveEvent;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\CMS\User\UserFactoryInterface;
 use Joomla\Database\DatabaseAwareInterface;
 use Joomla\Database\DatabaseAwareTrait;
-use Joomla\Database\DatabaseDriver;
 use Joomla\Event\SubscriberInterface;
 use Kunena\Forum\Administrator\Event\KunenaGetAvatarEvent;
 use Kunena\Forum\Administrator\Event\KunenaGetProfileEvent;
-use Kunena\Forum\Libraries\Factory\KunenaFactory;
-use Kunena\Forum\Libraries\Forum\KunenaForum;
-use Kunena\Forum\Libraries\User\KunenaBan;
 use Kunena\Forum\Plugin\Kunena\Kunena\Helper\KunenaAvatarKunena;
 use Kunena\Forum\Plugin\Kunena\Kunena\Helper\KunenaProfileKunena;
 

4efb149bc0a92cbe7fba58f0cf753fe66b54fe54
xillibit <xillibit@gmail.com>
2025-08-04 17:54:14 +0200

    [K6.5] Missing ip in the properties in method save for users

--- src/libraries/kunena/src/User/KunenaUser.php
+++ src/libraries/kunena/src/User/KunenaUser.php
@@ -77,6 +77,7 @@ use stdClass;
  * @property    int     $canSubscribe
  * @property    int     $userListtime
  * @property    int     $thankyou
+ * @property    string  $ip
  * From Joomla users table
  * @property    string  $name
  * @property    string  $username
@@ -1077,6 +1078,7 @@ class KunenaUser
             'canSubscribe' => $this->canSubscribe,
             'userListtime' => $this->userListtime,
             'thankyou'     => $this->thankyou,
+            'ip'           => $this->ip,
         ];
 
         $table->bind($properties);

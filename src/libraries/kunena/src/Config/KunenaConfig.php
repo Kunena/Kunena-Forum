45f5ec11cce2e2689f89fe65900069e6ea9f1bab
Ruud van Lent <info@onlinecommunityhub.nl>
2025-09-26 14:06:15 +0200

    added no-config-parameters (hidden)

--- src/libraries/kunena/src/Config/KunenaConfig.php
+++ src/libraries/kunena/src/Config/KunenaConfig.php
@@ -35,6 +35,7 @@ use Joomla\Database\DatabaseInterface;
  * @property string $allowAvatarGallery                    Use Avatars Gallery
  * @property string $allowAvatarUpload                     Allow Avatar Upload
  * @property string $allowChangeSubject                    Let users edit the subject
+ * @property string $allowEditPoll                         Allow Edit Poll
  * @property string $allowFavorites                        Allow Favorites
  * @property string $allowSubscriptions                    Allow Subscriptions
  * @property string $allowUserEditPoll                     Set if you want allow the user to edit the poll when someone has voted
@@ -74,6 +75,7 @@ use Joomla\Database\DatabaseInterface;
  * @property string $defaultAvatar                         Default avatar
  * @property string $defaultAvatarSmall                    Default small avatar
  * @property string $defaultSort                           Message Ordering
+ * @property string $disableEmoticons                      Disable Emoticons
  * @property string $disableRe                             Disable Re: on subject
  * @property string $displayFilenameAttachment             Display attachment filename
  * @property string $ebayAffiliateId                       eBay Affiliate ID
@@ -91,6 +93,7 @@ use Joomla\Database\DatabaseInterface;
  * @property string $emailVisibleAddress                   Visible E-mail Recipient
  * @property string $enableForumJump                       Enable Category Jump
  * @property string $enableRss                             Enable RSS Feeds
+ * @property string $enableThreadedLayouts                 Enable Threaded Layouts
  * @property string $fallbackEnglish                       Use English On Missing Strings
  * @property string $fileSize                              Maximum File Size (KB)
  * @property string $fileTypes                             Allowed File Types

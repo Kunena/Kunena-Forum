b1448a4009b053d0389d0a821319c4ce02688028
xillibit <xillibit@gmail.com>
2025-10-10 11:02:30 +0200

    [K7.0] Remove methods for TFA which has not used anymore and deprecated in K6.4

--- src/libraries/kunena/src/Login/KunenaLogin.php
+++ src/libraries/kunena/src/Login/KunenaLogin.php
@@ -82,20 +82,6 @@ class KunenaLogin
     }
 
     /**
-     * Method to check if TFA is enabled when user isn't logged
-     *
-     * @return  integer
-     *
-     * @since   Kunena 6.0
-     *
-     * @throws  Exception
-     */
-    public static function getTwoFactorMethods(): int
-    {
-        return \count(AuthenticationHelper::getTwoFactorMethods());
-    }
-
-    /**
      * @return  boolean
      *
      * @since   Kunena 6.0
@@ -242,39 +228,6 @@ class KunenaLogin
         return false;
     }
 
-    /**
-     * Checks if the Two Factor Authentication method is globally enabled and if the
-     * user has enabled a specific TFA method on their account. Only if both conditions
-     * are met will this method return true;
-     *
-     * @param   integer  $userId  The user ID to check. Skip to use the current user.
-     *
-     * @return  boolean True if TFA is enabled for this user
-     *
-     * @since   Kunena 6.0
-     *
-     * @throws  Exception
-     */
-    public function isTFAEnabled($userId = null): bool
-    {
-        // Is TFA globally turned off?
-        $twoFactorMethods = UsersHelper::getTwoFactorMethods();
-
-        if (\count($twoFactorMethods) <= 1) {
-            return false;
-        }
-
-        // Do we need to get the User ID?
-        if (empty($userId)) {
-            $userId = Factory::getApplication()->getIdentity()->id;
-        }
-
-        // Has this user turned on TFA on their account?
-        $model     = new UserModel();
-        $otpConfig = $model->getOtpConfig($userId);
-
-        return !(empty($otpConfig->method) || ($otpConfig->method == 'none'));
-    }
 
     /**
      * Return the parameters of the plugin

7f7f1069a89b7d30bbd67625c783d23ae4b22fca
xillibit <xillibit@gmail.com>
2025-10-25 16:19:37 +0200

    [K7.0] Improve PDF attachment display with responsive height and download button in pdf.php #9931

--- src/site/template/system/layouts/bbcode/attachment/pdf.php
+++ src/site/template/system/layouts/bbcode/attachment/pdf.php
@@ -15,6 +15,8 @@ namespace Kunena\Forum\Site;
 
 \defined('_JEXEC') or die();
 
+use Joomla\CMS\Language\Text;
+
 $attachment = $this->attachment;
 $location   = $attachment->getUrl();
 
@@ -24,10 +26,44 @@ if (!$attachment->isPdf()) {
 ?>
 <div class="clearfix"></div>
 
-<object class="pdf" data="<?php echo $location; ?>" type="application/pdf" width="100%" height="auto"
-        style="min-height: 300px;">
+<style>
+  /* Default (Desktop) */
+  .pdf-viewer {
+    width: 100%;
+    min-height: 400px;
+    height: 80vh;
+  }
+
+  /* Tablet */
+  @media (max-width: 991px) {
+    .pdf-viewer {
+      min-height: 300px;
+      height: 70vh;
+    }
+  }
+
+  /* Smartphone */
+  @media (max-width: 575px) {
+    .pdf-viewer {
+      min-height: 200px;
+      height: 60vh;
+    }
+  }
+</style>
+
+<object class="pdf pdf-viewer"
+        data="<?php echo $location; ?>"
+        type="application/pdf">
     <p>
-        This browser does not support PDFs. Please download the PDF to view it: <a href="<?php echo $location; ?>">Download
-            PDF</a>
+        <?php echo Text::_('COM_KUNENA_BBCODE_PDF_BROWSER_NOT_SUPPORT_EMBEDDED_PDF') ?>  
+        <a href="<?php echo $location; ?>" download><?php echo Text::_('COM_KUNENA_BBCODE_PDF_DOWNLOAD_PDF') ?></a>.
     </p>
 </object>
+
+<div class="d-flex justify-content-center justify-content-md-end mt-2">
+  <a class="btn btn-secondary"
+     href="<?php echo $location; ?>"
+     download>
+    <i class="fas fa-download"></i> <?php echo Text::_('COM_KUNENA_BBCODE_PDF_DOWNLOAD_PDF') ?>
+  </a>
+</div>

e379307c8b3e4117ba5df1cfb4ef043a02eb6fbb
xillibit <xillibit@gmail.com>
2025-11-09 10:47:26 +0100

    [K6.4] Deprecated: Creation of dynamic property Kunena... when forum access is displayed #9953

---
+++ src/site/src/Layout/Widget/WidgetWriteaccess.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Kunena Component
+ * @package         Kunena.Site
+ * @subpackage      Layout.widget
+ *
+ * @copyright       Copyright (C) 2008 - @currentyear@ Kunena Team. All rights reserved.
+ * @license         https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            https://www.kunena.org
+ **/
+
+namespace Kunena\Forum\Site\Layout\Widget;
+
+\defined('_JEXEC') or die;
+
+use Kunena\Forum\Libraries\Layout\KunenaLayout;
+
+/**
+ * KunenaLayout
+ *
+ * @since  K6.4
+ */
+class WidgetWriteaccess extends KunenaLayout
+{
+    public $id;
+}

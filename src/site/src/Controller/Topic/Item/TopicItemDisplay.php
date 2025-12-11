10ac729e5599756f06c1e3acf227d6ac4d366a65
Christopher Mavros <mavrosxristoforos@gmail.com>
2025-12-11 19:47:51 +0200

    Add canonical link to topic item display

--- src/site/src/Controller/Topic/Item/TopicItemDisplay.php
+++ src/site/src/Controller/Topic/Item/TopicItemDisplay.php
@@ -654,6 +654,10 @@ class TopicItemDisplay extends KunenaControllerDisplay
                 }
             }
         }
+        else {
+            $uri = trim(strtok($this->topic->getUrl(), '?'));
+            $doc->addHeadLink($uri, 'canonical');
+        }
 
         $menu_item = $this->app->getMenu()->getActive();
 

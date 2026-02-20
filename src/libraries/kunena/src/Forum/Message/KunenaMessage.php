0617dd8ac3eee9d7238ec92adda687e9f31b77b5
Ruud van Lent <info@onlinecommunityhub.nl>
2026-02-20 10:32:27 +0100

    Fix: set sender name from configuration

--- src/libraries/kunena/src/Forum/Message/KunenaMessage.php
+++ src/libraries/kunena/src/Forum/Message/KunenaMessage.php
@@ -671,7 +671,7 @@ class KunenaMessage extends KunenaDatabaseObject
                 }
             }
 
-            $mailnamesender  = !empty($config->emailSenderName) ? MailHelper::cleanAddress($config->emailSenderName) : MailHelper::cleanAddress($config->boardTitle);
+            $mailnamesender  = !empty((string) $config->emailSenderName) ? MailHelper::cleanAddress($config->emailSenderName) : MailHelper::cleanAddress($config->boardTitle);
             $mailsubject = MailHelper::cleanSubject($topic->subject . " (" . $this->getCategory()->name . ")");
             $subject     = $this->subject ? $this->subject : $topic->subject;
 

f55cbaa14578277305290d6580b20e992405eee9
xillibit <xillibit@gmail.com>
2026-03-18 20:33:49 +0100

    Try to revert changes about attempt to fix error 500 when URL not correct

--- src/site/src/Service/Router.php
+++ src/site/src/Service/Router.php
@@ -217,12 +217,11 @@ class Router extends RouterView
             $id = (int) $query['id'];
 
             if ($id) {
-                $subject = KunenaTopicHelper::get($id)->subject;
+                $subject = KunenaRoute::stringURLSafe(KunenaTopicHelper::get($id)->subject);
 
                 if (empty($subject)) {
                     $segments[] = $id;
-                } else {
-                    $subject    = KunenaRoute::stringURLSafe($subject);
+                } else {                    
                     $segments[] = "{$id}-{$subject}";
                 }
 

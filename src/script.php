9417ca39d1b3539c6b8e79e471973a1a255ec00d
xillibit <xillibit@gmail.com>
2026-04-14 18:38:29 +0200

    [K7.0] Update Joomla! requirements

--- src/script.php
+++ src/script.php
@@ -103,9 +103,9 @@ return new class() implements ServiceProviderInterface {
                         '0' => '12.2.2', // Preferred version
                     ],
                     'Joomla!' => [
-                        '6.1' => '6.1-rc2',
+                        '6.1' => '6.1.0',
                         '6.0' => '6.0.4',
-                        '5.4' => '5.4.4',
+                        '5.4' => '5.4.5',
                         '5.3' => '5.3.4',
                         '0' => '6.1.0',  // Preferred version
                     ],

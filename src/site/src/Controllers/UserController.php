d9ba3b006c6abd3edc6959d0a69922881d7ba1af
Florian DAL FITTO <xillibit@gmail.com>
2026-05-16 22:43:34 +0200

    [K7.0] Don't trust userid received by url on avatar upload and get it by - thanks to 0xBassia (#10068)
    Joomla! - thanks to 0xBassia

--- src/site/src/Controllers/UserController.php
+++ src/site/src/Controllers/UserController.php
@@ -1130,7 +1130,8 @@ class UserController extends KunenaController
         }
 
         $upload = KunenaUpload::getInstance();
-        $user   = KunenaFactory::getUser($this->app->getInput()->getInt('userid', 0));
+        $joomlaUserId = Factory::getApplication()->getIdentity()->id;
+        $user   = KunenaFactory::getUser($joomlaUserId);
 
         // We are converting all exceptions into JSON.
         try {

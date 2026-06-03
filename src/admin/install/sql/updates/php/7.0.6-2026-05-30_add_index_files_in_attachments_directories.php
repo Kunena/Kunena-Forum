c479a6caf35fd4d7e0f667c38ca1468a5ffff967
Florian DAL FITTO <xillibit@gmail.com>
2026-06-03 21:48:59 +0200

    [K7.0] On update create the index.hmtml file into users attachment (#10100)
    directory

---
+++ src/admin/install/sql/updates/php/7.0.6-2026-05-30_add_index_files_in_attachments_directories.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Kunena Component
+ *
+ * @package        Kunena.Installer
+ *
+ * @copyright      Copyright (C) 2008 - @currentyear@ Kunena Team. All rights reserved.
+ * @license        https://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link           https://www.kunena.org
+ **/
+defined('_JEXEC') or die();
+
+use Joomla\CMS\Language\Text;
+use Joomla\Filesystem\Folder;
+use Joomla\Filesystem\File;
+
+/**
+ * @param $parent
+ *
+ * @return array
+ * @throws Exception
+ * @since Kunena 7.0.6
+ */
+function kunena_706_2026_05_30_add_index_files_in_attachments_directories($parent) {
+    $path = JPATH_ROOT . '/media/kunena/attachments/';
+    
+    $attachmensFolders = Folder::folders($path, '.', false, true);
+    
+    foreach($attachmensFolders as $folder) {
+        // Create index.hml into the user folder
+        $content = '<html><body></body></html>';
+        File::write($folder . '/index.html', $content);
+    }
+    
+    return array('action' => '', 'name' => Text::_('COM_KUNENA_INSTALL_706_ADD_INDEX_FILES_ATTACHMENTS_DIRECTORIES'), 'success' => true);
+}

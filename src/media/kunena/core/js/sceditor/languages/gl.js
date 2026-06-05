eb6e3b4c82a931da9093923478b73186e949c3ca
Florian DAL FITTO <xillibit@gmail.com>
2026-06-05 16:55:04 +0200

    [K7.0] Add support languages in SCeditor - thanks joomod #10096 (#10102)

---
+++ src/media/kunena/core/js/sceditor/languages/gl.js
@@ -0,0 +1,68 @@
+/**
+ * @author Fran Sobrino
+ * @license [MIT](http://www.opensource.org/licenses/mit-license.php)
+ */
+(function () {
+	'use strict';
+
+	sceditor.locale['gl'] = {
+		'Bold': 'Negrita',
+		'Italic': 'Cursiva',
+		'Underline': 'Subrayar',
+		'Strikethrough': 'Riscar',
+		'Subscript': 'Sub\u00edndice',
+		'Superscript': 'Super\u00edndice',
+		'Align left': 'Alinear á Esquerda',
+		'Center': 'Centrar',
+		'Align right': 'Alinear á Dereita',
+		'Justify': 'Xustificar',
+		'Font Name': 'Tipo de Letra',
+		'Font Size': 'Tama\u00f1o de Letra',
+		'Font Color': 'Cor de Fonte',
+		'Remove Formatting': 'Quitar Formatos',
+		'Cut': 'Cortar',
+		'Your browser does not allow the cut command. Please use the keyboard shortcut Ctrl/Cmd-X': 'O seu navegador non acepta o comando cortar. Por favor, empregue a combinaci\u00f3n Ctrl/Cmd-X',
+		'Copy': 'Copiar',
+		'Your browser does not allow the copy command. Please use the keyboard shortcut Ctrl/Cmd-C': 'O seu navegador non acepta o comando cortar. Por favor, empregue a combinaci\u00f3n Ctrl/Cmd-C',
+		'Paste': 'Pegar',
+		'Your browser does not allow the paste command. Please use the keyboard shortcut Ctrl/Cmd-V': 'O seu navegador non acepta o comando cortar. Por favor, empregue a combinaci\u00f3n Ctrl/Cmd-V',
+		'Paste your text inside the following box:': 'Pega o texto dentro do seguinte recadro',
+		'Paste Text': 'Pegar Texto',
+		'Bullet list': 'Lista de Vi\u00f1etas',
+		'Numbered list': 'Lista Numerada',
+		'Undo': 'Desfacer',
+		'Redo': 'Refacer',
+		'Rows:': 'Ringleiras',
+		'Cols:': 'Columnas',
+		'Insert a table': 'Engadir unha Tabla',
+		'Insert a horizontal rule': 'Engadir unha Regla Horizontal',
+		'Code': 'C\u00f3digo',
+		'Width (optional):': 'Ancho (Opcional)',
+		'Height (optional):': 'Altura (Opcional)',
+		'Insert an image': 'Engadir unha Imaxen',
+		'E-mail:': 'E-mail',
+		'Insert an email': 'Engadir un Email',
+		'URL:': 'URL',
+		'Insert a link': 'Engadir un V\u00ednculo',
+		'Unlink': 'Quitar V\u00ednculo',
+		'More': 'M\u00e1is',
+		'Insert an emoticon': 'Engadir un emoticon',
+		'Video URL:': 'URL do V\u00eddeo',
+		'Insert': 'Engadir',
+		'Insert a YouTube video': 'Engadir un v\u00eddeo de YouTube',
+		'Insert current date': 'Engadir data actual',
+		'Insert current time': 'Engadir hora actual',
+		'Print': 'Imprimir',
+		'View source': 'Ver C\u00f3digo',
+		'Description (optional):': 'Descripci\u00f3n (Opcional):',
+		'Enter the image URL:': 'Ingresar a URL da imaxen:',
+		'Enter the e-mail address:': 'Ingresar o correo electr\u00f3nico:',
+		'Enter the displayed text:': 'Ingresar o texto mostrado:',
+		'Enter URL:': 'Ingresar URL:',
+		'Enter the YouTube video URL or ID:': 'Ingresar URL ou ID de YouTube',
+		'Insert a Quote': 'Engadir Cita',
+		'Invalid YouTube video': 'V\u00eddeo de YouTube Inv\u00e1lido',
+
+		dateFormat: 'day-month-year'
+	};
+})();
---
+++ src/media/kunena/core/js/sceditor/languages/lt.js
@@ -0,0 +1,68 @@
+/**
+ * @author Team from www.klaustukai.lt
+ * @license [MIT](http://www.opensource.org/licenses/mit-license.php)
+ */
+(function () {
+	'use strict';
+
+	sceditor.locale['lt'] = {
+		'Bold': 'Paryškintas',
+		'Italic': 'Pasvirasis',
+		'Underline': 'Pabraukti',
+		'Strikethrough': 'Perbraukti',
+		'Subscript': 'Parašyti sumažintas raides po žodžio',
+		'Superscript': 'Parašyti sumažintas raides virš žodžio',
+		'Align left': 'Kairysis lygiavimas',
+		'Center': 'Centrinis lygiavimas',
+		'Align right': 'Dešinysis lygiavimas',
+		'Justify': 'Išlygintas tekstas',
+		'Font Name': 'Šrifto pavadinimas',
+		'Font Size': 'Šrifto dydis',
+		'Font Color': 'Šrifto spalva',
+		'Remove Formatting': 'Panaikinti teksto formatavimą',
+		'Cut': 'Iškirpti',
+		'Your browser does not allow the cut command. Please use the keyboard shortcut Ctrl/Cmd-X': 'Jūsų paieškos sistema neleidžia atlikti šios funkcijos. Norėdami iškirpti spauskite Ctrl/Cmd-x',
+		'Copy': 'Kopijuoti',
+		'Your browser does not allow the copy command. Please use the keyboard shortcut Ctrl/Cmd-C': 'Jūsų paieškos sistema neleidžia atlikti šios komandos. Norėdami nukopijuoti spauskite Ctrl/Cmd - C',
+		'Paste': 'Įklijuoti',
+		'Your browser does not allow the paste command. Please use the keyboard shortcut Ctrl/Cmd-V': 'Jūsų paieškos sistema neleidžia atlikti šios komandos. Norėdami įklijuoti spauskite Ctrl/Cmd - V',
+		'Paste your text inside the following box:': 'Įklijuokite tekstą nurodytoje vietoje',
+		'Paste Text': 'Įklijuoti tekstą',
+		'Bullet list': 'Sugrupuotas sąrašas',
+		'Numbered list': 'Sunumeruotas sąrašas',
+		'Undo': 'panaikinti',
+		'Redo': 'atitaisyti',
+		'Rows:': 'Eilutės',
+		'Cols:': 'Stulpeliai',
+		'Insert a table': 'Įterpti lentelę',
+		'Insert a horizontal rule': 'Įterpti horizontalią liniją',
+		'Code': 'Šalies kodas',
+		'Width (optional):': 'plotis (laisvai pasirenkamas)',
+		'Height (optional):': 'aukštis (laisvai pasirenkamas)',
+		'Insert an image': 'Įterpti nuotrauką',
+		'E-mail:': 'Elektroninis paštas',
+		'Insert an email': 'Įterpti elktroninio pašto nuorodą',
+		'URL:': 'Internetinės svetainės adresas:',
+		'Insert a link': 'Įterpti nuorodą',
+		'Unlink': 'Atjungti',
+		'More': 'Daugiau',
+		'Insert an emoticon': 'Įterpti šypsenėlę',
+		'Video URL:': 'Vaizdo klipo nuoroda',
+		'Insert': 'Įterpti',
+		'Insert a YouTube video': 'Įterpti Youtube vaizdo klipą',
+		'Insert current date': 'Įterpti esamą datą (diena-mėnuo-metai)',
+		'Insert current time': 'Įterpti esamą laiką',
+		'Print': 'Atspausdinti',
+		'View source': 'Peržiūrėti šaltinį',
+		'Description (optional):': 'Aprašymas (laisvai pasirenkamas)',
+		'Enter the image URL:': 'Įterpti nuotraukos adresą',
+		'Enter the e-mail address:': 'Įterpti elektroninio pašto adresą',
+		'Enter the displayed text:': 'Įvesti pavaizduotą tekstą',
+		'Enter URL:': 'Įvesti internetinį adresą',
+		'Enter the YouTube video URL or ID:': 'Įrašykite Youtube vaizdo klipo nuorodą ar ID',
+		'Insert a Quote': 'Įterpti citatą',
+		'Invalid YouTube video': 'YouTube vaizdo įrašas neveikia',
+
+		dateFormat: 'year-month-day'
+	};
+})();

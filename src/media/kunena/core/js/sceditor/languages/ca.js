b0c998be6c313cc7abf2a3179c6a4c658f8b3fda
xillibit <xillibit@gmail.com>
2026-06-05 16:51:32 +0200

    [K7.0] Add support languages in SCeditor - thanks joomod #10096

---
+++ src/media/kunena/core/js/sceditor/languages/ca.js
@@ -0,0 +1,68 @@
+/**
+ * @author Fran Sobrino
+ * @license [MIT](http://www.opensource.org/licenses/mit-license.php)
+ */
+(function () {
+	'use strict';
+
+	sceditor.locale['ca'] = {
+		'Bold': 'Negrita',
+		'Italic': 'Cursiva',
+		'Underline': 'Subratlla',
+		'Strikethrough': 'Ratllar',
+		'Subscript': 'Sub\u00edndice',
+		'Superscript': 'Super\u00edndice',
+		'Align left': 'Alinear a l\'Esquerra',
+		'Center': 'Centrar',
+		'Align right': 'Alinear a la dreta',
+		'Justify': 'Justificar',
+		'Font Name': 'Tipus de Lletra',
+		'Font Size': 'Mida de Lletra',
+		'Font Color': 'Color de Font',
+		'Remove Formatting': 'Treure Formats',
+		'Cut': 'Tallar',
+		'Your browser does not allow the cut command. Please use the keyboard shortcut Ctrl/Cmd-X': 'O seu navegador non acepta o comando cortar. Por favor, empregue a combinaci\u00f3n Ctrl/Cmd-X',
+		'Copy': 'Copiar',
+		'Your browser does not allow the copy command. Please use the keyboard shortcut Ctrl/Cmd-C': 'O seu navegador non acepta o comando cortar. Por favor, empregue a combinaci\u00f3n Ctrl/Cmd-C',
+		'Paste': 'Pegar',
+		'Your browser does not allow the paste command. Please use the keyboard shortcut Ctrl/Cmd-V': 'O seu navegador non acepta o comando cortar. Por favor, empregue a combinaci\u00f3n Ctrl/Cmd-V',
+		'Paste your text inside the following box:': 'Pega o texto dentro do seguinte recadro',
+		'Paste Text': 'Pegar Texto',
+		'Bullet list': 'Llista d\'Vinyetes',
+		'Numbered list': 'Llista numerada',
+		'Undo': 'Desfer',
+		'Redo': 'Refer',
+		'Rows:': 'Files',
+		'Cols:': 'Columnes',
+		'Insert a table': 'Inserir una taula',
+		'Insert a horizontal rule': 'Insereix una Regla horitzontal',
+		'Code': 'C\u00f3digo',
+		'Width (optional):': 'Ample (Opcional)',
+		'Height (optional):': 'Alçada (Opcional)',
+		'Insert an image': 'Insereix una imatge',
+		'E-mail:': 'Correu electrònic',
+		'Insert an email': 'Insereix un Email',
+		'URL:': 'URL',
+		'Insert a link': 'Inserir un enllaç',
+		'Unlink': 'Treure un enllaç',
+		'More': 'Més',
+		'Insert an emoticon': 'Inserir un emoticon',
+		'Video URL:': 'URL del V\u00eddeo',
+		'Insert': 'Insereix',
+		'Insert a YouTube video': 'Insereix un v\u00eddeo de YouTube',
+		'Insert current date': 'Insereix data actual',
+		'Insert current time': 'Insereix hora actual',
+		'Print': 'Imprimir',
+		'View source': 'Veure C\u00f3digo',
+		'Description (optional):': 'Descripci\u00f3 (Opcional):',
+		'Enter the image URL:': 'Ingressar la URL de la imatge:',
+		'Enter the e-mail address:': 'Ingressar el correu electr\u00f3nico:',
+		'Enter the displayed text:': 'Ingressar el texto mostrat:',
+		'Enter URL:': 'Entrada URL:',
+		'Enter the YouTube video URL or ID:': 'Entrada URL ou ID de YouTube',
+		'Insert a Quote': 'v Insereix',
+		'Invalid YouTube video': 'V\u00eddeo de YouTube Inv\u00e1lido',
+
+		dateFormat: 'day-month-year'
+	};
+})();
---
+++ src/media/kunena/core/js/sceditor/languages/id.js
@@ -0,0 +1,68 @@
+/**
+ * @author Sandy Irawan (sndbkct@gmail.com)
+ * @license [MIT](http://www.opensource.org/licenses/mit-license.php)
+ */
+(function () {
+	'use strict';
+
+	sceditor.locale['id'] = {
+		'Bold': 'Tebal',
+		'Italic': 'Miring',
+		'Underline': 'Garis Bawah',
+		'Strikethrough': 'Coret',
+		'Subscript': 'Tulisan dibawah garis',
+		'Superscript': 'Tulisan diatas garis',
+		'Align left': 'Rata Kiri',
+		'Center': 'Rata Tengah',
+		'Align right': 'Rata Kanan',
+		'Justify': 'Rata Kanan-Kiri',
+		'Font Name': 'Nama Fon',
+		'Font Size': 'Ukuran Fon',
+		'Font Color': 'Warna Fon',
+		'Remove Formatting': 'Hapus Format',
+		'Cut': 'Potong',
+		'Your browser does not allow the cut command. Please use the keyboard shortcut Ctrl/Cmd-X': 'Browser Anda tidak memungkinkan perintah cut. Silakan gunakan shortcut keyboard Ctrl / Cmd-X ',
+		'Copy': 'Salin',
+		'Your browser does not allow the copy command. Please use the keyboard shortcut Ctrl/Cmd-C': 'Browser Anda tidak memungkinkan perintah copy. Silakan gunakan shortcut keyboard Ctrl / Cmd-C ',
+		'Paste': 'Rekatkan',
+		'Your browser does not allow the paste command. Please use the keyboard shortcut Ctrl/Cmd-V': 'Browser Anda tidak memungkinkan perintah paste. Silakan gunakan shortcut keyboard Ctrl / Cmd-V ',
+		'Paste your text inside the following box:': 'Rekatkan teks Anda dalam kotak berikut:',
+		'Paste Text': 'Rekatkan Teks',
+		'Bullet list': 'Daftar Bullet',
+		'Numbered list': 'Daftar Nomor',
+		'Undo': 'Kembalikan',
+		'Redo': 'Ulangi',
+		'Rows:': 'Baris',
+		'Cols:': 'Kolom',
+		'Insert a table': 'Sisipkan sebuah tabel',
+		'Insert a horizontal rule': 'Sisipkan aturan horisontal',
+		'Code': 'Kode',
+		'Width (optional):': 'Lebar (opsional)',
+		'Height (optional):': 'Tinggi (opsional)',
+		'Insert an image': 'Sisipkan Gambar',
+		'E-mail:': 'Surel',
+		'Insert an email': 'Sisipkan surel',
+		'URL:': 'URL',
+		'Insert a link': 'Sisipkan link',
+		'Unlink': 'Buang Link',
+		'More': 'Lainnya',
+		'Insert an emoticon': 'Sisipkan emotikon',
+		'Video URL:': 'URL Video',
+		'Insert': 'Sisipkan',
+		'Insert a YouTube video': 'Sisipkan video Youtube',
+		'Insert current date': 'Sisipkan tanggal sekarang',
+		'Insert current time': 'Sisipkan waktu sekarang',
+		'Print': 'Print',
+		'View source': 'Lihat sumber',
+		'Description (optional):': 'Deskripsi (opsional)',
+		'Enter the image URL:': 'Masukkan URL gambar',
+		'Enter the e-mail address:': 'Masukkan alamat surel',
+		'Enter the displayed text:': 'Masukkan teks yang ditampilkan',
+		'Enter URL:': 'Masukkan URL',
+		'Enter the YouTube video URL or ID:': 'Masukkan URL video YouTube atau ID',
+		'Insert a Quote': 'Sisipkan kutipan',
+		'Invalid YouTube video': 'Video YouTube yang tidak valid',
+
+		dateFormat: 'day-month-year'
+	};
+})();

64f8346aa529313670bb9f34bf3b8fadbe66c38a
xillibit <xillibit@gmail.com>
2026-07-13 16:51:17 +0200

    [K7.1] Improve KunenaUserTabs and KunenaUserTabsEdit events

--- src/libraries/kunena/src/Event/KunenaUserTabs.php
+++ src/libraries/kunena/src/Event/KunenaUserTabs.php
@@ -15,15 +15,33 @@ namespace Kunena\Forum\Libraries\Event;
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Event\AbstractEvent;
+use Joomla\CMS\Event\GenericEvent;
+use Joomla\CMS\Event\Result\ResultAware;
+use Joomla\CMS\Event\Result\ResultAwareInterface;
+use Joomla\CMS\Event\Result\ResultTypeStringAware;
 
 /**
  * Class for Model event.
  * Example:
- *  new KunenaBeforeDeleteEvent('onEventName', ['context' => 'com_example.example', 'subject' => $itemObjectToDelete]);
+ *  new KunenaUserTabs('onEventName', ['context' => 'com_example.example', 'subject' => $itemObjectToDelete]);
  *
  * Joomla Core doesn't have a Finder\BeforeDeleteEvent
  * 
  * @since  7.1.0
  */
-class KunenaUserTabs extends AbstractEvent {}
+class KunenaUserTabs extends GenericEvent implements ResultAwareInterface {
+    use ResultAware;
+    use ResultTypeStringAware;
+    
+    /**
+     * Getter for the socials argument.
+     *
+     * @return  array
+     *
+     * @since  7.1.0
+     */
+    public function getUserTabs(): array
+    {
+        return $this->arguments['usertabs'];
+    }
+}
--- src/libraries/kunena/src/Event/KunenaUserTabsEdit.php
+++ src/libraries/kunena/src/Event/KunenaUserTabsEdit.php
@@ -15,15 +15,33 @@ namespace Kunena\Forum\Libraries\Event;
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Event\AbstractEvent;
+use Joomla\CMS\Event\GenericEvent;
+use Joomla\CMS\Event\Result\ResultAware;
+use Joomla\CMS\Event\Result\ResultAwareInterface;
+use Joomla\CMS\Event\Result\ResultTypeStringAware;
 
 /**
  * Class for Model event.
  * Example:
- *  new KunenaBeforeDeleteEvent('onEventName', ['context' => 'com_example.example', 'subject' => $itemObjectToDelete]);
+ *  new KunenaUserTabsEdit('onEventName', ['context' => 'com_example.example', 'subject' => $itemObjectToDelete]);
  *
  * Joomla Core doesn't have a Finder\BeforeDeleteEvent
  * 
  * @since  7.1.0
  */
-class KunenaUserTabsEdit extends AbstractEvent {}
+class KunenaUserTabsEdit  extends GenericEvent implements ResultAwareInterface {
+    use ResultAware;
+    use ResultTypeStringAware;
+    
+    /**
+     * Getter for the socials argument.
+     *
+     * @return  array
+     *
+     * @since  7.1.0
+     */
+    public function getUserTabsEdit(): array
+    {
+        return $this->arguments['usertabsedit'];
+    }
+}

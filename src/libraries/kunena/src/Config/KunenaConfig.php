f5f6993db5a5de7f29e608182bccd30fe681a11e
xillibit <xillibit@gmail.com>
2026-07-30 22:32:29 +0200

    [K7.1] Add config setting to define the number of mails sent by batch in the task plugin

--- src/libraries/kunena/src/Config/KunenaConfig.php
+++ src/libraries/kunena/src/Config/KunenaConfig.php
@@ -87,6 +87,7 @@ use Joomla\Database\DatabaseInterface;
  * @property string $emailHeaderSizeX                      Header Width
  * @property string $emailHeaderSizeY                      Header Height
  * @property string $emailRecipientCount                   E-mail to Multiple Recipients
+ * @property int    $emailBatchSize                        Max mails sent per scheduled task run
  * @property string $emailRecipientPrivacy                 E-mail Recipient Privacy
  * @property string $emailSenderName                       Sender name for mail
  * @property string $emailVisibleAddress                   Visible E-mail Recipient

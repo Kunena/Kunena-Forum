e0f4823e1dd33a7c1e379e61337b9d8bf7e7a61e
Astrid van Eerd <Pinkeltje@users.noreply.github.com>
2026-09-10 06:38:40 +0200

    Validate email before generating mailto link
    Add check for empty email before returning mailto link.

--- src/libraries/kunena/src/User/KunenaUser.php
+++ src/libraries/kunena/src/User/KunenaUser.php
@@ -1917,6 +1917,9 @@ class KunenaUser
                 return '<span data-bs-toggle="tooltip" data-placement="right" title="' . Text::_('COM_KUNENA_VIEW_PMS') . '" >' . $pms->showIcon($this->userid) . '</span>';
                 break;
             case 'email':
+				 if (empty($this->email)) {
+        			return '';
+    		}
                 return '<a href="mailto:' . $this->email . '" data-bs-toggle="tooltip" data-placement="right" title="' . $this->escape($this->email) . '">' . KunenaIcons::email() . '</a>';
                 break;
             case 'profile':
